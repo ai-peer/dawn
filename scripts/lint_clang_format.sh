38763b25b788b2a2ad6cca02843cf3ea630f39e7
Corentin Wallez <cwallez@chromium.org>
2018-08-27 10:10:28 +0200

    temp presubmit
    Change-Id: I4d8208472a8a6bd32ae3ef41c3145abf270a4c37

---
+++ scripts/lint_clang_format.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+clang_format=$1
+base_commit=$2
+
+skipped_directories="(examples|generator|src/tests/(unittests|end2end)|third_party)"
+# Find the files modified that need formatting
+files_to_check=$(git diff --diff-filter=ACMR --name-only $base_commit | grep -E "*\.(c|cpp|mm|h)$" | grep -vE "^$skipped_directories/*")
+if [ -z "$files_to_check" ]; then
+    echo "No modified files to format."
+    exit 0
+fi
+echo "Checking formatting diff on these files:"
+echo "$files_to_check"
+echo
+files_to_check=$(echo $files_to_check | tr '\n' ' ')
+
+# Run git-clang-format, check if it formatted anything
+format_output=$(scripts/git-clang-format --binary $clang_format --commit $base_commit --diff --style=file $files_to_check)
+if [ "$format_output" == "clang-format did not modify any files" ] || [ "$format_output" == "no modified files to format" ] ; then
+    exit 0
+fi
+
+# clang-format made changes, print them and fail Travis
+echo "Following formatting changes needed:"
+echo
+echo "$format_output"
+echo
+exit 1
