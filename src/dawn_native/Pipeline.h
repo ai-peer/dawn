6985bc0e033b684f01fb9d5dd8078a900fc65f3d
Corentin Wallez <cwallez@chromium.org>
2018-09-25 11:10:56 -0700

    Put the reference to DeviceBase in a new ObjectBase
    ObjectBase will contain data that all WebGPU objects have such as a
    pointer back to the device, a refcount (via inheriting from RefCounted),
    a name, an error status etc.
    
    BUG=dawn:24
    
    Change-Id: I919e1a6d4a68811ceb6e503b2a793815c92f2528

--- src/dawn_native/Pipeline.h
+++ src/dawn_native/Pipeline.h
@@ -19,7 +19,7 @@
 #include "dawn_native/Forward.h"
 #include "dawn_native/PerStage.h"
 #include "dawn_native/PipelineLayout.h"
-#include "dawn_native/RefCounted.h"
+#include "dawn_native/ObjectBase.h"
 #include "dawn_native/ShaderModule.h"
 
 #include "dawn_native/dawn_platform.h"
@@ -37,7 +37,7 @@ namespace dawn_native {
 
     class PipelineBuilder;
 
-    class PipelineBase {
+    class PipelineBase : public ObjectBase {
       public:
         PipelineBase(DeviceBase* device, PipelineLayoutBase* layout, dawn::ShaderStageBit stages);
         PipelineBase(DeviceBase* device, PipelineBuilder* builder);
