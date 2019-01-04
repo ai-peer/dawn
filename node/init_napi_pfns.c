3485c87f718b5d3c756fd1b4de6bb8877db55e26
Kai Ninomiya <kainino@chromium.org>
2019-01-04 05:54:14 +0000

    Add Node bindings to Dawn
    Change-Id: Ide577e3445f5a16a3343a92cd308bbcc9aed5ac7

---
+++ node/init_napi_pfns.c
@@ -0,0 +1,31 @@
+#include "js_native_api.h"
+#include "node_api.h"
+
+#include <assert.h>
+#include <dlfcn.h>
+#include <stddef.h>
+
+#define X(ret, name, args) PFN_##name name;
+  _NAPI_PFNS_JS_NATIVE_API
+  _NAPI_PFNS_JS_NATIVE_API_EXPERIMENTAL
+  _NAPI_PFNS_NODE_API
+  _NAPI_PFNS_NODE_API_GE2
+  _NAPI_PFNS_NODE_API_GE3
+  _NAPI_PFNS_NODE_API_EXPERIMENTAL
+#undef X
+
+void init_napi_pfns() {
+  void* self = dlopen(NULL, RTLD_LAZY);
+  assert(self);
+
+#define X(ret, name, args) \
+  name = (PFN_##name) dlsym(self, #name); \
+  assert(name);
+  _NAPI_PFNS_JS_NATIVE_API
+  _NAPI_PFNS_JS_NATIVE_API_EXPERIMENTAL
+  _NAPI_PFNS_NODE_API
+  _NAPI_PFNS_NODE_API_GE2
+  _NAPI_PFNS_NODE_API_GE3
+  _NAPI_PFNS_NODE_API_EXPERIMENTAL
+#undef X
+}
