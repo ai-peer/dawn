065c229e638b8f46934eca7c897aa4b054e7e02e
Corentin Wallez <cwallez@chromium.org>
2019-01-09 17:57:25 +0100

    D3D12: Implement the backend connection and adapter.
    BUG=dawn:29
    
    Change-Id: Id4497b4a91ba58b79d1cd976a52df5a0fa5d60a8

--- src/utils/D3D12Binding.cpp
+++ src/utils/D3D12Binding.cpp
@@ -30,8 +30,20 @@ namespace utils {
         }
 
         dawnDevice CreateDevice() override {
-            mBackendDevice = dawn_native::d3d12::CreateDevice();
-            return mBackendDevice;
+            // Make an instance and find a D3D12 adapter
+            mInstance = std::make_unique<dawn_native::Instance>();
+            mInstance->DiscoverDefaultAdapters();
+
+            std::vector<dawn_native::Adapter> adapters = mInstance->GetAdapters();
+            for (dawn_native::Adapter adapter : adapters) {
+                if (adapter.GetBackendType() == dawn_native::BackendType::D3D12) {
+                    mBackendDevice = adapter.CreateDevice();
+                    return mBackendDevice;
+                }
+            }
+
+            UNREACHABLE();
+            return {};
         }
 
         uint64_t GetSwapChainImplementation() override {
@@ -49,6 +61,7 @@ namespace utils {
         }
 
       private:
+        std::unique_ptr<dawn_native::Instance> mInstance;
         dawnDevice mBackendDevice = nullptr;
         dawnSwapChainImplementation mSwapchainImpl = {};
     };
