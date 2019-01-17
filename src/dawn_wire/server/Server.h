e4551696e6f5b9aeb7fa828d7d4e5b908f040ce0
Austin Eng <enga@chromium.org>
2019-01-17 15:04:10 -0800

    Factor wire server handlers into proper command handlers and doers
    Bug: dawn:88
    Change-Id: If637e7ac694d68ad7a4a1e85fd241e8b48e62f6a

--- src/dawn_wire/server/Server.h
+++ src/dawn_wire/server/Server.h
@@ -48,13 +48,6 @@ namespace dawn_wire { namespace server {
                                            MapUserdata* userdata);
         void OnFenceCompletedValueUpdated(FenceCompletionUserdata* userdata);
 
-        // Command handlers
-        bool PreHandleBufferUnmap(const BufferUnmapCmd& cmd);
-        bool PostHandleQueueSignal(const QueueSignalCmd& cmd);
-        bool HandleBufferMapAsync(const char** commands, size_t* size);
-        bool HandleBufferUpdateMappedData(const char** commands, size_t* size);
-        bool HandleDestroyObject(const char** commands, size_t* size);
-
         void* GetCmdSpace(size_t size) {
             return mSerializer->GetCmdSpace(size);
         }
