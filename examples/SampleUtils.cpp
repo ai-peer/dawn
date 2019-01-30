ac5ee39bf76eef300b0cf70a90dba7d5640e04e4
Austin Eng <enga@chromium.org>
2019-01-30 14:42:33 -0800

    Create wire Client and Server classes with private impl
    This creates proper Client and Server interfaces which will be necessary
    for adding additional features to the Wire for chrome integration
    
    Bug: dawn:88
    Change-Id: I181e95079b0bac85c2c6152ad8066a94b80c8d28

--- examples/SampleUtils.cpp
+++ examples/SampleUtils.cpp
@@ -23,6 +23,8 @@
 #include <dawn/dawncpp.h>
 #include <dawn/dawn_wsi.h>
 #include <dawn_native/DawnNative.h>
+#include <dawn_wire/Client.h>
+#include <dawn_wire/Server.h>
 #include "GLFW/glfw3.h"
 
 #include <cstring>
@@ -61,8 +63,8 @@ static utils::BackendBinding* binding = nullptr;
 
 static GLFWwindow* window = nullptr;
 
-static dawn_wire::CommandHandler* wireServer = nullptr;
-static dawn_wire::CommandHandler* wireClient = nullptr;
+static dawn_wire::Server* wireServer = nullptr;
+static dawn_wire::Client* wireClient = nullptr;
 static utils::TerribleCommandBuffer* c2sBuf = nullptr;
 static utils::TerribleCommandBuffer* s2cBuf = nullptr;
 
@@ -101,12 +103,12 @@ dawn::Device CreateCppDawnDevice() {
                 c2sBuf = new utils::TerribleCommandBuffer();
                 s2cBuf = new utils::TerribleCommandBuffer();
 
-                wireServer = dawn_wire::NewServerCommandHandler(backendDevice, backendProcs, s2cBuf);
+                wireServer = new dawn_wire::Server(backendDevice, backendProcs, s2cBuf);
                 c2sBuf->SetHandler(wireServer);
 
-                dawnDevice clientDevice;
-                dawnProcTable clientProcs;
-                wireClient = dawn_wire::NewClientDevice(&clientProcs, &clientDevice, c2sBuf);
+                wireClient = new dawn_wire::Client(c2sBuf);
+                dawnDevice clientDevice = wireClient->GetDevice();
+                dawnProcTable clientProcs = wireClient->GetProcs();
                 s2cBuf->SetHandler(wireClient);
 
                 procs = clientProcs;
