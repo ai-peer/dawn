85459eb1f2f22550fa2463bc81a3d4d4f60c5d8e
Austin Eng <enga@chromium.org>
2019-02-12 19:34:33 -0800

    Implement CreateBufferMappedAsync in dawn_native (1)
    This adds implements CreateBufferMappedAsync in Dawn
    on top of CreateBuffer and MapWriteAsync as part of matching
    WebGPU. Buffer initialization can be optimized further in the
    future.
    
    Bug: dawn:7
    Change-Id: Ie70312c43017235577070e032c13fbe114be70c7

--- src/dawn_native/null/DeviceNull.cpp
+++ src/dawn_native/null/DeviceNull.cpp
@@ -75,6 +75,11 @@ namespace dawn_native { namespace null {
     ResultOrError<BufferBase*> Device::CreateBufferImpl(const BufferDescriptor* descriptor) {
         return new Buffer(this, descriptor);
     }
+    MaybeError Device::CreateBufferMappedAsyncImpl(const BufferDescriptor* descriptor,
+                                                   dawnCreateBufferMappedCallback callback,
+                                                   dawnCallbackUserdata userdata) {
+        return BufferBase::CreateMappedAsync(this, descriptor, callback, userdata);
+    }
     CommandBufferBase* Device::CreateCommandBuffer(CommandBufferBuilder* builder) {
         return new CommandBuffer(builder);
     }
