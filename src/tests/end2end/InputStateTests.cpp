845623f7502a03f638f414e44d09fdbf506489e8
Corentin Wallez <cwallez@chromium.org>
2019-02-14 15:20:06 +0100

    WebGPU error handling 2: CommandEncoder for samples and end2end tests
    CommandEncoder is the replacement for CommandBufferBuilder. This commit
    adds the dawn.json definition for it and an initial implementation that
    wraps CommandBufferBuilder. This is done so that the code can be ported
    to CommandEncoder gradually, otherwise the commit would be too big and
    would risk many merge conflicts.
    
    This converts all samples and end2end tests to use CommandEncoder.
    
    BUG=dawn:8
    
    Change-Id: If4ce86e6fb39ba4e0c2af6328d40e63be17d18c1

--- src/tests/end2end/InputStateTests.cpp
+++ src/tests/end2end/InputStateTests.cpp
@@ -174,9 +174,9 @@ class InputStateTest : public DawnTest {
             EXPECT_LE(triangles, 4u);
             EXPECT_LE(instances, 4u);
 
-            dawn::CommandBufferBuilder builder = device.CreateCommandBufferBuilder();
+            dawn::CommandEncoder encoder = device.CreateCommandEncoder();
 
-            dawn::RenderPassEncoder pass = builder.BeginRenderPass(renderPass.renderPassInfo);
+            dawn::RenderPassEncoder pass = encoder.BeginRenderPass(renderPass.renderPassInfo);
             pass.SetPipeline(pipeline);
 
             uint32_t zeroOffset = 0;
@@ -187,7 +187,7 @@ class InputStateTest : public DawnTest {
             pass.Draw(triangles * 3, instances, 0, 0);
             pass.EndPass();
 
-            dawn::CommandBuffer commands = builder.GetResult();
+            dawn::CommandBuffer commands = encoder.Finish();
             queue.Submit(1, &commands);
 
             CheckResult(triangles, instances);
@@ -448,9 +448,9 @@ TEST_P(InputStateTest, UnusedVertexSlot) {
         3, 4, 5, 6,
     });
 
-    dawn::CommandBufferBuilder builder = device.CreateCommandBufferBuilder();
+    dawn::CommandEncoder encoder = device.CreateCommandEncoder();
 
-    dawn::RenderPassEncoder pass = builder.BeginRenderPass(renderPass.renderPassInfo);
+    dawn::RenderPassEncoder pass = encoder.BeginRenderPass(renderPass.renderPassInfo);
 
     uint32_t zeroOffset = 0;
     pass.SetVertexBuffers(0, 1, &buffer, &zeroOffset);
@@ -461,7 +461,7 @@ TEST_P(InputStateTest, UnusedVertexSlot) {
 
     pass.EndPass();
 
-    dawn::CommandBuffer commands = builder.GetResult();
+    dawn::CommandBuffer commands = encoder.Finish();
     queue.Submit(1, &commands);
 
     CheckResult(1, 4);
@@ -493,9 +493,9 @@ TEST_P(InputStateTest, MultiplePipelinesMixedInputState) {
         3, 4, 5, 6,
     });
 
-    dawn::CommandBufferBuilder builder = device.CreateCommandBufferBuilder();
+    dawn::CommandEncoder encoder = device.CreateCommandEncoder();
 
-    dawn::RenderPassEncoder pass = builder.BeginRenderPass(renderPass.renderPassInfo);
+    dawn::RenderPassEncoder pass = encoder.BeginRenderPass(renderPass.renderPassInfo);
 
     uint32_t zeroOffset = 0;
     pass.SetVertexBuffers(0, 1, &buffer, &zeroOffset);
@@ -509,7 +509,7 @@ TEST_P(InputStateTest, MultiplePipelinesMixedInputState) {
 
     pass.EndPass();
 
-    dawn::CommandBuffer commands = builder.GetResult();
+    dawn::CommandBuffer commands = encoder.Finish();
     queue.Submit(1, &commands);
 
     CheckResult(1, 4);
