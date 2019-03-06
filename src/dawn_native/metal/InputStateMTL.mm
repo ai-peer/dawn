82a316099ff37d487b250619a14df7dcdb0ec81a
Li Hao <hao.x.li@intel.com>
2019-03-06 16:15:19 +0800

    Fix zero stride in input state for Metal validation
    Update stride value to max(attribute.offset + sizeof(attribute) for
    each attribute) when input stride is 0 in Metal backend
    
    BUG=dawn:75
    TEST=dawn_end2end_tests
    
    Change-Id: Ic0b2bfc685b8aeb92d1d4401174a819279386ea3

--- src/dawn_native/metal/InputStateMTL.mm
+++ src/dawn_native/metal/InputStateMTL.mm
@@ -82,12 +82,20 @@ namespace dawn_native { namespace metal {
             auto layoutDesc = [MTLVertexBufferLayoutDescriptor new];
             if (info.stride == 0) {
                 // For MTLVertexStepFunctionConstant, the stepRate must be 0,
-                // but the stride must NOT be 0, so I made up a value (256).
-                // TODO(cwallez@chromium.org): the made up value will need to be at least
-                //    max(attrib.offset + sizeof(attrib) for each attrib)
+                // but the stride must NOT be 0, so we made up it with
+                // max(attrib.offset + sizeof(attrib) for each attrib)
+                uint32_t max_stride = 0;
+                for (uint32_t n = 0; n < attributesSetMask.size(); ++n) {
+                    if (!attributesSetMask[n]) {
+                        continue;
+                    }
+                    const VertexAttributeDescriptor& attribute = GetAttribute(n);
+                    uint32_t stride = VertexFormatSize(attribute.format) + attribute.offset;
+                    max_stride = max_stride > stride ? max_stride : stride;
+                }
                 layoutDesc.stepFunction = MTLVertexStepFunctionConstant;
                 layoutDesc.stepRate = 0;
-                layoutDesc.stride = 256;
+                layoutDesc.stride = max_stride;
             } else {
                 layoutDesc.stepFunction = InputStepModeFunction(info.stepMode);
                 layoutDesc.stepRate = 1;
