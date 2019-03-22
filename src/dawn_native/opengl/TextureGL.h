caff81f2acf3e4ed69ab4d2a691af9af5e29654c
Natasha Lee <natlee@microsoft.com>
2019-03-22 12:15:42 -0700

    Destroy frontend and backend for Textures
    Same idea as for buffers, Destroy can be used to free GPU memory
    associated with resources without waiting for javascript garbage
    collection to occur.
    
    Bug: dawn:46
    Change-Id: Ia796b06b5228cbec4cfe8d78a500f967181d8c1f

--- src/dawn_native/opengl/TextureGL.h
+++ src/dawn_native/opengl/TextureGL.h
@@ -33,6 +33,10 @@ namespace dawn_native { namespace opengl {
       public:
         Texture(Device* device, const TextureDescriptor* descriptor);
         Texture(Device* device, const TextureDescriptor* descriptor, GLuint handle);
+        Texture(Device* device,
+                const TextureDescriptor* descriptor,
+                GLuint handle,
+                TextureState state);
         ~Texture();
 
         GLuint GetHandle() const;
@@ -40,6 +44,8 @@ namespace dawn_native { namespace opengl {
         TextureFormatInfo GetGLFormat() const;
 
       private:
+        void DestroyImpl() override;
+
         GLuint mHandle;
         GLenum mTarget;
     };
