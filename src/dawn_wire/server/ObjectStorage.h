b221a2a670ba463e66d664251f4bbbb7b679f37b
Corentin Wallez <cwallez@chromium.org>
2019-03-28 16:52:12 +0100

    Nuke Builders Part 2: remove all builder code from wire
    This removes blocks of code that were obviously builder-specific but
    also removes the ObjectStorage::valid member that was used to implement
    the maybe monad on the wire server side. This is no longer needed since
    dawn_native handles the maybe monad internally now.
    
    BUG=dawn:125
    
    Change-Id: I8c30daae9fc70853bc1996d85a860b4877c5976c

--- src/dawn_wire/server/ObjectStorage.h
+++ src/dawn_wire/server/ObjectStorage.h
@@ -29,26 +29,17 @@ namespace dawn_wire { namespace server {
         T handle;
         uint32_t serial = 0;
 
-        // Used by the error-propagation mechanism to know if this object is an error.
-        // TODO(cwallez@chromium.org): this is doubling the memory usage of
-        // std::vector<ObjectDataBase> consider making it a special marker value in handle instead.
-        bool valid;
         // Whether this object has been allocated, used by the KnownObjects queries
         // TODO(cwallez@chromium.org): make this an internal bit vector in KnownObjects.
         bool allocated;
     };
 
     // Stores what the backend knows about the type.
-    template <typename T, bool IsBuilder = IsBuilderType<T>::value>
+    template <typename T>
     struct ObjectData : public ObjectDataBase<T> {};
 
-    template <typename T>
-    struct ObjectData<T, true> : public ObjectDataBase<T> {
-        ObjectHandle builtObject = ObjectHandle{0, 0};
-    };
-
     template <>
-    struct ObjectData<DawnBuffer, false> : public ObjectDataBase<DawnBuffer> {
+    struct ObjectData<DawnBuffer> : public ObjectDataBase<DawnBuffer> {
         void* mappedData = nullptr;
         size_t mappedDataSize = 0;
     };
@@ -65,7 +56,6 @@ namespace dawn_wire { namespace server {
             // KnownObjects for ID 0.
             Data reservation;
             reservation.handle = nullptr;
-            reservation.valid = false;
             reservation.allocated = false;
             mKnown.push_back(reservation);
         }
@@ -109,7 +99,6 @@ namespace dawn_wire { namespace server {
 
             Data data;
             data.allocated = true;
-            data.valid = false;
             data.handle = nullptr;
 
             if (id >= mKnown.size()) {
@@ -136,7 +125,6 @@ namespace dawn_wire { namespace server {
             for (Data& data : mKnown) {
                 if (data.allocated && data.handle != nullptr) {
                     objects.push_back(data.handle);
-                    data.valid = false;
                     data.allocated = false;
                     data.handle = nullptr;
                 }
