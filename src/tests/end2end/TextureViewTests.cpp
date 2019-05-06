185755803b721efb557297843a8012c5e2191e92
Jiawei Shao <jiawei.shao@intel.com>
2019-05-06 15:45:14 +0800

    [Not for review] Test
    BUG=dawn:39
    
    Change-Id: I24cea0924d36c589c80890116c420ef3d754fe25

--- src/tests/end2end/TextureViewTests.cpp
+++ src/tests/end2end/TextureViewTests.cpp
@@ -387,7 +387,6 @@ TEST_P(TextureViewSamplingTest, Texture2DViewOn2DArrayTexture) {
 
 // Test sampling from a 2D array texture view created on a 2D array texture.
 TEST_P(TextureViewSamplingTest, Texture2DArrayViewOn2DArrayTexture) {
-    DAWN_SKIP_TEST_IF(IsMetal() && IsIntel());
     Texture2DArrayViewTest(6, 1, 2, 0);
 }
 
@@ -403,7 +402,6 @@ TEST_P(TextureViewSamplingTest, Texture2DViewOnOneLevelOf2DArrayTexture) {
 
 // Test sampling from a 2D array texture view created on a mipmap level of a 2D array texture.
 TEST_P(TextureViewSamplingTest, Texture2DArrayViewOnOneLevelOf2DArrayTexture) {
-    DAWN_SKIP_TEST_IF(IsMetal() && IsIntel());
     Texture2DArrayViewTest(6, 6, 2, 4);
 }
 
