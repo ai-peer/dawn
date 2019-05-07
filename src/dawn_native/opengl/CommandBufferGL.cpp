5e29439c94a03a5b7213f650779919362a5a6a34
Jiawei Shao <jiawei.shao@intel.com>
2019-05-07 14:28:43 +0800

    OpenGL: set blend color, viewport and scissor rectangle before clear
    This patch fixes a bug on the OpenGL backend by setting blend color,
    viewport and scissor rectangle before clearing the attachments of the
    render pass so that the full area of the attachments can be cleared
    correctly.
    
    BUG=dawn:126, dawn:141
    TEST=dawn_end2end_tests
    
    Change-Id: I9ddf74a67413ed51f83a69a3dc2c2377a0622cac

--- src/dawn_native/opengl/CommandBufferGL.cpp
+++ src/dawn_native/opengl/CommandBufferGL.cpp
@@ -680,6 +680,12 @@ namespace dawn_native { namespace opengl {
             }
         }
 
+        // Set blend color, viewport and scissor rectangles so that we can clear the attachments in
+        // the right area.
+        glBlendColor(0, 0, 0, 0);
+        glViewport(0, 0, renderPass->width, renderPass->height);
+        glScissor(0, 0, renderPass->width, renderPass->height);
+
         // Clear framebuffer attachments as needed
         {
             for (uint32_t i : IterateBitSet(renderPass->colorAttachmentsSet)) {
@@ -732,9 +738,6 @@ namespace dawn_native { namespace opengl {
 
         // Set defaults for dynamic state
         persistentPipelineState.SetDefaultState();
-        glBlendColor(0, 0, 0, 0);
-        glViewport(0, 0, renderPass->width, renderPass->height);
-        glScissor(0, 0, renderPass->width, renderPass->height);
 
         Command type;
         while (mCommands.NextCommandId(&type)) {
