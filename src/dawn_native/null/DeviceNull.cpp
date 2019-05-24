16fad267b6e211c139d8edb06d50c1cb2f383e10
Kai Ninomiya <kainino@chromium.org>
2019-05-23 20:01:12 -0700

    Null backend: disallow more than 256M of Buffer allocations
    Should prevent some uninteresting fuzzer failures.
    
    Bug: dawn:37
    Change-Id: I6b2bef01deb700239e02ef5afa9313023ec83899

--- src/dawn_native/null/DeviceNull.cpp
+++ src/dawn_native/null/DeviceNull.cpp
@@ -68,6 +68,7 @@ namespace dawn_native { namespace null {
 
     Device::~Device() {
         mDynamicUploader = nullptr;
+        ASSERT(mMemoryUsage == 0);
     }
 
     ResultOrError<BindGroupBase*> Device::CreateBindGroupImpl(
@@ -79,6 +80,7 @@ namespace dawn_native { namespace null {
         return new BindGroupLayout(this, descriptor);
     }
     ResultOrError<BufferBase*> Device::CreateBufferImpl(const BufferDescriptor* descriptor) {
+        DAWN_TRY(IncrementMemoryUsage(descriptor->size));
         return new Buffer(this, descriptor);
     }
     CommandBufferBase* Device::CreateCommandBuffer(CommandEncoderBase* encoder) {
@@ -138,6 +140,20 @@ namespace dawn_native { namespace null {
         return DAWN_UNIMPLEMENTED_ERROR("Device unable to copy from staging buffer.");
     }
 
+    MaybeError Device::IncrementMemoryUsage(size_t bytes) {
+        static_assert(kMaxMemoryUsage <= std::numeric_limits<size_t>::max() / 2, "");
+        if (bytes > kMaxMemoryUsage || mMemoryUsage + bytes > kMaxMemoryUsage) {
+            return DAWN_CONTEXT_LOST_ERROR("Out of memory.");
+        }
+        mMemoryUsage += bytes;
+        return {};
+    }
+
+    void Device::DecrementMemoryUsage(size_t bytes) {
+        ASSERT(mMemoryUsage >= bytes);
+        mMemoryUsage -= bytes;
+    }
+
     Serial Device::GetCompletedCommandSerial() const {
         return mCompletedSerial;
     }
@@ -189,6 +205,7 @@ namespace dawn_native { namespace null {
     }
 
     Buffer::~Buffer() {
+        ToBackend(GetDevice())->DecrementMemoryUsage(GetSize());
     }
 
     MaybeError Buffer::MapAtCreationImpl(uint8_t** mappedPointer) {
