ba01804c3667ed1f47288f7aa8c898fab24e2f6d
Li, Hao <hao.x.li@intel.com>
2019-06-13 16:31:24 +0800

    Build Vulkan validation layers in third_party
    Use self build Vulkan validation layers instead of system installed
    to avoid API compatible issue in old versions.
    URL: https://github.com/KhronosGroup/Vulkan-ValidationLayers
    Commit: 10a98fac8fae947e20f18b84f2b91fe5250227c2
    
    BUG=dawn:150
    
    Change-Id: I6321ab61b642549f7ee149b7c18afa974b276574

---
+++ third_party/vulkan-validation-layers/update_vulkan_registry_script.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# Copyright 2019 The Dawn Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script update Vulkan-Headers/registry/generator.py to run with python2
+# and python3 for pathlib importing
+import os, sys
+
+def find(str, lines):
+    for line in lines:
+        if str in line:
+            return True
+    return False
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: %s FILE" % os.path.basename(__file__))
+        sys.exit(1)
+
+    with open(sys.argv[1], "r") as f:
+        lines = f.readlines()
+
+    if find("from pathlib2 import Path", lines):
+        sys.exit(0)
+
+    with open(sys.argv[1], "w") as fw:
+        pathlib = "from pathlib import Path"
+        for line in lines:
+            if pathlib in line:
+                pathlib2 = "try:\n    from pathlib import Path\nexcept:\n    from pathlib2 import Path\n"
+                line = line.replace(pathlib, pathlib2)
+            
+            fw.write(line)
+
+    f.close()
+    fw.close()
