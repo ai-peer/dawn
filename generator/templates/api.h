8f677df056c6cf0d952ad72ffa5d3554d61774ec
Corentin Wallez <cwallez@chromium.org>
2019-07-08 12:19:35 +0200

    Changes for webgpu.h
    Change-Id: Ie16ebc1bbfe2c7dacdc3e31a68e49ac736fe86fe

--- generator/templates/api.h
+++ generator/templates/api.h
@@ -1,46 +1,51 @@
-//* Copyright 2017 The Dawn Authors
-//*
-//* Licensed under the Apache License, Version 2.0 (the "License");
-//* you may not use this file except in compliance with the License.
-//* You may obtain a copy of the License at
-//*
-//*     http://www.apache.org/licenses/LICENSE-2.0
-//*
-//* Unless required by applicable law or agreed to in writing, software
-//* distributed under the License is distributed on an "AS IS" BASIS,
-//* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//* See the License for the specific language governing permissions and
-//* limitations under the License.
+#ifndef WGPU_WGPU_H_
+#define WGPU_WGPU_H_
 
-#ifndef DAWN_DAWN_H_
-#define DAWN_DAWN_H_
-
-#include "dawn/dawn_export.h"
+#if !defined(WGPU_EXPORT)
+#    if defined(_WIN32)
+#        if defined(WGPU_IMPLEMENTATION)
+#            define WGPU_EXPORT __declspec(dllexport)
+#        else
+#            define WGPU_EXPORT __declspec(dllimport)
+#        endif
+#    else  // defined(_WIN32)
+#        if defined(WGPU_IMPLEMENTATION)
+#            define WGPU_EXPORT __attribute__((visibility("default")))
+#        else
+#            define WGPU_EXPORT
+#        endif
+#    endif  // defined(_WIN32)
+#endif  // defined(WGPU_EXPORT)
 
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
 
-const uint64_t DAWN_WHOLE_SIZE = 0xffffffffffffffffULL; // UINT64_MAX
+const uint64_t WGPU_WHOLE_SIZE = 0xffffffffffffffffULL; // UINT64_MAX
+
+typedef uint32_t WGPUFlags;
 
 {% for type in by_category["object"] %}
     typedef struct {{as_cType(type.name)}}Impl* {{as_cType(type.name)}};
 {% endfor %}
 
 {% for type in by_category["enum"] + by_category["bitmask"] %}
-    typedef enum {
+    typedef enum {{as_cType(type.name)}} {
         {% for value in type.values %}
             {{as_cEnum(type.name, value.name)}} = 0x{{format(value.value, "08X")}},
         {% endfor %}
         {{as_cEnum(type.name, Name("force32"))}} = 0x7FFFFFFF
     } {{as_cType(type.name)}};
+    {% if type.category == "bitmask" %}
+        typedef WGPUFlags {{as_cType(type.name)}}Flags;
+    {% endif %}
 
 {% endfor %}
 
 {% for type in by_category["structure"] %}
     typedef struct {{as_cType(type.name)}} {
         {% if type.extensible %}
-            const void* nextInChain;
+            void const * nextInChain;
         {% endif %}
         {% for member in type.members %}
             {{as_annotated_cType(member)}};
@@ -49,26 +54,30 @@ const uint64_t DAWN_WHOLE_SIZE = 0xffffffffffffffffULL; // UINT64_MAX
 
 {% endfor %}
 
-// Custom types depending on the target language
-typedef void (*DawnDeviceErrorCallback)(const char* message, void* userdata);
-typedef void (*DawnBufferCreateMappedCallback)(DawnBufferMapAsyncStatus status,
-                                               DawnCreateBufferMappedResult result,
-                                               void* userdata);
-typedef void (*DawnBufferMapReadCallback)(DawnBufferMapAsyncStatus status,
-                                          const void* data,
-                                          uint64_t dataLength,
-                                          void* userdata);
-typedef void (*DawnBufferMapWriteCallback)(DawnBufferMapAsyncStatus status,
-                                           void* data,
-                                           uint64_t dataLength,
-                                           void* userdata);
-typedef void (*DawnFenceOnCompletionCallback)(DawnFenceCompletionStatus status, void* userdata);
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-{% for type in by_category["object"] %}
+typedef void (*WGPUBufferCreateMappedCallback)(WGPUBufferMapAsyncStatus status,
+                                               WGPUCreateBufferMappedResult result,
+                                               void* userdata);
+typedef void (*WGPUBufferMapReadCallback)(WGPUBufferMapAsyncStatus status,
+                                          const void* data,
+                                          uint64_t dataLength,
+                                          void* userdata);
+typedef void (*WGPUBufferMapWriteCallback)(WGPUBufferMapAsyncStatus status,
+                                           void* data,
+                                           uint64_t dataLength,
+                                           void* userdata);
+typedef void (*WGPUFenceOnCompletionCallback)(WGPUFenceCompletionStatus status, void* userdata);
+
+typedef void (*WGPUProc)();
+
+#if !defined(WGPU_SKIP_PROCS)
+
+typedef WGPUProc (*WGPUProcGetProcAddress)(WGPUDevice device, const char* procName);
+
+{% for type in by_category["object"] if len(native_methods(type)) > 0 %}
     // Procs of {{type.name.CamelCase()}}
     {% for method in native_methods(type) %}
         typedef {{as_cType(method.return_type.name)}} (*{{as_cProc(type.name, method.name)}})(
@@ -80,26 +89,16 @@ extern "C" {
     {% endfor %}
 
 {% endfor %}
+#endif  // !defined(WGPU_SKIP_PROCS)
 
-struct DawnProcTable_s {
-    {% for type in by_category["object"] %}
-        {% for method in native_methods(type) %}
-            {{as_cProc(type.name, method.name)}} {{as_varName(type.name, method.name)}};
-        {% endfor %}
+#if !defined(WGPU_SKIP_DECLARATIONS)
 
-    {% endfor %}
-};
-typedef struct DawnProcTable_s DawnProcTable;
+WGPU_EXPORT WGPUProc WGPUGetProcAddress(WGPUDevice device, const char* procName);
 
-// Stuff below is for convenience and will forward calls to a static DawnProcTable.
-
-// Set which DawnProcTable will be used
-DAWN_EXPORT void dawnSetProcs(const DawnProcTable* procs);
-
-{% for type in by_category["object"] %}
+{% for type in by_category["object"] if len(native_methods(type)) > 0 %}
     // Methods of {{type.name.CamelCase()}}
     {% for method in native_methods(type) %}
-        DAWN_EXPORT {{as_cType(method.return_type.name)}} {{as_cMethod(type.name, method.name)}}(
+        WGPU_EXPORT {{as_cType(method.return_type.name)}} {{as_cMethod(type.name, method.name)}}(
             {{-as_cType(type.name)}} {{as_varName(type.name)}}
             {%- for arg in method.arguments -%}
                 , {{as_annotated_cType(arg)}}
@@ -108,9 +107,10 @@ DAWN_EXPORT void dawnSetProcs(const DawnProcTable* procs);
     {% endfor %}
 
 {% endfor %}
+#endif  // !defined(WGPU_SKIP_DECLARATIONS)
 
 #ifdef __cplusplus
 } // extern "C"
 #endif
 
-#endif // DAWN_DAWN_H_
+#endif // WGPU_WGPU_H_
