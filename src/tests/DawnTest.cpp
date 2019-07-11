2bbe45045f4d73634e8b6abea3c97ddda8488dcd
Austin Eng <enga@chromium.org>
2019-07-11 08:52:46 -0700

    Add MemoryTransfer interfaces to the wire
    This patch adds MemoryTransfer client/server interfaces and
    uses it to implement data transfers for buffer mapping.
    
    This patch also provides a default "inline" implementation of
    the MemoryTransfer which is used if the embedder does not
    provide one on initialization.
    
    Because implementations of MemoryTransfer perform their own
    serialization, a skip_serialize option is added to WireCmd records.
    
    Bug: dawn:156
    Change-Id: I2fa035517628a3ad465b0bc18a6ffc477e2bd67f

--- src/tests/DawnTest.cpp
+++ src/tests/DawnTest.cpp
@@ -397,10 +397,10 @@ void DawnTest::SetUp() {
         mC2sBuf = std::make_unique<utils::TerribleCommandBuffer>();
         mS2cBuf = std::make_unique<utils::TerribleCommandBuffer>();
 
-        mWireServer.reset(new dawn_wire::WireServer(backendDevice, backendProcs, mS2cBuf.get()));
+        mWireServer.reset(new dawn_wire::WireServer({backendDevice, backendProcs, mS2cBuf.get()}));
         mC2sBuf->SetHandler(mWireServer.get());
 
-        mWireClient.reset(new dawn_wire::WireClient(mC2sBuf.get()));
+        mWireClient.reset(new dawn_wire::WireClient({mC2sBuf.get()}));
         DawnDevice clientDevice = mWireClient->GetDevice();
         DawnProcTable clientProcs = mWireClient->GetProcs();
         mS2cBuf->SetHandler(mWireClient.get());
