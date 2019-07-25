e3755a2efda1cd4261a9b5272380d6740ab2aa91
Bryan Bernhart <bryan.bernhart@intel.com>
2019-07-24 17:04:12 -0700

    Resource Management 5: Support for placed resources.
    Replace CreateCommitedResource with CreatePlacedResource
    with CreateHeap using the resource heap allocator.
    
    BUG=dawn:27
    
    Change-Id: I090b6ab40e7eaa0d7ea5ce1e8b760e961be9b559

---
+++ src/dawn_native/ResourceMemoryAllocation.h
@@ -0,0 +1,45 @@
+// Copyright 2018 The Dawn Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef DAWNNATIVE_RESOURCEMEMORYALLOCATION_H_
+#define DAWNNATIVE_RESOURCEMEMORYALLOCATION_H_
+
+#include <cstddef>
+#include <cstdint>
+
+namespace dawn_native {
+
+    class ResourceHeapBase;
+
+    // Handle into a resource heap pool.
+    class ResourceMemoryAllocation {
+      public:
+        ResourceMemoryAllocation() = default;
+        ResourceMemoryAllocation(uint64_t offset,
+                                 ResourceHeapBase* resourceHeap = nullptr,
+                                 bool isDirect = false);
+        ~ResourceMemoryAllocation() = default;
+
+        ResourceHeapBase* GetResourceHeap() const;
+        uint64_t GetOffset() const;
+        bool IsDirect() const;
+
+      private:
+        bool mIsDirect;
+        uint64_t mOffset;
+        ResourceHeapBase* mResourceHeap = nullptr;
+    };
+}  // namespace dawn_native
+
+#endif  // DAWNNATIVE_RESOURCEMEMORYALLOCATION_H_
---
+++ src/tests/unittests/ResourceAllocatorTests.cpp
@@ -0,0 +1,84 @@
+// Copyright 2019 The Dawn Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest/gtest.h>
+
+#include "dawn_native/ResourceHeap.h"
+#include "dawn_native/ResourceHeapAllocator.h"
+
+using namespace dawn_native;
+
+// Mock objects used to test allocators without requiring a device.
+class DummyResourceHeap : public ResourceHeapBase {
+  public:
+    DummyResourceHeap() = default;
+    ~DummyResourceHeap() = default;
+
+    MaybeError MapImpl() override {
+        return DAWN_UNIMPLEMENTED_ERROR("Cannot map a dummy resource");
+    }
+    void UnmapImpl() override {
+    }
+};
+
+class DummyResourceHeapAllocator {
+  public:
+    ResourceHeapBase* CreateHeap(uint64_t heapSize, int heapFlags = 0) {
+        return new DummyResourceHeap();
+    }
+    void FreeHeap(ResourceHeapBase* heap) {
+        delete heap;
+    }
+};
+
+class DirectResourceMemoryAllocatorTests : public testing::Test {
+  public:
+    typedef DirectResourceMemoryAllocator<DummyResourceHeapAllocator> DirectResourceMemoryAllocator;
+};
+
+// Verify direct allocation with a single resource.
+TEST_F(DirectResourceMemoryAllocatorTests, SingleResourceHeap) {
+    DirectResourceMemoryAllocator allocator;
+
+    constexpr uint64_t allocationSize = 5;  // NPOT allowed in direct allocation.
+    ResourceMemoryAllocation allocation = allocator.Allocate(allocationSize);
+    ASSERT_EQ(allocation.GetOffset(), 0u);
+
+    ASSERT_TRUE(allocation.IsDirect());
+    ASSERT_TRUE(allocation.GetResourceHeap() != nullptr);
+
+    allocator.Deallocate(allocation);
+}
+
+// Verify direct allocation using multiple resources.
+TEST_F(DirectResourceMemoryAllocatorTests, MultiResourceHeap) {
+    DirectResourceMemoryAllocator allocator;
+
+    // Allocate two 4-byte blocks on seperate resource heaps.
+    ResourceMemoryAllocation allocationA = allocator.Allocate(5);
+    ASSERT_EQ(allocationA.GetOffset(), 0u);
+    ASSERT_TRUE(allocationA.IsDirect());
+    ASSERT_TRUE(allocationA.GetResourceHeap() != nullptr);
+
+    ResourceMemoryAllocation allocationB = allocator.Allocate(10);
+    ASSERT_EQ(allocationB.GetOffset(), 0u);
+    ASSERT_TRUE(allocationB.IsDirect());
+    ASSERT_TRUE(allocationA.GetResourceHeap() != nullptr);
+
+    // Both allocations must be backed by seperate resource heaps.
+    ASSERT_TRUE(allocationB.GetResourceHeap() != allocationA.GetResourceHeap());
+
+    allocator.Deallocate(allocationA);
+    allocator.Deallocate(allocationB);
+}
