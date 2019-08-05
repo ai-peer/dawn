e12f6bd2b3e36d1c40a181314847d4871dc7d24b
Idan Raiter <idanr@google.com>
2019-08-05 16:11:50 -0700

    [WIP] Add wrapping for using external vulkan images as textures
    Bug: chromium:976495
    Change-Id: I9f07eaf436e10aa6bd88cffdc74fd23834d62ee0

---
+++ src/dawn_native/vulkan/external_memory/MemoryServiceNull.cpp
@@ -0,0 +1,30 @@
+#include "dawn_native/vulkan/DeviceVk.h"
+#include "dawn_native/vulkan/VulkanError.h"
+#include "dawn_native/vulkan/external_memory/MemoryService.h"
+
+#define NULL_ERROR DAWN_UNIMPLEMENTED_ERROR("Using null memory service to interop inside Vulkan")
+
+namespace dawn_native { namespace vulkan { namespace external_memory {
+
+    Service::Service() {
+    }
+
+    Service::~Service() {
+    }
+
+    bool Service::Supported() {
+        return false;
+    }
+
+    ServiceType Service::GetType() {
+        return ServiceType::Null;
+    }
+
+    ResultOrError<VkDeviceMemory> Service::ImportImageMemory(Device* device,
+                                                             ExternalHandle handle,
+                                                             VkDeviceSize allocationSize,
+                                                             uint32_t memoryTypeIndex) {
+        return NULL_ERROR;
+    }
+
+}}}  // namespace dawn_native::vulkan::external_memory
