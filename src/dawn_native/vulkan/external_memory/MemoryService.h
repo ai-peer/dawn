6aaa69c0bbd8cc3e3bec7562449091e2fded4773
Idan Raiter <idanr@google.com>
2019-08-06 13:24:31 -0700

    [WIP] Add wrapping for using external vulkan images as textures
    Bug: chromium:976495
    Change-Id: I9f07eaf436e10aa6bd88cffdc74fd23834d62ee0

---
+++ src/dawn_native/vulkan/external_memory/MemoryService.h
@@ -0,0 +1,31 @@
+#ifndef DAWNNATIVE_VULKAN_EXTERNALMEMORY_SERVICE_H_
+#define DAWNNATIVE_VULKAN_EXTERNALMEMORY_SERVICE_H_
+#include <memory>
+#include "common/Platform.h"
+#include "common/vulkan_platform.h"
+#include "dawn_native/Error.h"
+#include "dawn_native/vulkan/ExternalHandle.h"
+
+namespace dawn_native { namespace vulkan {
+    class Device;
+}}  // namespace dawn_native::vulkan
+
+namespace dawn_native { namespace vulkan { namespace external_memory {
+
+    class Service {
+      public:
+        Service(Device* device);
+        ~Service();
+        bool Supported();
+
+        ResultOrError<VkDeviceMemory> ImportImageMemory(ExternalMemoryHandle handle,
+                                                        VkDeviceSize allocationSize,
+                                                        uint32_t memoryTypeIndex);
+
+      private:
+        Device* mDevice = nullptr;
+    };
+
+}}}  // namespace dawn_native::vulkan::external_memory
+
+#endif  // DAWNNATIVE_VULKAN_EXTERNALMEMORY_SERVICE_H_
---
+++ src/dawn_native/vulkan/external_memory/MemoryServiceNull.cpp
@@ -0,0 +1,25 @@
+#include "dawn_native/vulkan/DeviceVk.h"
+#include "dawn_native/vulkan/VulkanError.h"
+#include "dawn_native/vulkan/external_memory/MemoryService.h"
+
+#define NULL_ERROR DAWN_UNIMPLEMENTED_ERROR("Using null memory service to interop inside Vulkan")
+
+namespace dawn_native { namespace vulkan { namespace external_memory {
+
+    Service::Service(Device* device) : mDevice(device) {
+    }
+
+    Service::~Service() {
+    }
+
+    bool Service::Supported() {
+        return false;
+    }
+
+    ResultOrError<VkDeviceMemory> Service::ImportImageMemory(ExternalMemoryHandle handle,
+                                                             VkDeviceSize allocationSize,
+                                                             uint32_t memoryTypeIndex) {
+        return NULL_ERROR;
+    }
+
+}}}  // namespace dawn_native::vulkan::external_memory
