c6f981b28ec8c9574b8f286bc634fad55b5684a6
Idan Raiter <idanr@google.com>
2019-08-07 14:38:33 -0700

    Add wrapping for using external vulkan images as textures
    Bug: chromium:976495
    Change-Id: I9f07eaf436e10aa6bd88cffdc74fd23834d62ee0

---
+++ src/dawn_native/vulkan/external_semaphore/SemaphoreServiceNull.cpp
@@ -0,0 +1,33 @@
+#include "dawn_native/vulkan/DeviceVk.h"
+#include "dawn_native/vulkan/VulkanError.h"
+#include "dawn_native/vulkan/external_semaphore/SemaphoreService.h"
+
+#define NULL_ERROR DAWN_UNIMPLEMENTED_ERROR("Using null semaphore service to interop inside Vulkan")
+
+namespace dawn_native { namespace vulkan { namespace external_semaphore {
+
+    Service::Service(Device* device) : mDevice(device) {
+        // Silence compiler warning about mDevice being unused
+        (void)mDevice;
+    }
+
+    Service::~Service() {
+    }
+
+    bool Service::Supported() {
+        return false;
+    }
+
+    ResultOrError<VkSemaphore> Service::ImportSemaphore(ExternalSemaphoreHandle handle) {
+        return NULL_ERROR;
+    }
+
+    ResultOrError<VkSemaphore> Service::CreateExportableSemaphore() {
+        return NULL_ERROR;
+    }
+
+    ResultOrError<ExternalSemaphoreHandle> Service::ExportSemaphore(VkSemaphore semaphore) {
+        return NULL_ERROR;
+    }
+
+}}}  // namespace dawn_native::vulkan::external_semaphore
