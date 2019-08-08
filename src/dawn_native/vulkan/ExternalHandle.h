722c1df316b7a11b86245d881b5e83fe4633432f
Idan Raiter <idanr@google.com>
2019-08-08 14:51:43 -0700

    Add wrapping for using external vulkan images as textures
    This change adds platform-dependent services that handle creating
    semaphores and importing image memory. Then, we use them to wrap a
    texture from an outside source, and release a signal semaphore back
    when we're done with it. This will be used to allow chrome to render
    dawn on Vulkan platforms.
    
    Bug: chromium:976495
    Change-Id: I9f07eaf436e10aa6bd88cffdc74fd23834d62ee0

---
+++ src/dawn_native/vulkan/ExternalHandle.h
@@ -0,0 +1,18 @@
+#ifndef DAWNNATIVE_VULKAN_EXTERNALHANDLE_H_
+#define DAWNNATIVE_VULKAN_EXTERNALHANDLE_H_
+
+namespace dawn_native { namespace vulkan {
+
+#ifdef DAWN_PLATFORM_LINUX
+    // File descriptor
+    using ExternalMemoryHandle = int;
+    // File descriptor
+    using ExternalSemaphoreHandle = int;
+#else
+    using ExternalMemoryHandle = void*;
+    using ExternalSemaphoreHandle = void*;
+#endif
+
+}}  // namespace dawn_native::vulkan
+
+#endif  // DAWNNATIVE_VULKAN_EXTERNALHANDLE_H_
