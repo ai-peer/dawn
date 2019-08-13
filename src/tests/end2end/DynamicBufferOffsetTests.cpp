6fdc243f4fac95134cde228f247ded5d8146453f
Yan, Shaobo <shaobo.yan@intel.com>
2019-08-13 16:33:52 +0800

    Disable InheritDynamicOffsetsComputePipeline
    This tests may failed unexpected, disable it for now.
    
    BUG=dawn:55
    
    Change-Id: I537ef66ee6b0844e8b1dfd46fae47c5921451e55

--- src/tests/end2end/DynamicBufferOffsetTests.cpp
+++ src/tests/end2end/DynamicBufferOffsetTests.cpp
@@ -330,8 +330,7 @@ TEST_P(DynamicBufferOffsetTests, InheritDynamicOffestsRenderPipeline) {
 // TODO(shaobo.yan@intel.com) : Try this test on GTX1080 and cannot reproduce the failure.
 // Suspect it is due to dawn doesn't handle sync between two dispatch and disable this case.
 // Will double check root cause after got GTX1660.
-TEST_P(DynamicBufferOffsetTests, InheritDynamicOffestsComputePipeline) {
-    DAWN_SKIP_TEST_IF(IsWindows() && IsNvidia());
+TEST_P(DynamicBufferOffsetTests, DISABLED_InheritDynamicOffestsComputePipeline) {
     dawn::ComputePipeline pipeline = CreateComputePipeline();
     dawn::ComputePipeline testPipeline = CreateComputePipeline(true);
 
