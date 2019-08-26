dced1e66a8f20eff206a168d64c57dc5d9d86d1d
Kai Ninomiya <kainino@chromium.org>
2019-08-26 15:34:01 -0700

    Use TextureFormat::None for RenderBundleEncoder and AttachmentState
    Bug: dawn:214
    Change-Id: Ia9432582b0a5627c00d46ebcfa63ebf1ba246651

--- src/dawn_native/AttachmentState.h
+++ src/dawn_native/AttachmentState.h
@@ -51,8 +51,7 @@ namespace dawn_native {
       protected:
         std::bitset<kMaxColorAttachments> mColorAttachmentsSet;
         std::array<dawn::TextureFormat, kMaxColorAttachments> mColorFormats;
-        bool mHasDepthStencilAttachment = false;
-        dawn::TextureFormat mDepthStencilFormat;
+        dawn::TextureFormat mDepthStencilFormat = dawn::TextureFormat::None;
         uint32_t mSampleCount = 0;
     };
 
