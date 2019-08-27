53800d695d8cd5314644a2cff2e547f1cb576538
Bryan Bernhart <bryan.bernhart@intel.com>
2019-08-26 18:35:52 -0700

    Resource Management 5: D3D support for resource allocation.
    Refactor existing resource allocators by adding a memory type
    and memory handle.
    
    BUG=dawn:27
    
    Change-Id: I090b6ab40e7eaa0d7ea5ce1e8b760e961be9b559

---
+++ src/dawn_native/ResourceMemoryAllocation.cpp
@@ -0,0 +1,41 @@
+// Copyright 2019 The Dawn Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "dawn_native/ResourceMemoryAllocation.h"
+
+namespace dawn_native {
+
+    ResourceMemoryAllocation::ResourceMemoryAllocation(uint64_t offset,
+                                                       ResourceHeapBase* resourceHeap,
+                                                       AllocationMethod method)
+        : mMethod(method), mOffset(offset), mResourceHeap(resourceHeap) {
+    }
+
+    ResourceHeapBase* ResourceMemoryAllocation::GetResourceHeap() const {
+        return mResourceHeap;
+    }
+
+    uint64_t ResourceMemoryAllocation::GetOffset() const {
+        return mOffset;
+    }
+
+    AllocationMethod ResourceMemoryAllocation::GetAllocationMethod() const {
+        return mMethod;
+    }
+
+    void ResourceMemoryAllocation::Invalidate() {
+        mResourceHeap = nullptr;
+        mMethod = AllocationMethod::kInvalid;
+    }
+}  // namespace dawn_native
