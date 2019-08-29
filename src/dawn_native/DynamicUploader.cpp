93d8f4524fbc4b16239d33b4a0c1f2de20bf9515
Natasha Lee <natlee@microsoft.com>
2019-08-29 14:33:37 -0700

    D3D12: clear nonrenderable texture formats
    Clears nonrenderable color formats and merges zero vs nonzero clears
    to use the same code path.
    
    Bug: dawn:145
    Change-Id: Ibe0745907688e149e6d017ff088a05f4b613b4a8

--- src/dawn_native/DynamicUploader.cpp
+++ src/dawn_native/DynamicUploader.cpp
@@ -64,7 +64,7 @@ namespace dawn_native {
         // request.
         if (uploadHandle.mappedBuffer == nullptr) {
             // Compute the new max size (in powers of two to preserve alignment).
-            size_t newMaxSize = targetRingBuffer->GetSize() * 2;
+            size_t newMaxSize = targetRingBuffer->GetSize();
             while (newMaxSize < size) {
                 newMaxSize *= 2;
             }
