9e7aac9eb3fd3e51413ce96469daba77fa7fa17b
Jiawei Shao <jiawei.shao@intel.com>
2019-10-08 11:33:40 +0800

    Add a toggle to report error when passing a nullptr into ValidateObject
    This patch adds a toggle to report validation error when a nullptr is
    passed into Object->ValidateObject().
    
    This toggle is enabled by default on the null backend and the debug
    build of Dawn and it is useful to catch if a crash is caused by nullptr
    when writing tests. For example, when the shader stage module is set
    to null in either render or compute state descriptors.
    
    BUG=dawn:209
    TEST=dawn_unittests
    
    Change-Id: I352d6038a902602fe215290cdfae5694ce76efec

--- src/dawn_native/Device.cpp
+++ src/dawn_native/Device.cpp
@@ -142,6 +142,13 @@ namespace dawn_native {
     }
 
     MaybeError DeviceBase::ValidateObject(const ObjectBase* object) const {
+#if defined(DAWN_ENABLE_ASSERTS)
+        if (IsToggleEnabled(Toggle::ReportErrorOnNullptrObjectInDeviceValidateObject) &&
+            object == nullptr) {
+            return DAWN_VALIDATION_ERROR("Object is nullptr");
+        }
+#endif
+
         if (DAWN_UNLIKELY(object->GetDevice() != this)) {
             return DAWN_VALIDATION_ERROR("Object from a different device.");
         }
@@ -614,6 +621,9 @@ namespace dawn_native {
     void DeviceBase::SetDefaultToggles() {
         // Sets the default-enabled toggles
         mTogglesSet.SetToggle(Toggle::LazyClearResourceOnFirstUse, true);
+#if defined(DAWN_ENABLE_ASSERTS)
+        mTogglesSet.SetToggle(Toggle::ReportErrorOnNullptrObjectInDeviceValidateObject, true);
+#endif
     }
 
     // Implementation details of object creation
