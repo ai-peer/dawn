59d762fe87c5be212ce7797689acae08de474428
Yan <shaobo.yan@intel.com>
2019-11-05 23:21:23 +0800

    Add large buffer to handle super large data block commands
    TerribleCommandBuffer has space for 10,000,000 bytes worth of commands. If
    commands contain super large data block (e.g. setsubdata), it will
    return nullptr and crash dawn wire layer.
    
    This patch adds a large buffer to handle super large data block.
    
    BUG=dawn:251
    
    Change-Id: Ib007e92b5282afbb93aef63cfffe5a3965f6d5c1

--- src/utils/TerribleCommandBuffer.cpp
+++ src/utils/TerribleCommandBuffer.cpp
@@ -34,9 +34,40 @@ namespace utils {
         //   (Here and/or in the caller?) It might be good to make the wire receiver get a nullptr
         //   instead of pointer to zero-sized allocation in mBuffer.
 
+        // Cannot have commands in mBuffer and mLargeBuffer at same time.
+        ASSERT(mOffset == 0 || mLargeBufferCmdSize == 0);
+
         if (size > sizeof(mBuffer)) {
-            return nullptr;
+            // Flush current cmds in mBuffer to keep order.
+            if (mOffset > 0) {
+                if (!Flush()) {
+                    return nullptr;
+                }
+                return GetCmdSpace(size);
+            }
+
+            // Resize large buffer to the size that can
+            // contain incoming command if needed.
+            if (mLargeBuffer.size() < size) {
+                mLargeBuffer.resize(size);
+            }
+
+            // Record whole cmd space.
+            mLargeBufferCmdSize = size;
+
+            return mLargeBuffer.data();
         }
+
+        // Trigger flush if large buffer contain cmds.
+        if (mLargeBufferCmdSize > 0) {
+            if (!Flush()) {
+                return nullptr;
+            }
+            return GetCmdSpace(size);
+        }
+
+        // Need to flush large buffer first.
+        ASSERT(mLargeBufferCmdSize == 0);
 
         char* result = &mBuffer[mOffset];
         mOffset += size;
@@ -52,8 +83,21 @@ namespace utils {
     }
 
     bool TerribleCommandBuffer::Flush() {
-        bool success = mHandler->HandleCommands(mBuffer, mOffset) != nullptr;
+        // Cannot have commands in mBuffer and mLargeBuffer at same time.
+        ASSERT(mOffset == 0 || mLargeBufferCmdSize == 0);
+
+        bool success = false;
+        // Big buffer not empty, flush it!
+        if (mLargeBufferCmdSize > 0) {
+            success = mHandler->HandleCommands(mLargeBuffer.data(), mLargeBufferCmdSize) != nullptr;
+            // Clear big command buffers.
+            mLargeBufferCmdSize = 0;
+            return success;
+        }
+
+        success = mHandler->HandleCommands(mBuffer, mOffset) != nullptr;
         mOffset = 0;
+
         return success;
     }
 
