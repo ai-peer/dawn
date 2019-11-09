70acdb3281792b18d61183962d44cb4fad3bc4ad
Yunchao He <yunchao.he@intel.com>
2019-11-09 10:44:25 -0900

    Setting proper value in a test
    Bug=
    Change-Id: Ie5c8b2fd8265fe90b7b54c48af48106c356564b6

--- src/tests/end2end/BindGroupTests.cpp
+++ src/tests/end2end/BindGroupTests.cpp
@@ -466,7 +466,7 @@ TEST_P(BindGroupTests, DrawTwiceInSamePipelineWithFourBindGroupSets) {
 
     pass.SetPipeline(pipeline);
 
-    std::array<float, 4> color = {0.25, 0, 0, 0.25};
+    std::array<float, 4> color = {0.125, 0, 0, 1};
     wgpu::Buffer uniformBuffer =
         utils::CreateBufferFromData(device, &color, sizeof(color), wgpu::BufferUsage::Uniform);
     wgpu::BindGroup bindGroup =
