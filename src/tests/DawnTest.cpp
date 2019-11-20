88c657bc8081044e3cf6912e7d48a56a24ed6051
Natasha Lee <natlee@microsoft.com>
2019-11-20 15:35:45 -0800

    Check and handle Device Lost error
    ValidateDeviceIsAlive at DeviceBase entry points to ensure device is alive
    before any commands are executed.
    
    CheckAndHandleDeviceLost will check with backend if device/context has been lost.
    If it has been lost, it will LoseDevice.
    
    LoseDevice calls DeviceLostImpl, handles the device lost callback error, and sets
    device as lost.
    
    DeviceLostImpl deallocates the device, cleans up resources.
    
    Once device is lost, ignore all pending and future commands.
    
    Bug: dawn:19 dawn:68
    Change-Id: Icbbbadf278cae5e6213050d00439118789c863dc

--- src/tests/DawnTest.cpp
+++ src/tests/DawnTest.cpp
@@ -460,6 +460,7 @@ void DawnTestBase::SetUp() {
     queue = device.CreateQueue();
 
     device.SetUncapturedErrorCallback(OnDeviceError, this);
+    device.SetDeviceLostCallback(OnDeviceLost, this);
 }
 
 void DawnTestBase::TearDown() {
@@ -498,6 +499,12 @@ void DawnTestBase::OnDeviceError(WGPUErrorType type, const char* message, void* 
     ASSERT_TRUE(self->mExpectError) << "Got unexpected device error: " << message;
     ASSERT_FALSE(self->mError) << "Got two errors in expect block";
     self->mError = true;
+}
+
+void DawnTestBase::OnDeviceLost(const char* message, void* userdata) {
+    // FAIL() << "Device was lost during the test";
+    DawnTestBase* self = static_cast<DawnTestBase*>(userdata);
+    self->mExpectError = true;
 }
 
 std::ostringstream& DawnTestBase::AddBufferExpectation(const char* file,
