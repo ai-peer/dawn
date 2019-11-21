d344ec963496ec12db37c4eacae8e0a4d47ae71d
Natasha Lee <natlee@microsoft.com>
2019-11-20 16:02:49 -0800

    Check and handle Device Lost error
    ValidateDeviceIsAlive at DeviceBase entry points to ensure device is alive
    before any commands are executed.
    
    CheckAndHandleDeviceLost will check with backend if device/context has been lost.
    If it has been lost, it will LoseDevice.
    
    LoseDevice calls DeviceLostImpl, handles the device lost callback error, and sets
    device as lost.
    
    DeviceLostImpl deallocates the device, cleans up resources.
    
    Once device is lost, ignore all pending and future commands.
    
    Bug: dawn:19 dawn:68
    Change-Id: Icbbbadf278cae5e6213050d00439118789c863dc

--- src/dawn_native/opengl/DeviceGL.h
+++ src/dawn_native/opengl/DeviceGL.h
@@ -86,6 +86,8 @@ namespace dawn_native { namespace opengl {
             const TextureViewDescriptor* descriptor) override;
 
         void CheckPassedFences();
+        void Destroy() override;
+        void WaitForIdleForDestructionImpl() override;
 
         Serial mCompletedSerial = 0;
         Serial mLastSubmittedSerial = 0;
--- src/dawn_native/vulkan/DeviceVk.h
+++ src/dawn_native/vulkan/DeviceVk.h
@@ -124,6 +124,8 @@ namespace dawn_native { namespace vulkan {
         void GatherQueueFromDevice();
 
         void InitTogglesFromDriver();
+        void Destroy() override;
+        void WaitForIdleForDestructionImpl() override;
 
         // To make it easier to use fn it is a public const member. However
         // the Device is allowed to mutate them through these private methods.
