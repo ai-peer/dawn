94ccbccc8aee044bce872e7f829fb77bf7af9e3d
Austin Eng <enga@chromium.org>
2019-12-09 15:16:31 -0800

    Add script to generate fuzzer seed corpus from tests
    This adds a script which runs the end2end_tests, captures a wire trace,
    and then minimizes the corpus with the fuzzer. Minimizing the corpus
    requires libfuzzer, so this only works in a Chromium checkout.
    
    Change-Id: I8d0db5121745bd5ee4a350cf46fb37cfa434e3dc
    Bug: dawn:295

---
+++ scripts/update_fuzzer_seed_corpus.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# Copyright 2019 The Dawn Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# PREREQUISITES
+# -----------------------------------------------------------------------------
+# 1. Make sure gcloud is installed:
+#    https://g3doc.corp.google.com/cloud/sdk/g3doc/index.md?cl=head
+#
+#   sudo apt install -y google-cloud-sdk
+#
+# 2. Login with @google.com credentials
+#
+#   gcloud auth login
+#
+# 3. This script must be run from a Chromium checkout with use_libfuzzer=true
+
+fuzzer_name=$1
+test_name=$2
+base_dir=$(dirname "$0")
+
+testcase_dir=/tmp/testcases/${fuzzer_name}/
+minimized_testcase_dir=/tmp/testcases/${fuzzer_name}_minimized/
+
+# Exit if anything fails
+set -e
+
+# Make a directory for temporarily storing testcases
+mkdir -p $testcase_dir
+
+# Make an empty directory for temporarily storing minimized testcases
+rm -rf $minimized_testcase_dir
+mkdir -p $minimized_testcase_dir
+
+# Download the existing corpus. First argument is src, second is dst.
+gsutil -m rsync gs://clusterfuzz-corpus/libfuzzer/${fuzzer_name}/ $testcase_dir
+
+# Run the test binary, tracing commands, and then run the fuzzer to minimize the corpus.
+python $base_dir/generate_seed_corpus_from_tests.py \
+    --fuzzer $fuzzer_name \
+    --test $test_name \
+    --output $testcase_dir \
+    --minimized_output $minimized_testcase_dir \
+    --test_args "--use-wire --wire-trace-dir=${testcase_dir}"
+
+# If the python script returned nonzero, exit with the same status code
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+if [ -z "$(ls -A $minimized_testcase_dir)" ]; then
+cat << EOF
+
+Minimized testcase directory is empty!
+Are you building with use_libfuzzer=true ?
+
+EOF
+    exit 1
+fi
+
+cat << EOF
+
+Please test the corpus in $minimized_testcase_dir with $fuzzer_name and confirm it works as expected.
+
+Then, run the following command to upload new testcases to the seed corpus:
+
+    gsutil -m rsync -d $minimized_testcase_dir gs://clusterfuzz-corpus/libfuzzer/${fuzzer_name}/
+
+    WARNING: This will delete all GCS files that are not also in $minimized_testcase_dir
+
+EOF
