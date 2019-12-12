1b297663dfc668586d07ca3890bf5d1504850364
Natasha Lee <natlee@microsoft.com>
2019-12-11 18:14:16 -0800

    Add DeviceLostCallback
    Handle DeviceLostCallback once DeviceLost error occurs.
    Disallow any other commands or actions on device to happen after device
    has been lost.
    
    We will take care of cleaning up resources and destroying the device
    in a future change. This change will focus on the callback and status check
    of the device.
    
    Bug: dawn:19 dawn:68
    Change-Id: Icbbbadf278cae5e6213050d00439118789c863dc

--- src/dawn_native/Device.h
+++ src/dawn_native/Device.h
@@ -29,9 +29,6 @@
 #include <memory>
 
 namespace dawn_native {
-
-    using ErrorCallback = void (*)(const char* errorMessage, void* userData);
-
     class AdapterBase;
     class AttachmentState;
     class AttachmentStateBlueprint;
@@ -158,6 +155,7 @@ namespace dawn_native {
         TextureBase* CreateTexture(const TextureDescriptor* descriptor);
         TextureViewBase* CreateTextureView(TextureBase* texture,
                                            const TextureViewDescriptor* descriptor);
+        void SetDeviceLostCallback(wgpu::DeviceLostCallback callback, void* userdata);
 
         void InjectError(wgpu::ErrorType type, const char* message);
 
@@ -166,6 +164,9 @@ namespace dawn_native {
         void SetUncapturedErrorCallback(wgpu::ErrorCallback callback, void* userdata);
         void PushErrorScope(wgpu::ErrorFilter filter);
         bool PopErrorScope(wgpu::ErrorCallback callback, void* userdata);
+
+        MaybeError ValidateIsAlive() const;
+
         ErrorScope* GetCurrentErrorScope();
 
         void Reference();
@@ -188,12 +189,20 @@ namespace dawn_native {
         bool IsValidationEnabled() const;
         size_t GetLazyClearCountForTesting();
         void IncrementLazyClearCountForTesting();
+        void LoseForTesting();
 
       protected:
         void SetToggle(Toggle toggle, bool isEnabled);
         void ApplyToggleOverrides(const DeviceDescriptor* deviceDescriptor);
 
         std::unique_ptr<DynamicUploader> mDynamicUploader;
+        // LossStatus::Alive means the device is alive and can be used normally.
+        // LossStatus::BeingLost means the device is in the process of being lost and should not
+        //              accept any new commands.
+        // LossStatus::AlreadyLost means the device has been lost and can no longer be used,
+        //             all resources have been freed.
+        enum class LossStatus { Alive, BeingLost, AlreadyLost };
+        LossStatus mLossStatus = LossStatus::Alive;
 
       private:
         virtual ResultOrError<BindGroupBase*> CreateBindGroupImpl(
@@ -250,6 +259,10 @@ namespace dawn_native {
         void SetDefaultToggles();
 
         void ConsumeError(ErrorData* error);
+        void HandleLoss(const char* message);
+
+        wgpu::DeviceLostCallback mDeviceLostCallback = nullptr;
+        void* mDeviceLostUserdata;
 
         AdapterBase* mAdapter = nullptr;
 
