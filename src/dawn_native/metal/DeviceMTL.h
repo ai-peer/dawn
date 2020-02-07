98433fbd13732c243bacbb12fd9d179c18812798
Austin Eng <enga@chromium.org>
2020-02-07 15:05:17 -0800

    Slab-allocate Metal bind groups
    Bug: dawn:340
    Change-Id: I6185e41d9c71c49953a4de91e5f3042968679fd6

--- src/dawn_native/metal/DeviceMTL.h
+++ src/dawn_native/metal/DeviceMTL.h
@@ -18,6 +18,7 @@
 #include "dawn_native/dawn_platform.h"
 
 #include "common/Serial.h"
+#include "common/SlabAllocator.h"
 #include "dawn_native/Device.h"
 #include "dawn_native/metal/CommandRecordingContext.h"
 #include "dawn_native/metal/Forward.h"
@@ -54,6 +55,7 @@ namespace dawn_native { namespace metal {
         void SubmitPendingCommandBuffer();
 
         MapRequestTracker* GetMapTracker() const;
+        SlabAllocator<BindGroup>* GetBindGroupAllocator();
 
         TextureBase* CreateTextureWrappingIOSurface(const TextureDescriptor* descriptor,
                                                     IOSurfaceRef ioSurface,
@@ -113,6 +115,8 @@ namespace dawn_native { namespace metal {
         // a different thread so we guard access to it with a mutex.
         std::mutex mLastSubmittedCommandsMutex;
         id<MTLCommandBuffer> mLastSubmittedCommands = nil;
+
+        SlabAllocator<BindGroup, 64> mBindGroupAllocator;
     };
 
 }}  // namespace dawn_native::metal
