a09241f1c2e12ae1b7db7a86caaf1ae7ced60d32
Brandon Jones <brandon1.jones@intel.com>
2020-03-02 14:51:51 -0800

    Residency 5: Implement and Integrate Residency Management
    Adds all D3D12 residency management logic.
    
    Bug: dawn:193
    Change-Id: Ibc160289c29cc85817dcfaaef1b92d04599aa802

--- src/dawn_native/d3d12/HeapD3D12.cpp
+++ src/dawn_native/d3d12/HeapD3D12.cpp
@@ -19,6 +19,12 @@ namespace dawn_native { namespace d3d12 {
         : mD3d12Pageable(std::move(d3d12Pageable)), mSize(size) {
     }
 
+    Heap::~Heap() {
+        if (IsResident()) {
+            RemoveFromList();
+        }
+    }
+
     // This function should only be used when mD3D12Pageable was initialized from a ID3D12Pageable
     // that was initially created as an ID3D12Heap (i.e. SubAllocation). If the ID3D12Pageable was
     // initially created as an ID3D12Resource (i.e. DirectAllocation), then use GetD3D12Pageable().
@@ -44,7 +50,33 @@ namespace dawn_native { namespace d3d12 {
         mLastRecordingSerial = serial;
     }
 
+    // The residency manager must know the last serial that any portion of the heap was submitted
+    // for use so that we can ensure this heap stays in resident memory at least until that serial
+    // has completed.
+    uint64_t Heap::GetLastSubmissionSerial() const {
+        return mLastSubmissionSerial;
+    }
+
+    void Heap::SetLastSubmissionSerial(Serial serial) {
+        mLastSubmissionSerial = serial;
+    }
+
     uint64_t Heap::GetSize() const {
         return mSize;
+    }
+
+    bool Heap::IsResident() const {
+        return next() != nullptr || previous() != nullptr;
+    }
+
+    // In some scenarios, such as async buffer mapping, we must lock residency to ensure the heap
+    // cannot be evicted.
+    void Heap::SetResidencyLock(bool residencyLock) {
+        ASSERT(IsResident());
+        mResidencyLock = residencyLock;
+    }
+
+    bool Heap::IsResidencyLocked() const {
+        return mResidencyLock;
     }
 }}  // namespace dawn_native::d3d12
