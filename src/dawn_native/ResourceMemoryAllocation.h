cb344e0341e370311c486986b31695ee741d11e6
Brandon Jones <brandon1.jones@intel.com>
2020-03-02 09:49:49 -0800

    Residency 2: Create a d3d12::Heap During Direct Allocations
    When creating a directly allocated resource in D3D12, also create a
    dawn_native::d3d12::Heap to represent that allocation alongside the
    ResourceHeapAllocation. This matches D3D12's allocation model when using
    CreateCommittedResource and makes residency management much easier.
    
    Bug: dawn:193
    Change-Id: I2280863dcfca57bad72962a2b097f8f2d4cc7dad

--- src/dawn_native/ResourceMemoryAllocation.h
+++ src/dawn_native/ResourceMemoryAllocation.h
@@ -31,6 +31,9 @@ namespace dawn_native {
         // Memory sub-divided using one or more blocks of various sizes.
         kSubAllocated,
 
+        // Memory was allocated outside of Dawn.
+        kUnknown,
+
         // Memory not allocated or freed.
         kInvalid
     };
