5fa53d11651df8e782ddc72b53c29960994e568c
Rafael Cintron <rafael.cintron@microsoft.com>
2020-04-17 16:27:01 -0700

    Move and improve RefCounted
    - Move RefCounted to common (from dawn_native) so that we can use
    it from additional places.
    - Use EXPECT_ macros instead of ASSERT_ in RefCounted tests for
    improved logging on failures.
    - Add a missing test for Ref::Detach.
    
    Change-Id: Iaa7b11b5a6fa146e3c322143279a21a4ac027547

--- src/common/RefCounted.cpp
+++ src/common/RefCounted.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "dawn_native/RefCounted.h"
+#include "common/RefCounted.h"
 
 #include "common/Assert.h"
 
