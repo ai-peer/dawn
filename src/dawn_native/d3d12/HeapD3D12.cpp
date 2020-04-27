243402e321a0e90c1aa5ec049d4dc5f1b45e0bfa
Brandon Jones <brandon1.jones@intel.com>
2020-04-27 13:36:14 -0700

    Descriptor Residency 1: Add Pageable and ShaderVisibleDescriptorHeap
    Refactors ShaderVisibleDescriptorAllocator to use d3d12::Heap to
    represent ID3D12DescriptorHeaps.
    
    Bug: dawn:193
    Change-Id: If0a9df0bc138c4d6f1ad110750ab1e6e8084b80f

--- src/dawn_native/d3d12/HeapD3D12.cpp
+++ src/dawn_native/d3d12/HeapD3D12.cpp
@@ -16,15 +16,11 @@
 
 namespace dawn_native { namespace d3d12 {
     Heap::Heap(ComPtr<ID3D12Pageable> d3d12Pageable, MemorySegment memorySegment, uint64_t size)
-        : mD3d12Pageable(std::move(d3d12Pageable)), mMemorySegment(memorySegment), mSize(size) {
-    }
-
-    Heap::~Heap() {
-        // When a heap is destroyed, it no longer resides in resident memory, so we must evict
-        // it from the LRU cache. If this heap is not manually removed from the LRU-cache, the
-        // ResidencyManager will attempt to use it after it has been deallocated.
-        if (IsInResidencyLRUCache()) {
-            RemoveFromList();
+        : Pageable(std::move(d3d12Pageable), memorySegment, size) {
+        ComPtr<ID3D12Heap> heap;
+        HRESULT result = mD3d12Pageable.As(&heap);
+        if (SUCCEEDED(result)) {
+            mD3D12Heap = heap.Get();
         }
     }
 
@@ -33,58 +29,8 @@ namespace dawn_native { namespace d3d12 {
     // ID3D12Pageable was initially created as an ID3D12Resource (i.e. DirectAllocation), then
     // use GetD3D12Pageable().
     ID3D12Heap* Heap::GetD3D12Heap() const {
-        ComPtr<ID3D12Heap> heap;
-        HRESULT result = mD3d12Pageable.As(&heap);
-        ASSERT(SUCCEEDED(result));
-        return heap.Get();
-    }
-
-    ID3D12Pageable* Heap::GetD3D12Pageable() const {
-        return mD3d12Pageable.Get();
-    }
-
-    MemorySegment Heap::GetMemorySegment() const {
-        return mMemorySegment;
-    }
-
-    Serial Heap::GetLastUsage() const {
-        return mLastUsage;
-    }
-
-    void Heap::SetLastUsage(Serial serial) {
-        mLastUsage = serial;
-    }
-
-    uint64_t Heap::GetLastSubmission() const {
-        return mLastSubmission;
-    }
-
-    void Heap::SetLastSubmission(Serial serial) {
-        mLastSubmission = serial;
-    }
-
-    uint64_t Heap::GetSize() const {
-        return mSize;
-    }
-
-    bool Heap::IsInResidencyLRUCache() const {
-        return IsInList();
-    }
-
-    void Heap::IncrementResidencyLock() {
-        mResidencyLockRefCount++;
-    }
-
-    void Heap::DecrementResidencyLock() {
-        mResidencyLockRefCount--;
-    }
-
-    bool Heap::IsResidencyLocked() const {
-        if (mResidencyLockRefCount == 0) {
-            return false;
-        }
-
-        return true;
+        ASSERT(mD3D12Heap != nullptr);
+        return mD3D12Heap;
     }
 
 }}  // namespace dawn_native::d3d12
