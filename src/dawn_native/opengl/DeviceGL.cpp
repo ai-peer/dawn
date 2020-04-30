aba8123f72e1108fbb8256af3bf95199259c1ba5
Natasha Lee <natlee@microsoft.com>
2020-04-29 18:44:58 -0700

    Refactor Serial tracking to be owned by Device fronthend.
    Move mCompletedSerial and mLastSubmittedSerial to Device frontend and
    add getters and setters for the device backend to access such.
    This is to aid the Device in taking more ownership of Serials and Ticking.
    
    Change-Id: Ifa53ac294a871e484716842a3d212373b57847c4

--- src/dawn_native/opengl/DeviceGL.cpp
+++ src/dawn_native/opengl/DeviceGL.cpp
@@ -147,20 +147,8 @@ namespace dawn_native { namespace opengl {
 
     void Device::SubmitFenceSync() {
         GLsync sync = gl.FenceSync(GL_SYNC_GPU_COMMANDS_COMPLETE, 0);
-        mLastSubmittedSerial++;
-        mFencesInFlight.emplace(sync, mLastSubmittedSerial);
-    }
-
-    Serial Device::GetCompletedCommandSerial() const {
-        return mCompletedSerial;
-    }
-
-    Serial Device::GetLastSubmittedCommandSerial() const {
-        return mLastSubmittedSerial;
-    }
-
-    Serial Device::GetPendingCommandSerial() const {
-        return mLastSubmittedSerial + 1;
+        IncrementLastSubmittedCommandSerial();
+        mFencesInFlight.emplace(sync, GetLastSubmittedCommandSerial());
     }
 
     MaybeError Device::TickImpl() {
@@ -168,7 +156,7 @@ namespace dawn_native { namespace opengl {
         return {};
     }
 
-    void Device::CheckPassedFences() {
+    Serial Device::CheckCompletedSerial() {
         while (!mFencesInFlight.empty()) {
             GLsync sync = mFencesInFlight.front().first;
             Serial fenceSerial = mFencesInFlight.front().second;
@@ -184,9 +172,10 @@ namespace dawn_native { namespace opengl {
 
             mFencesInFlight.pop();
 
-            ASSERT(fenceSerial > mCompletedSerial);
-            mCompletedSerial = fenceSerial;
+            ASSERT(fenceSerial > GetCompletedCommandSerial());
+            return fenceSerial;
         }
+        return 0;
     }
 
     ResultOrError<std::unique_ptr<StagingBufferBase>> Device::CreateStagingBuffer(size_t size) {
@@ -207,7 +196,7 @@ namespace dawn_native { namespace opengl {
         // Some operations might have been started since the last submit and waiting
         // on a serial that doesn't have a corresponding fence enqueued. Force all
         // operations to look as if they were completed (because they were).
-        mCompletedSerial = mLastSubmittedSerial + 1;
+        AssumeCommandsComplete();
     }
 
     MaybeError Device::WaitForIdleForDestruction() {
