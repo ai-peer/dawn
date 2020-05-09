4c9bcca7c03809db88b840586f89e832c6e09dea
Felix Maier <xilefmai@gmail.com>
2020-05-09 14:05:48 +0200

    DXC
    This patch adds the following:
     - UseD3D12DXCompiler toggle
     - Adds LoadModuleDirectory
     - Loads DXC (and DXIL to sign the DXBC) in d3d12/PlatformFunctions
    
    Also adds the following methods to ShaderModuleD3D12 (unused yet):
     - ConvertEncodingBlobToString
     - IsValidDirectXByteCode which validates DXBC output
    
    In the node-webgpu bindings, I got error 126 when doing LoadLibrary for dxcompiler.dll which seems to be a path issue. I added LoadModuleDirectory to load dxcompiler.dll from the path where dawn_native.dll is located
    
    This patch doesn't add DXC to RenderPipelineD3D12 nor ComputePipelineD3D12 yet.
    
    Bug: 402
    Change-Id: I2b8e4a2b7df31b9c766c748f92e11050c0aec3a0

--- src/dawn_native/d3d12/DeviceD3D12.cpp
+++ src/dawn_native/d3d12/DeviceD3D12.cpp
@@ -15,6 +15,7 @@
 #include "dawn_native/d3d12/DeviceD3D12.h"
 
 #include "common/Assert.h"
+#include "common/Log.h"
 #include "dawn_native/BackendConnection.h"
 #include "dawn_native/ErrorData.h"
 #include "dawn_native/d3d12/AdapterD3D12.h"
@@ -432,6 +433,7 @@ namespace dawn_native { namespace d3d12 {
         SetToggle(Toggle::UseD3D12ResourceHeapTier2, useResourceHeapTier2);
         SetToggle(Toggle::UseD3D12RenderPass, GetDeviceInfo().supportsRenderPass);
         SetToggle(Toggle::UseD3D12ResidencyManagement, true);
+        SetToggle(Toggle::UseD3D12DXCompiler, GetFunctions()->dxcCreateInstance != nullptr);
 
         // By default use the maximum shader-visible heap size allowed.
         SetToggle(Toggle::UseD3D12SmallShaderVisibleHeapForTesting, false);
