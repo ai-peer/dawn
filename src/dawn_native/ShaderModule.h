443a8661098be0e0292e66812b42379969bed2bf
Ryan Harrison <rharrison@chromium.org>
2020-05-14 14:25:37 -0400

    Add support for WGSL shaders via Tint
    BUG=dawn:405
    
    Change-Id: I7a79a0d7ce58ff995ec1ff917dd427875fb4deaf

--- src/dawn_native/ShaderModule.h
+++ src/dawn_native/ShaderModule.h
@@ -43,6 +43,8 @@ namespace dawn_native {
 
     class ShaderModuleBase : public CachedObject {
       public:
+        enum Type { TypeUndefined, TypeSpirv, TypeWgsl };
+
         ShaderModuleBase(DeviceBase* device, const ShaderModuleDescriptor* descriptor);
         ~ShaderModuleBase() override;
 
@@ -89,6 +91,7 @@ namespace dawn_native {
       protected:
         static MaybeError CheckSpvcSuccess(shaderc_spvc_status status, const char* error_msg);
         shaderc_spvc::CompileOptions GetCompileOptions() const;
+        virtual MaybeError Initialize();
 
         shaderc_spvc::Context mSpvcContext;
 
@@ -102,7 +105,13 @@ namespace dawn_native {
         MaybeError ExtractSpirvInfoWithSpvc();
         MaybeError ExtractSpirvInfoWithSpirvCross(const spirv_cross::Compiler& compiler);
 
+#ifdef DAWN_ENABLE_WGSL
+        Type mType;
+#endif  // DAWN_ENABLE_WGSL
         std::vector<uint32_t> mSpirv;
+#ifdef DAWN_ENABLE_WGSL
+        std::string mWgsl;
+#endif  // DAWN_ENABLE_WGSL
 
         ModuleBindingInfo mBindingInfo;
         std::bitset<kMaxVertexAttributes> mUsedVertexAttributes;
