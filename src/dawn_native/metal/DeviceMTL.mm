fde730ea882afdf87b1e86d4ab2fe6e77905e389
Natasha Lee <natlee@microsoft.com>
2020-05-13 18:39:48 -0700

    NOT FOR REVIEW Refactor Serial tracking to be owned by fronthend
    Change-Id: I491e0859b673a81e51f1f3f9b964424d059b0858

--- src/dawn_native/metal/DeviceMTL.mm
+++ src/dawn_native/metal/DeviceMTL.mm
@@ -156,9 +156,9 @@ namespace dawn_native { namespace metal {
 
     Serial Device::CheckAndUpdateCompletedSerials() {
         if (GetCompletedCommandSerial() > mCompletedSerial) {
-            // sometimes we artificially increase the serials, in which case the completed serial in
+            // sometimes we increase the serials, in which case the completed serial in
             // the device base will surpass the completed serial we have in the metal backend, so we
-            // must update ours when we see that the completed serial from the frontend has
+            // must update ours when we see that the completed serial from device base has
             // increased.
             mCompletedSerial = GetCompletedCommandSerial();
         }
@@ -167,17 +167,12 @@ namespace dawn_native { namespace metal {
     }
 
     MaybeError Device::TickImpl() {
-        CheckPassedSerials();
         Serial completedSerial = GetCompletedCommandSerial();
 
         mMapTracker->Tick(completedSerial);
 
         if (mCommandContext.GetCommands() != nil) {
             SubmitPendingCommandBuffer();
-        } else if (completedSerial == GetLastSubmittedCommandSerial()) {
-            // If there's no GPU work in flight we still need to artificially increment the serial
-            // so that CPU operations waiting on GPU completion can know they don't have to wait.
-            ArtificiallyIncrementSerials();
         }
 
         return {};
@@ -307,13 +302,11 @@ namespace dawn_native { namespace metal {
             CheckPassedSerials();
         }
 
-        // Artificially increase the serials so work that was pending knows it can complete.
-        ArtificiallyIncrementSerials();
-
-        DAWN_TRY(TickImpl());
-
         // Force all operations to look as if they were completed
         AssumeCommandsComplete();
+        CheckPassedSerials();
+
+        DAWN_TRY(TickImpl());
 
         return {};
     }
