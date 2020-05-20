dc1e22fdfc9fe3661d0461690bcf4de07f9dd40a
Austin Eng <enga@chromium.org>
2020-05-20 16:44:30 +0000

    Fix VulkanImageWrappingUsageTests.ClearImageAcrossDevicesAliased
    In Vulkan, importing memory by file descriptor takes ownership of
    the file descriptor. It is necessary to dup it in
    ClearImageAcrossDevicesAliased because the texture is imported
    twice. This fixes these tests on SwiftShader.
    
    Bug: dawn:417
    Change-Id: I08b6464c4b8bd31f738037678e29fd6d066e7888

--- src/include/dawn_native/VulkanBackend.h
+++ src/include/dawn_native/VulkanBackend.h
@@ -34,7 +34,9 @@ namespace dawn_native { namespace vulkan {
 
 // Can't use DAWN_PLATFORM_LINUX since header included in both dawn and chrome
 #ifdef __linux__
-        // Common properties of external images represented by FDs
+        // Common properties of external images represented by FDs on success the file descriptor's
+        // ownership is transferred to the Dawn implementation and they shouldn't be used outside
+        // of Dawn again.
         struct DAWN_NATIVE_EXPORT ExternalImageDescriptorFD : ExternalImageDescriptor {
           public:
             int memoryFD;  // A file descriptor from an export of the memory of the image
