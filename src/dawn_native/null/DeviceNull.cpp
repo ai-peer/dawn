0657dca642fd21a3c563fff340714af6905bbee8
Yunchao He <yunchao.he@intel.com>
2020-05-28 06:24:47 -0800

    Optimize subresource tracking - use texture view
    Bug: dawn:441
    
    Change-Id: I5cdfc1d84b27eb0b127d34ee829ceedf15f1150c

--- src/dawn_native/null/DeviceNull.cpp
+++ src/dawn_native/null/DeviceNull.cpp
@@ -421,7 +421,7 @@ namespace dawn_native { namespace null {
         return GetDevice()->CreateTexture(descriptor);
     }
 
-    MaybeError OldSwapChain::OnBeforePresent(TextureBase*) {
+    MaybeError OldSwapChain::OnBeforePresent(TextureViewBase*) {
         return {};
     }
 
