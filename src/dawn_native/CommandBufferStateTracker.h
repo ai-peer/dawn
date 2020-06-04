f60d14805ccc48340f868150e3dca03c7e25da3a
Idan Raiter <idanr@google.com>
2020-06-04 15:27:22 -0500

    WIP: Add minimum buffer size validation
    Change-Id: I755cc0ada7be7b1cb71724cb410ab0c3a88cea24

--- src/dawn_native/CommandBufferStateTracker.h
+++ src/dawn_native/CommandBufferStateTracker.h
@@ -40,7 +40,7 @@ namespace dawn_native {
         void SetIndexBuffer();
         void SetVertexBuffer(uint32_t slot);
 
-        static constexpr size_t kNumAspects = 4;
+        static constexpr size_t kNumAspects = 5;
         using ValidationAspects = std::bitset<kNumAspects>;
 
       private:
@@ -57,6 +57,9 @@ namespace dawn_native {
 
         PipelineLayoutBase* mLastPipelineLayout = nullptr;
         RenderPipelineBase* mLastRenderPipeline = nullptr;
+
+        using BufferSizesArray = std::array<std::vector<uint64_t>, kMaxBindGroups>;
+        const BufferSizesArray* mMinimumBufferSizes = nullptr;
     };
 
 }  // namespace dawn_native
