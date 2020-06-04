897e48c72f9bc9f826eadf6995eb7213b0e9aa81
Austin Eng <enga@chromium.org>
2020-06-04 12:04:07 -0700

    Cleanup dawn_wire client-side error handling
    This CL checks buffer sizes before creating map read/write handles.
    It is an error to map a buffer that can't be addressed on the CPU.
    
    It also changes client-side synchronous errors on mapAsync to be
    normal map failures, and not device lost errors. These should be
    recoverable.
    
    Bug: chromium:1014740
    Change-Id: Ieb34c04c3d01c429b7e3b7810729d5e91ecb6270

--- src/dawn_native/Buffer.cpp
+++ src/dawn_native/Buffer.cpp
@@ -201,7 +201,7 @@ namespace dawn_native {
     void BufferBase::CallMapReadCallback(uint32_t serial,
                                          WGPUBufferMapAsyncStatus status,
                                          const void* pointer,
-                                         uint32_t dataLength) {
+                                         uint64_t dataLength) {
         ASSERT(!IsError());
         if (mMapReadCallback != nullptr && serial == mMapSerial) {
             ASSERT(mMapWriteCallback == nullptr);
@@ -222,7 +222,7 @@ namespace dawn_native {
     void BufferBase::CallMapWriteCallback(uint32_t serial,
                                           WGPUBufferMapAsyncStatus status,
                                           void* pointer,
-                                          uint32_t dataLength) {
+                                          uint64_t dataLength) {
         ASSERT(!IsError());
         if (mMapWriteCallback != nullptr && serial == mMapSerial) {
             ASSERT(mMapReadCallback == nullptr);
