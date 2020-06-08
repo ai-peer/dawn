ccc4ad5bd9d90976dd28791d8b6aa6967207c9cc
Natasha Lee <natlee@microsoft.com>
2020-06-08 15:58:28 -0700

    NOT FOR REVIEW Refactor Serial tracking to be owned by fronthend
    Change-Id: I491e0859b673a81e51f1f3f9b964424d059b0858

--- src/dawn_native/Device.cpp
+++ src/dawn_native/Device.cpp
@@ -127,6 +127,7 @@ namespace dawn_native {
                 // Assert that errors are device loss so that we can continue with destruction
                 AssertAndIgnoreDeviceLossError(WaitForIdleForDestruction());
                 AssumeCommandsComplete();
+                AssertAndIgnoreDeviceLossError(TickImpl());
                 break;
 
             case State::BeingDisconnected:
@@ -139,7 +140,6 @@ namespace dawn_native {
             case State::Disconnected:
                 break;
         }
-        ASSERT(mCompletedSerial == mLastSubmittedSerial);
         ASSERT(mFutureCallbackSerial <= mCompletedSerial);
 
         // Skip handling device facilities if they haven't even been created (or failed doing so)
