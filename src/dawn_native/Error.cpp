f04a5af4c0543f3d9db6cfc7159f13816a1d281e
Natasha Lee <natlee@microsoft.com>
2020-06-15 18:09:52 -0700

    Fix AssertAndIgnoreDeviceLossError to handle errors injected by fuzzer
    Fuzzer recently started sometimes injecting errors with
    CheckHRESULT returning E_FAKE_ERROR_FOR_TESTING, which needs to also be ignored
    in AssertAndIgnoreDeviceLossError so that we can continue with destruction.
    
    This CL also adds Austin's pretty printing of HRESULT results as hex values
    and adds printing of "E_FAKE_ERROR_FOR_TESTING" error.
    
    Bug: chromium:1094448
    Change-Id: I6715a5eec02919a3e3b4c86d493b3f7e84c5e206

--- src/dawn_native/Error.cpp
+++ src/dawn_native/Error.cpp
@@ -22,7 +22,11 @@ namespace dawn_native {
     void AssertAndIgnoreDeviceLossError(MaybeError maybeError) {
         if (maybeError.IsError()) {
             std::unique_ptr<ErrorData> errorData = maybeError.AcquireError();
-            ASSERT(errorData->GetType() == InternalErrorType::DeviceLost);
+            // During shutdown and destruction, device lost errors can be ignored.
+            // We can also ignore other unexpected internal errors on shut down and treat it as
+            // device lost so that we can continue with destruction.
+            ASSERT(errorData->GetType() == InternalErrorType::DeviceLost ||
+                   errorData->GetType() == InternalErrorType::Internal);
         }
     }
 
