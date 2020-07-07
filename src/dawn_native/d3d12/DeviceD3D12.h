c10ca31ef1ebb15c19473c0fad61ad22742e399c
Austin Eng <enga@chromium.org>
2020-07-06 17:59:51 -0700

    Remove kMaxBindingsPerGroup limit
    kMaxBindingsPerGroup is not a real WebGPU limit. Code in Dawn that
    uses it now uses dynamically sized vectors, optimistic stack_vecs or
    a different limit like kMaxDynamicBuffersPerPipelineLayout.
    
    The CL introduces kMaxOptimalBindingsPerGroup=32 which is used to size
    the static portion of stack_vecs, but the value is tentative.
    
    Bug: dawn:443
    Change-Id: I08e06bed6531bed8b4365f36cf2fc0579ac5f180

--- src/dawn_native/d3d12/DeviceD3D12.h
+++ src/dawn_native/d3d12/DeviceD3D12.h
@@ -184,13 +184,16 @@ namespace dawn_native { namespace d3d12 {
         std::unique_ptr<ResourceAllocatorManager> mResourceAllocatorManager;
         std::unique_ptr<ResidencyManager> mResidencyManager;
 
-        // Index corresponds to the descriptor count in the range [0, kMaxBindingsPerGroup].
-        static constexpr uint32_t kNumOfStagingDescriptorAllocators = kMaxBindingsPerGroup + 1;
+        // TODO(enga): Consider bucketing these if the count is too many.
+        static constexpr uint32_t kMaxSamplerDescriptorsPerBindGroup = 3 * kMaxSamplersPerShaderStage;
+        static constexpr uint32_t kMaxViewDescriptorsPerBindGroup = kMaxBindingsPerPipelineLayout - kMaxSamplerDescriptorsPerBindGroup;
 
-        std::array<std::unique_ptr<StagingDescriptorAllocator>, kNumOfStagingDescriptorAllocators>
+        // Index corresponds to the descriptor count in the range [0, kMaxSamplerDescriptorsPerBindGroup].
+        std::array<std::unique_ptr<StagingDescriptorAllocator>, kMaxSamplerDescriptorsPerBindGroup + 1>
             mViewAllocators;
 
-        std::array<std::unique_ptr<StagingDescriptorAllocator>, kNumOfStagingDescriptorAllocators>
+        // Index corresponds to the descriptor count in the range [0, kMaxViewDescriptorsPerBindGroup].
+        std::array<std::unique_ptr<StagingDescriptorAllocator>, kMaxViewDescriptorsPerBindGroup + 1>
             mSamplerAllocators;
 
         std::unique_ptr<StagingDescriptorAllocator> mRenderTargetViewAllocator;
