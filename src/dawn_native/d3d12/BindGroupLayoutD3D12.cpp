ce6f810eee10f234176646c69b0d09e33d407968
Austin Eng <enga@chromium.org>
2020-07-06 17:59:51 -0700

    Remove kMaxBindingsPerGroup limit
    kMaxBindingsPerGroup is not a real WebGPU limit. Code in Dawn that
    uses it now uses dynamically sized vectors, optimistic stack_vecs or
    a different limit like kMaxDynamicBuffersPerPipelineLayout.
    
    The CL introduces kMaxOptimalBindingsPerGroup=32 which is used to size
    the static portion of stack_vecs, but the value is tentative.
    
    Bug: dawn:443
    Change-Id: I08e06bed6531bed8b4365f36cf2fc0579ac5f180

--- src/dawn_native/d3d12/BindGroupLayoutD3D12.cpp
+++ src/dawn_native/d3d12/BindGroupLayoutD3D12.cpp
@@ -47,6 +47,7 @@ namespace dawn_native { namespace d3d12 {
     BindGroupLayout::BindGroupLayout(Device* device, const BindGroupLayoutDescriptor* descriptor)
         : BindGroupLayoutBase(device, descriptor),
           mDescriptorCounts{},
+          mBindingOffsets(GetDynamicBufferCount()),
           mBindGroupAllocator(MakeFrontendBindGroupAllocator<BindGroup>(4096)) {
         for (BindingIndex bindingIndex = GetDynamicBufferCount(); bindingIndex < GetBindingCount();
              ++bindingIndex) {
@@ -170,9 +171,8 @@ namespace dawn_native { namespace d3d12 {
         mBindGroupAllocator.Deallocate(bindGroup);
     }
 
-    const ityp::array<BindingIndex, uint32_t, kMaxBindingsPerGroup>&
-    BindGroupLayout::GetBindingOffsets() const {
-        return mBindingOffsets;
+    ityp::span<BindingIndex, const uint32_t> BindGroupLayout::GetBindingOffsets() const {
+        return {mBindingOffsets.data(), mBindingOffsets.size()};
     }
 
     uint32_t BindGroupLayout::GetCbvUavSrvDescriptorTableSize() const {
