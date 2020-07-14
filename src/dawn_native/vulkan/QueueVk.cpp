b4f721b1d78c6e0819876af9fcaf6b1eb3374ec5
Tomek Ponitka <tommek@google.com>
2020-07-14 10:48:19 +0200

    Implementing Queue::WriteTexture in Vulkan
    Added implementation of writeTexture in Vulkan.
    
    Bug: dawn:483
    Change-Id: Id74b6518c46caf59e07a9d16dd51d8c28340fd50

--- src/dawn_native/vulkan/QueueVk.cpp
+++ src/dawn_native/vulkan/QueueVk.cpp
@@ -14,38 +14,124 @@
 
 #include "dawn_native/vulkan/QueueVk.h"
 
+#include "common/Math.h"
+#include "dawn_native/Buffer.h"
+#include "dawn_native/CommandValidation.h"
+#include "dawn_native/Commands.h"
+#include "dawn_native/DynamicUploader.h"
 #include "dawn_native/vulkan/CommandBufferVk.h"
 #include "dawn_native/vulkan/CommandRecordingContext.h"
 #include "dawn_native/vulkan/DeviceVk.h"
 #include "dawn_platform/DawnPlatform.h"
 #include "dawn_platform/tracing/TraceEvent.h"
 
-namespace dawn_native { namespace vulkan {
+namespace dawn_native {
 
-    // static
-    Queue* Queue::Create(Device* device) {
-        return new Queue(device);
-    }
+    namespace {
+        ResultOrError<UploadHandle> UploadTextureDataAligningBytesPerRow(
+            DeviceBase* device,
+            const void* data,
+            size_t dataSize,
+            uint32_t alignedBytesPerRow,
+            const TextureDataLayout* dataLayout,
+            const Format& textureFormat,
+            const Extent3D* writeSize) {
+            uint32_t newDataSize;
+            ComputeRequiredBytesInCopy(textureFormat, *writeSize, alignedBytesPerRow,
+                                       dataLayout->rowsPerImage, &newDataSize);
 
-    Queue::~Queue() {
-    }
+            UploadHandle uploadHandle;
+            DAWN_TRY_ASSIGN(uploadHandle, device->GetDynamicUploader()->Allocate(
+                                              newDataSize, device->GetPendingCommandSerial()));
+            ASSERT(uploadHandle.mappedBuffer != nullptr);
 
-    MaybeError Queue::SubmitImpl(uint32_t commandCount, CommandBufferBase* const* commands) {
-        Device* device = ToBackend(GetDevice());
+            uint8_t* dstPointer = static_cast<uint8_t*>(uploadHandle.mappedBuffer);
+            const uint8_t* srcPointer = static_cast<const uint8_t*>(data);
+            srcPointer += dataLayout->offset;
 
-        device->Tick();
+            ASSERT(writeSize->depth >= 1);
+            for (uint32_t d = 0; d < writeSize->depth - 1; ++d) {
+                for (uint32_t h = 0; h < dataLayout->rowsPerImage; ++h) {
+                    memcpy(dstPointer, srcPointer, alignedBytesPerRow);
+                    dstPointer += alignedBytesPerRow;
+                    srcPointer += dataLayout->bytesPerRow;
+                }
+            }
 
-        TRACE_EVENT_BEGIN0(GetDevice()->GetPlatform(), Recording,
-                           "CommandBufferVk::RecordCommands");
-        CommandRecordingContext* recordingContext = device->GetPendingRecordingContext();
-        for (uint32_t i = 0; i < commandCount; ++i) {
-            DAWN_TRY(ToBackend(commands[i])->RecordCommands(recordingContext));
+            ASSERT(writeSize->height >= 1);
+            for (uint32_t h = 0; h < writeSize->height - 1; ++h) {
+                memcpy(dstPointer, srcPointer, alignedBytesPerRow);
+                dstPointer += alignedBytesPerRow;
+                srcPointer += dataLayout->bytesPerRow;
+            }
+
+            memcpy(dstPointer, srcPointer, writeSize->width * textureFormat.blockByteSize);
+
+            return uploadHandle;
         }
-        TRACE_EVENT_END0(GetDevice()->GetPlatform(), Recording, "CommandBufferVk::RecordCommands");
+    }  // namespace
 
-        DAWN_TRY(device->SubmitPendingCommands());
+    namespace vulkan {
 
-        return {};
-    }
+        // static
+        Queue* Queue::Create(Device* device) {
+            return new Queue(device);
+        }
 
-}}  // namespace dawn_native::vulkan
+        Queue::~Queue() {
+        }
+
+        MaybeError Queue::SubmitImpl(uint32_t commandCount, CommandBufferBase* const* commands) {
+            Device* device = ToBackend(GetDevice());
+
+            device->Tick();
+
+            TRACE_EVENT_BEGIN0(GetDevice()->GetPlatform(), Recording,
+                               "CommandBufferVk::RecordCommands");
+            CommandRecordingContext* recordingContext = device->GetPendingRecordingContext();
+            for (uint32_t i = 0; i < commandCount; ++i) {
+                DAWN_TRY(ToBackend(commands[i])->RecordCommands(recordingContext));
+            }
+            TRACE_EVENT_END0(GetDevice()->GetPlatform(), Recording,
+                             "CommandBufferVk::RecordCommands");
+
+            DAWN_TRY(device->SubmitPendingCommands());
+
+            return {};
+        }
+
+        MaybeError Queue::WriteTextureImpl(const TextureCopyView* destination,
+                                           const void* data,
+                                           size_t dataSize,
+                                           const TextureDataLayout* dataLayout,
+                                           const Extent3D* writeSize) {
+            uint32_t blockSize = destination->texture->GetFormat().blockByteSize;
+            // We are not copying the last bytes of data in each row when bytesPerRow is not
+            // blockSize aligned.
+            uint32_t alignedBytesPerRow = (dataLayout->bytesPerRow) / blockSize * blockSize;
+
+            UploadHandle uploadHandle;
+            DAWN_TRY_ASSIGN(uploadHandle,
+                            UploadTextureDataAligningBytesPerRow(
+                                GetDevice(), data, dataSize, alignedBytesPerRow, dataLayout,
+                                destination->texture->GetFormat(), writeSize));
+
+            TextureDataLayout passDataLayout = *dataLayout;
+            passDataLayout.offset = uploadHandle.startOffset;
+            passDataLayout.bytesPerRow = alignedBytesPerRow;
+            if (passDataLayout.rowsPerImage == 0) {
+                passDataLayout.rowsPerImage = writeSize->height;
+            }
+
+            TextureCopy textureCopy;
+            textureCopy.texture = destination->texture;
+            textureCopy.mipLevel = destination->mipLevel;
+            textureCopy.origin = destination->origin;
+
+            return ToBackend(GetDevice())
+                ->CopyFromStagingToTexture(uploadHandle.stagingBuffer, passDataLayout, &textureCopy,
+                                           *writeSize);
+        }
+
+    }  // namespace vulkan
+}  // namespace dawn_native
