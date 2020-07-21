77133ddd9c6871336eef20fcba5fc3a805666f1c
Tomek Ponitka <tommek@google.com>
2020-07-21 12:04:47 +0200

    Enabling sampleMask usage in RenderPipelineVk
    Added the sampleMask functionality in Vulkan with some
    end2end tests.
    
    Change-Id: Ib39682d8857729adb4776bc2f2d8e759bf75677b

--- src/tests/end2end/MultisampledSamplingTests.cpp
+++ src/tests/end2end/MultisampledSamplingTests.cpp
@@ -47,8 +47,7 @@ class MultisampledSamplingTest : public DawnTest {
     // A compute pipeline to texelFetch the sample locations and output the results to a buffer.
     wgpu::ComputePipeline checkSamplePipeline;
 
-    void SetUp() override {
-        DawnTest::SetUp();
+    void InitializePipelines(uint32_t sampleMask) {
         {
             utils::ComboRenderPipelineDescriptor desc(device);
 
@@ -79,6 +78,7 @@ class MultisampledSamplingTest : public DawnTest {
             desc.depthStencilState = &desc.cDepthStencilState;
 
             desc.sampleCount = kSampleCount;
+            desc.sampleMask = sampleMask;
             desc.colorStateCount = 1;
             desc.cColorStates[0].format = kColorFormat;
 
@@ -114,150 +114,156 @@ class MultisampledSamplingTest : public DawnTest {
             checkSamplePipeline = device.CreateComputePipeline(&desc);
         }
     }
-};
 
-// Test that the multisampling sample positions are correct. This test works by drawing a
-// thin quad multiple times from left to right and from top to bottom on a 1x1 canvas.
-// Each time, the quad should cover a single sample position.
-// After drawing, a compute shader fetches all of the samples (both color and depth),
-// and we check that only the one covered has data.
-// We "scan" the vertical and horizontal dimensions separately to check that the triangle
-// must cover both the X and Y coordinates of the sample position (no false positives if
-// it covers the X position but not the Y, or vice versa).
-TEST_P(MultisampledSamplingTest, SamplePositions) {
-    static constexpr wgpu::Extent3D kTextureSize = {1, 1, 1};
+    // Test that the multisampling sample positions are correct. This test works by drawing a
+    // thin quad multiple times from left to right and from top to bottom on a 1x1 canvas.
+    // Each time, the quad should cover a single sample position.
+    // After drawing, a compute shader fetches all of the samples (both color and depth),
+    // and we check that only the one covered has data.
+    // We "scan" the vertical and horizontal dimensions separately to check that the triangle
+    // must cover both the X and Y coordinates of the sample position (no false positives if
+    // it covers the X position but not the Y, or vice versa).
+    void DoTest(uint32_t sampleMask) {
+        InitializePipelines(sampleMask);
 
-    wgpu::Texture colorTexture;
-    {
-        wgpu::TextureDescriptor desc = {};
-        desc.usage = wgpu::TextureUsage::Sampled | wgpu::TextureUsage::OutputAttachment;
-        desc.size = kTextureSize;
-        desc.format = kColorFormat;
-        desc.sampleCount = kSampleCount;
-        colorTexture = device.CreateTexture(&desc);
-    }
+        static constexpr wgpu::Extent3D kTextureSize = {1, 1, 1};
 
-    wgpu::Texture depthTexture;
-    {
-        wgpu::TextureDescriptor desc = {};
-        desc.usage = wgpu::TextureUsage::Sampled | wgpu::TextureUsage::OutputAttachment;
-        desc.size = kTextureSize;
-        desc.format = kDepthFormat;
-        desc.sampleCount = kSampleCount;
-        depthTexture = device.CreateTexture(&desc);
-    }
+        wgpu::Texture colorTexture;
+        {
+            wgpu::TextureDescriptor desc = {};
+            desc.usage = wgpu::TextureUsage::Sampled | wgpu::TextureUsage::OutputAttachment;
+            desc.size = kTextureSize;
+            desc.format = kColorFormat;
+            desc.sampleCount = kSampleCount;
+            colorTexture = device.CreateTexture(&desc);
+        }
 
-    static constexpr float kQuadWidth = 0.075;
-    std::vector<float> vBufferData;
+        wgpu::Texture depthTexture;
+        {
+            wgpu::TextureDescriptor desc = {};
+            desc.usage = wgpu::TextureUsage::Sampled | wgpu::TextureUsage::OutputAttachment;
+            desc.size = kTextureSize;
+            desc.format = kDepthFormat;
+            desc.sampleCount = kSampleCount;
+            depthTexture = device.CreateTexture(&desc);
+        }
 
-    // Add vertices for vertical quads
-    for (uint32_t s = 0; s < kSampleCount; ++s) {
-        // clang-format off
-        vBufferData.insert(vBufferData.end(), {
-            kSamplePositions[s][0] - kQuadWidth, -1.0,
-            kSamplePositions[s][0] - kQuadWidth,  1.0,
-            kSamplePositions[s][0] + kQuadWidth, -1.0,
-            kSamplePositions[s][0] + kQuadWidth,  1.0,
-        });
-        // clang-format on
-    }
+        static constexpr float kQuadWidth = 0.075;
+        std::vector<float> vBufferData;
 
-    // Add vertices for horizontal quads
-    for (uint32_t s = 0; s < kSampleCount; ++s) {
-        // clang-format off
-        vBufferData.insert(vBufferData.end(), {
-            -1.0, kSamplePositions[s][1] - kQuadWidth,
-            -1.0, kSamplePositions[s][1] + kQuadWidth,
-             1.0, kSamplePositions[s][1] - kQuadWidth,
-             1.0, kSamplePositions[s][1] + kQuadWidth,
-        });
-        // clang-format on
-    }
+        // Add vertices for vertical quads
+        for (uint32_t s = 0; s < kSampleCount; ++s) {
+            // clang-format off
+            vBufferData.insert(vBufferData.end(), {
+                kSamplePositions[s][0] - kQuadWidth, -1.0,
+                kSamplePositions[s][0] - kQuadWidth,  1.0,
+                kSamplePositions[s][0] + kQuadWidth, -1.0,
+                kSamplePositions[s][0] + kQuadWidth,  1.0,
+            });
+            // clang-format on
+        }
 
-    wgpu::Buffer vBuffer = utils::CreateBufferFromData(
-        device, vBufferData.data(), static_cast<uint32_t>(vBufferData.size() * sizeof(float)),
-        wgpu::BufferUsage::Vertex);
+        // Add vertices for horizontal quads
+        for (uint32_t s = 0; s < kSampleCount; ++s) {
+            // clang-format off
+            vBufferData.insert(vBufferData.end(), {
+                -1.0, kSamplePositions[s][1] - kQuadWidth,
+                -1.0, kSamplePositions[s][1] + kQuadWidth,
+                 1.0, kSamplePositions[s][1] - kQuadWidth,
+                 1.0, kSamplePositions[s][1] + kQuadWidth,
+            });
+            // clang-format on
+        }
 
-    static constexpr uint32_t kQuadNumBytes = 8 * sizeof(float);
+        wgpu::Buffer vBuffer = utils::CreateBufferFromData(
+            device, vBufferData.data(), static_cast<uint32_t>(vBufferData.size() * sizeof(float)),
+            wgpu::BufferUsage::Vertex);
 
-    wgpu::SamplerDescriptor samplerDesc = {};
-    wgpu::Sampler sampler = device.CreateSampler(&samplerDesc);
-    wgpu::TextureView colorView = colorTexture.CreateView();
-    wgpu::TextureView depthView = depthTexture.CreateView();
+        static constexpr uint32_t kQuadNumBytes = 8 * sizeof(float);
 
-    static constexpr uint64_t kResultSize = 4 * sizeof(float) + 4 * sizeof(float);
-    uint64_t alignedResultSize = Align(kResultSize, 256);
+        wgpu::SamplerDescriptor samplerDesc = {};
+        wgpu::Sampler sampler = device.CreateSampler(&samplerDesc);
+        wgpu::TextureView colorView = colorTexture.CreateView();
+        wgpu::TextureView depthView = depthTexture.CreateView();
 
-    wgpu::BufferDescriptor outputBufferDesc = {};
-    outputBufferDesc.usage = wgpu::BufferUsage::Storage | wgpu::BufferUsage::CopySrc;
-    outputBufferDesc.size = alignedResultSize * 8;
-    wgpu::Buffer outputBuffer = device.CreateBuffer(&outputBufferDesc);
+        static constexpr uint64_t kResultSize = 4 * sizeof(float) + 4 * sizeof(float);
+        uint64_t alignedResultSize = Align(kResultSize, 256);
 
-    wgpu::CommandEncoder commandEncoder = device.CreateCommandEncoder();
-    for (uint32_t iter = 0; iter < 2; ++iter) {
-        for (uint32_t sample = 0; sample < kSampleCount; ++sample) {
-            uint32_t sampleOffset = (iter * kSampleCount + sample);
+        wgpu::BufferDescriptor outputBufferDesc = {};
+        outputBufferDesc.usage = wgpu::BufferUsage::Storage | wgpu::BufferUsage::CopySrc;
+        outputBufferDesc.size = alignedResultSize * 8;
+        wgpu::Buffer outputBuffer = device.CreateBuffer(&outputBufferDesc);
 
-            utils::ComboRenderPassDescriptor renderPass({colorView}, depthView);
-            renderPass.cDepthStencilAttachmentInfo.clearDepth = 0.f;
+        wgpu::CommandEncoder commandEncoder = device.CreateCommandEncoder();
+        for (uint32_t iter = 0; iter < 2; ++iter) {
+            for (uint32_t sample = 0; sample < kSampleCount; ++sample) {
+                uint32_t sampleOffset = (iter * kSampleCount + sample);
 
-            wgpu::RenderPassEncoder renderPassEncoder = commandEncoder.BeginRenderPass(&renderPass);
-            renderPassEncoder.SetPipeline(drawPipeline);
-            renderPassEncoder.SetVertexBuffer(0, vBuffer, kQuadNumBytes * sampleOffset,
-                                              kQuadNumBytes);
-            renderPassEncoder.Draw(4);
-            renderPassEncoder.EndPass();
+                utils::ComboRenderPassDescriptor renderPass({colorView}, depthView);
+                renderPass.cDepthStencilAttachmentInfo.clearDepth = 0.f;
 
-            wgpu::ComputePassEncoder computePassEncoder = commandEncoder.BeginComputePass();
-            computePassEncoder.SetPipeline(checkSamplePipeline);
-            computePassEncoder.SetBindGroup(
-                0, utils::MakeBindGroup(
-                       device, checkSamplePipeline.GetBindGroupLayout(0),
-                       {{0, sampler},
-                        {1, colorView},
-                        {2, depthView},
-                        {3, outputBuffer, alignedResultSize * sampleOffset, kResultSize}}));
-            computePassEncoder.Dispatch(1);
-            computePassEncoder.EndPass();
+                wgpu::RenderPassEncoder renderPassEncoder =
+                    commandEncoder.BeginRenderPass(&renderPass);
+                renderPassEncoder.SetPipeline(drawPipeline);
+                renderPassEncoder.SetVertexBuffer(0, vBuffer, kQuadNumBytes * sampleOffset,
+                                                  kQuadNumBytes);
+                renderPassEncoder.Draw(4);
+                renderPassEncoder.EndPass();
+
+                wgpu::ComputePassEncoder computePassEncoder = commandEncoder.BeginComputePass();
+                computePassEncoder.SetPipeline(checkSamplePipeline);
+                computePassEncoder.SetBindGroup(
+                    0, utils::MakeBindGroup(
+                           device, checkSamplePipeline.GetBindGroupLayout(0),
+                           {{0, sampler},
+                            {1, colorView},
+                            {2, depthView},
+                            {3, outputBuffer, alignedResultSize * sampleOffset, kResultSize}}));
+                computePassEncoder.Dispatch(1);
+                computePassEncoder.EndPass();
+            }
+        }
+
+        wgpu::CommandBuffer commandBuffer = commandEncoder.Finish();
+        queue.Submit(1, &commandBuffer);
+
+        std::array<float, 8> expectedData;
+
+        for (uint32_t s = 0; s < kSampleCount; ++s) {
+            expectedData = {0, 0, 0, 0, 0, 0, 0, 0};
+            if ((1 << s) & sampleMask) {
+                expectedData[s] = 1;
+                expectedData[kSampleCount + s] = 0.7;
+            }
+            EXPECT_BUFFER_FLOAT_RANGE_EQ(expectedData.data(), outputBuffer, s * alignedResultSize,
+                                         8)
+                << "vertical sample " << s;
+            EXPECT_BUFFER_FLOAT_RANGE_EQ(expectedData.data(), outputBuffer,
+                                         (kSampleCount + s) * alignedResultSize, 8)
+                << "horizontal sample " << s;
         }
     }
+};
 
-    wgpu::CommandBuffer commandBuffer = commandEncoder.Finish();
-    queue.Submit(1, &commandBuffer);
+// Test all the samples
+TEST_P(MultisampledSamplingTest, SamplePositions) {
+    DoTest(0xFFFFFFFF);
+}
 
-    std::array<float, 8> expectedData;
+// Test a subset of the samples
+TEST_P(MultisampledSamplingTest, SamplePositionsWithSampleMask) {
+    // TODO(tommek@google.com): Remove this condition after enabling sampleMask usage in those
+    // backends.
+    DAWN_SKIP_TEST_IF(IsOpenGL() || IsMetal() || IsD3D12());
 
-    expectedData = {1, 0, 0, 0, 0.7, 0, 0, 0};
-    EXPECT_BUFFER_FLOAT_RANGE_EQ(expectedData.data(), outputBuffer, 0 * alignedResultSize, 8)
-        << "vertical sample 0";
-
-    expectedData = {0, 1, 0, 0, 0, 0.7, 0, 0};
-    EXPECT_BUFFER_FLOAT_RANGE_EQ(expectedData.data(), outputBuffer, 1 * alignedResultSize, 8)
-        << "vertical sample 1";
-
-    expectedData = {0, 0, 1, 0, 0, 0, 0.7, 0};
-    EXPECT_BUFFER_FLOAT_RANGE_EQ(expectedData.data(), outputBuffer, 2 * alignedResultSize, 8)
-        << "vertical sample 2";
-
-    expectedData = {0, 0, 0, 1, 0, 0, 0, 0.7};
-    EXPECT_BUFFER_FLOAT_RANGE_EQ(expectedData.data(), outputBuffer, 3 * alignedResultSize, 8)
-        << "vertical sample 3";
-
-    expectedData = {1, 0, 0, 0, 0.7, 0, 0, 0};
-    EXPECT_BUFFER_FLOAT_RANGE_EQ(expectedData.data(), outputBuffer, 4 * alignedResultSize, 8)
-        << "horizontal sample 0";
-
-    expectedData = {0, 1, 0, 0, 0, 0.7, 0, 0};
-    EXPECT_BUFFER_FLOAT_RANGE_EQ(expectedData.data(), outputBuffer, 5 * alignedResultSize, 8)
-        << "horizontal sample 1";
-
-    expectedData = {0, 0, 1, 0, 0, 0, 0.7, 0};
-    EXPECT_BUFFER_FLOAT_RANGE_EQ(expectedData.data(), outputBuffer, 6 * alignedResultSize, 8)
-        << "horizontal sample 2";
-
-    expectedData = {0, 0, 0, 1, 0, 0, 0, 0.7};
-    EXPECT_BUFFER_FLOAT_RANGE_EQ(expectedData.data(), outputBuffer, 7 * alignedResultSize, 8)
-        << "horizontal sample 3";
+    // None of the samples is included
+    DoTest(0x00000000);
+    // Only the second sample position is included
+    DoTest(0x00000002);
+    // The first three sample positions are included
+    DoTest(0x00000007);
+    // The last three sample positions are included
+    DoTest(0x0000000E);
 }
 
 DAWN_INSTANTIATE_TEST(MultisampledSamplingTest,
