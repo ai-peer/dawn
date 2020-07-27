d89300a57df911367e9593613af07c869e7e057b
Tomek Ponitka <tommek@google.com>
2020-07-27 12:24:57 +0200

    Enabling alphaToCoverage
    Added the alphaToCoverage functionality with some
    end2end tests. There is no validation for disabling alphaToCoverage
    mode if SV_Coverage is statically used by the shader yet.
    
    Bug: dawn:494
    Change-Id: I9df15b35697ea05a064b092edae9d5d20f73c4d8

--- src/dawn_native/vulkan/DeviceVk.cpp
+++ src/dawn_native/vulkan/DeviceVk.cpp
@@ -853,10 +853,10 @@ namespace dawn_native { namespace vulkan {
         // Immediately tag the recording context as unused so we don't try to submit it in Tick.
         mRecordingContext.used = false;
         if (mRecordingContext.commandPool != VK_NULL_HANDLE) {
-            // The VkCommandBuffer memory should be wholly owned by the pool and freed when it is
-            // destroyed, but that's not the case in some drivers and the leak memory.
-            // So we call FreeCommandBuffers before DestroyCommandPool to be safe.
-            // TODO(enga): Only do this on a known list of bad drivers.
+             // The VkCommandBuffer memory should be wholly owned by the pool and freed when it is
+             // destroyed, but that's not the case in some drivers and the leak memory.
+             // So we call FreeCommandBuffers before DestroyCommandPool to be safe.
+             // TODO(enga): Only do this on a known list of bad drivers.
             fn.FreeCommandBuffers(mVkDevice, mRecordingContext.commandPool, 1,
                                   &mRecordingContext.commandBuffer);
             fn.DestroyCommandPool(mVkDevice, mRecordingContext.commandPool, nullptr);
@@ -874,10 +874,10 @@ namespace dawn_native { namespace vulkan {
 
         ASSERT(mCommandsInFlight.Empty());
         for (const CommandPoolAndBuffer& commands : mUnusedCommands) {
-             // The VkCommandBuffer memory should be wholly owned by the pool and freed when it is
-             // destroyed, but that's not the case in some drivers and the leak memory.
-             // So we call FreeCommandBuffers before DestroyCommandPool to be safe.
-             // TODO(enga): Only do this on a known list of bad drivers.
+            // The VkCommandBuffer memory should be wholly owned by the pool and freed when it is
+            // destroyed, but that's not the case in some drivers and the leak memory.
+            // So we call FreeCommandBuffers before DestroyCommandPool to be safe.
+            // TODO(enga): Only do this on a known list of bad drivers.
             fn.FreeCommandBuffers(mVkDevice, commands.pool, 1, &commands.commandBuffer);
             fn.DestroyCommandPool(mVkDevice, commands.pool, nullptr);
         }
