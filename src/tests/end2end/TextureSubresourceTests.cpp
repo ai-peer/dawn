4260da95f6d1f83290c2db0a8b092c9200dd1c30
Austin Eng <enga@chromium.org>
2020-09-02 12:59:19 -0700

    Expand TextureSubresourceTest to check both src/dst subresources
    Bug: none
    Change-Id: I739bd06f02487ba20fd74a1b889799166a368f3a

--- src/tests/end2end/TextureSubresourceTests.cpp
+++ src/tests/end2end/TextureSubresourceTests.cpp
@@ -48,6 +48,40 @@ class TextureSubresourceTest : public DawnTest {
         return texture.CreateView(&viewDesc);
     }
 
+    void SetUp() override {
+        DawnTest::SetUp();
+
+        utils::ComboRenderPipelineDescriptor pipelineDesc(device);
+        pipelineDesc.vertexStage.module =
+            utils::CreateShaderModule(device, utils::SingleShaderStage::Vertex, R"(
+                #version 450
+                layout (location = 0) out vec2 fUV;
+                void main() {
+                    const vec2 pos[6] = vec2[6](
+                        vec2(-1.f, -1.f), vec2(1.f, 1.f), vec2(-1.f, 1.f),
+                        vec2(-1.f, -1.f), vec2(1.f, -1.f), vec2(1.f, 1.f));
+                    const vec2 uv[6] = vec2[6](
+                        vec2(0.f, 1.f), vec2(1.f, 0.f), vec2(0.f, 0.f),
+                        vec2(0.f, 1.f), vec2(1.f, 1.f), vec2(1.f, 0.f));
+                    fUV = uv[gl_VertexIndex];
+                    gl_Position = vec4(pos[gl_VertexIndex], 0.f, 1.f);
+                 })");
+        pipelineDesc.cFragmentStage.module =
+            utils::CreateShaderModule(device, utils::SingleShaderStage::Fragment, R"(
+                #version 450
+                layout (set = 0, binding = 0) uniform sampler samp;
+                layout (set = 0, binding = 1) uniform texture2D tex;
+                layout (location = 0) in vec2 fUV;
+                layout (location = 0) out vec4 fragColor;
+                void main() {
+                    fragColor = texture(sampler2D(tex, samp), fUV);
+                })");
+        pipelineDesc.primitiveTopology = wgpu::PrimitiveTopology::TriangleStrip;
+        pipelineDesc.cColorStates[0].format = kFormat;
+
+        mSampleAndDrawPipeline = device.CreateRenderPipeline(&pipelineDesc);
+    }
+
     void DrawTriangle(const wgpu::TextureView& view) {
         wgpu::ShaderModule vsModule =
             utils::CreateShaderModule(device, utils::SingleShaderStage::Vertex, R"(
@@ -86,53 +120,34 @@ class TextureSubresourceTest : public DawnTest {
         queue.Submit(1, &commands);
     }
 
-    void SampleAndDraw(const wgpu::TextureView& samplerView, const wgpu::TextureView& renderView) {
-        wgpu::ShaderModule vsModule =
-            utils::CreateShaderModule(device, utils::SingleShaderStage::Vertex, R"(
-                #version 450
-                void main() {
-                    const vec2 pos[6] = vec2[6](
-                        vec2(-1.f, -1.f), vec2(1.f, 1.f), vec2(-1.f, 1.f),
-                        vec2(-1.f, -1.f), vec2(1.f, -1.f), vec2(1.f, 1.f));
-                    gl_Position = vec4(pos[gl_VertexIndex], 0.f, 1.f);
-                 })");
-
-        wgpu::ShaderModule fsModule =
-            utils::CreateShaderModule(device, utils::SingleShaderStage::Fragment, R"(
-                #version 450
-                layout (set = 0, binding = 0) uniform sampler samp;
-                layout (set = 0, binding = 1) uniform texture2D tex;
-                layout (location = 0) out vec4 fragColor;
-                void main() {
-                    fragColor = texture(sampler2D(tex, samp), gl_FragCoord.xy / 4);
-                })");
-
-        utils::ComboRenderPipelineDescriptor descriptor(device);
-        descriptor.vertexStage.module = vsModule;
-        descriptor.cFragmentStage.module = fsModule;
-        descriptor.primitiveTopology = wgpu::PrimitiveTopology::TriangleStrip;
-        descriptor.cColorStates[0].format = kFormat;
-
+    void SampleAndDraw(wgpu::CommandEncoder encoder,
+                       const wgpu::TextureView& samplerView,
+                       const wgpu::TextureView& renderView) {
         wgpu::SamplerDescriptor samplerDescriptor = {};
         wgpu::Sampler sampler = device.CreateSampler(&samplerDescriptor);
 
-        wgpu::RenderPipeline rp = device.CreateRenderPipeline(&descriptor);
-        wgpu::BindGroupLayout bgl = rp.GetBindGroupLayout(0);
+        wgpu::BindGroupLayout bgl = mSampleAndDrawPipeline.GetBindGroupLayout(0);
         wgpu::BindGroup bindGroup =
             utils::MakeBindGroup(device, bgl, {{0, sampler}, {1, samplerView}});
-
-        wgpu::CommandEncoder encoder = device.CreateCommandEncoder();
 
         utils::ComboRenderPassDescriptor renderPassDesc({renderView});
         renderPassDesc.cColorAttachments[0].clearColor = {0.0f, 0.0f, 0.0f, 1.0f};
         wgpu::RenderPassEncoder pass = encoder.BeginRenderPass(&renderPassDesc);
-        pass.SetPipeline(rp);
+        pass.SetPipeline(mSampleAndDrawPipeline);
         pass.SetBindGroup(0, bindGroup);
         pass.Draw(6);
         pass.EndPass();
+    }
+
+    void SampleAndDraw(const wgpu::TextureView& samplerView, const wgpu::TextureView& renderView) {
+        wgpu::CommandEncoder encoder = device.CreateCommandEncoder();
+        SampleAndDraw(encoder, samplerView, renderView);
         wgpu::CommandBuffer commands = encoder.Finish();
         queue.Submit(1, &commands);
     }
+
+  protected:
+    wgpu::RenderPipeline mSampleAndDrawPipeline;
 };
 
 // Test different mipmap levels
@@ -153,12 +168,69 @@ TEST_P(TextureSubresourceTest, MipmapLevelsTest) {
     // Sample from one subresource and draw into another subresource in the same texture
     SampleAndDraw(samplerView, renderView);
 
-    // Verify that pixel at bottom-left corner is red, while pixel at top-right corner is background
-    // black in render view (mip level 1).
-    RGBA8 topRight = RGBA8::kBlack;
-    RGBA8 bottomLeft = RGBA8::kRed;
-    EXPECT_TEXTURE_RGBA8_EQ(&topRight, texture, kSize / 2 - 1, 0, 1, 1, 1, 0);
-    EXPECT_TEXTURE_RGBA8_EQ(&bottomLeft, texture, 0, kSize / 2 - 1, 1, 1, 1, 0);
+    // Check both subresources.
+    std::vector<RGBA8> mip0Expected = {
+        RGBA8::kBlack, RGBA8::kBlack, RGBA8::kBlack, RGBA8::kBlack,  //
+        RGBA8::kRed,   RGBA8::kBlack, RGBA8::kBlack, RGBA8::kBlack,  //
+        RGBA8::kRed,   RGBA8::kRed,   RGBA8::kBlack, RGBA8::kBlack,  //
+        RGBA8::kRed,   RGBA8::kRed,   RGBA8::kRed,   RGBA8::kBlack,  //
+    };
+
+    std::vector<RGBA8> mip1Expected = {
+        RGBA8::kBlack, RGBA8::kBlack,  //
+        RGBA8::kRed, RGBA8::kBlack,    //
+    };
+
+    EXPECT_TEXTURE_EQ(mip0Expected.data(), texture, 0, 0, kSize, kSize, 0);
+    EXPECT_TEXTURE_EQ(mip1Expected.data(), texture, 0, 0, kSize / 2, kSize / 2, 1);
+}
+
+// Test generating a long mip chain in single command buffer
+TEST_P(TextureSubresourceTest, LongMipMapGeneration) {
+    wgpu::TextureDescriptor texDesc;
+    texDesc.size = {32, 32, 1};
+    texDesc.mipLevelCount = 6;
+    texDesc.usage = wgpu::TextureUsage::Sampled | wgpu::TextureUsage::OutputAttachment |
+                    wgpu::TextureUsage::CopySrc;
+    texDesc.format = kFormat;
+    wgpu::Texture texture = device.CreateTexture(&texDesc);
+
+    wgpu::TextureViewDescriptor viewDesc = {};
+    viewDesc.baseMipLevel = 0;
+    viewDesc.mipLevelCount = 1;
+    wgpu::TextureView srcView = texture.CreateView(&viewDesc);
+
+    DrawTriangle(srcView);
+
+    wgpu::CommandEncoder encoder = device.CreateCommandEncoder();
+
+    for (uint32_t i = 1; i < texDesc.mipLevelCount; ++i) {
+        wgpu::TextureViewDescriptor viewDesc = {};
+        viewDesc.baseMipLevel = i;
+        viewDesc.mipLevelCount = 1;
+        wgpu::TextureView dstView = texture.CreateView(&viewDesc);
+
+        SampleAndDraw(encoder, srcView, dstView);
+        srcView = dstView;
+    }
+
+    wgpu::CommandBuffer commands = encoder.Finish();
+    queue.Submit(1, &commands);
+
+    for (uint32_t i = 0; i < texDesc.mipLevelCount; ++i) {
+        uint32_t mipSize = texDesc.size.width >> i;
+        std::vector<RGBA8> expected(mipSize * mipSize);
+        for (uint32_t y = 0; y < mipSize; ++y) {
+            for (uint32_t x = 0; x < mipSize; ++x) {
+                if (x + (mipSize - y) < mipSize) {
+                    expected[y * mipSize + x] = RGBA8::kRed;
+                } else {
+                    expected[y * mipSize + x] = RGBA8::kBlack;
+                }
+            }
+        }
+        EXPECT_TEXTURE_EQ(expected.data(), texture, 0, 0, mipSize, mipSize, i);
+    }
 }
 
 // Test different array layers
@@ -179,12 +251,16 @@ TEST_P(TextureSubresourceTest, ArrayLayersTest) {
     // Sample from one subresource and draw into another subresource in the same texture
     SampleAndDraw(samplerView, renderView);
 
-    // Verify that pixel at bottom-left corner is red, while pixel at top-right corner is background
-    // black in render view (array layer 1).
-    RGBA8 topRight = RGBA8::kBlack;
-    RGBA8 bottomLeft = RGBA8::kRed;
-    EXPECT_TEXTURE_RGBA8_EQ(&topRight, texture, kSize - 1, 0, 1, 1, 0, 1);
-    EXPECT_TEXTURE_RGBA8_EQ(&bottomLeft, texture, 0, kSize - 1, 1, 1, 0, 1);
+    // Check both subresources.
+    std::vector<RGBA8> expected = {
+        RGBA8::kBlack, RGBA8::kBlack, RGBA8::kBlack, RGBA8::kBlack,  //
+        RGBA8::kRed,   RGBA8::kBlack, RGBA8::kBlack, RGBA8::kBlack,  //
+        RGBA8::kRed,   RGBA8::kRed,   RGBA8::kBlack, RGBA8::kBlack,  //
+        RGBA8::kRed,   RGBA8::kRed,   RGBA8::kRed,   RGBA8::kBlack,  //
+    };
+
+    EXPECT_TEXTURE_EQ(expected.data(), texture, 0, 0, kSize, kSize, 0, 0);
+    EXPECT_TEXTURE_EQ(expected.data(), texture, 0, 0, kSize, kSize, 0, 1);
 }
 
 // TODO (yunchao.he@intel.com):
