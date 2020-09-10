714e3f19030bd228450f9c16d7eade196ffa2239
Brandon Jones <brandon1.jones@intel.com>
2020-09-10 13:23:00 -0700

    Change wgpu::Color from floats to doubles
    Change dawn.json to reflect changes in the WebGPU IDL. Also fixes any
    conversion issues in Dawn.
    
    Bug: dawn:525
    Change-Id: Ifb46329f073bcf31d43342f20f4819eac061d5a1

--- src/dawn_native/metal/CommandBufferMTL.mm
+++ src/dawn_native/metal/CommandBufferMTL.mm
@@ -62,13 +62,14 @@ namespace dawn_native { namespace metal {
                 auto& attachmentInfo = renderPass->colorAttachments[attachment];
 
                 switch (attachmentInfo.loadOp) {
-                    case wgpu::LoadOp::Clear:
+                    case wgpu::LoadOp::Clear: {
                         descriptor.colorAttachments[i].loadAction = MTLLoadActionClear;
+                        const std::array<double, 4> clearColor =
+                            ConvertToFloatToDoubleColor(attachmentInfo.clearColor);
                         descriptor.colorAttachments[i].clearColor = MTLClearColorMake(
-                            attachmentInfo.clearColor.r, attachmentInfo.clearColor.g,
-                            attachmentInfo.clearColor.b, attachmentInfo.clearColor.a);
+                            clearColor[0], clearColor[1], clearColor[2], clearColor[3]);
                         break;
-
+                    }
                     case wgpu::LoadOp::Load:
                         descriptor.colorAttachments[i].loadAction = MTLLoadActionLoad;
                         break;
