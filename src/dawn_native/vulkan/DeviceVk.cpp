25cfef1f2eb6539cc6951ae57697614e65cccbcc
Enrico Galli <enrico.galli@intel.com>
2020-09-28 16:48:23 -0700

    Add support for depthBias, depthBiasSlope, and depthBiasClamp
    Bug: dawn:524
    Change-Id: I2586aadbc326f58889314a2d10794bcc0572aaba

--- src/dawn_native/vulkan/DeviceVk.cpp
+++ src/dawn_native/vulkan/DeviceVk.cpp
@@ -298,6 +298,8 @@ namespace dawn_native { namespace vulkan {
         usedKnobs.features.imageCubeArray = VK_TRUE;
         // Always require fragmentStoresAndAtomics because it is required by end2end tests.
         usedKnobs.features.fragmentStoresAndAtomics = VK_TRUE;
+        // Always require imageCubeArray because it is a core Dawn feature
+        usedKnobs.features.depthBiasClamp = VK_TRUE;
 
         if (IsRobustnessEnabled()) {
             usedKnobs.features.robustBufferAccess = VK_TRUE;
