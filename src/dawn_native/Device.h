336b7845b4791a571fa695f06a054ee32db56004
Jiawei Shao <jiawei.shao@intel.com>
2020-10-16 10:16:25 +0800

    Add the entry point of CreateReadyComputePipeline
    This patch adds the entry point of CreateReadyComputePipeline in both
    dawn_native and dawn_wire.
    
    TODOs:
    1. Add more tests in dawn_unittests and dawn_end2end_tests.
    2. Put the main logic of creating a pipeline into a separate thread.
    
    BUG=dawn:529
    TEST=dawn_end2end_tests
    
    Change-Id: I7edd269a5422a8b85320a7f9173df925decba633

--- src/dawn_native/Device.h
+++ src/dawn_native/Device.h
@@ -145,6 +145,9 @@ namespace dawn_native {
         PipelineLayoutBase* CreatePipelineLayout(const PipelineLayoutDescriptor* descriptor);
         QuerySetBase* CreateQuerySet(const QuerySetDescriptor* descriptor);
         QueueBase* CreateQueue();
+        void CreateReadyComputePipeline(const ComputePipelineDescriptor* descriptor,
+                                        WGPUCreateReadyComputePipelineCallback callback,
+                                        void* userdata);
         RenderBundleEncoder* CreateRenderBundleEncoder(
             const RenderBundleEncoderDescriptor* descriptor);
         RenderPipelineBase* CreateRenderPipeline(const RenderPipelineDescriptor* descriptor);
