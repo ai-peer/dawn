c24a09c320c3e6ba6c4192c62d0901891a0be836
Bryan Bernhart <bryan.bernhart@intel.com>
2020-10-21 11:05:51 -0700

    Pipeline caching: object recording
    * Refactors object hashing into a seperate
    class so it may be used by different caches.
    * Combines hashing and comparision checks
    by using the same recorded key.
    *  Remembers the recorded key to avoid
    re-recording.
    
    BUG=dawn:549
    
    Change-Id: Ica64d58ae6a3c6266435cfc3f776c820190f7895

--- src/dawn_native/RenderPipeline.h
+++ src/dawn_native/RenderPipeline.h
@@ -91,16 +91,11 @@ namespace dawn_native {
 
         const AttachmentState* GetAttachmentState() const;
 
-        // Functors necessary for the unordered_set<RenderPipelineBase*>-based cache.
-        struct HashFunc {
-            size_t operator()(const RenderPipelineBase* pipeline) const;
-        };
-        struct EqualityFunc {
-            bool operator()(const RenderPipelineBase* a, const RenderPipelineBase* b) const;
-        };
-
       private:
         RenderPipelineBase(DeviceBase* device, ObjectBase::ErrorTag tag);
+
+        // RecoredObject implementation
+        void Fingerprint(FingerprintRecorder* recorder) override;
 
         // Vertex state
         VertexStateDescriptor mVertexState;
