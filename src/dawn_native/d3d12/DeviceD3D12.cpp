1b9e6ad8395863bdb9009f2ad064e7043dfca877
Brandon Jones <brandon1.jones@intel.com>
2020-11-05 11:21:25 -0800

    Only increment serials when necessary on D3D12
    Change-Id: Ibe5312a3bfb81e7354b89c00040aadabcef74b78

--- src/dawn_native/d3d12/DeviceD3D12.cpp
+++ src/dawn_native/d3d12/DeviceD3D12.cpp
@@ -228,8 +228,11 @@ namespace dawn_native { namespace d3d12 {
         mRenderTargetViewAllocator->Tick(completedSerial);
         mDepthStencilViewAllocator->Tick(completedSerial);
         mUsedComObjectRefs.ClearUpTo(completedSerial);
-        DAWN_TRY(ExecutePendingCommandContext());
-        DAWN_TRY(NextSerial());
+
+        if (mPendingCommands.IsOpen()) {
+            DAWN_TRY(ExecutePendingCommandContext());
+            DAWN_TRY(NextSerial());
+        }
 
         DAWN_TRY(CheckDebugLayerAndGenerateErrors());
 
@@ -256,7 +259,13 @@ namespace dawn_native { namespace d3d12 {
     }
 
     ExecutionSerial Device::CheckAndUpdateCompletedSerials() {
-        return ExecutionSerial(mFence->GetCompletedValue());
+        ExecutionSerial serial = ExecutionSerial(mFence->GetCompletedValue());
+
+        if (serial < GetCompletedCommandSerial()) {
+            return ExecutionSerial(0);
+        }
+
+        return serial;
     }
 
     void Device::ReferenceUntilUnused(ComPtr<IUnknown> object) {
