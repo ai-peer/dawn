fa86bb86e2ac601a95a5963bad3cd797b2ef46a0
Corentin Wallez <cwallez@chromium.org>
2020-11-06 14:57:08 +0100

    Vulkan: Work around NVIDIA not clamping the biased depth.
    Bug: dawn:536
    Change-Id: I45aa93f8638df693de8378a1005bd9561df47cbe

--- src/dawn_native/vulkan/VulkanInfo.cpp
+++ src/dawn_native/vulkan/VulkanInfo.cpp
@@ -285,6 +285,11 @@ namespace dawn_native { namespace vulkan {
                                 VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_DRIVER_PROPERTIES);
         }
 
+        if (info.extensions.Has(DeviceExt::DepthClipEnable)) {
+            propertiesChain.Add(&info.depthClipEnableFeatures,
+                                VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_DEPTH_CLIP_ENABLE_FEATURES_EXT);
+        }
+
         // If we have DeviceExt::GetPhysicalDeviceProperties2, use features2 and properties2 so
         // that features no covered by VkPhysicalDevice{Features,Properties} can be queried.
         //
