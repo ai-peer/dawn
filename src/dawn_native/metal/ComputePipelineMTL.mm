0cefd467d909fded4207c1e8d8702a0b0c51db5e
Corentin Wallez <cwallez@chromium.org>
2020-11-09 18:22:30 +0100

    Metal: Wrap NS classes and protocols in NSRef.
    This makes refcounting of these objects more automatic to try and
    prevent leaks or use-after-frees in the future.
    
    Also removes operator* from RefBase (and Ref) because it is never used
    and cannot work in a normal way for ObjectiveC protocals that cannot be
    dereferenced.
    
    Bug: dawn:89
    
    Change-Id: I2e3fbfd638e2ba76d8c563f30bc489a384152552

--- src/dawn_native/metal/ComputePipelineMTL.mm
+++ src/dawn_native/metal/ComputePipelineMTL.mm
@@ -38,8 +38,9 @@ namespace dawn_native { namespace metal {
                                                ToBackend(GetLayout()), &computeData));
 
         NSError* error = nil;
-        mMtlComputePipelineState =
-            [mtlDevice newComputePipelineStateWithFunction:computeData.function error:&error];
+        mMtlComputePipelineState.Acquire([mtlDevice
+            newComputePipelineStateWithFunction:computeData.function.Get()
+                                          error:&error]);
         if (error != nil) {
             NSLog(@" error => %@", error);
             return DAWN_INTERNAL_ERROR("Error creating pipeline state");
@@ -53,12 +54,8 @@ namespace dawn_native { namespace metal {
         return {};
     }
 
-    ComputePipeline::~ComputePipeline() {
-        [mMtlComputePipelineState release];
-    }
-
     void ComputePipeline::Encode(id<MTLComputeCommandEncoder> encoder) {
-        [encoder setComputePipelineState:mMtlComputePipelineState];
+        [encoder setComputePipelineState:mMtlComputePipelineState.Get()];
     }
 
     MTLSize ComputePipeline::GetLocalWorkGroupSize() const {
