a96740f1c862a99b52b0715bbfd1cad10a524b8f
Bryan Bernhart <bryan.bernhart@intel.com>
2020-11-10 16:03:51 -0800

    D3D12: Support caching DX shaders.
    This change is a prerequisite to D3D pipeline caching.
    
    This change introduces:
    - Caching interface which enables the cache.
    - DawnTest implemention of the caching interface for E2E testing.
    - Helper for backends to load/store blobs to be cached.
    
    Bug:dawn:549
    Change-Id: I2af759882d18b3f45dc63e49dcb6a3caa1be3485

--- src/include/dawn_platform/DawnPlatform.h
+++ src/include/dawn_platform/DawnPlatform.h
@@ -17,6 +17,7 @@
 
 #include "dawn_platform/dawn_platform_export.h"
 
+#include <cstddef>
 #include <cstdint>
 
 namespace dawn_platform {
@@ -26,6 +27,24 @@ namespace dawn_platform {
         Validation,  // Dawn validation
         Recording,   // Native command recording
         GPUWork,     // Actual GPU work
+    };
+
+    class CachingInterface {
+      public:
+        CachingInterface() = default;
+        virtual ~CachingInterface() = default;
+
+        virtual size_t loadData(void* device,
+                                const void* key,
+                                size_t keySize,
+                                void* valueOut,
+                                size_t valueSize) = 0;
+
+        virtual bool storeData(void* device,
+                               const void* key,
+                               size_t keySize,
+                               const void* value,
+                               size_t valueSize) = 0;
     };
 
     class DAWN_PLATFORM_EXPORT Platform {
@@ -47,6 +66,16 @@ namespace dawn_platform {
                                        const unsigned char* argTypes,
                                        const uint64_t* argValues,
                                        unsigned char flags) = 0;
+
+        // PersistentCache API. Not abstract since not all platforms have support.
+        virtual dawn_platform::CachingInterface* CachingInterface(void* fingerprint,
+                                                                  size_t fingerprintSize) {
+            return nullptr;
+        }
+
+        virtual dawn_platform::CachingInterface* GetCachingInterface() {
+            return nullptr;
+        }
 
       private:
         Platform(const Platform&) = delete;
