a6648947e37bb0a53c448b00a227b960b18bd62c
Ryan Harrison <rharrison@chromium.org>
2020-11-12 13:43:16 -0500

    Use remapped entry point name in D3D12
    Tint may remap entry point names when translating a shader, so need to
    use the Inspector to get the remapped name for calling into DXC/FXC.
    
    Change-Id: I62cdd78334d4b3dacfdd7cf5668917622bc22ea2

--- src/dawn_native/d3d12/ShaderModuleD3D12.cpp
+++ src/dawn_native/d3d12/ShaderModuleD3D12.cpp
@@ -180,9 +180,11 @@ namespace dawn_native { namespace d3d12 {
         : ShaderModuleBase(device, descriptor) {
     }
 
-    ResultOrError<std::string> ShaderModule::TranslateToHLSLWithTint(const char* entryPointName,
-                                                                     SingleShaderStage stage,
-                                                                     PipelineLayout* layout) const {
+    ResultOrError<std::string> ShaderModule::TranslateToHLSLWithTint(
+        const char* entryPointName,
+        SingleShaderStage stage,
+        PipelineLayout* layout,
+        std::string* remappedEntryPointName) const {
         ASSERT(!IsError());
 
 #ifdef DAWN_ENABLE_WGSL
@@ -221,6 +223,11 @@ namespace dawn_native { namespace d3d12 {
             errorStream << "Bound Array Accessors Transform: " << boundArrayTransformer.error()
                         << std::endl;
             return DAWN_VALIDATION_ERROR(errorStream.str().c_str());
+        }
+
+        if (remappedEntryPointName) {
+            tint::inspector::Inspector inspector(module);
+            *remappedEntryPointName = inspector.GetRemappedNameForEntryPoint(entryPointName);
         }
 
         tint::writer::hlsl::Generator generator(std::move(module));
