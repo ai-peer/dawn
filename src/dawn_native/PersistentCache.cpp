6b25b671609b47681799d9d3800ea8e559ee3f61
Bryan Bernhart <bryan.bernhart@intel.com>
2020-11-12 10:28:16 -0800

    D3D12: Support caching DX shaders.
    This change is a prerequisite to D3D pipeline caching.
    
    This change introduces:
    - Caching interface which enables the cache.
    - DawnTest implemention of the caching interface for E2E testing.
    - Helper for backends to load/store blobs to be cached.
    
    Bug:dawn:549
    Change-Id: I2af759882d18b3f45dc63e49dcb6a3caa1be3485

---
+++ src/dawn_native/PersistentCache.cpp
@@ -0,0 +1,53 @@
+// Copyright 2020 The Dawn Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "dawn_native/PersistentCache.h"
+
+#include "common/Assert.h"
+#include "dawn_native/Device.h"
+#include "dawn_platform/DawnPlatform.h"
+
+namespace dawn_native {
+
+    PersistentCache::PersistentCache(DeviceBase* device) : mDevice(device) {
+    }
+
+    ScopedCachedBlob PersistentCache::LoadData(const PersistentCacheKey& key) {
+        dawn_platform::CachingInterface* cache = mDevice->GetPlatform()->GetCachingInterface();
+        ScopedCachedBlob blob = {};
+        if (cache == nullptr) {
+            return blob;
+        }
+        blob.bufferSize = cache->LoadData(reinterpret_cast<WGPUDevice>(mDevice), key.data(),
+                                          key.size(), nullptr, 0);
+        if (blob.bufferSize > 0) {
+            blob.buffer.reset(new uint8_t[blob.bufferSize]);
+            ASSERT(cache->LoadData(reinterpret_cast<WGPUDevice>(mDevice), key.data(), key.size(),
+                                   blob.buffer.get(), blob.bufferSize) == blob.bufferSize);
+            return blob;
+        }
+        return blob;
+    }
+
+    bool PersistentCache::StoreData(const PersistentCacheKey& key, const void* value, size_t size) {
+        dawn_platform::CachingInterface* cache = mDevice->GetPlatform()->GetCachingInterface();
+        if (cache == nullptr) {
+            return false;
+        }
+        ASSERT(value != nullptr);
+        ASSERT(size > 0);
+        return cache->StoreData(reinterpret_cast<WGPUDevice>(mDevice), key.data(), key.size(),
+                                value, size);
+    }
+}  // namespace dawn_native
---
+++ src/dawn_native/PersistentCache.h
@@ -0,0 +1,63 @@
+// Copyright 2020 The Dawn Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef DAWNNATIVE_PERSISTENTCACHE_H_
+#define DAWNNATIVE_PERSISTENTCACHE_H_
+
+#include "dawn_native/Error.h"
+
+#include <vector>
+
+namespace dawn_native {
+
+    using PersistentCacheKey = std::vector<uint8_t>;
+
+    struct ScopedCachedBlob {
+        std::unique_ptr<uint8_t[]> buffer;
+        size_t bufferSize = 0;
+    };
+
+    class DeviceBase;
+
+    class PersistentCache {
+      public:
+        PersistentCache(DeviceBase* device);
+
+        // Combines load/store operations into a single call.
+        // If the load was successful, a non-empty blob is returned to the caller.
+        // Else, the creation callback |createFn| gets invoked with a callback
+        // |doCache| to store the newly created blob back in the cache.
+        template <typename CreateFn>
+        ResultOrError<ScopedCachedBlob> GetOrCreate(const PersistentCacheKey& key,
+                                                    CreateFn&& createFn) {
+            ScopedCachedBlob blob = LoadData(key);
+            if (blob.bufferSize > 0) {
+                return std::move(blob);
+            }
+            DAWN_TRY(createFn([this, key](const void* value, size_t size) {
+                this->StoreData(key, value, size);
+            }));
+            return std::move(blob);
+        }
+
+      private:
+        // PersistentCache impl
+        ScopedCachedBlob LoadData(const PersistentCacheKey& key);
+        bool StoreData(const PersistentCacheKey& key, const void* value, size_t size);
+
+        DeviceBase* mDevice = nullptr;
+    };
+}  // namespace dawn_native
+
+#endif  // DAWNNATIVE_PERSISTENTCACHE_H_
