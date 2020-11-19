baea0ca6e550b1bc889a8e0f132c335c08e49b35
Stephen White <senorblanco@chromium.org>
2020-11-19 11:56:29 -0500

    WIP: implement EGL support in Dawn.
    This is an experimental patch not intended for landing.
    
    It adds ANGLE to Dawn's DEPS at third_party/angle.
    
    Currently, you must build ANGLE inside its own subdirectory, then copy
    the DLLs to Dawn's executable build directory (e.g., out\Debug, etc).
    
    EGL context creation works, and some rudimentary end2end tests work, but
    shaders don't work (likely to be problems with SPIRV-cross).
    
    Change-Id: I4a4240ca695a22388c55073fd2aee0323cd4afc9

--- src/include/dawn_native/OpenGLBackend.h
+++ src/include/dawn_native/OpenGLBackend.h
@@ -21,7 +21,7 @@
 namespace dawn_native { namespace opengl {
 
     struct DAWN_NATIVE_EXPORT AdapterDiscoveryOptions : public AdapterDiscoveryOptionsBase {
-        AdapterDiscoveryOptions();
+        AdapterDiscoveryOptions(WGPUBackendType backendType);
 
         void* (*getProc)(const char*);
     };
