b529e15a0fba6a671c4fbee3c023f260ca589eaf
Stephen White <senorblanco@chromium.org>
2020-11-20 14:29:06 -0500

    Refactor OpenGLVersion out of OpenGLFunctions.
    We need the ability to parse the GL version irrespective of
    GL extension handling, so pull it out into its own class.
    
    Change-Id: I620267146159ba8e4fa8cba5f6ebff57e981add0

--- src/dawn_native/opengl/ShaderModuleGL.cpp
+++ src/dawn_native/opengl/ShaderModuleGL.cpp
@@ -77,12 +77,9 @@ namespace dawn_native { namespace opengl {
         options.vertex.flip_vert_y = true;
         options.vertex.fixup_clipspace = true;
 
-        // TODO(cwallez@chromium.org): discover the backing context version and use that.
-#if defined(DAWN_PLATFORM_APPLE)
-        options.version = 410;
-#else
-        options.version = 440;
-#endif
+        const OpenGLVersion& version = ToBackend(GetDevice())->gl.GetVersion();
+        options.es = version.IsES();
+        options.version = version.GetMajor() * 100 + version.GetMinor() * 10;
 
         spirv_cross::CompilerGLSL compiler(GetSpirv());
         compiler.set_common_options(options);
