d927cdeee7ccfe5a8f3784d3973bbb0b5a2002f6
Hao Li <hao.x.li@intel.com>
2020-11-27 22:10:20 +0800

    Query API: Test the accuracy of timestamp compute shader
    Due to the uint64 is not fully supported in shading language, we use
    uint32 and float to simulate the subtraction and multiplication of
    uint64, but there is accuracy loss between the results and the expected
    results computed by uint64. This test checks that the accuracy loss is
    within a reasonable range (1e-5).
    
    Bug: dawn:434
    Change-Id: I6f5c842b6915f101441886bdfa4f9feb2827d174

--- src/tests/end2end/QueryTests.cpp
+++ src/tests/end2end/QueryTests.cpp
@@ -21,6 +21,15 @@
 
 #include "utils/WGPUHelpers.h"
 
+namespace {
+
+    struct TsParams {
+        uint32_t maxCount;
+        float period;
+    };
+
+}  // anonymous namespace
+
 class QueryTests : public DawnTest {
   protected:
     wgpu::Buffer CreateResolveBuffer(uint64_t size) {
@@ -109,6 +118,36 @@ class TimestampExpectation : public detail::Expectation {
     }
 };
 
+class InternalShaderExpectation : public detail::Expectation {
+  public:
+    ~InternalShaderExpectation() override = default;
+
+    InternalShaderExpectation(const uint64_t* values, const unsigned int count) {
+        mExpected.assign(values, values + count);
+    }
+
+    // Expect the actual results are approximately equal to the expected values.
+    testing::AssertionResult Check(const void* data, size_t size) override {
+        DAWN_ASSERT(size == sizeof(uint64_t) * mExpected.size());
+        const uint64_t* actual = static_cast<const uint64_t*>(data);
+        for (size_t i = 0; i < mExpected.size(); ++i) {
+            if (abs(static_cast<int64_t>(mExpected[i] - actual[i])) >
+                mExpected[i] * mExpectedErrorRate) {
+                return testing::AssertionFailure()
+                       << "Expected data[" << i << "] to be " << mExpected[i] << ", actual "
+                       << actual[i] << ". Error rate is larger than " << mExpectedErrorRate
+                       << std::endl;
+            }
+        }
+
+        return testing::AssertionSuccess();
+    }
+
+  private:
+    std::vector<uint64_t> mExpected;
+    float mExpectedErrorRate = 1.0e-5;
+};
+
 class TimestampQueryTests : public QueryTests {
   protected:
     void SetUp() override {
@@ -126,13 +165,124 @@ class TimestampQueryTests : public QueryTests {
         return requiredExtensions;
     }
 
-    wgpu::QuerySet CreateQuerySetForTimestamp(uint32_t queryCount) {
-        wgpu::QuerySetDescriptor descriptor;
-        descriptor.count = queryCount;
-        descriptor.type = wgpu::QueryType::Timestamp;
-        return device.CreateQuerySet(&descriptor);
-    }
+    wgpu::QuerySet CreateQuerySetForTimestamp(uint32_t queryCount);
+
+    void InternalComputeShaderTest(const char* shader);
 };
+
+wgpu::QuerySet TimestampQueryTests::CreateQuerySetForTimestamp(uint32_t queryCount) {
+    wgpu::QuerySetDescriptor descriptor;
+    descriptor.count = queryCount;
+    descriptor.type = wgpu::QueryType::Timestamp;
+    return device.CreateQuerySet(&descriptor);
+}
+
+void TimestampQueryTests::InternalComputeShaderTest(const char* shader) {
+    constexpr uint32_t kTimestampCount = 8;
+    // A gpu frequency on Intel D3D12 (ticks/second)
+    constexpr uint64_t kGPUFrequency = 12000048;
+    constexpr uint64_t kNsPerSecond = 1000000000;
+
+    // Original timestamp values for testing
+    std::array<uint64_t, kTimestampCount> timestamps;
+    timestamps[0] = 0;            // not written at beginning
+    timestamps[1] = 10079569507;  // t0
+    timestamps[2] = 10394415012;  // t1
+    timestamps[3] = 0;            // not written between timestamps
+    timestamps[4] = 11713454943;  // t2
+    timestamps[5] = 38912556941;  // t3 (t3 - t0 is a big delta)
+    timestamps[6] = 10080295766;  // t4 (reset)
+    timestamps[7] = 39872473956;  // t5 (after reset)
+
+    // Find the first non-zero value as a first timestamp
+    size_t baseIndex = 0;
+    uint64_t preTimestamp = 0;
+    bool timestampReset = false;
+    for (size_t i = 0; i < timestamps.size(); i++) {
+        if (timestamps[i] != 0) {
+            baseIndex = i;
+            preTimestamp = timestamps[i];
+            break;
+        }
+    }
+
+    // Expected results: timestamp value (non zero) - first timestamp value
+    std::array<uint64_t, timestamps.size()> expected;
+    for (size_t i = 0; i < timestamps.size(); i++) {
+        if (timestamps[i] == 0) {
+            // Not a timestamp value, keep original value
+            expected[i] = timestamps[i];
+        } else {
+            // If the timestamp is less than the previous timestamp, set all the following to zero.
+            if (timestampReset || timestamps[i] < preTimestamp) {
+                timestampReset = true;
+                expected[i] = 0;
+                continue;
+            }
+
+            // Maybe the timestamp delta * 10^9 is larger than the maximum of uint64, so cast the
+            // delta value to double (higher precision than float)
+            expected[i] =
+                static_cast<uint64_t>(static_cast<double>(timestamps[i] - timestamps[baseIndex]) *
+                                      kNsPerSecond / kGPUFrequency);
+
+            preTimestamp = timestamps[i];
+        }
+    }
+
+    // Set up compute shader and pipeline
+    wgpu::ShaderModule module = utils::CreateShaderModuleFromWGSL(device, shader);
+
+    wgpu::ComputePipelineDescriptor csDesc;
+    csDesc.computeStage.module = module;
+    csDesc.computeStage.entryPoint = "main";
+    wgpu::ComputePipeline pipeline = device.CreateComputePipeline(&csDesc);
+
+    // Set up input storage buffer
+    wgpu::Buffer inputBuf =
+        utils::CreateBufferFromData(device, timestamps.data(), kTimestampCount * sizeof(uint64_t),
+                                    wgpu::BufferUsage::Storage | wgpu::BufferUsage::CopySrc);
+    EXPECT_BUFFER_U64_RANGE_EQ(timestamps.data(), inputBuf, 0, kTimestampCount);
+
+    // Set up output storage buffer
+    wgpu::BufferDescriptor outputDesc;
+    outputDesc.size = kTimestampCount * sizeof(uint64_t);
+    outputDesc.usage =
+        wgpu::BufferUsage::Storage | wgpu::BufferUsage::CopySrc | wgpu::BufferUsage::CopyDst;
+    wgpu::Buffer outputBuf = device.CreateBuffer(&outputDesc);
+
+    // Set up params uniform buffer
+    TsParams params = {kTimestampCount, static_cast<float>(kNsPerSecond) /
+                                            kGPUFrequency /* timestamp period in ns */};
+    wgpu::Buffer paramsBuf =
+        utils::CreateBufferFromData(device, &params, sizeof(params), wgpu::BufferUsage::Uniform);
+
+    // Set up bind group and issue dispatch
+    wgpu::BindGroup bindGroup =
+        utils::MakeBindGroup(device, pipeline.GetBindGroupLayout(0),
+                             {
+                                 {0, inputBuf, 0, kTimestampCount * sizeof(uint64_t)},
+                                 {1, outputBuf, 0, kTimestampCount * sizeof(uint64_t)},
+                                 {2, paramsBuf, 0, sizeof(params)},
+                             });
+
+    wgpu::CommandBuffer commands;
+    {
+        wgpu::CommandEncoder encoder = device.CreateCommandEncoder();
+        wgpu::ComputePassEncoder pass = encoder.BeginComputePass();
+        pass.SetPipeline(pipeline);
+        pass.SetBindGroup(0, bindGroup);
+        pass.Dispatch(kTimestampCount);
+        pass.EndPass();
+
+        commands = encoder.Finish();
+    }
+
+    queue.Submit(1, &commands);
+
+    EXPECT_BUFFER(outputBuf, 0, kTimestampCount * sizeof(uint64_t),
+                  new InternalShaderExpectation(expected.data(), kTimestampCount));
+}
 
 // Test creating query set with the type of Timestamp
 TEST_P(TimestampQueryTests, QuerySetCreation) {
@@ -243,6 +393,104 @@ TEST_P(TimestampQueryTests, ResolveToBufferWithOffset) {
         EXPECT_BUFFER_U64_RANGE_EQ(&kZero, destination, 0, 1);
         EXPECT_BUFFER(destination, sizeof(uint64_t), sizeof(uint64_t), new TimestampExpectation);
     }
+}
+
+// Test the accuracy of timestamp compute shader which uses unsinged 32-bit integers and floats to
+// simulate the subtraction and multiplication of unsinged 64-bit integers
+TEST_P(TimestampQueryTests, TimestampComputeShader) {
+    InternalComputeShaderTest(R"(
+        [[block]] struct Timestamps {
+            # Low 32-bits : t[0].x
+            # High 32-bits: t[0].y
+            [[offset(0)]] t : [[stride(8)]] array<vec2<u32>>;
+        };
+
+        [[block]] struct Params {
+            [[offset(0)]] maxCount : u32;
+            [[offset(4)]] period : f32;
+        };
+
+        [[set(0), binding(0)]] var<storage_buffer> input : Timestamps;
+        [[set(0), binding(1)]] var<storage_buffer> output : Timestamps;
+        [[set(0), binding(2)]] var<uniform> params : Params;
+
+        [[builtin(global_invocation_id)]] var<in> GlobalInvocationID : vec3<u32>;
+
+        [[stage(compute), workgroup_size(1, 1, 1)]]
+        fn main() -> void {
+            var index : u32 = GlobalInvocationID.x;
+
+            if (index >= params.maxCount) { return; }
+            if (input.t[index].x == 0u && input.t[index].y == 0u) { return; }
+
+            # Find the first non-zero value as a timestamp baseline
+            var baseIndex : u32 = 0u;
+            for (var i : u32 = 0u; i <= index; i = i + 1) {
+                if (input.t[i].x != 0 || input.t[i].y != 0) {
+                    baseIndex = i;
+                    break;
+                }
+            }
+
+            # If the timestamp is less than the last timestamp, it means this is a
+            # timestamp which has been reset, set all the following to zero.
+            var prev : vec2<u32> = vec2<u32>(0u, 0u);
+            for (var i : u32 = 1u; i <= index; i = i + 1) {
+                # Record the previos timestamp. For example,
+                # if the src data is [0, t0, 0, t1], the previos timestamp should be t0.
+                if (prev.x == 0 && prev.y == 0 && input.t[i].x != 0 && input.t[i].y != 0 ) {
+                    prev.x = input.t[i].x;
+                    prev.y = input.t[i].y;
+                }
+
+                # Skip the zero value checking which is not a timestamp.
+                if (input.t[i].x == 0 && input.t[i].y == 0) { continue; }
+
+                if (input.t[i].y < prev.y) {
+                    return;
+                }
+                if (input.t[i].y == prev.y && input.t[i].x < prev.x) {
+                    return;
+                }
+
+                # Record the current timestamp as the previos.
+                prev.x = input.t[i].x;
+                prev.y = input.t[i].y;
+            }
+
+            # Subtract each value from the baseline
+            output.t[index].y = input.t[index].y - input.t[baseIndex].y;
+            if (input.t[index].x < input.t[baseIndex].x) {
+               output.t[index].y = output.t[index].y - 1u;
+            }
+            output.t[index].x = input.t[index].x - input.t[baseIndex].x;
+
+            # Multiply output values with params.period
+            var w: u32 = 0u;
+            # Check if the product of low 32-bits and the period exceeds the maximum of u32.
+            # Otherwise, just do the multiplication
+            if (output.t[index].x > u32(f32(0xFFFFFFFFu) / params.period)) {
+                # Use two u32 to represent the high 16-bits and low 16-bits of this u32
+                var lo : u32 = output.t[index].x & 0xFFFF;
+                var hi : u32 = output.t[index].x >> 16;
+
+                var t0 : u32 = u32(round(f32(lo) * params.period));
+                var t1 : u32 = u32(round(f32(hi) * params.period)) + (t0 >> 16);
+                w = t1 >> 16;
+
+                var result_lo : u32 = t1 << 16;
+                result_lo = result_lo | (t0 & 0xFFFF);
+                output.t[index].x = result_lo;
+            } else {
+                output.t[index].x = u32(round(f32(output.t[index].x) * params.period));
+            }
+
+            # Get the nearest integer to the float result. For high 32bits, the round function will
+            # greatly help reduce the accuracy loss of the final result.
+            output.t[index].y = u32(round(f32(output.t[index].y) * params.period)) + w;
+
+            return;
+        })");
 }
 
 DAWN_INSTANTIATE_TEST(TimestampQueryTests, D3D12Backend(), MetalBackend(), VulkanBackend());
