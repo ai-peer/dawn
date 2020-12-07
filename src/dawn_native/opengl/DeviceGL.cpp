0fcf9428dd95392ddc21a8da534ddd9100c811a3
Stephen White <senorblanco@chromium.org>
2020-12-07 16:56:12 -0500

    Add ANGLE to Dawn build.
    Change-Id: I77726651ca00d64768bddc0bb476fd920e11851f

--- src/dawn_native/opengl/DeviceGL.cpp
+++ src/dawn_native/opengl/DeviceGL.cpp
@@ -173,6 +173,7 @@ namespace dawn_native { namespace opengl {
 
             // Fence are added in order, so we can stop searching as soon
             // as we see one that's not ready.
+            gl.Flush();
             GLenum result = gl.ClientWaitSync(sync, GL_SYNC_FLUSH_COMMANDS_BIT, 0);
             if (result == GL_TIMEOUT_EXPIRED) {
                 return fenceSerial;
