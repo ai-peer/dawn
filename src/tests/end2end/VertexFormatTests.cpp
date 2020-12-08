19f84dd325917abaed1af4eb5767ea4d6bc4b865
Austin Eng <enga@chromium.org>
2020-12-07 21:41:56 -0800

    Enable use_tint_generator w/ test suppresions for SPIR-V based backends
    This runs dawn_end2end_tests both with/without use_tint_generator on
    Vulkan, OpenGL, and OpenGL ES backends. This is a temporary solution
    until we add an dawn_end2end_use_tint_generator_tests suite in the
    bot configuration after all backends can support use_tint_generator or
    have the appropriate test suppressions.
    
    Bug: dawn:571
    Change-Id: I44ab0ba7261160e34dbad512d98602427dc7e966

--- src/tests/end2end/VertexFormatTests.cpp
+++ src/tests/end2end/VertexFormatTests.cpp
@@ -375,6 +375,11 @@ class VertexFormatTest : public DawnTest {
     void DoVertexFormatTest(wgpu::VertexFormat format,
                             std::vector<VertexType> vertex,
                             std::vector<ExpectedType> expectedData) {
+        // TODO(crbug.com/tint/402): Unimplemented min / max
+        DAWN_SKIP_TEST_IF(
+            (IsFloatFormat(format) || IsHalfFormat(format) || IsNormalizedFormat(format)) &&
+            HasToggleEnabled("use_tint_generator"));
+
         wgpu::RenderPipeline pipeline = MakeTestPipeline(format, expectedData);
         wgpu::Buffer vertexBuffer = utils::CreateBufferFromData(
             device, vertex.data(), vertex.size() * sizeof(VertexType), wgpu::BufferUsage::Vertex);
@@ -495,6 +500,8 @@ TEST_P(VertexFormatTest, Char4) {
 }
 
 TEST_P(VertexFormatTest, UChar2Norm) {
+    DAWN_SKIP_TEST_IF(HasToggleEnabled("use_tint_generator"));
+
     // TODO(cwallez@chromium.org): Failing because of a SPIRV-Cross issue.
     // See http://crbug.com/dawn/259
     DAWN_SKIP_TEST_IF(IsMetal() && IsIntel());
