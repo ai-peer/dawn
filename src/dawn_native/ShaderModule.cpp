185cc902de0cf28bc565e5925f0a804fbd57e5f8
Brandon Jones <bajones@chromium.org>
2020-12-11 11:00:14 -0800

    Updating BindGroupLayoutEntry interface to match latest spec
    Updates BindGroupLayoutEntry to allow for the newly split-up descriptors
    that define each binding type in it's own member (buffer, texture, etc.)
    The previous style of descriptor is still supported but is deprecated.
    
    For the sake of keeping the scope reasonable, this change does not alter
    the BindingInfo structure that's used internally by the various
    backends. That will come as a followup.
    
    Bug: dawn:527
    Change-Id: I2f301f5f36fa2ce7ff15126ac90dc4c19d5e32ca

--- src/dawn_native/ShaderModule.cpp
+++ src/dawn_native/ShaderModule.cpp
@@ -512,6 +512,9 @@ namespace dawn_native {
                     case wgpu::BindingType::Sampler:
                     case wgpu::BindingType::ComparisonSampler:
                         break;
+
+                    case wgpu::BindingType::Undefined:
+                        UNREACHABLE();
                 }
             }
 
