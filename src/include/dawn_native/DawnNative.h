4cf22842cc9ff7551b83fb140aae91babc2cdc5b
Austin Eng <enga@chromium.org>
2020-12-16 18:00:35 -0800

    Rename DeviceDescriptor to DeprecatedDeviceDescriptor
    Dawn's device descriptor will become an extension struct
    on the webgpu.h device descriptor.
    
    Bug: dawn:384
    Change-Id: I4b3a87e2c812712d87e3cb83ce7bc8434d36ef4b

--- src/include/dawn_native/DawnNative.h
+++ src/include/dawn_native/DawnNative.h
@@ -62,11 +62,13 @@ namespace dawn_native {
 
     // An optional parameter of Adapter::CreateDevice() to send additional information when creating
     // a Device. For example, we can use it to enable a workaround, optimization or feature.
-    struct DAWN_NATIVE_EXPORT DeviceDescriptor {
+    struct DAWN_NATIVE_EXPORT DeprecatedDeviceDescriptor {
         std::vector<const char*> requiredExtensions;
         std::vector<const char*> forceEnabledToggles;
         std::vector<const char*> forceDisabledToggles;
     };
+
+    using DeviceDescriptor = DeprecatedDeviceDescriptor;
 
     // A struct to record the information of a toggle. A toggle is a code path in Dawn device that
     // can be manually configured to run or not outside Dawn, including workarounds, special
