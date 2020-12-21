ca202fbbb16cda15425cb1cec4859bd1523c72c9
Hao Li <hao.x.li@intel.com>
2020-12-21 14:05:44 +0800

    Enabel use_tint_generator tests
    Change-Id: I6f6a35010571940aac2b80e7bb367ae3176a6bca

--- src/dawn_native/opengl/DeviceGL.h
+++ src/dawn_native/opengl/DeviceGL.h
@@ -70,6 +70,8 @@ namespace dawn_native { namespace opengl {
         uint32_t GetOptimalBytesPerRowAlignment() const override;
         uint64_t GetOptimalBufferToTextureCopyOffsetAlignment() const override;
 
+        float GetTimestampPeriod() const override;
+
       private:
         Device(AdapterBase* adapter,
                const DeviceDescriptor* descriptor,
