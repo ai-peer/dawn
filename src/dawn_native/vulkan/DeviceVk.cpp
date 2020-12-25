d0dd1f141c4975e367ab3f2c8b03029e1de1b95a
Hao Li <hao.x.li@intel.com>
2020-12-25 11:12:58 +0800

    Get timestamp period from device
    - Get timestamp period on each backend
      D3D12: Get GPU frequency(HZ) from queue and calculate the period in ns
      Vulkan: Get timestampPeriod from device properties
      Metal and others: don't need the period
    
    Bug: dawn:434
    Change-Id: Ia5588a3dccadfe92d7384b9fdf1e6848c6e5c6e2

--- src/dawn_native/vulkan/DeviceVk.cpp
+++ src/dawn_native/vulkan/DeviceVk.cpp
@@ -941,4 +941,8 @@ namespace dawn_native { namespace vulkan {
         return mDeviceInfo.properties.limits.optimalBufferCopyOffsetAlignment;
     }
 
+    float Device::GetTimestampPeriodInNS() const {
+        return mDeviceInfo.properties.limits.timestampPeriod;
+    }
+
 }}  // namespace dawn_native::vulkan
