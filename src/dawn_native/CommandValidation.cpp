268bba5c3ce57d38c12726969afbb879ed831bb0
Jiawei Shao <jiawei.shao@intel.com>
2021-01-21 16:55:03 +0800

    Allow using DepthOnly aspect on the depth-only formats in T2T copies
    This patch allows the use of DepthOnly as the parameter 'aspect' in the
    command CopyTextureToTexture() on the depth-only formats (Depth24Plus
    and Depth32Float) to match the latest WebGPU SPEC.
    
    BUG=dawn:439
    TEST=dawn_unittests
    
    Change-Id: I73c4055bb0a90bed2b5751ce9dff5b319787efca

--- src/dawn_native/CommandValidation.cpp
+++ src/dawn_native/CommandValidation.cpp
@@ -698,10 +698,15 @@ namespace dawn_native {
             return DAWN_VALIDATION_ERROR("Source and destination texture formats must match.");
         }
 
-        if (src.aspect != wgpu::TextureAspect::All || dst.aspect != wgpu::TextureAspect::All) {
-            // Metal cannot select a single aspect for texture-to-texture copies
+        // Metal cannot select a single aspect for texture-to-texture copies.
+        const Format& format = src.texture->GetFormat();
+        if (SelectFormatAspects(format, src.aspect) != format.aspects) {
             return DAWN_VALIDATION_ERROR(
-                "Texture aspect must be \"all\" for texture to texture copies");
+                "Source aspect is invalid against the source texture format.");
+        }
+        if (SelectFormatAspects(format, dst.aspect) != format.aspects) {
+            return DAWN_VALIDATION_ERROR(
+                "Destination aspect is invalid against the destination texture format");
         }
 
         if (src.texture == dst.texture && src.mipLevel == dst.mipLevel) {
