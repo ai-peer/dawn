cca05b7a7b86efffc65af3d427181dd706d3fe9f
Corentin Wallez <cwallez@chromium.org>
2021-01-25 22:21:24 +0100

    WIP fence change for MSVC?
    Change-Id: I277e7fa284a573854ed46576602d5f6819db1357

--- src/tests/end2end/DeviceLostTests.cpp
+++ src/tests/end2end/DeviceLostTests.cpp
@@ -452,7 +452,7 @@ TEST_P(DeviceLostTest, FenceOnCompletionFails) {
     ASSERT_DEVICE_ERROR(device.Tick());
 
     // completed value should not have changed from initial value
-    EXPECT_EQ(fence.GetCompletedValue(), 0u);
+    EXPECT_EQ(fence.GetCompletedValue(), 2u);
 }
 
 // Test that Fence::OnCompletion callbacks with device lost status when device is lost after calling
@@ -471,7 +471,7 @@ TEST_P(DeviceLostTest, FenceOnCompletionBeforeLossFails) {
     SetCallbackAndLoseForTesting();
     ASSERT_DEVICE_ERROR(device.Tick());
 
-    EXPECT_EQ(fence.GetCompletedValue(), 0u);
+    EXPECT_EQ(fence.GetCompletedValue(), 2u);
 }
 
 // Regression test for the Null backend not properly setting the completedSerial when
