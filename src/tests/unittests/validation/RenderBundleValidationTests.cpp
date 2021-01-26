d6e2c70d984fbbefc035d55a884a6164e360d96a
Corentin Wallez <cwallez@chromium.org>
2021-01-26 15:18:12 +0100

    dawn_native: Move debug marker validation at encoding time.
    This also adds missing coverage for push/pop debug group in render
    bundles. The RenderBundleEncoder didn't validate itself on Finish, so
    add a regression test for that too.
    
    The overarching goal with this CL is to move validation at encoding time
    which will help produce SyncScopeResourceUsage in the frontend for
    dispatch() calls so that they can be reused by the backends.
    
    Bug: dawn:635
    Change-Id: Ie5a2d987fda3854b3145ba4b7a34994ea605e820

--- src/tests/unittests/validation/RenderBundleValidationTests.cpp
+++ src/tests/unittests/validation/RenderBundleValidationTests.cpp
@@ -141,6 +141,26 @@ TEST_F(RenderBundleValidationTest, Empty) {
     commandEncoder.Finish();
 }
 
+// Test that an empty error bundle encoder produces an error bundle.
+TEST_F(RenderBundleValidationTest, EmptyErrorEncoderProducesErrorBundle) {
+    DummyRenderPass renderPass(device);
+
+    utils::ComboRenderBundleEncoderDescriptor desc = {};
+    // Having 0 attachments is invalid!
+    desc.colorFormatsCount = 0;
+
+    wgpu::RenderBundleEncoder renderBundleEncoder;
+    ASSERT_DEVICE_ERROR(renderBundleEncoder = device.CreateRenderBundleEncoder(&desc));
+    wgpu::RenderBundle renderBundle;
+    ASSERT_DEVICE_ERROR(renderBundle = renderBundleEncoder.Finish());
+
+    wgpu::CommandEncoder commandEncoder = device.CreateCommandEncoder();
+    wgpu::RenderPassEncoder pass = commandEncoder.BeginRenderPass(&renderPass);
+    pass.ExecuteBundles(1, &renderBundle);
+    pass.EndPass();
+    ASSERT_DEVICE_ERROR(commandEncoder.Finish());
+}
+
 // Test executing zero render bundles.
 TEST_F(RenderBundleValidationTest, ZeroBundles) {
     DummyRenderPass renderPass(device);
