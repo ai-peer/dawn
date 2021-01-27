df0aac7087adc5a257a208b07f5a505b78720a1f
Stephen White <senorblanco@chromium.org>
2021-01-27 09:30:47 -0500

    Implement depth/stencil texture-to-texture copies for OpenGL ES 3.1.
    Bug: dawn:634
    Change-Id: I30f0c1496e5443bda9bee1905162eae39ce2461c

--- src/dawn_native/opengl/CommandBufferGL.cpp
+++ src/dawn_native/opengl/CommandBufferGL.cpp
@@ -799,32 +799,59 @@ namespace dawn_native { namespace opengl {
                         gl.BindFramebuffer(GL_READ_FRAMEBUFFER, readFBO);
                         gl.BindFramebuffer(GL_DRAW_FRAMEBUFFER, drawFBO);
                         gl.Disable(GL_SCISSOR_TEST);
+                        GLenum blitMask = 0;
+                        if (src.aspect & Aspect::Color) {
+                            blitMask |= GL_COLOR_BUFFER_BIT;
+                        }
+                        if (src.aspect & Aspect::Depth) {
+                            blitMask |= GL_DEPTH_BUFFER_BIT;
+                        }
+                        if (src.aspect & Aspect::Stencil) {
+                            blitMask |= GL_STENCIL_BUFFER_BIT;
+                        }
                         for (uint32_t layer = 0; layer < copy->copySize.depth; ++layer) {
-                            if (srcTexture->GetArrayLayers() == 1) {
-                                gl.FramebufferTexture2D(GL_READ_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
-                                                        srcTexture->GetGLTarget(),
-                                                        srcTexture->GetHandle(), src.mipLevel);
-                            } else {
-                                gl.FramebufferTextureLayer(
-                                    GL_READ_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
-                                    srcTexture->GetHandle(), static_cast<GLint>(src.mipLevel),
-                                    static_cast<GLint>(src.origin.z + layer));
-                            }
-                            if (dstTexture->GetArrayLayers() == 1) {
-                                gl.FramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
-                                                        dstTexture->GetGLTarget(),
-                                                        dstTexture->GetHandle(), dst.mipLevel);
-                            } else {
-                                gl.FramebufferTextureLayer(
-                                    GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
-                                    dstTexture->GetHandle(), static_cast<GLint>(dst.mipLevel),
-                                    static_cast<GLint>(dst.origin.z + layer));
+                            for (Aspect aspect : IterateEnumMask(src.aspect)) {
+                                GLenum glAttachment;
+                                switch (aspect) {
+                                    case Aspect::Color:
+                                        glAttachment = GL_COLOR_ATTACHMENT0;
+                                        break;
+                                    case Aspect::Depth:
+                                        glAttachment = GL_DEPTH_ATTACHMENT;
+                                        break;
+                                    case Aspect::Stencil:
+                                        glAttachment = GL_STENCIL_ATTACHMENT;
+                                        break;
+                                    case Aspect::CombinedDepthStencil:
+                                    case Aspect::None:
+                                        UNREACHABLE();
+                                }
+                                if (srcTexture->GetArrayLayers() == 1) {
+                                    gl.FramebufferTexture2D(GL_READ_FRAMEBUFFER, glAttachment,
+                                                            srcTexture->GetGLTarget(),
+                                                            srcTexture->GetHandle(), src.mipLevel);
+                                } else {
+                                    gl.FramebufferTextureLayer(
+                                        GL_READ_FRAMEBUFFER, glAttachment, srcTexture->GetHandle(),
+                                        static_cast<GLint>(src.mipLevel),
+                                        static_cast<GLint>(src.origin.z + layer));
+                                }
+                                if (dstTexture->GetArrayLayers() == 1) {
+                                    gl.FramebufferTexture2D(GL_DRAW_FRAMEBUFFER, glAttachment,
+                                                            dstTexture->GetGLTarget(),
+                                                            dstTexture->GetHandle(), dst.mipLevel);
+                                } else {
+                                    gl.FramebufferTextureLayer(
+                                        GL_DRAW_FRAMEBUFFER, glAttachment, dstTexture->GetHandle(),
+                                        static_cast<GLint>(dst.mipLevel),
+                                        static_cast<GLint>(dst.origin.z + layer));
+                                }
                             }
                             gl.BlitFramebuffer(
                                 src.origin.x, src.origin.y, src.origin.x + copySize.width,
                                 src.origin.y + copySize.height, dst.origin.x, dst.origin.y,
                                 dst.origin.x + copySize.width, dst.origin.y + copySize.height,
-                                GL_COLOR_BUFFER_BIT, GL_NEAREST);
+                                blitMask, GL_NEAREST);
                         }
                         gl.Enable(GL_SCISSOR_TEST);
                         gl.DeleteFramebuffers(1, &readFBO);
