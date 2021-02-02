b406bad9baeb0f5ea3d6125546dd502d41866473
Corentin Wallez <cwallez@chromium.org>
2021-02-02 12:10:40 +0100

    Device: Deprecated GetDefaultQueue in favor of ::GetQueue()
    Bug: dawn:22
    
    Change-Id: I103ea933ca5b93f20d8bf11c6671bd9f603d8ff3

--- src/tests/DawnTest.cpp
+++ src/tests/DawnTest.cpp
@@ -827,7 +827,7 @@ void DawnTestBase::SetUp() {
         "_" + ::testing::UnitTest::GetInstance()->current_test_info()->name();
     mWireHelper->BeginWireTrace(traceName.c_str());
 
-    queue = device.GetDefaultQueue();
+    queue = device.GetQueue();
 
     device.SetUncapturedErrorCallback(OnDeviceError, this);
     device.SetDeviceLostCallback(OnDeviceLost, this);
@@ -993,7 +993,7 @@ void DawnTestBase::FlushWire() {
 }
 
 void DawnTestBase::WaitForAllOperations() {
-    wgpu::Queue queue = device.GetDefaultQueue();
+    wgpu::Queue queue = device.GetQueue();
     wgpu::Fence fence = queue.CreateFence();
 
     // Force the currently submitted operations to completed.
