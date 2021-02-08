4fad2a35c3978eec7d3ec227da6249538f0bc53a
Stephen White <senorblanco@chromium.org>
2021-02-08 12:05:46 -0500

    Skip testing of SNORM and stencil formats where non-copyable.
    SNORM textures are non-renderable on vanilla ES, and stencil formats
    are non-copyable. Use the EXT_render_snorm and NV_read_stencil
    extensions where available.
    
    Bug: dawn:624 dawn:636 dawn:647 dawn:667
    Change-Id: Ic50368032d6168060b6b52889b4ba952ce662f02

--- src/tests/end2end/TextureZeroInitTests.cpp
+++ src/tests/end2end/TextureZeroInitTests.cpp
@@ -1427,6 +1427,8 @@ TEST_P(TextureZeroInitTest, PreservesInitializedArrayLayer) {
 // This is a regression test for crbug.com/dawn/451 where the lazy texture
 // init path on D3D12 had a divide-by-zero exception in the copy split logic.
 TEST_P(TextureZeroInitTest, CopyTextureToBufferNonRenderableUnaligned) {
+    DAWN_SKIP_TEST_IF(HasToggleEnabled("disable_snorm_read"));
+
     wgpu::TextureDescriptor descriptor;
     descriptor.size.width = kUnalignedSize;
     descriptor.size.height = kUnalignedSize;
