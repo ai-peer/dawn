45374d15e7193e79518d6a5de3985537c79f3f70
Yunchao He <yunchao.he@intel.com>
2021-03-29 16:23:14 -0700

    Implement 3D texture copy on D3D12: T2B and T2T
    This change implements 3D texture copy on D3D12 for texture to
    buffer and texture to texture copy for an entire 3D texture.
    
    It also use a function named CopyBufferToTexture to wrap
    CopyBufferTo3DTexture and CopyBufferTo2DTextureWithCopySplits.
    Likewise, it uses another function named CopyTextureToBuffer to
    wrap Copy3DTextureToBuffer and Copy2DTextureToBufferWithCopySplits.
    
    BUG: dawn: 547
    
    Change-Id: I4293f6ca4d37f604e6f1c10827686d17469a07ca

--- src/dawn_native/d3d12/UtilsD3D12.h
+++ src/dawn_native/d3d12/UtilsD3D12.h
@@ -74,6 +74,16 @@ namespace dawn_native { namespace d3d12 {
                                Texture* texture,
                                Aspect aspect);
 
+    void CopyBufferToTexture(CommandRecordingContext* commandContext,
+                             const TextureCopy& textureCopy,
+                             ID3D12Resource* bufferResource,
+                             const uint64_t offset,
+                             const uint32_t bytesPerRow,
+                             const uint32_t rowsPerImage,
+                             const Extent3D& copySize,
+                             Texture* texture,
+                             Aspect aspect);
+
     void RecordCopyTextureToBufferFromTextureCopySplit(ID3D12GraphicsCommandList* commandList,
                                                        const Texture2DCopySplit& baseCopySplit,
                                                        Buffer* buffer,
@@ -97,6 +107,13 @@ namespace dawn_native { namespace d3d12 {
                                Texture* texture,
                                Buffer* buffer,
                                const Extent3D& copySize);
+
+    void CopyTextureToBuffer(ID3D12GraphicsCommandList* commandList,
+                             const TextureCopy& textureCopy,
+                             const BufferCopy& bufferCopy,
+                             Texture* texture,
+                             Buffer* buffer,
+                             const Extent3D& copySize);
 
 }}  // namespace dawn_native::d3d12
 
