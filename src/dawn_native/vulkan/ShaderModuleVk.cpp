6b9e8e6e6b479d5587e5637ec9e8dc133bccac01
shrekshao <shrekshao@google.com>
2021-04-08 14:31:09 -0700

    Remap BindGroup bindingIndex for vulkan backend when using Tint Generator
    Bug: dawn:750
    Change-Id: I239f5544a5822422d61a249f2ef028df326f90ed

--- src/dawn_native/vulkan/ShaderModuleVk.cpp
+++ src/dawn_native/vulkan/ShaderModuleVk.cpp
@@ -15,8 +15,10 @@
 #include "dawn_native/vulkan/ShaderModuleVk.h"
 
 #include "dawn_native/TintUtils.h"
+#include "dawn_native/vulkan/BindGroupLayoutVk.h"
 #include "dawn_native/vulkan/DeviceVk.h"
 #include "dawn_native/vulkan/FencedDeleter.h"
+#include "dawn_native/vulkan/PipelineLayoutVk.h"
 #include "dawn_native/vulkan/VulkanError.h"
 
 #include <spirv_cross.hpp>
@@ -43,44 +45,18 @@ namespace dawn_native { namespace vulkan {
     }
 
     MaybeError ShaderModule::Initialize(ShaderModuleParseResult* parseResult) {
-        std::vector<uint32_t> spirv;
+        ScopedTintICEHandler scopedICEHandler(GetDevice());
+        if (GetDevice()->IsToggleEnabled(Toggle::UseTintGenerator)) {
+            // Tint Generator path, defer creation of VkShaderModule
+            // since we might have bindIndex remapping happens when creating pipeline
+            return InitializeBase(parseResult);
+        }
+
+        // SPRIV-Cross path, immediately create shader module
         const std::vector<uint32_t>* spirvPtr;
 
-        ScopedTintICEHandler scopedICEHandler(GetDevice());
-
-        if (GetDevice()->IsToggleEnabled(Toggle::UseTintGenerator)) {
-            std::ostringstream errorStream;
-            errorStream << "Tint SPIR-V writer failure:" << std::endl;
-
-            tint::transform::Manager transformManager;
-            transformManager.append(std::make_unique<tint::transform::BoundArrayAccessors>());
-            transformManager.append(std::make_unique<tint::transform::EmitVertexPointSize>());
-            transformManager.append(std::make_unique<tint::transform::Spirv>());
-
-            tint::Program program;
-            DAWN_TRY_ASSIGN(program,
-                            RunTransforms(&transformManager, parseResult->tintProgram.get(),
-                                          CompilationMessages()));
-
-            tint::writer::spirv::Generator generator(&program);
-            if (!generator.Generate()) {
-                errorStream << "Generator: " << generator.error() << std::endl;
-                return DAWN_VALIDATION_ERROR(errorStream.str().c_str());
-            }
-
-            spirv = generator.result();
-            spirvPtr = &spirv;
-
-            ShaderModuleParseResult transformedParseResult;
-            transformedParseResult.tintProgram =
-                std::make_unique<tint::Program>(std::move(program));
-            transformedParseResult.spirv = spirv;
-
-            DAWN_TRY(InitializeBase(&transformedParseResult));
-        } else {
-            DAWN_TRY(InitializeBase(parseResult));
-            spirvPtr = &GetSpirv();
-        }
+        DAWN_TRY(InitializeBase(parseResult));
+        spirvPtr = &GetSpirv();
 
         VkShaderModuleCreateInfo createInfo;
         createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
@@ -107,6 +83,94 @@ namespace dawn_native { namespace vulkan {
 
     VkShaderModule ShaderModule::GetHandle() const {
         return mHandle;
+    }
+
+    MaybeError ShaderModule::InitializeTransformedModule(const char* entryPointName,
+                                                         PipelineLayout* layout) {
+        ScopedTintICEHandler scopedICEHandler(GetDevice());
+
+        if (GetDevice()->IsToggleEnabled(Toggle::UseTintGenerator)) {
+            // Creation of VkShaderModule is deferred to this point when using tint generator
+            std::ostringstream errorStream;
+            errorStream << "Tint SPIR-V writer failure:" << std::endl;
+
+            // Remap BindingNumber to BindingIndex in WGSL shader
+            using BindingRemapper = tint::transform::BindingRemapper;
+            using BindingPoint = tint::transform::BindingPoint;
+            BindingRemapper::BindingPoints bindingPoints;
+            BindingRemapper::AccessControls accessControls;
+
+            const EntryPointMetadata::BindingInfoArray& moduleBindingInfo =
+                GetEntryPoint(entryPointName).bindings;
+
+            for (BindGroupIndex group : IterateBitSet(layout->GetBindGroupLayoutsMask())) {
+                const BindGroupLayout* bgl = ToBackend(layout->GetBindGroupLayout(group));
+                const auto& groupBindingInfo = moduleBindingInfo[group];
+                for (const auto& it : groupBindingInfo) {
+                    BindingNumber binding = it.first;
+                    BindingIndex bindingIndex = bgl->GetBindingIndex(binding);
+                    BindingPoint srcBindingPoint{static_cast<uint32_t>(group),
+                                                 static_cast<uint32_t>(binding)};
+
+                    BindingPoint dstBindingPoint{static_cast<uint32_t>(group),
+                                                 static_cast<uint32_t>(bindingIndex)};
+                    if (srcBindingPoint != dstBindingPoint) {
+                        bindingPoints.emplace(srcBindingPoint, dstBindingPoint);
+                    }
+                }
+            }
+
+            tint::transform::Manager transformManager;
+            transformManager.append(std::make_unique<tint::transform::BindingRemapper>());
+            transformManager.append(std::make_unique<tint::transform::BoundArrayAccessors>());
+            transformManager.append(std::make_unique<tint::transform::EmitVertexPointSize>());
+            transformManager.append(std::make_unique<tint::transform::Spirv>());
+
+            tint::transform::DataMap transformInputs;
+            transformInputs.Add<BindingRemapper::Remappings>(std::move(bindingPoints),
+                                                             std::move(accessControls));
+            tint::transform::Transform::Output output =
+                transformManager.Run(GetTintProgram(), transformInputs);
+
+            tint::Program& program = output.program;
+            if (!program.IsValid()) {
+                errorStream << "Tint program transform error: " << program.Diagnostics().str()
+                            << std::endl;
+                return DAWN_VALIDATION_ERROR(errorStream.str().c_str());
+            }
+
+            tint::writer::spirv::Generator generator(&program);
+            if (!generator.Generate()) {
+                errorStream << "Generator: " << generator.error() << std::endl;
+                return DAWN_VALIDATION_ERROR(errorStream.str().c_str());
+            }
+
+            std::vector<uint32_t> spirv = generator.result();
+            const std::vector<uint32_t>* spirvPtr = &spirv;
+
+            ShaderModuleParseResult transformedParseResult;
+            transformedParseResult.tintProgram =
+                std::make_unique<tint::Program>(std::move(program));
+            transformedParseResult.spirv = spirv;
+
+            DAWN_TRY(InitializeBase(&transformedParseResult));
+
+            VkShaderModuleCreateInfo createInfo;
+            createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+            createInfo.pNext = nullptr;
+            createInfo.flags = 0;
+            std::vector<uint32_t> vulkanSource;
+            createInfo.codeSize = spirvPtr->size() * sizeof(uint32_t);
+            createInfo.pCode = spirvPtr->data();
+
+            Device* device = ToBackend(GetDevice());
+            return CheckVkSuccess(device->fn.CreateShaderModule(device->GetVkDevice(), &createInfo,
+                                                                nullptr, &*mHandle),
+                                  "CreateShaderModule");
+        }
+
+        // SPRIV-Cross path, VkShaderModule is already created at Initialize time
+        return {};
     }
 
 }}  // namespace dawn_native::vulkan
