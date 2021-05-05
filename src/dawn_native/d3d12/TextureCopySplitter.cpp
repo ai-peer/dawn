5b27e2a5e24eb95bd2ba220c7dc432de075969a4
Yunchao He <yunchao.he@intel.com>
2021-05-04 22:31:20 -0700

    Implement unaligned full copy for 3D texture on D3D12
    If data is not 256B-aligned, we need to carefully split the data
    into a few parts with well-designed offset, size, etc., in both
    src and dst resources. It turns out that it's better to write
    separate copy splitter functions for 3D textures because the
    new splitter functions are quite different from 2D texture's,
    making code reuse difficult.
    
    Bug: dawn:547
    
    Change-Id: I333fd50c6c14d8f67bdfa266340dfcf01b85dea1

--- src/dawn_native/d3d12/TextureCopySplitter.cpp
+++ src/dawn_native/d3d12/TextureCopySplitter.cpp
@@ -33,12 +33,12 @@ namespace dawn_native { namespace d3d12 {
         }
     }  // namespace
 
-    Texture2DCopySplit ComputeTextureCopySplit(Origin3D origin,
-                                               Extent3D copySize,
-                                               const TexelBlockInfo& blockInfo,
-                                               uint64_t offset,
-                                               uint32_t bytesPerRow,
-                                               uint32_t rowsPerImage) {
+    Texture2DCopySplit Compute2DTextureCopySplit(Origin3D origin,
+                                                 Extent3D copySize,
+                                                 const TexelBlockInfo& blockInfo,
+                                                 uint64_t offset,
+                                                 uint32_t bytesPerRow,
+                                                 uint32_t rowsPerImage) {
         Texture2DCopySplit copy;
 
         ASSERT(bytesPerRow % blockInfo.byteSize == 0);
@@ -48,12 +48,12 @@ namespace dawn_native { namespace d3d12 {
         uint64_t alignedOffset =
             offset & ~static_cast<uint64_t>(D3D12_TEXTURE_DATA_PLACEMENT_ALIGNMENT - 1);
 
-        copy.offset = alignedOffset;
-
         // If the provided offset to the data was already 512-aligned, we can simply copy the data
         // without further translation.
         if (offset == alignedOffset) {
             copy.count = 1;
+
+            copy.copies[0].offset = alignedOffset;
 
             copy.copies[0].textureOffset = origin;
 
@@ -124,6 +124,8 @@ namespace dawn_native { namespace d3d12 {
 
             copy.count = 1;
 
+            copy.copies[0].offset = alignedOffset;
+
             copy.copies[0].textureOffset = origin;
 
             copy.copies[0].copySize = copySize;
@@ -172,6 +174,8 @@ namespace dawn_native { namespace d3d12 {
 
         copy.count = 2;
 
+        copy.copies[0].offset = alignedOffset;
+
         copy.copies[0].textureOffset = origin;
 
         ASSERT(bytesPerRow > byteOffsetInRowPitch);
@@ -185,6 +189,8 @@ namespace dawn_native { namespace d3d12 {
         copy.copies[0].bufferSize.height = rowsPerImageInTexels + texelOffset.y;
         copy.copies[0].bufferSize.depthOrArrayLayers = copySize.depthOrArrayLayers;
 
+        copy.copies[1].offset = alignedOffset;
+
         copy.copies[1].textureOffset.x = origin.x + copy.copies[0].copySize.width;
         copy.copies[1].textureOffset.y = origin.y;
         copy.copies[1].textureOffset.z = origin.z;
@@ -197,6 +203,7 @@ namespace dawn_native { namespace d3d12 {
         copy.copies[1].bufferOffset.x = 0;
         copy.copies[1].bufferOffset.y = texelOffset.y + blockInfo.height;
         copy.copies[1].bufferOffset.z = 0;
+
         copy.copies[1].bufferSize.width = copy.copies[1].copySize.width;
         copy.copies[1].bufferSize.height = rowsPerImageInTexels + texelOffset.y + blockInfo.height;
         copy.copies[1].bufferSize.depthOrArrayLayers = copySize.depthOrArrayLayers;
@@ -204,13 +211,12 @@ namespace dawn_native { namespace d3d12 {
         return copy;
     }
 
-    TextureCopySplits ComputeTextureCopySplits(Origin3D origin,
-                                               Extent3D copySize,
-                                               const TexelBlockInfo& blockInfo,
-                                               uint64_t offset,
-                                               uint32_t bytesPerRow,
-                                               uint32_t rowsPerImage,
-                                               bool is3DTexture) {
+    TextureCopySplits Compute2DTextureCopySplits(Origin3D origin,
+                                                 Extent3D copySize,
+                                                 const TexelBlockInfo& blockInfo,
+                                                 uint64_t offset,
+                                                 uint32_t bytesPerRow,
+                                                 uint32_t rowsPerImage) {
         TextureCopySplits copies;
 
         const uint64_t bytesPerSlice = bytesPerRow * rowsPerImage;
@@ -218,39 +224,235 @@ namespace dawn_native { namespace d3d12 {
         // The function ComputeTextureCopySplit() decides how to split the copy based on:
         // - the alignment of the buffer offset with D3D12_TEXTURE_DATA_PLACEMENT_ALIGNMENT (512)
         // - the alignment of the buffer offset with D3D12_TEXTURE_DATA_PITCH_ALIGNMENT (256)
-        // Each slice of a 2D array or 3D copy might need to be split, but because of the WebGPU
-        // constraint that "bytesPerRow" must be a multiple of 256, all odd (resp. all even) slices
+        // Each layer of a 2D array might need to be split, but because of the WebGPU
+        // constraint that "bytesPerRow" must be a multiple of 256, all odd (resp. all even) layers
         // will be at an offset multiple of 512 of each other, which means they will all result in
         // the same 2D split. Thus we can just compute the copy splits for the first and second
-        // slices, and reuse them for the remaining slices by adding the related offset of each
-        // slice. Moreover, if "rowsPerImage" is even, both the first and second copy layers can
+        // layers, and reuse them for the remaining slices by adding the related offset of each
+        // layer. Moreover, if "rowsPerImage" is even, both the first and second copy layers can
         // share the same copy split, so in this situation we just need to compute copy split once
-        // and reuse it for all the slices.
+        // and reuse it for all the layers.
         Extent3D copyOneLayerSize = copySize;
         Origin3D copyFirstLayerOrigin = origin;
-        if (!is3DTexture) {
-            copyOneLayerSize.depthOrArrayLayers = 1;
-            copyFirstLayerOrigin.z = 0;
-        }
+        copyOneLayerSize.depthOrArrayLayers = 1;
+        copyFirstLayerOrigin.z = 0;
 
-        copies.copies2D[0] = ComputeTextureCopySplit(copyFirstLayerOrigin, copyOneLayerSize,
-                                                     blockInfo, offset, bytesPerRow, rowsPerImage);
+        copies.copies2D[0] = Compute2DTextureCopySplit(
+            copyFirstLayerOrigin, copyOneLayerSize, blockInfo, offset, bytesPerRow, rowsPerImage);
 
-        // When the copy only refers one texture 2D array layer or a 3D texture, copies.copies2D[1]
+        // When the copy only refers one texture 2D array layer, copies.copies2D[1]
         // will never be used so we can safely early return here.
-        if (copySize.depthOrArrayLayers == 1 || is3DTexture) {
+        if (copySize.depthOrArrayLayers == 1) {
             return copies;
         }
 
         if (bytesPerSlice % D3D12_TEXTURE_DATA_PLACEMENT_ALIGNMENT == 0) {
             copies.copies2D[1] = copies.copies2D[0];
-            copies.copies2D[1].offset += bytesPerSlice;
+            copies.copies2D[1].copies[0].offset += bytesPerSlice;
+            copies.copies2D[1].copies[1].offset += bytesPerSlice;
         } else {
             const uint64_t bufferOffsetNextLayer = offset + bytesPerSlice;
             copies.copies2D[1] =
-                ComputeTextureCopySplit(copyFirstLayerOrigin, copyOneLayerSize, blockInfo,
-                                        bufferOffsetNextLayer, bytesPerRow, rowsPerImage);
+                Compute2DTextureCopySplit(copyFirstLayerOrigin, copyOneLayerSize, blockInfo,
+                                          bufferOffsetNextLayer, bytesPerRow, rowsPerImage);
         }
+
+        return copies;
+    }
+
+    Texture2DCopySplit Compute3DTextureCopySplit(Origin3D origin,
+                                                 Extent3D copySize,
+                                                 const TexelBlockInfo& blockInfo,
+                                                 uint64_t offset,
+                                                 uint32_t bytesPerRow,
+                                                 uint32_t rowsPerImage) {
+        Texture2DCopySplit copy;
+
+        ASSERT(bytesPerRow % blockInfo.byteSize == 0);
+
+        // The copies must be 512-aligned. To do this, we calculate the first 512-aligned address
+        // preceding our data.
+        uint64_t alignedOffset =
+            offset & ~static_cast<uint64_t>(D3D12_TEXTURE_DATA_PLACEMENT_ALIGNMENT - 1);
+
+        // If the provided offset to the data was already 512-aligned, we can simply copy the data
+        // without further translation.
+        if (offset == alignedOffset) {
+            copy.count = 1;
+
+            copy.copies[0].offset = alignedOffset;
+
+            copy.copies[0].textureOffset = origin;
+
+            copy.copies[0].copySize = copySize;
+
+            copy.copies[0].bufferOffset.x = 0;
+            copy.copies[0].bufferOffset.y = 0;
+            copy.copies[0].bufferOffset.z = 0;
+
+            copy.copies[0].bufferSize = copySize;
+
+            return copy;
+        }
+
+        ASSERT(alignedOffset < offset);
+        ASSERT(offset - alignedOffset < D3D12_TEXTURE_DATA_PLACEMENT_ALIGNMENT);
+
+        // We must reinterpret our aligned offset into X and Y offsets with respect to the row
+        // pitch.
+        //
+        // You can visualize the data in the buffer like this:
+        // |-----------------------++++++++++++++++++++++++++++++++|
+        // ^ 512-aligned address   ^ Aligned offset               ^ End of copy data
+        //
+        // Now when you consider the row pitch, you can visualize the data like this:
+        //   __________________
+        //  /                /|
+        // /                / |
+        // |----------------| |
+        // |~~~~~~~~~~~~~~~~| |
+        // |~~~~~+++++++++++| |
+        // |++++++++++++++++| /
+        // |+++++~~~~~~~~~~~|/
+        // |<---row pitch-->|
+        //
+        // The X and Y offsets calculated in ComputeTexelOffsets can be visualized like this:
+        //   __________________
+        //  /                /|
+        // /                / |
+        // |----------------| |
+        // |YYYYYYYYYYYYYYYY| |
+        // |XXXXXX++++++++++| |
+        // |++++++++++++++++| /
+        // |++++++~~~~~~~~~~|/
+        // |<---row pitch-->|
+        Origin3D texelOffset = ComputeTexelOffsets(
+            blockInfo, static_cast<uint32_t>(offset - alignedOffset), bytesPerRow);
+
+        ASSERT(texelOffset.z == 0);
+
+        uint32_t copyBytesPerRowPitch = copySize.width / blockInfo.width * blockInfo.byteSize;
+        uint32_t byteOffsetInRowPitch = texelOffset.x / blockInfo.width * blockInfo.byteSize;
+        if (copyBytesPerRowPitch + byteOffsetInRowPitch <= bytesPerRow) {
+            // The region's rows fit inside the bytes per row. In this case, extend the width of the
+            // PlacedFootprint and copy the buffer with an offset location
+            //                |<------------- bytes per row ------------->|
+            //
+            //                 _____________________________________________
+            //                /                                           /|
+            //               /                                           / |
+            //               |-------------------------------------------| |
+            //  row (N - 1)  |                                           | |
+            //  row N        |                 +++++++++++++++++~~~~~~~~~| |
+            //  row (N + 1)  |~~~~~~~~~~~~~~~~~+++++++++++++++++~~~~~~~~~| |
+            //     ...       |~~~~~~~~~~~~~~~~~+++++++++++++++++~~~~~~~~~| |
+            //     ...       |~~~~~~~~~~~~~~~~~+++++++++++++++++~~~~~~~~~| /
+            //     ...       |~~~~~~~~~~~~~~~~~+++++++++++++++++         |/
+            //               |-------------------------------------------|
+
+            // Copy 0:
+            //                 ____________________________________
+            //                /                                  /|
+            //               /                                  / |
+            //               |----------------------------------| |
+            //  row (N - 1)  |                                  | |
+            //  row N        |                 +++++++++++++++++| |
+            //  row (N + 1)  |~~~~~~~~~~~~~~~~~+++++++++++++++++| |
+            //     ...       |~~~~~~~~~~~~~~~~~+++++++++++++++++| /
+            //     ...       |~~~~~~~~~~~~~~~~~+++++++++++++++++|/
+            //               |----------------------------------|
+
+            // Copy 1 (Copy 1 for the last row is optional, see the comments below):
+            //                 ____________________________________
+            //                /                                  /|
+            //               /                                  / |
+            //               |----------------------------------| |
+            //               |                                  | /
+            //  The last row |                 +++++++++++++++++|/
+            //               |----------------------------------|
+
+            copy.count = 1;
+
+            copy.copies[0].offset = alignedOffset;
+            copy.copies[0].textureOffset = origin;
+
+            copy.copies[0].copySize = copySize;
+            copy.copies[0].copySize.height = copySize.height - texelOffset.y;
+
+            copy.copies[0].bufferOffset = texelOffset;
+            copy.copies[0].bufferSize = copySize;
+            copy.copies[0].bufferSize.width = copySize.width + texelOffset.x;
+
+            // If textureOffset.y is not zero, it means that alignedOffset is at row (N - 1)
+            // but data in buffer starts from row N. So the last row of data in buffer is
+            // conceptually out of the current depth slice. For 3D texture copies, we can't
+            // put the last row into copy.copies[0] like 2D texture copy. Because we repositioned
+            // every alignedOffset for each layer for 2D texture copies but there is
+            // only one alignedOffset for all depth ranges in 3D texture copy. The copied data
+            // will be in a mess if we put the last row into copy.copies[0] and make
+            // copy.copies[0].bufferSize bigger than the real buffer size of each depth slice.
+            // As a result, we need a separate copy for the last row for this situation.
+            if (texelOffset.y > 0) {
+                copy.count = 2;
+                uint64_t offsetForLastRow = offset + bytesPerRow * copy.copies[0].copySize.height;
+                uint64_t alignedOffsetForLastRow =
+                    offsetForLastRow &
+                    ~static_cast<uint64_t>(D3D12_TEXTURE_DATA_PLACEMENT_ALIGNMENT - 1);
+
+                Origin3D texelOffsetForLastRow = ComputeTexelOffsets(
+                    blockInfo, static_cast<uint32_t>(offsetForLastRow - alignedOffsetForLastRow),
+                    bytesPerRow);
+
+                copy.copies[1].offset = alignedOffsetForLastRow;
+
+                copy.copies[1].textureOffset = origin;
+                copy.copies[1].textureOffset.y = origin.y + copy.copies[0].copySize.height;
+
+                copy.copies[1].copySize = copySize;
+                copy.copies[1].copySize.height = texelOffset.y;
+
+                copy.copies[1].bufferOffset = texelOffsetForLastRow;
+                copy.copies[1].bufferSize = copy.copies[0].bufferSize;
+            }
+
+            return copy;
+        }
+
+        // TODO (yunchao.he@intel.com): add code to handle the situation when copyBytesPerRowPitch +
+        // byteOffsetInRowPitch > bytesPerRow
+        return copy;
+    }
+
+    TextureCopySplits Compute3DTextureCopySplits(Origin3D origin,
+                                                 Extent3D copySize,
+                                                 const TexelBlockInfo& blockInfo,
+                                                 uint64_t offset,
+                                                 uint32_t bytesPerRow,
+                                                 uint32_t rowsPerImage) {
+        TextureCopySplits copies;
+
+        // The function Compute3DTextureCopySplit() decides how to split the copy based on:
+        // - the alignment of the buffer offset with D3D12_TEXTURE_DATA_PLACEMENT_ALIGNMENT (512)
+        // - the alignment of the buffer offset with D3D12_TEXTURE_DATA_PITCH_ALIGNMENT (256)
+        // All depth slices of a 3D texture on the same mip level can be copied together, because
+        // these depth slices belong to the same subresource. However, buffer offset in
+        // CopyTextureRegion must be 512B-aligned (aka alignedOffset). It can't start at any given
+        // buffer offset in real cases (aka offset). So data on the same row in buffer might be
+        // split into different rows during copies. Take the data on the first row for example, it
+        // might be split into two parts:
+        // - the head might be still on the same row with alignedOffset.
+        // - the tail will definitely on the following row after the head.
+        // We can expand the head to the rest rows of this slice on y-axis, and expand it to the
+        // rest of depth slices of this mip level on z-axis. The expanded data consists of a 3D
+        // block: head block. Likewise, we can expand tail on y-axis and z-axis and get another
+        // 3D block: tail block. Then issue two CopyTextureRegions for the head block and tail
+        // block.
+        // Sometimes, the last row of head block or tail block might exceed the whole 3D block
+        // of copySize because alignedOffset is different from offset in buffer, so we might need
+        // to issue a third CopyTextureRegion command to copy the last row of all depth slices.
+
+        copies.copies2D[0] = Compute3DTextureCopySplit(origin, copySize, blockInfo, offset,
+                                                       bytesPerRow, rowsPerImage);
 
         return copies;
     }
