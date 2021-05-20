97c66ad991436ba69525628e2d7415f46815d3e8
Yunchao He <yunchao.he@intel.com>
2021-05-20 16:51:44 -0700

    Impl 3DTexture copy splitter for empty first row issue on D3D12
    If there is an empty row at the beginning of a copy region due to
    alignment adjustment, this copy region split by 2D texture splitter
    will be definitely incorrect for 3D textures because every depth
    slice (except the first slice) will wrongly skip one row. We need
    to recompute this copy region via modifying this copy region and
    adding a couple more copy regions for the empty first row issue.
    
    The idea of recomputation is:
      - modify this copy region and don't copy the last row in order to
        make its bufferSize.height not exceed rowsPerImage,
      - use one more copy region to handle the last row of each depth
        slice except the last depth slice,
      - use another copy region to handle the last row of the last depth
        slice.
    
    Bug: dawn:547
    
    Change-Id: Ib2f6019963ed29d62a9f13d7316b5f04801db8c9

--- src/dawn_native/d3d12/TextureCopySplitter.h
+++ src/dawn_native/d3d12/TextureCopySplitter.h
@@ -28,7 +28,7 @@ namespace dawn_native {
 namespace dawn_native { namespace d3d12 {
 
     struct TextureCopySubresource {
-        static constexpr unsigned int kMaxTextureCopyRegions = 2;
+        static constexpr unsigned int kMaxTextureCopyRegions = 4;
 
         struct CopyInfo {
             uint64_t alignedOffset = 0;
@@ -83,6 +83,14 @@ namespace dawn_native { namespace d3d12 {
                                                  uint64_t offset,
                                                  uint32_t bytesPerRow,
                                                  uint32_t rowsPerImage);
+
+    TextureCopySubresource Compute3DTextureCopySubresourceForSpecialCases(
+        Origin3D origin,
+        Extent3D copySize,
+        const TexelBlockInfo& blockInfo,
+        uint64_t offset,
+        uint32_t bytesPerRow,
+        uint32_t rowsPerImage);
 
     TextureCopySubresource Compute3DTextureCopySplits(Origin3D origin,
                                                       Extent3D copySize,
