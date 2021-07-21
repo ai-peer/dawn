d3aabb4419a4b46db6eaaaa010f2efd5d7b86878
Ken Rockot <rockot@google.com>
2021-07-21 10:42:51 -0700

    Enforce per-dimension dispatch size limits
    Note that this is for direct dispatch calls only. Indirect dispatch
    calls are still not validated.
    
    Bug: dawn:1006
    Change-Id: I061c15208a01dfb803923823ba4afd38667cad22

--- src/tests/unittests/validation/ComputeValidationTests.cpp
+++ src/tests/unittests/validation/ComputeValidationTests.cpp
@@ -12,9 +12,51 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "common/Constants.h"
 #include "tests/unittests/validation/ValidationTest.h"
-
-class ComputeValidationTest : public ValidationTest {};
+#include "utils/WGPUHelpers.h"
 
 // TODO(cwallez@chromium.org): Add a regression test for Disptach validation trying to acces the
 // input state.
+
+class ComputeValidationTest : public ValidationTest {
+  protected:
+    void SetUp() override {
+        ValidationTest::SetUp();
+
+        wgpu::ShaderModule computeModule = utils::CreateShaderModule(device, R"(
+            [[stage(compute), workgroup_size(1)]] fn main() {
+            })");
+
+        // Set up compute pipeline
+        wgpu::PipelineLayout pl = utils::MakeBasicPipelineLayout(device, nullptr);
+
+        wgpu::ComputePipelineDescriptor csDesc;
+        csDesc.layout = pl;
+        csDesc.compute.module = computeModule;
+        csDesc.compute.entryPoint = "main";
+        pipeline = device.CreateComputePipeline(&csDesc);
+    }
+
+    void TestDispatch(uint32_t x, uint32_t y, uint32_t z) {
+        wgpu::CommandEncoder encoder = device.CreateCommandEncoder();
+        wgpu::ComputePassEncoder pass = encoder.BeginComputePass();
+        pass.SetPipeline(pipeline);
+        pass.Dispatch(x, y, z);
+        pass.EndPass();
+        encoder.Finish();
+    }
+
+    wgpu::ComputePipeline pipeline;
+};
+
+// Check that per-dimension dispatch size limits are enforced on direct dispatch calls.
+TEST_F(ComputeValidationTest, PerDimensionDispatchSizeLimits) {
+    constexpr uint32_t kMax = kMaxComputePerDimensionDispatchSize;
+    TestDispatch(1, 1, 1);
+    TestDispatch(kMax, kMax, kMax);
+    ASSERT_DEVICE_ERROR(TestDispatch(kMax + 1, 1, 1));
+    ASSERT_DEVICE_ERROR(TestDispatch(1, kMax + 1, 1));
+    ASSERT_DEVICE_ERROR(TestDispatch(1, 1, kMax + 1));
+    ASSERT_DEVICE_ERROR(TestDispatch(kMax + 1, kMax + 1, kMax + 1));
+}
