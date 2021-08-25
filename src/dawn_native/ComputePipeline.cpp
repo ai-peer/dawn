853c51325466217f530c99558086e9de101f52a9
Brandon Jones <brandon1.jones@intel.com>
2021-08-25 13:18:42 -0700

    Add Labels For Pipelines and ShaderModule for D3D12/Vk
    Adds labels for Pipelines and ShaderModule. Includes tests. Backend
    functionality is implemented for Pipelines, and completed to best effort
    for ShaderModule.
    
    Bug: dawn:840
    Change-Id: I55024a83f66d9fc2fc0e8b79e4b9a7ebc6f3cf1c

--- src/dawn_native/ComputePipeline.cpp
+++ src/dawn_native/ComputePipeline.cpp
@@ -51,6 +51,7 @@ namespace dawn_native {
                                              const ComputePipelineDescriptor* descriptor)
         : PipelineBase(device,
                        descriptor->layout,
+                       descriptor->label,
                        {{SingleShaderStage::Compute, descriptor->compute.module,
                          descriptor->compute.entryPoint}}) {
     }
