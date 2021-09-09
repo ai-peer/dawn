3e406b7a737a9612942c3686af21fb10ee65216d
Ken Rockot <rockot@google.com>
2021-09-09 15:33:10 -0700

    Introduce WriteBuffer command
    This command copies data from host memory into a GPU buffer. It's
    analogous to Queue::WriteBuffer, but executed in the context of a
    command buffer, sequenced with other encoded commands. This is useful
    for supporting a notion of a shared scratch buffer, with a single
    allocation whose contents may need to be overwritten with new data
    before each pass that uses it.
    
    Bug: dawn:809
    Change-Id: If58d49c52a41127e2980dd626fd687eb1c91fe28

--- src/dawn_native/d3d12/CommandBufferD3D12.cpp
+++ src/dawn_native/d3d12/CommandBufferD3D12.cpp
@@ -981,6 +981,38 @@ namespace dawn_native { namespace d3d12 {
                     break;
                 }
 
+                case Command::WriteBuffer: {
+                    WriteBufferCmd* write = mCommands.NextCommand<WriteBufferCmd>();
+                    const uint64_t offset = write->offset;
+                    const uint64_t size = write->size;
+                    if (size == 0) {
+                        continue;
+                    }
+
+                    Buffer* dstBuffer = ToBackend(write->buffer.Get());
+                    uint8_t* data = mCommands.NextData<uint8_t>(size);
+                    Device* device = ToBackend(GetDevice());
+                    DAWN_TRY(dstBuffer->ValidateCanUseOnQueueNow());
+
+                    UploadHandle uploadHandle;
+                    DAWN_TRY_ASSIGN(uploadHandle, device->GetDynamicUploader()->Allocate(
+                                                      size, device->GetPendingCommandSerial(),
+                                                      kCopyBufferToBufferOffsetAlignment));
+                    ASSERT(uploadHandle.mappedBuffer != nullptr);
+                    memcpy(uploadHandle.mappedBuffer, data, size);
+
+                    device->AddFutureSerial(device->GetPendingCommandSerial());
+                    DAWN_TRY(dstBuffer->EnsureDataInitializedAsDestination(commandContext, offset,
+                                                                           size));
+                    dstBuffer->TrackUsageAndTransitionNow(commandContext,
+                                                          wgpu::BufferUsage::CopyDst);
+                    commandList->CopyBufferRegion(
+                        dstBuffer->GetD3D12Resource(), offset,
+                        ToBackend(uploadHandle.stagingBuffer)->GetResource(),
+                        uploadHandle.startOffset, size);
+                    break;
+                }
+
                 default:
                     UNREACHABLE();
             }
