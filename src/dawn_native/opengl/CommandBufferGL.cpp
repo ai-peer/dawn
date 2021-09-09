ebc2fe54db3b4f4e3d135decf8558272a7ebaed9
Ken Rockot <rockot@google.com>
2021-09-09 15:33:10 -0700

    Introduce WriteBuffer command
    This command copies data from host memory into a GPU buffer. It's
    analogous to Queue::WriteBuffer, but executed in the context of a
    command buffer, sequenced with other encoded commands. This is useful
    for supporting a notion of a shared scratch buffer, with a single
    allocation whose contents may need to be overwritten with new data
    before each pass that uses it.
    
    Bug: dawn:809
    Change-Id: If58d49c52a41127e2980dd626fd687eb1c91fe28

--- src/dawn_native/opengl/CommandBufferGL.cpp
+++ src/dawn_native/opengl/CommandBufferGL.cpp
@@ -846,6 +846,24 @@ namespace dawn_native { namespace opengl {
                     break;
                 }
 
+                case Command::WriteBuffer: {
+                    WriteBufferCmd* write = mCommands.NextCommand<WriteBufferCmd>();
+                    uint64_t offset = write->offset;
+                    uint64_t size = write->size;
+                    if (size == 0) {
+                        continue;
+                    }
+
+                    Buffer* dstBuffer = ToBackend(write->buffer.Get());
+                    uint8_t* data = mCommands.NextData<uint8_t>(size);
+                    dstBuffer->EnsureDataInitializedAsDestination(offset, size);
+                    DAWN_TRY(dstBuffer->ValidateCanUseOnQueueNow());
+
+                    gl.BindBuffer(GL_ARRAY_BUFFER, dstBuffer->GetHandle());
+                    gl.BufferSubData(GL_ARRAY_BUFFER, offset, size, data);
+                    break;
+                }
+
                 default:
                     UNREACHABLE();
             }
