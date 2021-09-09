1c3e5b42cc599adbe15eec47d86d6af557960721
Li Hao <hao.x.li@intel.com>
2021-09-09 10:35:09 +0800

    Update destinationOffset alignment to 256 in resolveQuerySet
    From macOS 11+, Metal requires the minimum buffer offset alignment to be
    256 bytes. We have updated the destinationOffset alignment of
    resolveQuerySet to 256 in SPEC. This PR is changing the implementation.
    
    Bug: dawn:940
    Change-Id: Ie3c69d6a90adb76038dbac95ae2158a38da583f3

--- src/dawn_native/CommandEncoder.cpp
+++ src/dawn_native/CommandEncoder.cpp
@@ -426,10 +426,11 @@ namespace dawn_native {
                     "set");
             }
 
-            // The destinationOffset must be a multiple of 8 bytes on D3D12 and Vulkan
-            if (destinationOffset % 8 != 0) {
+            // The destinationOffset must be a multiple of 256 bytes on Metal. See
+            // crbug.com/dawn/940
+            if (destinationOffset % 256 != 0) {
                 return DAWN_VALIDATION_ERROR(
-                    "The alignment offset into the destination buffer must be a multiple of 8 "
+                    "The alignment offset into the destination buffer must be a multiple of 256 "
                     "bytes");
             }
 
