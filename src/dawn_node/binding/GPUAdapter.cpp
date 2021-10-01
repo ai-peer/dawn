681e5228d7da7b4ab15e5cd54ed14d43c57fc8a5
François Beaufort <beaufort.francois@gmail.com>
2021-10-01 10:39:38 +0200

    WIP
    Change-Id: I070e32d7ae042f9b846df01f200b39f6741a0a14

--- src/dawn_node/binding/GPUAdapter.cpp
+++ src/dawn_node/binding/GPUAdapter.cpp
@@ -106,20 +106,19 @@ namespace wgpu { namespace binding {
         interop::Promise<interop::Interface<interop::GPUDevice>> promise(env);
 
         if (descriptor.has_value()) {
-            // See src/dawn_native/Extensions.cpp for feature <-> extension mappings.
             for (auto required : descriptor->requiredFeatures) {
                 switch (required) {
                     case interop::GPUFeatureName::kDepthClamping:
-                        desc.requiredExtensions.emplace_back("depth_clamping");
+                        desc.requiredFeatures.emplace_back("depth_clamping");
                         continue;
                     case interop::GPUFeatureName::kPipelineStatisticsQuery:
-                        desc.requiredExtensions.emplace_back("pipeline_statistics_query");
+                        desc.requiredFeatures.emplace_back("pipeline_statistics_query");
                         continue;
                     case interop::GPUFeatureName::kTextureCompressionBc:
-                        desc.requiredExtensions.emplace_back("texture_compression_bc");
+                        desc.requiredFeatures.emplace_back("texture_compression_bc");
                         continue;
                     case interop::GPUFeatureName::kTimestampQuery:
-                        desc.requiredExtensions.emplace_back("timestamp_query");
+                        desc.requiredFeatures.emplace_back("timestamp_query");
                         continue;
                     case interop::GPUFeatureName::kDepth24UnormStencil8:
                     case interop::GPUFeatureName::kDepth32FloatStencil8:
