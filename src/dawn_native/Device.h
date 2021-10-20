8bd0280afe10b2c32431d7337dafe00cc7ce8baf
Austin Eng <enga@chromium.org>
2021-10-20 14:29:19 -0700

    Implement GPU-based validation for dispatchIndirect
    Bug: dawn:1039
    Change-Id: I1b77244d33b178c8e4d4b7d72dc038ccb9d65c48

--- src/dawn_native/Device.h
+++ src/dawn_native/Device.h
@@ -163,6 +163,9 @@ namespace dawn_native {
 
         BindGroupLayoutBase* GetEmptyBindGroupLayout();
 
+        BufferBase* GetConstantsBuffer() const;
+        uint64_t GetMaxComputeWorkgroupsPerDimensionOffset() const;
+
         void UncacheComputePipeline(ComputePipelineBase* obj);
 
         ResultOrError<Ref<PipelineLayoutBase>> GetOrCreatePipelineLayout(
@@ -489,6 +492,7 @@ namespace dawn_native {
         std::unique_ptr<Caches> mCaches;
 
         Ref<BindGroupLayoutBase> mEmptyBindGroupLayout;
+        Ref<BufferBase> mConstantsBuffer;
 
         std::unique_ptr<DynamicUploader> mDynamicUploader;
         std::unique_ptr<AsyncTaskManager> mAsyncTaskManager;
