3723a9698ee90f44878b1355b8f306046abc0c88
Loko Kung <lokokung@google.com>
2021-10-26 13:12:17 -0700

    Adds destruction handling for Textures when device is destroyed.
    Bug: dawn:628
    Change-Id: Iaaa62507592e12a724673a0226783c0425b04f35

--- src/dawn_native/Device.cpp
+++ src/dawn_native/Device.cpp
@@ -268,7 +268,7 @@ namespace dawn_native {
         // TODO(dawn/628) Add types into the array as they are implemented.
 
         // clang-format off
-        static constexpr std::array<ObjectType, 10> kObjectTypeDependencyOrder = {
+        static constexpr std::array<ObjectType, 13> kObjectTypeDependencyOrder = {
             ObjectType::RenderPipeline,
             ObjectType::ComputePipeline,
             ObjectType::PipelineLayout,
@@ -276,6 +276,9 @@ namespace dawn_native {
             ObjectType::BindGroup,
             ObjectType::BindGroupLayout,
             ObjectType::ShaderModule,
+            ObjectType::ExternalTexture,
+            ObjectType::TextureView,
+            ObjectType::Texture,
             ObjectType::QuerySet,
             ObjectType::Sampler,
             ObjectType::Buffer,
