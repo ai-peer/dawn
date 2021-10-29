02469c43b24656f584565855527f665ad6e8f8f7
Loko Kung <lokokung@google.com>
2021-10-29 18:37:55 +0000

    Revert "Adds destroy handling for simple objects without new backend changes yet."
    This reverts commit 9901c710d1f834fd3e98356f0842553e1ac4afb2.
    
    Reason for revert: Breaking dawn roll due to include in ShaderMock
    
    Original change's description:
    > Adds destroy handling for simple objects without new backend changes yet.
    >
    > Simple objects are defined here as objects that do not already have a destroy or destroy-like API available. They include:
    > - BindGroups
    > - ComputePipelines
    > - PipelineLayouts
    > - RenderPipelines
    > - Samplers
    > - ShaderModules
    > - SwapChains
    >
    > Changes include:
    > - Adds necessary constructors for testing and caching
    > - Adding mock objects, mock constructors, and tests
    >
    > Bug: dawn:628
    > Change-Id: I26a5e37bc5580b9064db299a75ef1243521b266a
    > Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/65864
    > Commit-Queue: Loko Kung <lokokung@google.com>
    > Reviewed-by: Austin Eng <enga@chromium.org>
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: dawn:628
    Change-Id: Ic727106f3dc701dc25418adf9824fbc5397aa656

--- src/dawn_native/Device.cpp
+++ src/dawn_native/Device.cpp
@@ -37,7 +37,6 @@
 #include "dawn_native/QuerySet.h"
 #include "dawn_native/Queue.h"
 #include "dawn_native/RenderBundleEncoder.h"
-#include "dawn_native/RenderPipeline.h"
 #include "dawn_native/Sampler.h"
 #include "dawn_native/Surface.h"
 #include "dawn_native/SwapChain.h"
@@ -275,19 +274,9 @@ namespace dawn_native {
         // that this only considers the immediate frontend dependencies, while backend objects could
         // add complications and extra dependencies.
         // TODO(dawn/628) Add types into the array as they are implemented.
-
-        // clang-format off
-        static constexpr std::array<ObjectType, 8> kObjectTypeDependencyOrder = {
-            ObjectType::RenderPipeline,
-            ObjectType::ComputePipeline,
-            ObjectType::PipelineLayout,
-            ObjectType::SwapChain,
-            ObjectType::BindGroup,
+        static constexpr std::array<ObjectType, 1> kObjectTypeDependencyOrder = {
             ObjectType::BindGroupLayout,
-            ObjectType::ShaderModule,
-            ObjectType::Sampler,
         };
-        // clang-format on
 
         // We first move all objects out from the tracking list into a separate list so that we can
         // avoid locking the same mutex twice. We can then iterate across the separate list to call
@@ -734,7 +723,7 @@ namespace dawn_native {
 
     ResultOrError<Ref<PipelineLayoutBase>> DeviceBase::GetOrCreatePipelineLayout(
         const PipelineLayoutDescriptor* descriptor) {
-        PipelineLayoutBase blueprint(this, descriptor, ApiObjectBase::kUntrackedByDevice);
+        PipelineLayoutBase blueprint(this, descriptor);
 
         const size_t blueprintHash = blueprint.ComputeContentHash();
         blueprint.SetContentHash(blueprintHash);
@@ -767,7 +756,7 @@ namespace dawn_native {
 
     ResultOrError<Ref<SamplerBase>> DeviceBase::GetOrCreateSampler(
         const SamplerDescriptor* descriptor) {
-        SamplerBase blueprint(this, descriptor, ApiObjectBase::kUntrackedByDevice);
+        SamplerBase blueprint(this, descriptor);
 
         const size_t blueprintHash = blueprint.ComputeContentHash();
         blueprint.SetContentHash(blueprintHash);
@@ -798,7 +787,7 @@ namespace dawn_native {
         OwnedCompilationMessages* compilationMessages) {
         ASSERT(parseResult != nullptr);
 
-        ShaderModuleBase blueprint(this, descriptor, ApiObjectBase::kUntrackedByDevice);
+        ShaderModuleBase blueprint(this, descriptor);
 
         const size_t blueprintHash = blueprint.ComputeContentHash();
         blueprint.SetContentHash(blueprintHash);
