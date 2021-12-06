6daa4393abfc18bc4581f4f9e5f4bbc528ead6db
Loko Kung <lokokung@google.com>
2021-12-06 18:06:29 -0500

    Fixes bad mem-read in Vulkan's ~DescriptorSetAllocator.
    Bug was a result of an external BGL reference that lingered after device was destroyed leading to a bad read on the device's FencedDeleter when the BGL reference was finally released. Fix just makes sure that the previous code path runs during the device destruction instead of afterwards.
    
    Bug: chromium:1276928
    Change-Id: Ibca5e3c313fc0c0980ecaaa9ad2c871e204ac153

--- src/dawn_native/vulkan/DescriptorSetAllocator.cpp
+++ src/dawn_native/vulkan/DescriptorSetAllocator.cpp
@@ -63,13 +63,7 @@ namespace dawn_native { namespace vulkan {
     }
 
     DescriptorSetAllocator::~DescriptorSetAllocator() {
-        for (auto& pool : mDescriptorPools) {
-            ASSERT(pool.freeSetIndices.size() == mMaxSets);
-            if (pool.vkPool != VK_NULL_HANDLE) {
-                Device* device = ToBackend(mLayout->GetDevice());
-                device->GetFencedDeleter()->DeleteWhenUnused(pool.vkPool);
-            }
-        }
+        ASSERT(mDescriptorPools.empty());
     }
 
     ResultOrError<DescriptorSetAllocation> DescriptorSetAllocator::Allocate() {
@@ -113,6 +107,17 @@ namespace dawn_native { namespace vulkan {
 
         // Clear the content of allocation so that use after frees are more visible.
         *allocationInfo = {};
+    }
+
+    void DescriptorSetAllocator::Destroy() {
+        for (auto& pool : mDescriptorPools) {
+            ASSERT(pool.freeSetIndices.size() == mMaxSets);
+            if (pool.vkPool != VK_NULL_HANDLE) {
+                Device* device = ToBackend(mLayout->GetDevice());
+                device->GetFencedDeleter()->DeleteWhenUnused(pool.vkPool);
+            }
+        }
+        mDescriptorPools.clear();
     }
 
     void DescriptorSetAllocator::FinishDeallocation(ExecutionSerial completedSerial) {
