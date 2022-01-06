86d91bbbf28e97b9662e8556bfc370d121219481
Austin Eng <enga@chromium.org>
2022-01-06 13:03:00 -0800

    Implement upstream RequestAdapter
    Bug: dawn:160
    Change-Id: Ifbce6f71fdf43a749c332bd691b63119929e0128

--- src/dawn_native/Instance.cpp
+++ src/dawn_native/Instance.cpp
@@ -15,11 +15,17 @@
 #include "dawn_native/Instance.h"
 
 #include "common/Assert.h"
+#include "common/GPUInfo.h"
 #include "common/Log.h"
 #include "dawn_native/ErrorData.h"
 #include "dawn_native/Surface.h"
 #include "dawn_native/ValidationUtils_autogen.h"
 #include "dawn_platform/DawnPlatform.h"
+
+// For SwiftShader fallback
+#if defined(DAWN_ENABLE_BACKEND_VULKAN)
+#    include "dawn_native/VulkanBackend.h"
+#endif  // defined(DAWN_ENABLE_BACKEND_VULKAN)
 
 #if defined(DAWN_USE_X11)
 #    include "dawn_native/XlibXcbFunctions.h"
@@ -102,7 +108,89 @@ namespace dawn_native {
     void InstanceBase::APIRequestAdapter(const RequestAdapterOptions* options,
                                          WGPURequestAdapterCallback callback,
                                          void* userdata) {
-        callback(WGPURequestAdapterStatus_Error, nullptr, "Not implemented", userdata);
+        auto result = RequestAdapterInternal(options);
+        if (result.IsError()) {
+            std::string msg = result.AcquireError()->GetFormattedMessage();
+            callback(WGPURequestAdapterStatus_Error, nullptr, msg.c_str(), userdata);
+        } else {
+            Ref<AdapterBase> adapter = result.AcquireSuccess();
+            callback(WGPURequestAdapterStatus_Success, ToAPI(adapter.Detach()), nullptr, userdata);
+        }
+    }
+
+    ResultOrError<Ref<AdapterBase>> InstanceBase::RequestAdapterInternal(
+        const RequestAdapterOptions* options) {
+        ASSERT(options != nullptr);
+        if (options->forceFallbackAdapter) {
+#if defined(DAWN_ENABLE_BACKEND_VULKAN)
+            dawn_native::vulkan::AdapterDiscoveryOptions vulkanOptions;
+            vulkanOptions.forceSwiftShader = true;
+            DAWN_TRY(DiscoverAdaptersInternal(&vulkanOptions));
+#else
+            return {nullptr};
+#endif  // defined(DAWN_ENABLE_BACKEND_VULKAN)
+        } else {
+            DiscoverDefaultAdapters();
+        }
+
+        wgpu::AdapterType preferredType;
+        switch (options->powerPreference) {
+            case wgpu::PowerPreference::LowPower:
+                preferredType = wgpu::AdapterType::IntegratedGPU;
+                break;
+            case wgpu::PowerPreference::Undefined:
+            case wgpu::PowerPreference::HighPerformance:
+                preferredType = wgpu::AdapterType::DiscreteGPU;
+                break;
+        }
+
+        std::optional<size_t> discreteGPUAdapterIndex;
+        std::optional<size_t> integratedGPUAdapterIndex;
+        std::optional<size_t> cpuAdapterIndex;
+        std::optional<size_t> unknownAdapterIndex;
+
+        for (size_t i = 0; i < mAdapters.size(); ++i) {
+            if (options->forceFallbackAdapter) {
+                const auto& pciInfo = mAdapters[i]->GetPCIInfo();
+                if (!gpu_info::IsSwiftshader(pciInfo.vendorId, pciInfo.deviceId)) {
+                    continue;
+                }
+                return mAdapters[i];
+            }
+            wgpu::AdapterType adapterType = mAdapters[i]->GetAdapterType();
+            if (adapterType == preferredType) {
+                return mAdapters[i];
+            }
+            switch (adapterType) {
+                case wgpu::AdapterType::DiscreteGPU:
+                    discreteGPUAdapterIndex = i;
+                    break;
+                case wgpu::AdapterType::IntegratedGPU:
+                    integratedGPUAdapterIndex = i;
+                    break;
+                case wgpu::AdapterType::CPU:
+                    cpuAdapterIndex = i;
+                    break;
+                case wgpu::AdapterType::Unknown:
+                    unknownAdapterIndex = i;
+                    break;
+            }
+        }
+
+        // For now, we always prefer the discrete GPU
+        if (discreteGPUAdapterIndex) {
+            return mAdapters[*discreteGPUAdapterIndex];
+        }
+        if (integratedGPUAdapterIndex) {
+            return mAdapters[*integratedGPUAdapterIndex];
+        }
+        if (cpuAdapterIndex) {
+            return mAdapters[*cpuAdapterIndex];
+        }
+        if (unknownAdapterIndex) {
+            return mAdapters[*unknownAdapterIndex];
+        }
+        return {nullptr};
     }
 
     void InstanceBase::DiscoverDefaultAdapters() {
@@ -116,10 +204,9 @@ namespace dawn_native {
 
         // Query and merge all default adapters for all backends
         for (std::unique_ptr<BackendConnection>& backend : mBackends) {
-            std::vector<std::unique_ptr<AdapterBase>> backendAdapters =
-                backend->DiscoverDefaultAdapters();
+            std::vector<Ref<AdapterBase>> backendAdapters = backend->DiscoverDefaultAdapters();
 
-            for (std::unique_ptr<AdapterBase>& adapter : backendAdapters) {
+            for (Ref<AdapterBase>& adapter : backendAdapters) {
                 ASSERT(adapter->GetBackendType() == backend->GetType());
                 ASSERT(adapter->GetInstance() == this);
                 mAdapters.push_back(std::move(adapter));
@@ -146,7 +233,7 @@ namespace dawn_native {
         return mFeaturesInfo.GetFeatureInfo(feature);
     }
 
-    const std::vector<std::unique_ptr<AdapterBase>>& InstanceBase::GetAdapters() const {
+    const std::vector<Ref<AdapterBase>>& InstanceBase::GetAdapters() const {
         return mAdapters;
     }
 
@@ -226,10 +313,10 @@ namespace dawn_native {
             }
             foundBackend = true;
 
-            std::vector<std::unique_ptr<AdapterBase>> newAdapters;
+            std::vector<Ref<AdapterBase>> newAdapters;
             DAWN_TRY_ASSIGN(newAdapters, backend->DiscoverAdapters(options));
 
-            for (std::unique_ptr<AdapterBase>& adapter : newAdapters) {
+            for (Ref<AdapterBase>& adapter : newAdapters) {
                 ASSERT(adapter->GetBackendType() == backend->GetType());
                 ASSERT(adapter->GetInstance() == this);
                 mAdapters.push_back(std::move(adapter));
@@ -246,7 +333,6 @@ namespace dawn_native {
 
             ASSERT(error != nullptr);
             dawn::InfoLog() << error->GetFormattedMessage();
-
             return true;
         }
         return false;
