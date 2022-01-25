a25131163faf760a1e389f34ecc1cb531710fba4
Brandon Jones <brandon1.jones@intel.com>
2022-01-25 13:26:27 -0800

    Do Not Review: Replace Single Plane External Texture Transform
    Bug: dawn:1082
    Change-Id: Ib5c042e5639b1a8efe2954680abc346c8c6c76d7

--- src/dawn_native/metal/ShaderModuleMTL.mm
+++ src/dawn_native/metal/ShaderModuleMTL.mm
@@ -132,6 +132,31 @@ namespace dawn::native::metal {
                                                          std::move(accessControls),
                                                          /* mayCollide */ true);
 
+        tint::transform::MultiplanarExternalTexture::BindingsMap newBindingsMap;
+
+        for (BindGroupIndex i : IterateBitSet(layout->GetBindGroupLayoutsMask())) {
+            BindGroupLayoutBase* bgl = layout->GetBindGroupLayout(i);
+
+            std::map<BindingNumber, dawn_native::ExternalTextureBindingExpansion> expansions =
+                bgl->GetExternalTextureBindingExpansions();
+            std::map<BindingNumber, dawn_native::ExternalTextureBindingExpansion>::iterator it =
+                expansions.begin();
+
+            while (it != expansions.end()) {
+                newBindingsMap[{static_cast<uint32_t>(i),
+                                static_cast<uint32_t>(bgl->GetBindingIndex(it->second.plane0))}] = {
+                    {static_cast<uint32_t>(i),
+                     static_cast<uint32_t>(bgl->GetBindingIndex(it->second.plane1))},
+                    {static_cast<uint32_t>(i),
+                     static_cast<uint32_t>(bgl->GetBindingIndex(it->second.params))}};
+                it++;
+            }
+        }
+
+        transformManager.Add<tint::transform::MultiplanarExternalTexture>();
+        transformInputs.Add<tint::transform::MultiplanarExternalTexture::NewBindingPoints>(
+            newBindingsMap);
+
         tint::Program program;
         tint::transform::DataMap transformOutputs;
         {
