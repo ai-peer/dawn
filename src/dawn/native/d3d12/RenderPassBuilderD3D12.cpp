174435834c145ee8a2d214d8a3f1f9323892ac70
Corentin Wallez <cwallez@chromium.org>
2022-01-31 12:11:42 +0100

    Implement the stencil8 format.
    Rolled in changes made by cwallez@ as part of
    https://dawn-review.googlesource.com/c/dawn/+/75983, which was
    originally based on this change.
    
    Bug: dawn:666
    
    Change-Id: I5d6ad592294ee8302f3b18f7f31bbfd982297251

--- src/dawn/native/d3d12/RenderPassBuilderD3D12.cpp
+++ src/dawn/native/d3d12/RenderPassBuilderD3D12.cpp
@@ -131,8 +131,8 @@ namespace dawn::native::d3d12 {
         return mColorAttachmentCount;
     }
 
-    bool RenderPassBuilder::HasDepth() const {
-        return mHasDepth;
+    bool RenderPassBuilder::HasDepthOrStencil() const {
+        return mHasDepthOrStencil;
     }
 
     ityp::span<ColorAttachmentIndex, const D3D12_RENDER_PASS_RENDER_TARGET_DESC>
@@ -199,7 +199,7 @@ namespace dawn::native::d3d12 {
                                            wgpu::StoreOp storeOp,
                                            float clearDepth,
                                            DXGI_FORMAT format) {
-        mHasDepth = true;
+        mHasDepthOrStencil = true;
         mRenderPassDepthStencilDesc.DepthBeginningAccess.Type = D3D12BeginningAccessType(loadOp);
         if (loadOp == wgpu::LoadOp::Clear) {
             mRenderPassDepthStencilDesc.DepthBeginningAccess.Clear.ClearValue.DepthStencil.Depth =
@@ -213,6 +213,7 @@ namespace dawn::native::d3d12 {
                                              wgpu::StoreOp storeOp,
                                              uint8_t clearStencil,
                                              DXGI_FORMAT format) {
+        mHasDepthOrStencil = true;
         mRenderPassDepthStencilDesc.StencilBeginningAccess.Type = D3D12BeginningAccessType(loadOp);
         if (loadOp == wgpu::LoadOp::Clear) {
             mRenderPassDepthStencilDesc.StencilBeginningAccess.Clear.ClearValue.DepthStencil
