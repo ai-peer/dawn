061d62b46e68a0b66dd09f9224b5fd7fc75436ba
Ben Clayton <bclayton@google.com>
2022-02-02 18:07:06 +0000

    WIP: Shuffle code
    Change-Id: I801c7e80468a8ee309155f732f06b65086724dcc

--- src/fuzzers/dawn/DawnWireServerAndFrontendFuzzer.cpp
+++ src/fuzzers/dawn/DawnWireServerAndFrontendFuzzer.cpp
@@ -14,8 +14,8 @@
 
 #include "DawnWireServerFuzzer.h"
 
-#include "common/Assert.h"
-#include "dawn_native/DawnNative.h"
+#include "dawn/common/Assert.h"
+#include "dawn/native/DawnNative.h"
 #include "testing/libfuzzer/libfuzzer_exports.h"
 
 extern "C" int LLVMFuzzerInitialize(int* argc, char*** argv) {
