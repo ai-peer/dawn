be725b8eaa34b885483e534a4ac7a7b141678e8c
Ben Clayton <bclayton@google.com>
2022-02-02 18:07:06 +0000

    WIP: Shuffle code
    Change-Id: I801c7e80468a8ee309155f732f06b65086724dcc

--- src/dawn/utils/WireHelper.cpp
+++ src/dawn/utils/WireHelper.cpp
@@ -18,10 +18,10 @@
 #include "dawn/common/Log.h"
 #include "dawn/common/SystemUtils.h"
 #include "dawn/dawn_proc.h"
+#include "dawn/native/DawnNative.h"
 #include "dawn/utils/TerribleCommandBuffer.h"
-#include "dawn_native/DawnNative.h"
-#include "dawn_wire/WireClient.h"
-#include "dawn_wire/WireServer.h"
+#include "dawn/wire/WireClient.h"
+#include "dawn/wire/WireServer.h"
 
 #include <algorithm>
 #include <cstring>
