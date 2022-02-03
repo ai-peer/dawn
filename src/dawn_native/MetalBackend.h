f3e2cc97233fa78bf8cdd48ca8bacd53c45c78cb
Ben Clayton <bclayton@google.com>
2022-02-03 14:04:29 +0000

    tint->dawn: Move src/dawn_native -> src/dawn/native
    Bug: dawn:1275
    Change-Id: Ic60a00107a015bc677ff929c492f1085ffc38482

---
+++ src/dawn_native/MetalBackend.h
@@ -0,0 +1,6 @@
+// TODO(crbug.com/dawn/1275) - remove this file
+//
+// ../../gpu/command_buffer/service/shared_image_backing_factory_iosurface.mm includes this file
+// #include <dawn_native/MetalBackend.h>
+
+#include "../dawn/native/MetalBackend.h"
