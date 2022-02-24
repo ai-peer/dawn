5e238159f002d53b92907f7660fcae79652427c4
Loko Kung <lokokung@google.com>
2022-02-24 13:45:31 -0800

    [WIP] Bunch of pipeline cache changes. Break up as needed.
    Change-Id: Ia5c6abfce44186af62c3b1b39af970abb7c38ebe

--- src/dawn/native/Device.cpp
+++ src/dawn/native/Device.cpp
@@ -35,7 +35,7 @@
 #include "dawn/native/Instance.h"
 #include "dawn/native/InternalPipelineStore.h"
 #include "dawn/native/ObjectType_autogen.h"
-#include "dawn/native/PersistentCache.h"
+#include "dawn/native/PipelineCache.h"
 #include "dawn/native/QuerySet.h"
 #include "dawn/native/Queue.h"
 #include "dawn/native/RenderBundleEncoder.h"
@@ -252,7 +252,6 @@ namespace dawn::native {
         mCallbackTaskManager = std::make_unique<CallbackTaskManager>();
         mDeprecationWarnings = std::make_unique<DeprecationWarnings>();
         mInternalPipelineStore = std::make_unique<InternalPipelineStore>(this);
-        mPersistentCache = std::make_unique<PersistentCache>(this);
 
         ASSERT(GetPlatform() != nullptr);
         mWorkerTaskPool = GetPlatform()->CreateWorkerTaskPool();
@@ -408,7 +407,6 @@ namespace dawn::native {
         mDynamicUploader = nullptr;
         mCallbackTaskManager = nullptr;
         mAsyncTaskManager = nullptr;
-        mPersistentCache = nullptr;
         mEmptyBindGroupLayout = nullptr;
         mInternalPipelineStore = nullptr;
         mExternalTextureDummyView = nullptr;
@@ -979,6 +977,10 @@ namespace dawn::native {
         ASSERT(removedCount == 1);
     }
 
+    Ref<PipelineCacheBase> DeviceBase::GetOrCreatePipelineCache(PipelineBase* pipeline) {
+        return GetOrCreatePipelineCacheImpl(pipeline);
+    }
+
     // Object creation API methods
 
     BindGroupBase* DeviceBase::APICreateBindGroup(const BindGroupDescriptor* descriptor) {
@@ -1421,6 +1423,11 @@ namespace dawn::native {
         }
 
         return {};
+    }
+
+    // Overwritten on the backends to return pipeline caches if supported.
+    Ref<PipelineCacheBase> DeviceBase::GetOrCreatePipelineCacheImpl(PipelineBase* pipeline) {
+        return nullptr;
     }
 
     // This function is overwritten with the async version on the backends that supports
