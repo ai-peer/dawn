eeb99882349f6a4f4c4fb8a8b526ea43a7a6543b
Loko Kung <lokokung@google.com>
2022-02-24 13:45:31 -0800

    [WIP] Bunch of pipeline cache changes. Break up as needed.
    Change-Id: Ia5c6abfce44186af62c3b1b39af970abb7c38ebe

--- src/dawn/native/vulkan/DeviceVk.h
+++ src/dawn/native/vulkan/DeviceVk.h
@@ -34,8 +34,6 @@
 
 namespace dawn::native::vulkan {
 
-    class Adapter;
-    class BindGroupLayout;
     class BufferUploader;
     class FencedDeleter;
     class RenderPassCache;
@@ -141,6 +139,7 @@ namespace dawn::native::vulkan {
             const ComputePipelineDescriptor* descriptor) override;
         Ref<RenderPipelineBase> CreateUninitializedRenderPipelineImpl(
             const RenderPipelineDescriptor* descriptor) override;
+        Ref<PipelineCacheBase> GetOrCreatePipelineCacheImpl(PipelineBase* pipeline) override;
         void InitializeComputePipelineAsyncImpl(Ref<ComputePipelineBase> computePipeline,
                                                 WGPUCreateComputePipelineAsyncCallback callback,
                                                 void* userdata) override;
