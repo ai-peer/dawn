72bc6e2ade3750091915d0229fcbcc996211f53b
Kai Ninomiya <kainino@chromium.org>
2022-03-15 15:29:16 -0700

    Add WebGPU CTS dep and build files
    Bug: 1306640
    Change-Id: Id0be5c155ec7c79252e724cd64e54d1ea9f3dbda

---
+++ third_party/webgpu-cts/scripts/dir_paths.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# Copyright 2022 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+
+webgpu_cts_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+dawn_third_party_dir = os.path.dirname(webgpu_cts_dir)
+chromium_third_party_dir = os.path.dirname(
+    os.path.dirname(dawn_third_party_dir))
+node_dir = os.path.join(chromium_third_party_dir, 'node')
