3e3ec9d02ba0ade8fcee32d371790b015e753847
Ben Clayton <bclayton@google.com>
2022-03-16 10:48:15 +0000

    DO NOT REVIEW - Experimenting with CQ
    Change-Id: I9820804b673b459ecba2ea46da66d6614380d53e

--- src/dawn/common/RefCounted.h
+++ src/dawn/common/RefCounted.h
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// A no-op change to force all tests to run
+
 #ifndef COMMON_REFCOUNTED_H_
 #define COMMON_REFCOUNTED_H_
 
