e83bf0ace67222606ec4a4468e9ec7e645311392
Ben Clayton <bclayton@google.com>
2022-03-17 21:33:36 +0000

    WIP: CTS expectation tooling
    Change-Id: I7c6b8b790f0055974edb05e585bad7b2d206900e

---
+++ tools/src/cts/result/status.go
@@ -0,0 +1,13 @@
+package result
+
+type Status string
+
+const (
+	Unknown = Status("Unknown")
+	Pass    = Status("Pass")
+	Failure = Status("Failure")
+	Crash   = Status("Crash")
+	Abort   = Status("Abort")
+	Skip    = Status("Skip")
+	Flaky   = Status("Flaky")
+)
