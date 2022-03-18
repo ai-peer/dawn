b644fef58ddbd1351230c82f49acb3d167371c51
Alexander Vestin <alex.vestin@gmail.com>
2022-03-18 20:45:22 +0100

    Initial Android surface
    Bug: dawn:286
    
    Change-Id: I50b45706f031254ac5beba5c07c3c4c3d7f9ea12

--- src/dawn/native/vulkan/NativeSwapChainImplVk.cpp
+++ src/dawn/native/vulkan/NativeSwapChainImplVk.cpp
@@ -11,19 +11,13 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 #include "dawn/native/vulkan/NativeSwapChainImplVk.h"
-
 #include "dawn/native/vulkan/DeviceVk.h"
 #include "dawn/native/vulkan/FencedDeleter.h"
 #include "dawn/native/vulkan/TextureVk.h"
-
 #include <limits>
-
 namespace dawn::native::vulkan {
-
     namespace {
-
         bool chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes,
                                    bool turnOffVsync,
                                    VkPresentModeKHR* presentMode) {
@@ -36,11 +30,9 @@ namespace dawn::native::vulkan {
                 }
                 return false;
             }
-
             *presentMode = VK_PRESENT_MODE_FIFO_KHR;
             return true;
         }
-
         bool ChooseSurfaceConfig(const VulkanSurfaceInfo& info,
                                  NativeSwapChainImpl::ChosenConfig* config,
                                  bool turnOffVsync) {
@@ -59,18 +51,15 @@ namespace dawn::native::vulkan {
             config->preTransform = info.capabilities.currentTransform;
             config->presentMode = presentMode;
             config->compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
-
             return true;
         }
     }  // anonymous namespace
-
     NativeSwapChainImpl::NativeSwapChainImpl(Device* device, VkSurfaceKHR surface)
         : mSurface(surface), mDevice(device) {
         // Call this immediately, so that BackendBinding::GetPreferredSwapChainTextureFormat
         // will return a correct result before a SwapChain is created.
         UpdateSurfaceConfig();
     }
-
     NativeSwapChainImpl::~NativeSwapChainImpl() {
         if (mSwapChain != VK_NULL_HANDLE) {
             mDevice->GetFencedDeleter()->DeleteWhenUnused(mSwapChain);
@@ -81,36 +70,29 @@ namespace dawn::native::vulkan {
             mSurface = VK_NULL_HANDLE;
         }
     }
-
     void NativeSwapChainImpl::UpdateSurfaceConfig() {
         if (mDevice->ConsumedError(GatherSurfaceInfo(*ToBackend(mDevice->GetAdapter()), mSurface),
                                    &mInfo)) {
             ASSERT(false);
         }
-
         if (!ChooseSurfaceConfig(mInfo, &mConfig, mDevice->IsToggleEnabled(Toggle::TurnOffVsync))) {
             ASSERT(false);
         }
     }
-
     void NativeSwapChainImpl::Init(DawnWSIContextVulkan* /*context*/) {
         UpdateSurfaceConfig();
     }
-
     DawnSwapChainError NativeSwapChainImpl::Configure(WGPUTextureFormat format,
                                                       WGPUTextureUsage usage,
                                                       uint32_t width,
                                                       uint32_t height) {
         UpdateSurfaceConfig();
-
         ASSERT(mInfo.capabilities.minImageExtent.width <= width);
         ASSERT(mInfo.capabilities.maxImageExtent.width >= width);
         ASSERT(mInfo.capabilities.minImageExtent.height <= height);
         ASSERT(mInfo.capabilities.maxImageExtent.height >= height);
-
         ASSERT(format == static_cast<WGPUTextureFormat>(GetPreferredFormat()));
         // TODO(crbug.com/dawn/269): need to check usage works too
-
         // Create the swapchain with the configuration we chose
         VkSwapchainKHR oldSwapchain = mSwapChain;
         VkSwapchainCreateInfoKHR createInfo;
@@ -134,12 +116,10 @@ namespace dawn::native::vulkan {
         createInfo.presentMode = mConfig.presentMode;
         createInfo.clipped = false;
         createInfo.oldSwapchain = oldSwapchain;
-
         if (mDevice->fn.CreateSwapchainKHR(mDevice->GetVkDevice(), &createInfo, nullptr,
                                            &*mSwapChain) != VK_SUCCESS) {
             ASSERT(false);
         }
-
         // Gather the swapchain's images. Implementations are allowed to return more images than the
         // number we asked for.
         uint32_t count = 0;
@@ -147,21 +127,17 @@ namespace dawn::native::vulkan {
                                               nullptr) != VK_SUCCESS) {
             ASSERT(false);
         }
-
         ASSERT(count >= mConfig.minImageCount);
         mSwapChainImages.resize(count);
         if (mDevice->fn.GetSwapchainImagesKHR(mDevice->GetVkDevice(), mSwapChain, &count,
                                               AsVkArray(mSwapChainImages.data())) != VK_SUCCESS) {
             ASSERT(false);
         }
-
         if (oldSwapchain != VK_NULL_HANDLE) {
             mDevice->GetFencedDeleter()->DeleteWhenUnused(oldSwapchain);
         }
-
         return DAWN_SWAP_CHAIN_NO_ERROR;
     }
-
     DawnSwapChainError NativeSwapChainImpl::GetNextTexture(DawnSwapChainNextTexture* nextTexture) {
         // Transiently create a semaphore that will be signaled when the presentation engine is done
         // with the swapchain image. Further operations on the image will wait for this semaphore.
@@ -176,27 +152,22 @@ namespace dawn::native::vulkan {
                 ASSERT(false);
             }
         }
-
         if (mDevice->fn.AcquireNextImageKHR(mDevice->GetVkDevice(), mSwapChain,
                                             std::numeric_limits<uint64_t>::max(), semaphore,
                                             VkFence{}, &mLastImageIndex) != VK_SUCCESS) {
             ASSERT(false);
         }
-
         nextTexture->texture.u64 =
 #if defined(DAWN_PLATFORM_64_BIT)
             reinterpret_cast<uint64_t>
 #endif
             (*mSwapChainImages[mLastImageIndex]);
         mDevice->GetPendingRecordingContext()->waitSemaphores.push_back(semaphore);
-
         return DAWN_SWAP_CHAIN_NO_ERROR;
     }
-
     DawnSwapChainError NativeSwapChainImpl::Present() {
         // This assumes that the image has already been transitioned to the PRESENT layout and
         // writes were made available to the stage.
-
         // Assuming that the present queue is the same as the graphics queue, the proper
         // synchronization has already been done on the queue so we don't need to wait on any
         // semaphores.
@@ -209,17 +180,13 @@ namespace dawn::native::vulkan {
         presentInfo.pSwapchains = &*mSwapChain;
         presentInfo.pImageIndices = &mLastImageIndex;
         presentInfo.pResults = nullptr;
-
         VkQueue queue = mDevice->GetQueue();
         if (mDevice->fn.QueuePresentKHR(queue, &presentInfo) != VK_SUCCESS) {
             ASSERT(false);
         }
-
         return DAWN_SWAP_CHAIN_NO_ERROR;
     }
-
     wgpu::TextureFormat NativeSwapChainImpl::GetPreferredFormat() const {
         return mConfig.format;
     }
-
 }  // namespace dawn::native::vulkan
