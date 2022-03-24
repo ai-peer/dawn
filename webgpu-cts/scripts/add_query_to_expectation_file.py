842d9853d6570d22dd42a5b9008b075f47fe6308
Brian Sheedy <bsheedy@google.com>
2022-03-24 13:40:57 -0700

    Format
    Change-Id: I0b7553d42a7a51c556f6a287516250e93ef05531

--- webgpu-cts/scripts/add_query_to_expectation_file.py
+++ webgpu-cts/scripts/add_query_to_expectation_file.py
@@ -13,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Script for easily adding expectations to expectations.txt
 
 Converts one or more WebGPU CTS queries into one or more individual expectations
@@ -30,15 +29,18 @@ import dir_paths
 
 LIST_SCRIPT_PATH = os.path.join(dir_paths.webgpu_cts_scripts_dir, 'list.py')
 TRANSPILE_DIR = os.path.join(dir_paths.dawn_dir, '.node_transpile_work_dir')
-EXPECTATION_FILE_PATH = os.path.join(
-        dir_paths.dawn_dir, 'webgpu-cts', 'expectations.txt')
+EXPECTATION_FILE_PATH = os.path.join(dir_paths.dawn_dir, 'webgpu-cts',
+                                     'expectations.txt')
 
 
 def expand_query(query):
     cmd = [
-        sys.executable, LIST_SCRIPT_PATH,
-        '--js-out-dir', TRANSPILE_DIR,
-        '--query', query,
+        sys.executable,
+        LIST_SCRIPT_PATH,
+        '--js-out-dir',
+        TRANSPILE_DIR,
+        '--query',
+        query,
     ]
     p = subprocess.run(cmd, stdout=subprocess.PIPE, check=True)
     return p.stdout.decode('utf-8').splitlines()
@@ -54,26 +56,31 @@ def generate_expectations(queries, tags, results, bug):
         if not test_names:
             logging.warning('Did not get any test names for query %s', q)
         for tn in test_names:
-            content += '{bug}{tags}{test}{results}\n'.format(
-                    bug=bug, tags=tags, test=tn, results=results)
+            content += '{bug}{tags}{test}{results}\n'.format(bug=bug,
+                                                             tags=tags,
+                                                             test=tn,
+                                                             results=results)
     with open(EXPECTATION_FILE_PATH, 'a') as outfile:
         outfile.write(content)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-            description=('Converts one or more WebGPU CTS queries into one or '
-                         'more individual expectations and appends them to the '
-                         'end of expectations.txt'))
-    parser.add_argument('-b', '--bug',
+        description=('Converts one or more WebGPU CTS queries into one or '
+                     'more individual expectations and appends them to the '
+                     'end of expectations.txt'))
+    parser.add_argument('-b',
+                        '--bug',
                         help='The bug link to associate with the expectations')
-    parser.add_argument('-t', '--tag',
+    parser.add_argument('-t',
+                        '--tag',
                         action='append',
                         default=[],
                         dest='tags',
                         help=('A tag to restrict the expectation to. Can be '
                               'specified multiple times.'))
-    parser.add_argument('-r', '--result',
+    parser.add_argument('-r',
+                        '--result',
                         action='append',
                         default=[],
                         dest='results',
@@ -81,7 +88,8 @@ if __name__ == '__main__':
                         help=('An expected result for the expectation. Can be '
                               'specified multiple times, although a single '
                               'result is the most common usage.'))
-    parser.add_argument('-q', '--query',
+    parser.add_argument('-q',
+                        '--query',
                         action='append',
                         default=[],
                         dest='queries',
