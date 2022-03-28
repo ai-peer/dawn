2bc3f98d2fbec93f9955d296508a39626340c0a6
Loko Kung <lokokung@google.com>
2022-03-28 13:36:45 -0700

    Add pipeline cache key generation for Vulkan.
    Change-Id: Ibe73183fbff15f7310eaaeae92fbd622be1ac096

---
+++ src/dawn/native/vulkan/CacheKeyVk.cpp
@@ -0,0 +1,49 @@
+// Copyright 2022 The Dawn Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "dawn/common/vulkan_platform.h"
+#include "dawn/native/CacheKey.h"
+
+namespace dawn::native {
+
+    template <>
+    void CacheKeySerializer<VkDescriptorSetLayoutBinding>::Serialize(
+        CacheKey* key,
+        const VkDescriptorSetLayoutBinding& t) {
+        key->Record(t.binding, t.descriptorType, t.descriptorCount, t.stageFlags);
+    }
+
+    template <>
+    void CacheKeySerializer<VkDescriptorSetLayoutCreateInfo>::Serialize(
+        CacheKey* key,
+        const VkDescriptorSetLayoutCreateInfo& t) {
+        key->Record(t.flags).RecordIterable(t.pBindings, t.bindingCount);
+    }
+
+    template <>
+    void CacheKeySerializer<VkPushConstantRange>::Serialize(CacheKey* key,
+                                                            const VkPushConstantRange& t) {
+        key->Record(t.stageFlags, t.offset, t.size);
+    }
+
+    template <>
+    void CacheKeySerializer<VkPipelineLayoutCreateInfo>::Serialize(
+        CacheKey* key,
+        const VkPipelineLayoutCreateInfo& t) {
+        // The set layouts are not serialized here because they are pointers to backend objects.
+        // They need to be cross-referenced with the frontend objects and serialized from there.
+        key->Record(t.flags).RecordIterable(t.pPushConstantRanges, t.pushConstantRangeCount);
+    }
+
+}  // namespace dawn::native
