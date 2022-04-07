88e18768e8ac7c6250e8f697bdbfd2531ab69a37
Antonio Maiorano <amaiorano@google.com>
2022-04-07 14:43:37 -0400

    tint: Make GLSL backend consistent with the others
    Remove transform::Glsl and replace with a Sanitize function. Cleans up
    the code, reduces allocations and copies, and makes it consistent with
    the other backends.
    
    Bug: tint:1495
    Change-Id: I8a44ffecc6b3d244517bceb374ed93063e96f218

--- include/tint/tint.h
+++ include/tint/tint.h
@@ -61,8 +61,7 @@
 #endif  // TINT_BUILD_HLSL_WRITER
 
 #if TINT_BUILD_GLSL_WRITER
-#include "src/tint/transform/glsl.h"
-#include "src/tint/writer/glsl/generator.h"
+#    include "src/tint/writer/glsl/generator.h"
 #endif  // TINT_BUILD_GLSL_WRITER
 
 #endif  // INCLUDE_TINT_TINT_H_
