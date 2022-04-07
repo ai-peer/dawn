4d370537bc01c1dfead7e6495c390553769b3397
dan sinclair <dsinclair@chromium.org>
2022-04-07 16:15:04 -0400

    Fixup whitespace/comments issues.
    This CL fixes a few tabs to be spaces which makes the whitespace/comments
    lint check pass. The check is set to enabled.
    
    Change-Id: I2a6c590aa3bfee888239adc89d1ce4c8c99f3e78

--- src/dawn/tests/unittests/validation/ResourceUsageTrackingTests.cpp
+++ src/dawn/tests/unittests/validation/ResourceUsageTrackingTests.cpp
@@ -1691,7 +1691,7 @@ namespace {
     // TODO (yunchao.he@intel.com):
     //
     //	* Add tests for multiple encoders upon the same resource simultaneously. This situation fits
-    //	some cases like VR, multi-threading, etc.
+    //  some cases like VR, multi-threading, etc.
     //
     //	* Add tests for bundle
 
