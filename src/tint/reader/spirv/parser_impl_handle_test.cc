f420ccd1e642db74baf577de1c71bd73e45cf88c
dan sinclair <dsinclair@chromium.org>
2022-04-06 22:21:29 -0400

    Condense namespaces in tint/reader/spirv.
    This PR condenses the namespaces in the tint/reader/spirv folder.
    
    Change-Id: I80cf1e7d3587020359d790381d147fde140b0ba5

--- src/tint/reader/spirv/parser_impl_handle_test.cc
+++ src/tint/reader/spirv/parser_impl_handle_test.cc
@@ -19,9 +19,7 @@
 #include "src/tint/reader/spirv/parser_impl_test_helper.h"
 #include "src/tint/reader/spirv/spirv_tools_helpers_test.h"
 
-namespace tint {
-namespace reader {
-namespace spirv {
+namespace tint::reader::spirv {
 namespace {
 
 using ::testing::Eq;
@@ -3950,6 +3948,4 @@ return;
 }
 
 }  // namespace
-}  // namespace spirv
-}  // namespace reader
-}  // namespace tint
+}  // namespace tint::reader::spirv
