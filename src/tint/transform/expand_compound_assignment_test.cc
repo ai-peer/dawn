de712b3f55d8bd1eecfd38069fc7e8cc669be14a
dan sinclair <dsinclair@chromium.org>
2022-04-06 22:21:15 -0400

    Condense namespaces in tint/transform.
    This PR condenses the namespaces in the tint/transform folder.
    
    Change-Id: Idf448870ccf90f892b9186f7aab7bb0ac9deda17

--- src/tint/transform/expand_compound_assignment_test.cc
+++ src/tint/transform/expand_compound_assignment_test.cc
@@ -18,8 +18,7 @@
 
 #include "src/tint/transform/test_helper.h"
 
-namespace tint {
-namespace transform {
+namespace tint::transform {
 namespace {
 
 using ExpandCompoundAssignmentTest = TransformTest;
@@ -453,5 +452,4 @@ fn main() {
 }
 
 }  // namespace
-}  // namespace transform
-}  // namespace tint
+}  // namespace tint::transform
