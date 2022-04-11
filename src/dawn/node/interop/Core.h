9e70722c1f4aea2a94738fc19af1675ca60f77a6
Ben Clayton <bclayton@google.com>
2022-04-11 17:08:52 +0100

    dawn/node: Fix GCC warning
    About comparisons of different datatypes
    
    Bug: dawn:1123
    Change-Id: Iefdb8195041b824f63344a0b367fd5bebbe3ca85

--- src/dawn/node/interop/Core.h
+++ src/dawn/node/interop/Core.h
@@ -119,7 +119,7 @@ namespace wgpu::interop {
         // stringified values append to the error message.
         // If Result is a success, then a success Result is returned.
         template <typename... VALUES>
-        Result Append(VALUES&&... values) {
+        Result Append(VALUES&& ... values) {
             if (*this) {
                 return *this;
             }
@@ -530,8 +530,8 @@ namespace wgpu::interop {
             }
 
             // Check for out of range and throw a type error.
-            constexpr T kMin = std::numeric_limits<T>::min();
-            constexpr T kMax = std::numeric_limits<T>::max();
+            constexpr double kMin = static_cast<double>(std::numeric_limits<T>::min());
+            constexpr double kMax = static_cast<double>(std::numeric_limits<T>::max());
             if (!(kMin <= doubleValue && doubleValue <= kMax)) {
                 return Error("Values are out of the range of that integer.");
             }
