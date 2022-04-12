44cdc7cc9d532fdf4fb41215b15401a68dd52baa
Loko Kung <lokokung@google.com>
2022-04-12 15:28:57 -0700

    Bunch of pipeline cache changes. Break up as needed.
    Change-Id: Ia5c6abfce44186af62c3b1b39af970abb7c38ebe

--- src/dawn/native/Adapter.h
+++ src/dawn/native/Adapter.h
@@ -22,6 +22,7 @@
 #include "dawn/native/Error.h"
 #include "dawn/native/Features.h"
 #include "dawn/native/Limits.h"
+#include "dawn/native/PersistentCache.h"
 #include "dawn/native/dawn_platform.h"
 
 #include <string>
