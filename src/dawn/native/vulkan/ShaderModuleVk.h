462f37c8d6216f100e3230e02285258e095206d9
dan sinclair <dsinclair@chromium.org>
2022-04-15 11:03:50 -0400

    Fixup build/include_order issues in src/dawn.
    This Cl moves the cpp includes to above the project includes fixing up
    the build/include_order issues and enabling the lint check.
    
    A couple includes are marked as NOLINT as the c header has to come after
    the project header due to setting defines.
    
    Bug: dawn:1339
    Change-Id: Ia47499c94fff99106397b83f6c5c7fe100c44a0e

--- src/dawn/native/vulkan/ShaderModuleVk.h
+++ src/dawn/native/vulkan/ShaderModuleVk.h
@@ -15,16 +15,16 @@
 #ifndef SRC_DAWN_NATIVE_VULKAN_SHADERMODULEVK_H_
 #define SRC_DAWN_NATIVE_VULKAN_SHADERMODULEVK_H_
 
+#include <memory>
+#include <mutex>
+#include <optional>  // NOLINT(build/include_order))
+#include <utility>
+#include <vector>
+
 #include "dawn/native/ShaderModule.h"
 
 #include "dawn/common/vulkan_platform.h"
 #include "dawn/native/Error.h"
-
-#include <memory>
-#include <mutex>
-#include <optional>
-#include <utility>
-#include <vector>
 
 namespace dawn::native::vulkan {
 
