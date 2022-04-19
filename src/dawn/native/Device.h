6a325a76367adf32cd964e013159749394e51baf
Enrico Galli <enrico.galli@intel.com>
2022-04-18 17:20:58 -0700

    D3D12: Duplicate first/baseVertex on Draw[Indexed]Indirect
    Adds support for non-zero first/baseVertex on Draw[Indexed]Indirect by
    duplicating the first/baseVertex indirect parameter onto a root
    constant in the indirect buffer.
    
    Change-Id: I280149065179806d3e57b07f1a396f9e2e4e8fcb
    Bug: dawn:548

--- src/dawn/native/Device.h
+++ src/dawn/native/Device.h
@@ -354,6 +354,11 @@ namespace dawn::native {
         virtual bool ShouldDuplicateNumWorkgroupsForDispatchIndirect(
             ComputePipelineBase* computePipeline) const;
 
+        virtual bool MayRequireDuplicationOfIndirectParameters() const;
+
+        virtual bool ShouldDuplicateParametersForDrawIndirect(
+            const RenderPipelineBase* renderPipelineBase) const;
+
         const CombinedLimits& GetLimits() const;
 
         AsyncTaskManager* GetAsyncTaskManager() const;
