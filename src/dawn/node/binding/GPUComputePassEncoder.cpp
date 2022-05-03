216f8ba8ab76fc84f680c85c8663940a2df5c07a
Ben Clayton <bclayton@google.com>
2022-05-03 17:52:00 +0100

    dawn/node: Roll to latest IDL
    Bug: dawn:1123
    Change-Id: I430d6b980dc34e37578fc26d3e0e126e75e11d97

--- src/dawn/node/binding/GPUComputePassEncoder.cpp
+++ src/dawn/node/binding/GPUComputePassEncoder.cpp
@@ -35,16 +35,17 @@ void GPUComputePassEncoder::setPipeline(Napi::Env,
     enc_.SetPipeline(*pipeline.As<GPUComputePipeline>());
 }
 
-void GPUComputePassEncoder::dispatch(Napi::Env,
-                                     interop::GPUSize32 workgroupCountX,
-                                     interop::GPUSize32 workgroupCountY,
-                                     interop::GPUSize32 workgroupCountZ) {
+void GPUComputePassEncoder::dispatchWorkgroups(Napi::Env,
+                                               interop::GPUSize32 workgroupCountX,
+                                               interop::GPUSize32 workgroupCountY,
+                                               interop::GPUSize32 workgroupCountZ) {
     enc_.Dispatch(workgroupCountX, workgroupCountY, workgroupCountZ);
 }
 
-void GPUComputePassEncoder::dispatchIndirect(Napi::Env,
-                                             interop::Interface<interop::GPUBuffer> indirectBuffer,
-                                             interop::GPUSize64 indirectOffset) {
+void GPUComputePassEncoder::dispatchWorkgroupsIndirect(
+    Napi::Env,
+    interop::Interface<interop::GPUBuffer> indirectBuffer,
+    interop::GPUSize64 indirectOffset) {
     enc_.DispatchIndirect(*indirectBuffer.As<GPUBuffer>(), indirectOffset);
 }
 
@@ -112,12 +113,11 @@ void GPUComputePassEncoder::insertDebugMarker(Napi::Env, std::string markerLabel
     enc_.InsertDebugMarker(markerLabel.c_str());
 }
 
-std::variant<std::string, interop::UndefinedType> GPUComputePassEncoder::getLabel(Napi::Env) {
+std::string GPUComputePassEncoder::getLabel(Napi::Env) {
     UNIMPLEMENTED();
 }
 
-void GPUComputePassEncoder::setLabel(Napi::Env,
-                                     std::variant<std::string, interop::UndefinedType> value) {
+void GPUComputePassEncoder::setLabel(Napi::Env, std::string value) {
     UNIMPLEMENTED();
 }
 
