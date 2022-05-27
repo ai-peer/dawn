c1813299b4df7d42128d0c91c1f404a8437e51d2
Antonio Maiorano <amaiorano@google.com>
2022-05-27 16:49:26 -0400

    Implement atomicCompareExchangeWeak returning struct instead of vec2
    Bug: tint:1185
    Change-Id: Id0e9f99d6368717511ef3a94473634c512e10cb8

--- test/tint/builtins/gen/atomicLoad/fe6cc3.wgsl.expected.hlsl
+++ test/tint/builtins/gen/atomicLoad/fe6cc3.wgsl.expected.hlsl
@@ -1,10 +1,11 @@
+RWByteAddressBuffer sb_rw : register(u0, space0);
+
 uint atomicLoad_1(RWByteAddressBuffer buffer, uint offset) {
   uint value = 0;
   buffer.InterlockedOr(offset, 0, value);
   return value;
 }
 
-RWByteAddressBuffer sb_rw : register(u0, space0);
 
 void atomicLoad_fe6cc3() {
   uint res = atomicLoad_1(sb_rw, 0u);
--- test/tint/builtins/gen/atomicStore/d1e9a6.wgsl.expected.hlsl
+++ test/tint/builtins/gen/atomicStore/d1e9a6.wgsl.expected.hlsl
@@ -1,9 +1,10 @@
+RWByteAddressBuffer sb_rw : register(u0, space0);
+
 void atomicStore_1(RWByteAddressBuffer buffer, uint offset, int value) {
   int ignored;
   buffer.InterlockedExchange(offset, value, ignored);
 }
 
-RWByteAddressBuffer sb_rw : register(u0, space0);
 
 void atomicStore_d1e9a6() {
   atomicStore_1(sb_rw, 0u, 1);
