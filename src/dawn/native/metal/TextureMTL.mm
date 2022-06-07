ef36c23de721693717bbe4cc679f2c00ff923376
Brandon Jones <bajones@chromium.org>
2022-06-07 14:02:04 -0700

    Remove support for depth24unorm-stencil8
    It was removed from the spec after determining it didn't provide any
    additional capabilities beyond depth24plus-stencil8.
    
    Bug: dawn:1454
    Change-Id: Ifba62f22cd38bea88866c849c8d1754a2aa683e2

--- src/dawn/native/metal/TextureMTL.mm
+++ src/dawn/native/metal/TextureMTL.mm
@@ -317,10 +317,14 @@ MTLPixelFormat MetalPixelFormat(wgpu::TextureFormat format) {
         case wgpu::TextureFormat::Stencil8:
             return MTLPixelFormatStencil8;
 
+<<<<<<< HEAD
 #if DAWN_PLATFORM_IS(MACOS)
         case wgpu::TextureFormat::Depth24UnormStencil8:
             return MTLPixelFormatDepth24Unorm_Stencil8;
 
+=======
+#if defined(DAWN_PLATFORM_MACOS)
+>>>>>>> 4e0387098 (Remove support for depth24unorm-stencil8)
         case wgpu::TextureFormat::BC1RGBAUnorm:
             return MTLPixelFormatBC1_RGBA;
         case wgpu::TextureFormat::BC1RGBAUnormSrgb:
@@ -350,8 +354,6 @@ MTLPixelFormat MetalPixelFormat(wgpu::TextureFormat format) {
         case wgpu::TextureFormat::BC7RGBAUnormSrgb:
             return MTLPixelFormatBC7_RGBAUnorm_sRGB;
 #else
-        case wgpu::TextureFormat::Depth24UnormStencil8:
-
         case wgpu::TextureFormat::BC1RGBAUnorm:
         case wgpu::TextureFormat::BC1RGBAUnormSrgb:
         case wgpu::TextureFormat::BC2RGBAUnorm:
@@ -1066,11 +1068,6 @@ MaybeError TextureView::Initialize(const TextureViewDescriptor* descriptor) {
                 if (textureFormat == MTLPixelFormatDepth32Float_Stencil8) {
                     viewFormat = MTLPixelFormatX32_Stencil8;
                 }
-#if DAWN_PLATFORM_IS(MACOS)
-                else if (textureFormat == MTLPixelFormatDepth24Unorm_Stencil8) {
-                    viewFormat = MTLPixelFormatX24_Stencil8;
-                }
-#endif
                 else {
                     UNREACHABLE();
                 }
