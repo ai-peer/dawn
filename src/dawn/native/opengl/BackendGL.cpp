2f76072fa73a8f6a17e1aaf6581032536062aa8d
Stephen White <senorblanco@chromium.org>
2022-06-16 15:13:54 -0400

    Implement EGL context creation in Dawn native.
    This version implements a per-device EGLContext.
    DeviceGL's "gl" member is made private, accessible only
    via GetGL() (which also makes the context current, if any).
    
    Bug: dawn:810
    Change-Id: Idfe30939f155d026fcad549787fc167cc43aa3cb

--- src/dawn/native/opengl/BackendGL.cpp
+++ src/dawn/native/opengl/BackendGL.cpp
@@ -14,10 +14,17 @@
 
 #include "dawn/native/opengl/BackendGL.h"
 
+#include <EGL/egl.h>
+
+#include <memory>
 #include <utility>
 
+#include "dawn/common/SystemUtils.h"
+#include "dawn/native/Instance.h"
 #include "dawn/native/OpenGLBackend.h"
 #include "dawn/native/opengl/AdapterGL.h"
+#include "dawn/native/opengl/ContextEGL.h"
+#include "dawn/native/opengl/EGLFunctions.h"
 
 namespace dawn::native::opengl {
 
@@ -27,8 +34,56 @@ Backend::Backend(InstanceBase* instance, wgpu::BackendType backendType)
     : BackendConnection(instance, backendType) {}
 
 std::vector<Ref<AdapterBase>> Backend::DiscoverDefaultAdapters() {
-    // The OpenGL backend needs at least "getProcAddress" to discover an adapter.
-    return {};
+    std::vector<Ref<AdapterBase>> adapters;
+#ifdef DAWN_ENABLE_BACKEND_OPENGLES
+    if (GetType() == wgpu::BackendType::OpenGLES) {
+#if DAWN_PLATFORM_IS(WINDOWS)
+        const char* eglLib = "libEGL.dll";
+#elif DAWN_PLATFORM_IS(MACOS)
+        const char* eglLib = "libEGL.dylib";
+#else
+        const char* eglLib = "libEGL.so";
+#endif
+        DynamicLib libEGL;
+
+        if (libEGL.Open(eglLib)) {
+            return {};
+        }
+
+        AdapterDiscoveryOptionsES options;
+        options.getProc =
+            reinterpret_cast<void* (*)(const char*)>(libEGL.GetProc("eglGetProcAddress"));
+        if (!options.getProc) {
+            return {};
+        }
+
+        egl.Init(reinterpret_cast<PFNEGLGETPROCADDRESSPROC>(options.getProc));
+        std::unique_ptr<Device::Context> context = ContextEGL::Create(egl);
+
+        if (!context) {
+            return {};
+        }
+
+        EGLDisplay prevDisplay = egl.GetCurrentDisplay();
+        EGLContext prevDrawSurface = egl.GetCurrentSurface(EGL_DRAW);
+        EGLContext prevReadSurface = egl.GetCurrentSurface(EGL_READ);
+        EGLContext prevContext = egl.GetCurrentContext();
+
+        context->MakeCurrent();
+
+        auto result = DiscoverAdapters(&options);
+
+        if (result.IsError()) {
+            GetInstance()->ConsumedError(result.AcquireError());
+        } else {
+            auto value = result.AcquireSuccess();
+            adapters.insert(adapters.end(), value.begin(), value.end());
+        }
+
+        egl.MakeCurrent(prevDisplay, prevDrawSurface, prevReadSurface, prevContext);
+    }
+#endif
+    return adapters;
 }
 
 ResultOrError<std::vector<Ref<AdapterBase>>> Backend::DiscoverAdapters(
