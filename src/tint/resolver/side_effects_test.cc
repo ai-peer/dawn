a250138860979b1f6a0752262db26616660055e8
Ben Clayton <bclayton@google.com>
2022-06-23 13:14:33 +0100

    tint: Rename Global() -> GlobalVar()
    We have `GlobalLet()`, `Let()`, `Var()`, so this just
    keeps things consistent
    
    Change-Id: Ie9f79b62e737a15b995c5a2b19f84621a5ac3cc9

--- src/tint/resolver/side_effects_test.cc
+++ src/tint/resolver/side_effects_test.cc
@@ -28,7 +28,7 @@ struct SideEffectsTest : ResolverTest {
     template <typename T>
     void MakeSideEffectFunc(const char* name) {
         auto global = Sym();
-        Global(global, ty.Of<T>(), ast::StorageClass::kPrivate);
+        GlobalVar(global, ty.Of<T>(), ast::StorageClass::kPrivate);
         auto local = Sym();
         Func(name, {}, ty.Of<T>(),
              {
@@ -41,7 +41,7 @@ struct SideEffectsTest : ResolverTest {
     template <typename MAKE_TYPE_FUNC>
     void MakeSideEffectFunc(const char* name, MAKE_TYPE_FUNC make_type) {
         auto global = Sym();
-        Global(global, make_type(), ast::StorageClass::kPrivate);
+        GlobalVar(global, make_type(), ast::StorageClass::kPrivate);
         auto local = Sym();
         Func(name, {}, make_type(),
              {
@@ -86,7 +86,7 @@ TEST_F(SideEffectsTest, VariableUser) {
 }
 
 TEST_F(SideEffectsTest, Call_Builtin_NoSE) {
-    Global("a", ty.f32(), ast::StorageClass::kPrivate);
+    GlobalVar("a", ty.f32(), ast::StorageClass::kPrivate);
     auto* expr = Call("dpdx", "a");
     Func("f", {}, ty.void_(), {Ignore(expr)},
          {create<ast::StageAttribute>(ast::PipelineStage::kFragment)});
@@ -112,7 +112,7 @@ TEST_F(SideEffectsTest, Call_Builtin_NoSE_WithSEArg) {
 }
 
 TEST_F(SideEffectsTest, Call_Builtin_SE) {
-    Global("a", ty.atomic(ty.i32()), ast::StorageClass::kWorkgroup);
+    GlobalVar("a", ty.atomic(ty.i32()), ast::StorageClass::kWorkgroup);
     auto* expr = Call("atomicAdd", AddressOf("a"), 1_i);
     WrapInFunction(expr);
 
