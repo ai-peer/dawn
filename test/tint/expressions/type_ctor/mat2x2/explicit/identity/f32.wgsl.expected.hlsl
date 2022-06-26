958b4a093b9e9afad1893ae7913e006116bfc70b
Ben Clayton <bclayton@google.com>
2022-06-26 13:34:44 +0100

    test/tint/expressions/type_ctor: Use 'var' instead of 'let'
    Module-scope 'let' is getting replaced by 'const'. For all backends,
    'const' will be inlined into the place of usage, making most of these
    tests produce no output (if replaced with 'const'). Instead switch to
    emitting with 'var'.
    
    Bug: tint:1580
    Change-Id: Ied5ddf9cdb7fbd3cef8e7b0c6f4983748aaa3d07

--- test/tint/expressions/type_ctor/mat2x2/explicit/identity/f32.wgsl.expected.hlsl
+++ test/tint/expressions/type_ctor/mat2x2/explicit/identity/f32.wgsl.expected.hlsl
@@ -3,9 +3,4 @@ void unused_entry_point() {
   return;
 }
 
-static const float2x2 m = float2x2(float2(0.0f, 1.0f), float2(2.0f, 3.0f));
-
-float2x2 f() {
-  const float2x2 m_1 = float2x2(float2(0.0f, 1.0f), float2(2.0f, 3.0f));
-  return m_1;
-}
+static float2x2 m = float2x2((0.0f).xx, (0.0f).xx);
