1304a463aa1187064e8694cc7378327c53fe943e
Sunny Sachanandani <sunnyps@chromium.org>
2022-07-08 12:11:55 -0700

    d3d12: Destroy ExternalImageDXGI resources on device destruction
    D3D12 objects can have implicit dependencies on device resources that
    are not captured by holding ComPtrs:
    
    "Direct3D 12 uses COM-style reference counting only for the lifetimes of
    interfaces (by using the weak reference model of Direct3D tied to the
    lifetime of the device). All resource and description memory lifetimes
    are the sole responsibly of the app to maintain for the proper duration,
    and are not reference counted. Direct3D 11 uses reference counting to
    manage the lifetimes of interface dependencies as well."
    
    Source: https://docs.microsoft.com/en-us/windows/win32/direct3d12/important-changes-from-directx-11-to-directx-12
    
    ExternalImageDXGI can outlive the device it was created on e.g. the D3D
    shared image backing holds on to the ExternalImageDXGI for its lifetime.
    ExternalImageDXGI destructor can invoke code that depends on D3D12
    resources that might have already been destroyed. In particular, this
    shows up as ComPtr::Release for ID3D12Fence crashing mysteriously, and
    is also speculated as the cause for a racy invalid function pointer
    dereference in crbug.com/1338470.
    
    This CL makes the D3D12 backend device destroy the ExternalImageDXGI's
    resources on device destruction making it effectively a weak pointer.
    This unblocks landing https://crrev.com/c/3700811 and hopefully fixes
    crbug.com/1338470 as well.
    
    This CL also deprecates unnecessary WGPUDevice param to ProduceTexture,
    and adds a GetDevice() accessor so that the shared image can compare it
    against current device and decide to recreate the ExternalImageDXGI.
    
    Bug: dawn:576, chromium:1338470
    Change-Id: I2122cf807587cf3b1218ba29ea291263df0cf698

--- include/dawn/native/D3D12Backend.h
+++ include/dawn/native/D3D12Backend.h
@@ -31,6 +31,7 @@ struct ID3D12Resource;
 namespace dawn::native::d3d12 {
 
 class D3D11on12ResourceCache;
+class Device;
 
 DAWN_NATIVE_EXPORT Microsoft::WRL::ComPtr<ID3D12Device> GetD3D12Device(WGPUDevice device);
 DAWN_NATIVE_EXPORT DawnSwapChainImplementation CreateNativeSwapChainImpl(WGPUDevice device,
@@ -95,19 +96,29 @@ class DAWN_NATIVE_EXPORT ExternalImageDXGI {
   public:
     ~ExternalImageDXGI();
 
-    // Note: SharedHandle must be a handle to a texture object.
     static std::unique_ptr<ExternalImageDXGI> Create(
         WGPUDevice device,
         const ExternalImageDescriptorDXGISharedHandle* descriptor);
 
+    WGPUDevice GetDevice() const;
+
+    // TODO(sunnyps): |device| is ignored - remove after Chromium migrates to single parameter call.
     WGPUTexture ProduceTexture(WGPUDevice device,
                                const ExternalImageAccessDescriptorDXGISharedHandle* descriptor);
 
+    WGPUTexture ProduceTexture(const ExternalImageAccessDescriptorDXGISharedHandle* descriptor);
+
+  protected:
+    friend class Device;
+    void Destroy();
+
   private:
-    ExternalImageDXGI(Microsoft::WRL::ComPtr<ID3D12Resource> d3d12Resource,
+    ExternalImageDXGI(Device* backendDevice,
+                      Microsoft::WRL::ComPtr<ID3D12Resource> d3d12Resource,
                       Microsoft::WRL::ComPtr<ID3D12Fence> d3d12Fence,
                       const WGPUTextureDescriptor* descriptor);
 
+    Device* mBackendDevice;
     Microsoft::WRL::ComPtr<ID3D12Resource> mD3D12Resource;
     Microsoft::WRL::ComPtr<ID3D12Fence> mD3D12Fence;
 
