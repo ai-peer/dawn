e6bb2c97e31b9d0ead1a971ae7081184a594e107
Ben Clayton <bclayton@google.com>
2022-07-14 12:15:59 +0100

    tint: Optimize sem node lookup
    Add a 'NodeID' to each ast::Node which is the sequentially allocated
    index of the node. Use this in sem::Info to map the AST node to the
    semantic node, instead of using a std::unordered_map.
    
    Optimised very hot code by entirely eliminating map lookups, and
    dramatically reducing cache misses (lookups are usually sequentually
    ordered).
    
    Timings running
    'webgpu:shader,execution,expression,call,builtin,atan2:f32:inputSource="const";vectorize="_undef_"'
    with dawn/node, using SwiftShader:
    
        Without change: 3.22647107s
        With change:    3.10578879s
    
    Bug: tint:1613
    Change-Id: I22ec48d933b2e5f9da04494bff4e979e6f7b1982

--- src/tint/writer/glsl/generator_impl.cc
+++ src/tint/writer/glsl/generator_impl.cc
@@ -2206,7 +2206,7 @@ bool GeneratorImpl::EmitEntryPointFunction(const ast::Function* func) {
         }
 
         if (!Is<ast::ReturnStatement>(func->body->Last())) {
-            ast::ReturnStatement ret(ProgramID(), Source{});
+            ast::ReturnStatement ret(ProgramID{}, ast::NodeID{}, Source{});
             if (!EmitStatement(&ret)) {
                 return false;
             }
