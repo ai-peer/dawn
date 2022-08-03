dbc1c06ee7de7c37eb3afd29c8a4f11d7d6a9876
Ben Clayton <bclayton@google.com>
2022-08-03 17:38:08 +0100

    tint/utils/UniqueVector: Use utils::Vector and utils::Hashset
    For fewer heap allocations, faster lookups.
    
    Change-Id: I02da7c1a63608096ec898b0d89f9f97c6db8733f

--- src/tint/utils/hashset.h
+++ src/tint/utils/hashset.h
@@ -18,6 +18,7 @@
 #include <stddef.h>
 #include <algorithm>
 #include <functional>
+#include <optional>
 #include <tuple>
 #include <utility>
 
@@ -40,7 +41,16 @@ enum class InsertAction {
 /// @see the fantastic tutorial: https://programming.guide/robin-hood-hashing.html
 template <typename T, size_t N, typename HASH = std::hash<T>, typename EQUAL = std::equal_to<T>>
 class Hashset {
-    struct Slot;
+    struct Slot {
+        template <typename V>
+        bool Equals(size_t value_hash, const V& val) const {
+            return value_hash == hash && EQUAL()(val, value.value());
+        }
+
+        std::optional<T> value;
+        size_t hash = 0;
+        size_t distance = 0;
+    };
 
     static constexpr size_t kRehashFactor = 150;  // percent
     static constexpr size_t kNumFixedSlots = (N * kRehashFactor) / 100;
@@ -191,9 +201,9 @@ class Hashset {
                 // Slot is equal to value. Replace or preserve?
                 if constexpr (REPLACE) {
                     slot.value = std::forward<V>(value);
-                    result = {InsertAction::kReplaced, &slot.value.value()};
+                    result = InsertionResult{InsertAction::kReplaced, &slot.value.value()};
                 } else {
-                    result = {InsertAction::kFoundExisting, &slot.value.value()};
+                    result = InsertionResult{InsertAction::kFoundExisting, &slot.value.value()};
                 }
                 return Action::Stop;
             }
@@ -211,7 +221,7 @@ class Hashset {
                 InsertShuffle(Wrap(index + 1), std::move(temp), hash, distance + 1);
 
                 count_++;
-                result = {InsertAction::kAdded, &slot.value.value()};
+                result = InsertionResult{InsertAction::kAdded, &slot.value.value()};
 
                 return Action::Stop;
             }
@@ -335,6 +345,8 @@ class Hashset {
     }
 
   private:
+    enum class Action { Continue, Stop };
+
     template <typename V>
     std::tuple<size_t, size_t> IndexAndHash(const V& value) const {
         const size_t hash = HASH()(value);
@@ -414,24 +426,11 @@ class Hashset {
         });
     }
 
-    enum class Action { Continue, Stop };
-
     bool ShouldRehash(size_t count) const {
         return ((count * kRehashFactor) / 100) > slots_.Length();
     }
 
     size_t Wrap(size_t index) const { return index % slots_.Length(); }
-
-    struct Slot {
-        template <typename V>
-        bool Equals(size_t value_hash, const V& val) const {
-            return value_hash == hash && EQUAL()(val, value.value());
-        }
-
-        std::optional<T> value;
-        size_t hash = 0;
-        size_t distance = 0;
-    };
 
     Vector<Slot, kNumFixedSlots> slots_;
     size_t count_ = 0;
