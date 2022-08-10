4e516baeadb10f476f2a9a79e2051f11bceac68f
jchen10 <jie.a.chen@intel.com>
2022-08-10 15:06:49 +0800

    Byte counting WriteBuffer/Texture to submit
    Only tag to submit when the total size is larger than the threshold,
    so that we can make as few submits as possible meanwhile avoiding OOM.
    
    Bug: chromium:1258986
    Change-Id: Ide9ba69b796a46fa8bb70b002f4e2aeb1622bffd

--- src/dawn/native/Device.h
+++ src/dawn/native/Device.h
@@ -402,6 +402,8 @@ class DeviceBase : public RefCountedWithExternalCount {
 
     virtual void AppendDebugLayerMessages(ErrorData* error) {}
 
+    virtual void ForceEventualFlushOfCommands() {}
+
   protected:
     // Constructor used only for mocking and testing.
     DeviceBase();
