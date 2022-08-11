48b24a985b4edf24b278aeff86aa88bd81c1148f
Austin Eng <enga@chromium.org>
2022-08-11 09:46:59 -0400

    Beautify visitable structs with more helpers
    Bug: dawn:1480
    Change-Id: I01450e1916766daba016f71c0ed825e6b6664541

--- src/dawn/native/Visitable.h
+++ src/dawn/native/Visitable.h
@@ -12,9 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef SRC_DAWN_NATIVE_VISITABLEMEMBERS_H_
-#define SRC_DAWN_NATIVE_VISITABLEMEMBERS_H_
+#ifndef SRC_DAWN_NATIVE_VISITABLE_H_
+#define SRC_DAWN_NATIVE_VISITABLE_H_
 
+#include <utility>
+
+#include "dawn/native/stream/BlobSource.h"
+#include "dawn/native/stream/ByteVectorSink.h"
 #include "dawn/native/stream/Stream.h"
 
 // Helper for X macro to declare a visitable member.
@@ -27,8 +31,8 @@ namespace dawn::native::detail {
 constexpr int kInternalVisitableUnusedForComma = 0;
 }  // namespace dawn::native::detail
 
-// Helper X macro to declare members of a class or struct, along with Visit
-// methods to call a functor for each member.
+// Helper X macro to declare members of a class or struct, along with VisitAll
+// methods to call a functor on all members.
 // Example usage:
 //   #define MEMBERS(X) \
 //       X(int, a)              \
@@ -58,4 +62,53 @@ constexpr int kInternalVisitableUnusedForComma = 0;
                    DAWN_INTERNAL_VISITABLE_MEMBER_ARG));                    \
     }
 
-#endif  // SRC_DAWN_NATIVE_VISITABLEMEMBERS_H_
+namespace dawn::native {
+
+// Base CRTP for implementing StreamIn/StreamOut/FromBlob/ToBlob for Derived,
+// assuming Derived has VisitAll methods provided by DAWN_VISITABLE_MEMBERS.
+template <typename Derived>
+class Visitable {
+  public:
+    friend void StreamIn(stream::Sink* s, const Derived& in) {
+        in.VisitAll([&](const auto&... members) { StreamIn(s, members...); });
+    }
+
+    friend MaybeError StreamOut(stream::Source* s, Derived* out) {
+        return out->VisitAll([&](auto&... members) { return StreamOut(s, &members...); });
+    }
+
+    static ResultOrError<Derived> FromBlob(Blob blob) {
+        stream::BlobSource source(std::move(blob));
+        Derived out;
+        DAWN_TRY(StreamOut(&source, &out));
+        return out;
+    }
+
+    Blob ToBlob() const {
+        stream::ByteVectorSink sink;
+        StreamIn(&sink, static_cast<const Derived&>(*this));
+        return CreateBlob(std::move(sink));
+    }
+};
+}  // namespace dawn::native
+
+// Helper macro to define a struct or class along with VisitAll methods to call
+// a functor on all members. Derives from Visitable which provides
+// implementations of StreamIn/StreamOut/FromBlob/ToBlob.
+// Example usage:
+//   #define MEMBERS(X) \
+//       X(int, a)              \
+//       X(float, b)            \
+//       X(Foo, foo)            \
+//       X(Bar, bar)
+//   DAWN_VISITABLE(struct, MyStruct, MEMBERS) {
+//      void SomeAdditionalMethod();
+//   };
+//   #undef MEMBERS
+#define DAWN_VISITABLE(qualifier, Name, MEMBERS) \
+    struct Name##__Contents {                    \
+        DAWN_VISITABLE_MEMBERS(MEMBERS)          \
+    };                                           \
+    qualifier Name : Name##__Contents, public ::dawn::native::Visitable<Name>
+
+#endif  // SRC_DAWN_NATIVE_VISITABLE_H_
