5fe9e95ddaa9c05704e4cca1a104ec07b92f0fda
Austin Eng <enga@chromium.org>
2022-08-15 11:41:30 -0400

    Send test heartbeats from the CTS runner after async WebGPU ops
    Bug: chromium:1340602
    Change-Id: I4d5c864dabd3f4215dac2e78d7658df0f6a7b0b7

--- webgpu-cts/test_runner.js
+++ webgpu-cts/test_runner.js
@@ -25,6 +25,59 @@ const LOGS_MAX_BYTES = 72000;
 
 var socket;
 
+// Returns a wrapper around `fn` which gets called at most once every `intervalMs`.
+// If the wrapper is called when `fn` was called too recently, `fn` is scheduled to
+// be called later in the future after the interval passes.
+// Returns [ wrappedFn, {start, stop}] where wrappedFn is the rate-limited function,
+// and start/stop control whether or not the function is enabled. If it is stopped, calls
+// to the fn will no-op. If it is started, calls will be rate-limited, starting from
+// the time `start` is called.
+function rateLimited(fn, intervalMs) {
+  let last = undefined;
+  let timer = undefined;
+  let serial = 0;
+  const wrappedFn = (...args) => {
+    if (timer !== undefined || last === undefined) {
+      // If there is already a fn call scheduled, or the function is
+      // not enabled, return.
+      return;
+    }
+    // Get the current time as a number.
+    const now = +new Date();
+    const diff = now - last;
+    if (diff >= intervalMs) {
+      // If the time interval has passed, call the function.
+      last = now;
+      fn(...args);
+    } else if (timer === undefined) {
+      // Otherwise, we have called `fn` too recently. To rate-limit it,
+      // schedule a future call if one has not already been scheduled.
+      let scheduledSerial = serial;
+      timer = setTimeout(() => {
+        // Clear the timer to indicate nothing is scheduled.
+        timer = undefined;
+        // Check that the serial is current. stop() has not been called.
+        if (scheduledSerial === serial) {
+          last = +new Date();
+          fn(...args);
+        }
+      }, intervalMs - diff + 1);
+    }
+  };
+  return [
+    wrappedFn,
+    {
+      start: () => {
+        last = +new Date();
+      },
+      stop: () => {
+        ++serial;
+        last = undefined;
+      },
+    }
+  ];
+}
+
 function byteSize(s) {
   return new Blob([s]).size;
 }
@@ -38,6 +91,35 @@ async function runCtsTestViaSocket(event) {
   let input = JSON.parse(event.data);
   runCtsTest(input['q'], input['w']);
 }
+
+// Make a rate-limited version `sendMessageTestHeartbeat` that executes
+// at most once every 500 ms.
+const [sendHeartbeat, {
+  start: beginHeartbeatScope,
+  stop: endHeartbeatScope
+}] = rateLimited(sendMessageTestHeartbeat, 500);
+
+function wrapPromiseWithHeartbeat(prototype, key) {
+  const old = prototype[key];
+  prototype[key] = function (...args) {
+    return new Promise((resolve, reject) => {
+      old.call(this, ...args)
+        .then(resolve)
+        .catch(reject)
+        .finally(sendHeartbeat);
+    });
+  }
+}
+
+wrapPromiseWithHeartbeat(GPU.prototype, 'requestAdapter');
+wrapPromiseWithHeartbeat(GPUAdapter.prototype, 'requestAdapterInfo');
+wrapPromiseWithHeartbeat(GPUAdapter.prototype, 'requestDevice');
+wrapPromiseWithHeartbeat(GPUDevice.prototype, 'createRenderPipelineAsync');
+wrapPromiseWithHeartbeat(GPUDevice.prototype, 'createComputePipelineAsync');
+wrapPromiseWithHeartbeat(GPUDevice.prototype, 'popErrorScope');
+wrapPromiseWithHeartbeat(GPUQueue.prototype, 'onSubmittedWorkDone');
+wrapPromiseWithHeartbeat(GPUBuffer.prototype, 'mapAsync');
+wrapPromiseWithHeartbeat(GPUShaderModule.prototype, 'compilationInfo');
 
 async function runCtsTest(query, use_worker) {
   const workerEnabled = use_worker;
@@ -57,18 +139,17 @@ async function runCtsTest(query, use_worker) {
     const wpt_fn = async () => {
       sendMessageTestStarted();
       const [rec, res] = log.record(name);
+
+      beginHeartbeatScope();
       if (worker) {
         await worker.run(rec, name, expectations);
       } else {
         await testcase.run(rec, expectations);
       }
+      endHeartbeatScope();
 
       sendMessageTestStatus(res.status, res.timems);
-
-      let fullLogs = (res.logs ?? []).map(prettyPrintLog);
-      fullLogs = fullLogs.join('\n\n\n');
-      let logPieces = splitLogsForPayload(fullLogs);
-      sendMessageTestLog(logPieces);
+      sendMessageTestLog(res.logs);
       sendMessageTestFinished();
     };
     await wpt_fn();
@@ -100,7 +181,11 @@ function splitLogsForPayload(fullLogs) {
 }
 
 function sendMessageTestStarted() {
-  socket.send(JSON.stringify({'type': 'TEST_STARTED'}));
+  socket.send('{"type":"TEST_STARTED"}');
+}
+
+function sendMessageTestHeartbeat() {
+  socket.send('{"type":"TEST_HEARTBEAT"}');
 }
 
 function sendMessageTestStatus(status, jsDurationMs) {
@@ -109,15 +194,18 @@ function sendMessageTestStatus(status, jsDurationMs) {
                               'js_duration_ms': jsDurationMs}));
 }
 
-function sendMessageTestLog(logPieces) {
-  logPieces.forEach((piece) => {
-    socket.send(JSON.stringify({'type': 'TEST_LOG',
-                                'log': piece}));
-  });
+function sendMessageTestLog(logs) {
+  splitLogsForPayload((logs ?? []).map(prettyPrintLog).join('\n\n'))
+    .forEach((piece) => {
+      socket.send(JSON.stringify({
+        'type': 'TEST_LOG',
+        'log': piece
+      }));
+    });
 }
 
 function sendMessageTestFinished() {
-  socket.send(JSON.stringify({'type': 'TEST_FINISHED'}));
+  socket.send('{"type":"TEST_FINISHED"}');
 }
 
 window.runCtsTest = runCtsTest;
