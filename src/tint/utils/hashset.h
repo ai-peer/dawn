3dbe11a07bf46ade7670403aece2d3eddc80489a
Ben Clayton <bclayton@google.com>
2022-08-16 01:52:38 +0100

    tint/utils: Default to using Hasher instead of std::hash
    STL std::hash<T*> implementations have a tendency of just reinterpreting the
    pointer as a size_t. This is fast, but produces a bad hash, as most pointers
    tend to be 4 or 16 byte aligned. The lack of entropy in the LSBs causes
    clustering of hashmap slots. Use tint::utils::Hasher to get better hashes.
    
    Change-Id: Ife768d573cd1875e746ca9d77a4ac19e43b06aca

--- src/tint/utils/hashset.h
+++ src/tint/utils/hashset.h
@@ -23,6 +23,7 @@
 #include <utility>
 
 #include "src/tint/debug.h"
+#include "src/tint/utils/hash.h"
 #include "src/tint/utils/vector.h"
 
 namespace tint::utils {
@@ -39,7 +40,7 @@ enum class InsertAction {
 
 /// An unordered set that uses a robin-hood hashing algorithm.
 /// @see the fantastic tutorial: https://programming.guide/robin-hood-hashing.html
-template <typename T, size_t N, typename HASH = std::hash<T>, typename EQUAL = std::equal_to<T>>
+template <typename T, size_t N, typename HASH = Hasher<T>, typename EQUAL = std::equal_to<T>>
 class Hashset {
     /// A slot is in single entry in the underlying vector.
     /// A slot can either be empty or filled with a value. If the slot is empty, #hash and #distance
@@ -354,8 +355,8 @@ class Hashset {
 
     template <typename V>
     std::tuple<size_t, size_t> IndexAndHash(const V& value) const {
-        const size_t hash = HASH()(value);
-        const size_t index = Wrap(hash);
+        size_t hash = HASH()(value);
+        size_t index = Wrap(hash);
         return {index, hash};
     }
 
