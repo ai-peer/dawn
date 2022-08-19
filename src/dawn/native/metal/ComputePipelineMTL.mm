d9ab2f99b2925249b315c146d46a17bf93f24e19
shrekshao <shrekshao@google.com>
2022-08-19 15:08:54 -0700

    Use SubstituteOverride transform to implement pipeline overrides
    Remove the old backend specific implementation for
    overrides. Use tint SubstituteOverride transform to replace
    overrides with const expressions and use the updated program
    at pipeline creation time.
    
    This CL also adds support for overrides used as workgroup size
    and related tests.
    
    Bug: dawn:1504
    Change-Id: I7df1fe9c3e358caa23235eacd6d13ba0b2998aec

--- src/dawn/native/metal/ComputePipelineMTL.mm
+++ src/dawn/native/metal/ComputePipelineMTL.mm
@@ -53,8 +53,7 @@ MaybeError ComputePipeline::Initialize() {
     ASSERT(mMtlComputePipelineState != nil);
 
     // Copy over the local workgroup size as it is passed to dispatch explicitly in Metal
-    Origin3D localSize = GetStage(SingleShaderStage::Compute).metadata->localWorkgroupSize;
-    mLocalWorkgroupSize = MTLSizeMake(localSize.x, localSize.y, localSize.z);
+    mLocalWorkgroupSize = computeData.localWorkgroupSize;
 
     mRequiresStorageBufferLength = computeData.needsStorageBufferLength;
     mWorkgroupAllocations = std::move(computeData.workgroupAllocations);
