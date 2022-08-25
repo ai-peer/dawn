5820a1bb3ddd4f39b93159b9318476fde671de7a
Zhaoming Jiang <zhaoming.jiang@intel.com>
2022-08-25 16:29:44 +0800

    tint: add missing F16 conversion expression support
    This CL add missing type conversions for f16, especially for SPIRV
    backend which require special handling. A transform,
    VectorizeMatrixConversions, are also added for SPIRV to replace a matrix
    conversion to a matrix construction with converted column vectors.
    Unittests for the transform and SPIRV writer, and end-to-end tests for
    all conversion rules are added.
    
    Bug: tint:1473, tint:1502, chromium:1356215
    Change-Id: Iaff125e5dd295d35c4ab74757eb56b642802a51a

---
+++ src/tint/transform/vectorize_matrix_conversions.cc
@@ -0,0 +1,92 @@
+// Copyright 2021 The Tint Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "src/tint/transform/vectorize_matrix_conversions.h"
+
+#include <unordered_map>
+#include <utility>
+
+#include "src/tint/program_builder.h"
+#include "src/tint/sem/abstract_numeric.h"
+#include "src/tint/sem/call.h"
+#include "src/tint/sem/expression.h"
+#include "src/tint/sem/type_conversion.h"
+#include "src/tint/utils/map.h"
+
+TINT_INSTANTIATE_TYPEINFO(tint::transform::VectorizeMatrixConversions);
+
+namespace tint::transform {
+
+VectorizeMatrixConversions::VectorizeMatrixConversions() = default;
+
+VectorizeMatrixConversions::~VectorizeMatrixConversions() = default;
+
+bool VectorizeMatrixConversions::ShouldRun(const Program* program, const DataMap&) const {
+    for (auto* node : program->ASTNodes().Objects()) {
+        if (auto* call = program->Sem().Get<sem::Call>(node)) {
+            if (call->Target()->Is<sem::TypeConversion>() && call->Type()->Is<sem::Matrix>()) {
+                auto& args = call->Arguments();
+                if (args.Length() == 1 && args[0]->Type()->UnwrapRef()->is_float_matrix()) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+void VectorizeMatrixConversions::Run(CloneContext& ctx, const DataMap&, DataMap&) const {
+    std::unordered_map<const sem::Matrix*, Symbol> scalar_ctors;
+
+    ctx.ReplaceAll([&](const ast::CallExpression* expr) -> const ast::CallExpression* {
+        auto* call = ctx.src->Sem().Get(expr)->UnwrapMaterialize()->As<sem::Call>();
+        auto* ty_conv = call->Target()->As<sem::TypeConversion>();
+        if (!ty_conv) {
+            return nullptr;
+        }
+        auto* dst_type = call->Type()->As<sem::Matrix>();
+        if (!dst_type) {
+            return nullptr;
+        }
+
+        auto& args = call->Arguments();
+        if (args.Length() != 1) {
+            return nullptr;
+        }
+
+        auto& src = args[0];
+
+        auto* src_type = args[0]->Type()->UnwrapRef()->As<sem::Matrix>();
+        if (!src_type) {
+            return nullptr;
+        }
+
+        // The source and destination type of a matrix conversion must have a same shape.
+        TINT_ASSERT(Transform, src_type->rows() == dst_type->rows() &&
+                                   src_type->columns() == dst_type->columns());
+
+        // Replace the matrix conversion to column vector conversions and a matrix construction.
+        utils::Vector<const ast::Expression*, 4> columns;
+        for (uint32_t c = 0; c < dst_type->columns(); c++) {
+            columns.Push(ctx.dst->Construct(CreateASTTypeFor(ctx, dst_type->ColumnType()),
+                                            ctx.dst->IndexAccessor(ctx.Clone(src->Declaration()),
+                                                                   ctx.dst->Expr(tint::AInt(c)))));
+        }
+        return ctx.dst->Construct(CreateASTTypeFor(ctx, dst_type), columns);
+    });
+
+    ctx.Clone();
+}
+
+}  // namespace tint::transform
