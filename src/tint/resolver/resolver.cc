cd83a910e9d63200843bd025ca71aaa9a4641c72
dan sinclair <dsinclair@chromium.org>
2022-09-01 15:45:16 -0400

    Store location value into `sem::Function`.
    This CL adds an optional location value to the `sem::Function`
    which will store the resolved `@location` value.
    
    Bug: tint:1633
    Change-Id: I95130858d8a1cecae1389be74120da29fec2b448

--- src/tint/resolver/resolver.cc
+++ src/tint/resolver/resolver.cc
@@ -888,6 +888,16 @@ sem::Function* Resolver::Function(const ast::Function* decl) {
         return_type = builder_->create<sem::Void>();
     }
 
+    // Determine if the return type has a location
+    std::optional<uint32_t> return_location;
+    for (auto* attr : decl->return_type_attributes) {
+        Mark(attr);
+
+        if (auto* a = attr->As<ast::LocationAttribute>()) {
+            return_location = a->value;
+        }
+    }
+
     if (auto* str = return_type->As<sem::Struct>()) {
         if (!ApplyStorageClassUsageToType(ast::StorageClass::kNone, str, decl->source)) {
             AddNote(
@@ -911,7 +921,8 @@ sem::Function* Resolver::Function(const ast::Function* decl) {
         }
     }
 
-    auto* func = builder_->create<sem::Function>(decl, return_type, std::move(parameters));
+    auto* func =
+        builder_->create<sem::Function>(decl, return_type, return_location, std::move(parameters));
     builder_->Sem().Add(decl, func);
 
     TINT_SCOPED_ASSIGNMENT(current_function_, func);
@@ -954,9 +965,6 @@ sem::Function* Resolver::Function(const ast::Function* decl) {
         return nullptr;
     }
 
-    for (auto* attr : decl->return_type_attributes) {
-        Mark(attr);
-    }
     if (!validator_.NoDuplicateAttributes(decl->return_type_attributes)) {
         return nullptr;
     }
