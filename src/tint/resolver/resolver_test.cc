c4a5f68a366aefe4120f05e4c3538fe4f2a8cbfb
dan sinclair <dsinclair@chromium.org>
2022-09-01 16:23:12 -0400

    Add location into `sem::Parameter`.
    This CL adds a `location` value into the `Parameter` sem object.
    This will be set if the parameter has an `@location` attribute
    applied.
    
    Bug: tint:1633
    Change-Id: I55cb5cbda951f70d071ebe1400865b63af1fb20a

--- src/tint/resolver/resolver_test.cc
+++ src/tint/resolver/resolver_test.cc
@@ -773,6 +773,43 @@ TEST_F(ResolverTest, Function_Parameters) {
     EXPECT_TRUE(func_sem->ReturnType()->Is<sem::Void>());
 }
 
+TEST_F(ResolverTest, Function_Parameters_Locations) {
+    auto* param_a = Param("a", ty.f32(), utils::Vector{Location(3)});
+    auto* param_b = Param("b", ty.u32(), utils::Vector{Builtin(ast::BuiltinValue::kVertexIndex)});
+    auto* param_c = Param("c", ty.u32(), utils::Vector{Location(1)});
+
+    GlobalVar("my_vec", ty.vec4<f32>(), ast::StorageClass::kPrivate);
+    auto* func = Func("my_func",
+                      utils::Vector{
+                          param_a,
+                          param_b,
+                          param_c,
+                      },
+                      ty.vec4<f32>(),
+                      utils::Vector{
+                          Return("my_vec"),
+                      },
+                      utils::Vector{
+                          Stage(ast::PipelineStage::kVertex),
+                      },
+                      utils::Vector{
+                          Builtin(ast::BuiltinValue::kPosition),
+                      });
+
+    EXPECT_TRUE(r()->Resolve()) << r()->error();
+
+    auto* func_sem = Sem().Get(func);
+    ASSERT_NE(func_sem, nullptr);
+    EXPECT_EQ(func_sem->Parameters().Length(), 3u);
+    ASSERT_TRUE(func_sem->Parameters()[0]->Location().has_value());
+    EXPECT_EQ(3u, func_sem->Parameters()[0]->Location().value());
+
+    EXPECT_FALSE(func_sem->Parameters()[1]->Location().has_value());
+
+    ASSERT_TRUE(func_sem->Parameters()[2]->Location().has_value());
+    EXPECT_EQ(1u, func_sem->Parameters()[2]->Location().value());
+}
+
 TEST_F(ResolverTest, Function_RegisterInputOutputVariables) {
     auto* s = Structure("S", utils::Vector{Member("m", ty.u32())});
 
@@ -808,7 +845,7 @@ TEST_F(ResolverTest, Function_ReturnType_Location) {
                           Return(1_f),
                       },
                       utils::Vector{
-                          Stage(ast::PipelineStage::kFragment),
+                          Stage(ast::PipelineStage::kCompute),
                       },
                       utils::Vector{
                           Location(2),
