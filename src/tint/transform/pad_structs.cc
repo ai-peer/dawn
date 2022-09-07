de2cc05ba595ac7da04f217af7ab780e24856750
Stephen White <senorblanco@chromium.org>
2022-09-07 16:06:19 -0400

    Simplify to null transform to debug.
    Change-Id: If5bd67a0f4ecf45d6551b9f6fad2c801ec3ee3eb

--- src/tint/transform/pad_structs.cc
+++ src/tint/transform/pad_structs.cc
@@ -14,68 +14,17 @@
 
 #include "src/tint/transform/pad_structs.h"
 
-#include "src/tint/program_builder.h"
-#include "src/tint/sem/module.h"
+// #include "src/tint/program_builder.h"
+// #include "src/tint/sem/module.h"
 
 TINT_INSTANTIATE_TYPEINFO(tint::transform::PadStructs);
 
 namespace tint::transform {
 
-namespace {
-const ast::StructMember* CreatePadding(ProgramBuilder* b, uint32_t bytes) {
-    auto name = b->Symbols().New("padding");
-    const ast::Type* type = b->ty.f32();
-    return b->Member(name, type);
-}
-
-}
-
 PadStructs::PadStructs() = default;
 
 PadStructs::~PadStructs() = default;
 
-void PadStructs::Run(CloneContext& ctx, const DataMap& inputs, DataMap&) const {
-    auto& sem = ctx.src->Sem();
-
-    ctx.ReplaceAll([&](const ast::Struct* ast_str) -> const ast::Struct* {
-        auto* str = sem.Get<sem::Struct>(ast_str);
-        if (!str) {
-            return nullptr;
-        }
-        uint32_t offset = 0;
-        bool host_shareable = str->IsHostShareable();
-        bool has_runtime_sized_array = false;
-        utils::Vector<const ast::StructMember*, 8> new_members;
-        for (auto* mem : str->Members()) {
-            auto name = ctx.src->Symbols().NameFor(mem->Name());
-
-            if (host_shareable && offset < mem->Offset()) {
-                new_members.Push(CreatePadding(ctx.dst, mem->Offset() - offset));
-                offset = mem->Offset();
-            }
-
-            auto* ty = mem->Type();
-            const ast::Type* type = CreateASTTypeFor(ctx, ty);
-            new_members.Push(ctx.dst->Member(name, type));
-
-            if (host_shareable) {
-                uint32_t size = ty->Size();
-                if (ty->Is<sem::Struct>()) {
-                    // GLSL structs are already padded out to a multiple of 16.
-                    size = utils::RoundUp(16u, size);
-                } else if (auto* array_ty = ty->As<sem::Array>()) {
-                    if (array_ty->Count() == 0) {
-                        has_runtime_sized_array = true;
-                    }
-                }
-                offset += size;
-            }
-        }
-        if (host_shareable && offset < str->Size() && !has_runtime_sized_array) {
-            new_members.Push(CreatePadding(ctx.dst, str->Size() - offset));
-        }
-        return ctx.dst->Structure(ctx.Clone(str->Name()), std::move(new_members));
-    });
-}
+void PadStructs::Run(CloneContext& ctx, const DataMap& inputs, DataMap&) const {}
 
 }  // namespace tint::transform
