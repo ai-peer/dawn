b072ea828b38f24a1f40063c80cfb544e5bc1942
Alastair Donaldson <afdx@google.com>
2022-09-16 16:21:50 +0000

    Fix out-of-bounds access in regex fuzzer
    Fixes the regex fuzzer so that when searching for an operator to
    replace, it takes account of the fact that the string being searched may
    be very small, avoiding an issue where unsigned integer underflow would
    occur.
    
    Bug: crbug.com/1359193
    Change-Id: I653a20429dc20385a64f8d684c81d023702458e6

--- src/tint/fuzzers/tint_regex_fuzzer/wgsl_mutator.cc
+++ src/tint/fuzzers/tint_regex_fuzzer/wgsl_mutator.cc
@@ -464,8 +464,9 @@ std::optional<std::pair<uint32_t, uint32_t>> WgslMutator::FindOperatorOccurrence
         char first_character = wgsl_code[current_index];
         char second_character =
             current_index == wgsl_code.size() - 1 ? '\0' : wgsl_code[current_index + 1];
-        char third_character =
-            current_index >= wgsl_code.size() - 2 ? '\0' : wgsl_code[current_index + 2];
+        char third_character = (wgsl_code.size() < 2 || current_index >= wgsl_code.size() - 2)
+                                   ? '\0'
+                                   : wgsl_code[current_index + 2];
 
         // This uses the extracted characters to match for the various WGSL operators.
         switch (first_character) {
