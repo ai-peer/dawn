0c36d4b1a6c8a8f18ed91bd4abdeccfb86158bba
Zhaoming Jiang <zhaoming.jiang@intel.com>
2022-09-16 10:00:11 +0800

    WIP: F16 uniform and storage buffer with std140 fix
    Change-Id: I5e80e1ba44812a3a61b30c6c112eee0c23430442

--- test/tint/buffer/uniform/std140/struct/mat3x2/to_private.wgsl.expected.fxc.hlsl
+++ test/tint/buffer/uniform/std140/struct/mat3x2/to_private.wgsl.expected.fxc.hlsl
@@ -10,19 +10,24 @@ cbuffer cbuffer_u : register(b0, space0) {
 static S p[4] = (S[4])0;
 
 float3x2 tint_symbol_3(uint4 buffer[20], uint offset) {
-  const uint scalar_offset = ((offset + 0u)) / 4;
-  uint4 ubo_load = buffer[scalar_offset / 4];
-  const uint scalar_offset_1 = ((offset + 8u)) / 4;
-  uint4 ubo_load_1 = buffer[scalar_offset_1 / 4];
-  const uint scalar_offset_2 = ((offset + 16u)) / 4;
-  uint4 ubo_load_2 = buffer[scalar_offset_2 / 4];
-  return float3x2(asfloat(((scalar_offset & 2) ? ubo_load.zw : ubo_load.xy)), asfloat(((scalar_offset_1 & 2) ? ubo_load_1.zw : ubo_load_1.xy)), asfloat(((scalar_offset_2 & 2) ? ubo_load_2.zw : ubo_load_2.xy)));
+  const uint scalar_offset_bytes = ((offset + 0u));
+  const uint scalar_offset_index = scalar_offset_bytes / 4;
+  uint4 ubo_load = buffer[scalar_offset_index / 4];
+  const uint scalar_offset_bytes_1 = ((offset + 8u));
+  const uint scalar_offset_index_1 = scalar_offset_bytes_1 / 4;
+  uint4 ubo_load_1 = buffer[scalar_offset_index_1 / 4];
+  const uint scalar_offset_bytes_2 = ((offset + 16u));
+  const uint scalar_offset_index_2 = scalar_offset_bytes_2 / 4;
+  uint4 ubo_load_2 = buffer[scalar_offset_index_2 / 4];
+  return float3x2(asfloat(((scalar_offset_index & 2) ? ubo_load.zw : ubo_load.xy)), asfloat(((scalar_offset_index_1 & 2) ? ubo_load_1.zw : ubo_load_1.xy)), asfloat(((scalar_offset_index_2 & 2) ? ubo_load_2.zw : ubo_load_2.xy)));
 }
 
 S tint_symbol_1(uint4 buffer[20], uint offset) {
-  const uint scalar_offset_3 = ((offset + 0u)) / 4;
-  const uint scalar_offset_4 = ((offset + 72u)) / 4;
-  const S tint_symbol_5 = {asint(buffer[scalar_offset_3 / 4][scalar_offset_3 % 4]), tint_symbol_3(buffer, (offset + 8u)), asint(buffer[scalar_offset_4 / 4][scalar_offset_4 % 4])};
+  const uint scalar_offset_bytes_3 = ((offset + 0u));
+  const uint scalar_offset_index_3 = scalar_offset_bytes_3 / 4;
+  const uint scalar_offset_bytes_4 = ((offset + 72u));
+  const uint scalar_offset_index_4 = scalar_offset_bytes_4 / 4;
+  const S tint_symbol_5 = {asint(buffer[scalar_offset_index_3 / 4][scalar_offset_index_3 % 4]), tint_symbol_3(buffer, (offset + 8u)), asint(buffer[scalar_offset_index_4 / 4][scalar_offset_index_4 % 4])};
   return tint_symbol_5;
 }
 
--- test/tint/buffer/uniform/types/mat2x3.wgsl.expected.dxc.hlsl
+++ test/tint/buffer/uniform/types/mat2x3.wgsl.expected.dxc.hlsl
@@ -3,9 +3,11 @@ cbuffer cbuffer_u : register(b0, space0) {
 };
 
 float2x3 tint_symbol(uint4 buffer[2], uint offset) {
-  const uint scalar_offset = ((offset + 0u)) / 4;
-  const uint scalar_offset_1 = ((offset + 16u)) / 4;
-  return float2x3(asfloat(buffer[scalar_offset / 4].xyz), asfloat(buffer[scalar_offset_1 / 4].xyz));
+  const uint scalar_offset_bytes = ((offset + 0u));
+  const uint scalar_offset_index = scalar_offset_bytes / 4;
+  const uint scalar_offset_bytes_1 = ((offset + 16u));
+  const uint scalar_offset_index_1 = scalar_offset_bytes_1 / 4;
+  return float2x3(asfloat(buffer[scalar_offset_index / 4].xyz), asfloat(buffer[scalar_offset_index_1 / 4].xyz));
 }
 
 [numthreads(1, 1, 1)]
