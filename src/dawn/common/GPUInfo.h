8bc4ae6ce8f6b025f45a4a148bc4424ab9994d68
Li Hao <hao.x.li@intel.com>
2022-09-20 16:21:16 +0800

    Disable Vulkan backend on old Intel Windows driver version
    Skip enabling Vulkan backend on Intel Windows driver version < 30.0.101.2111 due to many flaky issue.
    
    Bug: chromium:1338622, dawn:1392
    Change-Id: I6975783bdc18d8a94d6c35e134756e3713833a29

--- src/dawn/common/GPUInfo.h
+++ src/dawn/common/GPUInfo.h
@@ -15,22 +15,53 @@
 #ifndef SRC_DAWN_COMMON_GPUINFO_H_
 #define SRC_DAWN_COMMON_GPUINFO_H_
 
-#include "dawn/common/GPUInfo_autogen.h"
+#include <algorithm>
+#include <iterator>
+#include <sstream>
+#include <string>
+#include <vector>
 
-#include <array>
+#include "dawn/common/GPUInfo_autogen.h"
+#include "dawn/common/ityp_stack_vec.h"
 
 namespace gpu_info {
 
-using D3DDriverVersion = std::array<uint16_t, 4>;
+template <typename Index, typename Value, size_t StaticCapacity>
+class DriverVersionVector : public ityp::stack_vec<Index, Value, StaticCapacity> {
+    using Base = ityp::stack_vec<Index, Value, StaticCapacity>;
+
+  public:
+    DriverVersionVector() : Base() {}
+    explicit DriverVersionVector(const std::vector<Value>& ver) : Base(ver) {}
+
+    std::string toString() const {
+        std::ostringstream oss;
+        if (this->size() > 0) {
+            // Convert all but the last element to avoid a trailing "."
+            std::copy(this->begin(), this->end() - 1, std::ostream_iterator<Value>(oss, "."));
+            // Add the last element
+            oss << this->back();
+        }
+
+        return oss.str();
+    }
+};
+
+// Four uint16 fields could cover almost all driver version schemas:
+// D3D12: AA.BB.CCC.DDDD
+// Vulkan: AAA.BBB.CCC.DDD on Nvidia, CCC.DDDD for Intel Windows, and AA.BB.CCC for others,
+// See https://vulkan.gpuinfo.org/
+static constexpr uint32_t kMaxVersionFields = 4;
+using DriverVersion = DriverVersionVector<uint32_t, uint16_t, kMaxVersionFields>;
 
 // Do comparison between two driver versions. Currently we only support the comparison between
-// Intel D3D driver versions.
+// Intel Windows driver versions.
 // - Return -1 if build number of version1 is smaller
 // - Return 1 if build number of version1 is bigger
 // - Return 0 if version1 and version2 represent same driver version
-int CompareD3DDriverVersion(PCIVendorID vendorId,
-                            const D3DDriverVersion& version1,
-                            const D3DDriverVersion& version2);
+int CompareWindowsDriverVersion(PCIVendorID vendorId,
+                                const DriverVersion& version1,
+                                const DriverVersion& version2);
 
 // Intel architectures
 bool IsSkylake(PCIDeviceID deviceId);
