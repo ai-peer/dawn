2015be0e9969d00c1ae63fe4e12ae3fe72c6d926
dan sinclair <dsinclair@chromium.org>
2022-09-20 19:22:27 -0400

    Convert `align` attribute to expressions.
    This CL updates the align attribute to parse expressions.
    
    Bug: tint:1633
    Change-Id: I5412180ff62e6b286b35ea3297e2b4f136899960

--- src/tint/resolver/resolver.cc
+++ src/tint/resolver/resolver.cc
@@ -2280,7 +2280,7 @@ sem::Expression* Resolver::Identifier(const ast::IdentifierExpression* expr) {
             // Note: The spec is currently vague around the rules here. See
             // https://github.com/gpuweb/gpuweb/issues/3081. Remove this comment when resolved.
             std::string desc = "var '" + builder_->Symbols().NameFor(symbol) + "' ";
-            AddError(desc + "cannot not be referenced at module-scope", expr->source);
+            AddError(desc + "cannot be referenced at module-scope", expr->source);
             AddNote(desc + "declared here", variable->Declaration()->source);
             return nullptr;
         }
@@ -2826,9 +2826,14 @@ sem::Struct* Resolver::Structure(const ast::Struct* str) {
                 if (!materialized) {
                     return nullptr;
                 }
+                if (!materialized->Type()->Is<sem::I32>()) {
+                    AddError("'align' must be an i32 value", a->source);
+                    return nullptr;
+                }
+
                 auto const_value = materialized->ConstantValue();
                 if (!const_value) {
-                    AddError("'align' must be constant expression", a->expr->source);
+                    AddError("'align' must be constant expression", a->source);
                     return nullptr;
                 }
                 auto value = const_value->As<AInt>();
