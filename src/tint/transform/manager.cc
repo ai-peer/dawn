6b12c9d59246b78d1206df2e6841c202febf2bfb
shrekshao <shrekshao@google.com>
2022-10-04 14:35:25 -0700

    Tint truncate interstage variable transform
    Bug: dawn:1493
    Change-Id: I69081189ad7d4b76f2371fcc079f67dced2e9944

--- src/tint/transform/manager.cc
+++ src/tint/transform/manager.cc
@@ -13,10 +13,19 @@
 // limitations under the License.
 
 #include "src/tint/transform/manager.h"
+#include "include/tint/tint.h"
+// #include "src/tint/ast/module.h"
+// #include "src/tint/diagnostic/formatter.h"
+// #include "src/tint/program.h"
+// #include "src/tint/utils/hash.h"
+// #include "src/tint/writer/flatten_bindings.h"
+
+#include "src/tint/transform/truncate_interstage_variables.h"
 
 /// If set to 1 then the transform::Manager will dump the WGSL of the program
 /// before and after each transform. Helpful for debugging bad output.
-#define TINT_PRINT_PROGRAM_FOR_EACH_TRANSFORM 0
+// #define TINT_PRINT_PROGRAM_FOR_EACH_TRANSFORM 0
+#define TINT_PRINT_PROGRAM_FOR_EACH_TRANSFORM 1
 
 #if TINT_PRINT_PROGRAM_FOR_EACH_TRANSFORM
 #define TINT_IF_PRINT_PROGRAM(x) x
@@ -35,8 +44,19 @@ Transform::ApplyResult Manager::Apply(const Program* program,
                                       const DataMap& inputs,
                                       DataMap& outputs) const {
 #if TINT_PRINT_PROGRAM_FOR_EACH_TRANSFORM
+    Program::printer = [](const tint::Program* program) {
+        auto result = tint::writer::wgsl::Generate(program, {});
+        if (!result.error.empty()) {
+            return "error: " + result.error;
+        }
+        return result.wgsl;
+    };
+
     auto print_program = [&](const char* msg, const Transform* transform) {
-        auto wgsl = Program::printer(in);
+        if (!transform->Is<tint::transform::TruncateInterstageVariables>()) {
+            return;
+        }
+        auto wgsl = Program::printer(program);
         std::cout << "---------------------------------------------------------" << std::endl;
         std::cout << "-- " << msg << " " << transform->TypeInfo().name << ":" << std::endl;
         std::cout << "---------------------------------------------------------" << std::endl;
---
+++ src/tint/transform/truncate_interstage_variables.cc
@@ -0,0 +1,194 @@
+// Copyright 2022 The Tint Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "src/tint/transform/truncate_interstage_variables.h"
+
+#include <memory>
+#include <unordered_set>
+#include <utility>
+
+#include "src/tint/program_builder.h"
+#include "src/tint/sem/call.h"
+#include "src/tint/sem/function.h"
+#include "src/tint/sem/member_accessor_expression.h"
+#include "src/tint/sem/statement.h"
+#include "src/tint/sem/variable.h"
+#include "src/tint/text/unicode.h"
+
+TINT_INSTANTIATE_TYPEINFO(tint::transform::TruncateInterstageVariables);
+TINT_INSTANTIATE_TYPEINFO(tint::transform::TruncateInterstageVariables::Config);
+
+namespace tint::transform {
+
+namespace {
+
+bool ShouldRun(const Program* program, const DataMap& data) {
+    auto* cfg = data.Get<TruncateInterstageVariables::Config>();
+    if (!cfg) {
+        // Skip if there is no data for what interstage variables need preserved.
+        return false;
+    }
+
+    if (cfg->interstage_locations.IsEmpty()) {
+        return false;
+    }
+
+    for (auto* func_ast : program->AST().Functions()) {
+        if (!func_ast->IsEntryPoint()) {
+            continue;
+        }
+
+        if (func_ast->PipelineStage() == ast::PipelineStage::kVertex) {
+            // Run if function is vertex stage entry point, which is the only program stage with
+            // interstage outputs now.
+            return true;
+        }
+    }
+    return false;
+}
+
+}  // anonymous namespace
+
+TruncateInterstageVariables::TruncateInterstageVariables() = default;
+TruncateInterstageVariables::~TruncateInterstageVariables() = default;
+
+Transform::ApplyResult TruncateInterstageVariables::Apply(const Program* src,
+                                                          const DataMap& config,
+                                                          DataMap& outputs) const {
+    ProgramBuilder b;
+    CloneContext ctx{&b, src, /* auto_clone_symbols */ true};
+
+    if (!ShouldRun(src, config)) {
+        return SkipTransform;
+    }
+
+    const auto* data = config.Get<Config>();
+
+    auto& sem = ctx.src->Sem();
+    auto& sym = ctx.src->Symbols();
+
+    for (auto* func_ast : ctx.src->AST().Functions()) {
+        if (!func_ast->IsEntryPoint()) {
+            continue;
+        }
+
+        if (func_ast->PipelineStage() != ast::PipelineStage::kVertex) {
+            // Currently only vertex stage could have interstage output variables that need
+            // truncated.
+            continue;
+        }
+
+        utils::Vector<const ast::StructMember*, 20> truncated_members;
+        utils::Vector<const ast::StructMember*, 4> builtin_output_members;
+
+        auto* func_sem = sem.Get(func_ast);
+        auto* str = func_sem->ReturnType()->As<sem::Struct>();
+        if (str) {
+            // This transform is run after CanonicalizeEntryPointIO transform,
+            // So it is guaranteed that entry point inputs are already grouped in a struct.
+            const ast::Struct* struct_ty = str->Declaration();
+
+            // Create a new struct for the interstage inputs & outputs.
+            auto new_struct_sym = b.Symbols().New("TruncatedShaderIO");
+
+            // Statements inside the mapping function from original shader io to trancated shader
+            // io.
+            utils::Vector<const ast::Statement*, 22> truncate_func_statements{
+                b.Decl(b.Var("result", b.ty.type_name(new_struct_sym)))};
+
+            for (auto* member : struct_ty->members) {
+                if (auto* attr = ast::GetAttribute<ast::BuiltinAttribute>(member->attributes)) {
+                    // For builtin interstage variables, copy as is.
+                    std::string member_name = sym.NameFor(member->symbol);
+                    builtin_output_members.Push(b.Member(member_name, ctx.Clone(member->type),
+                                                         ctx.Clone(member->attributes)));
+                    truncate_func_statements.Push(
+                        b.Assign(b.MemberAccessor("result", ctx.Clone(member->symbol)),
+                                 b.MemberAccessor("io", ctx.Clone(member->symbol))));
+
+                    ctx.Remove(member->attributes, attr);
+
+                    // TODO: remove invariant for position built-in
+                } else if (auto* attr =
+                               ast::GetAttribute<ast::LocationAttribute>(member->attributes)) {
+                    // For user defined shader io, only preserve those with locations marked in the
+                    // input.
+                    auto* m = sem.Get(member);
+                    uint32_t location = m->Location().value();
+                    if (data->interstage_locations[location] == true) {
+                        std::string member_name = sym.NameFor(member->symbol);
+                        truncated_members.Push(b.Member(member_name, ctx.Clone(member->type),
+                                                        ctx.Clone(member->attributes)));
+
+                        truncate_func_statements.Push(
+                            b.Assign(b.MemberAccessor("result", ctx.Clone(member->symbol)),
+                                     b.MemberAccessor("io", ctx.Clone(member->symbol))));
+                    }
+                    ctx.Remove(member->attributes, attr);
+
+                    // TODO: other attributes?
+                    if (auto* interpolate_attr =
+                            ast::GetAttribute<ast::InterpolateAttribute>(member->attributes)) {
+                        ctx.Remove(member->attributes, interpolate_attr);
+                    }
+                }
+            }
+
+            truncate_func_statements.Push(b.Return("result"));
+
+            if (!builtin_output_members.IsEmpty()) {
+                for (const ast::StructMember* builtin_member : builtin_output_members) {
+                    truncated_members.Push(builtin_member);
+                }
+            }
+
+            auto* new_structure = b.Structure(new_struct_sym, truncated_members);
+
+            // Create the mapping function to truncate the shader io.
+            auto mapping_fn_sym = b.Symbols().New("truncate_shader_output");
+            auto* mapping_fn = b.Func(
+                mapping_fn_sym, utils::Vector{b.Param("io", ctx.Clone(func_ast->return_type))},
+                b.ty.type_name(new_struct_sym), truncate_func_statements);
+
+            ctx.Replace(func_ast->return_type, b.ty.type_name(new_struct_sym));
+
+            ctx.ReplaceAll(
+                [&](const ast::ReturnStatement* return_statement) -> const ast::ReturnStatement* {
+                    auto* return_sem = sem.Get(return_statement);
+                    // if (return_sem->As<sem::Statement>() == func_sem) {
+                    if (return_sem->Function() == func_sem) {
+                        return b.Return(return_statement->source,
+                                        b.Call(mapping_fn_sym, ctx.Clone(return_statement->value)));
+                    }
+                    return nullptr;
+                });
+        } else {
+            TINT_UNREACHABLE(Transform, ctx.dst->Diagnostics());
+        }
+    }
+
+    ctx.Clone();
+    return Program(std::move(b));
+}
+
+TruncateInterstageVariables::Config::Config() = default;
+
+TruncateInterstageVariables::Config::Config(const Config&) = default;
+
+TruncateInterstageVariables::Config::~Config() = default;
+
+TruncateInterstageVariables::Config& TruncateInterstageVariables::Config::operator=(const Config&) =
+    default;
+
+}  // namespace tint::transform
