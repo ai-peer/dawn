35ea3fea048f416ecbdfe1f8372651865ce70bd2
shrekshao <shrekshao@google.com>
2022-10-04 14:35:25 -0700

    Tint truncate interstage variable transform
    Add a transform to truncate unused user interstage variables by
    adding a new truncated shader io struct wrapper of the original
    one, with a truncate function to do the assignments called
    at the return statement.
    
    This transform is meant to be run after CanonicalizeEntryPointIO,
    and will only be run under hlsl/generator_impl.cc to workaround
    the extra register limitation for interstage variables on D3D FXC.
    
    Bug: dawn:1493
    Change-Id: I69081189ad7d4b76f2371fcc079f67dced2e9944

---
+++ src/tint/transform/truncate_interstage_variables.h
@@ -0,0 +1,68 @@
+// Copyright 2022 The Tint Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SRC_TINT_TRANSFORM_TRUNCATE_INTERSTAGE_VARIABLES_H_
+#define SRC_TINT_TRANSFORM_TRUNCATE_INTERSTAGE_VARIABLES_H_
+
+#include <array>
+#include <string>
+#include <unordered_map>
+
+#include "src/tint/sem/binding_point.h"
+#include "src/tint/transform/transform.h"
+#include "src/tint/utils/bitset.h"
+
+namespace tint::transform {
+
+/// TruncateInterstageVariables is a Transform that truncate certain inter variables based on its
+/// config.
+class TruncateInterstageVariables final : public Castable<TruncateInterstageVariables, Transform> {
+  public:
+    /// Configuration options for the transform
+    struct Config final : public Castable<Config, Data> {
+        /// Constructor
+        Config();
+
+        /// Copy constructor
+        Config(const Config&);
+
+        /// Destructor
+        ~Config() override;
+
+        /// Assignment operator
+        /// @returns this Config
+        Config& operator=(const Config&);
+
+        // Indicate which interstage io locations are actually used by the later stage.
+        utils::Bitset<16> interstage_locations;
+
+        /// Reflect the fields of this class so that it can be used by tint::ForeachField()
+        TINT_REFLECT(interstage_variables);
+    };
+
+    /// Constructor using a the configuration provided in the input Data
+    TruncateInterstageVariables();
+
+    /// Destructor
+    ~TruncateInterstageVariables() override;
+
+    /// @copydoc Transform::Apply
+    ApplyResult Apply(const Program* program,
+                      const DataMap& inputs,
+                      DataMap& outputs) const override;
+};
+
+}  // namespace tint::transform
+
+#endif  // SRC_TINT_TRANSFORM_TRUNCATE_INTERSTAGE_VARIABLES_H_
