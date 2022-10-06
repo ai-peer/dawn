e316eb9bd288d68f918884732aee106c1e391367
Ben Clayton <bclayton@google.com>
2022-10-06 14:56:14 +0100

    wgsl: Print abstract-floats with full precision.
    Change-Id: Ie95547f065b896983b90ffd5455538fdd843b81a

--- src/tint/writer/float_to_string.cc
+++ src/tint/writer/float_to_string.cc
@@ -59,6 +59,33 @@ std::string FloatToString(float f) {
     return sci.str();
 }
 
+std::string DoubleToString(double f) {
+    // Try printing the double in fixed point, with a smallish limit on the
+    // precision
+    std::stringstream fixed;
+    fixed.flags(fixed.flags() | std::ios_base::showpoint | std::ios_base::fixed);
+    fixed.precision(9);
+    fixed << f;
+
+    // If this string can be parsed without loss of information, use it
+    auto double_equal_no_warning = std::equal_to<double>();
+    if (double_equal_no_warning(std::stod(fixed.str()), f)) {
+        auto str = fixed.str();
+        while (str.length() >= 2 && str[str.size() - 1] == '0' && str[str.size() - 2] != '.') {
+            str.pop_back();
+        }
+
+        return str;
+    }
+
+    // Resort to scientific, with the minimum precision needed to preserve the
+    // whole float
+    std::stringstream sci;
+    sci.precision(std::numeric_limits<double>::max_digits10);
+    sci << f;
+    return sci.str();
+}
+
 std::string FloatToBitPreservingString(float f) {
     // For the NaN case, avoid handling the number as a floating point value.
     // Some machines will modify the top bit in the mantissa of a NaN.
@@ -129,6 +156,106 @@ std::string FloatToBitPreservingString(float f) {
                 // Subnormal, and not zero.
                 TINT_ASSERT(Writer, mantissa != 0);
                 const int kTopBit = (1 << kMantissaBits);
+
+                // Shift left until we get 1.x
+                while (0 == (kTopBit & mantissa)) {
+                    mantissa <<= 1;
+                    exponent--;
+                }
+                // Emit the leading 1, and remove it from the mantissa.
+                ss << "1";
+                mantissa = mantissa ^ kTopBit;
+                mantissa <<= 1;
+                exponent++;
+
+                // Emit the fractional part.
+                if (mantissa) {
+                    // Remove trailing zeroes, for tidyness
+                    while (0 == (0xf & mantissa)) {
+                        mantissa >>= 4;
+                        mantissaNibbles--;
+                    }
+                    ss << "." << std::hex << std::setfill('0') << std::setw(mantissaNibbles)
+                       << mantissa;
+                }
+                // Emit the exponent
+                ss << "p" << std::showpos << std::dec << exponent;
+            }
+        }
+    }
+    return ss.str();
+}
+
+std::string DoubleToBitPreservingString(double f) {
+    // For the NaN case, avoid handling the number as a floating point value.
+    // Some machines will modify the top bit in the mantissa of a NaN.
+
+    std::stringstream ss;
+
+    uint64_t double_bits = 0u;
+    std::memcpy(&double_bits, &f, sizeof(double_bits));
+
+    // Handle the sign.
+    const uint64_t kSignMask = 1ull << 63;
+    if (double_bits & kSignMask) {
+        // If `f` is -0.0 print -0.0.
+        ss << '-';
+        // Strip sign bit.
+        double_bits = double_bits & (~kSignMask);
+    }
+
+    switch (std::fpclassify(f)) {
+        case FP_ZERO:
+        case FP_NORMAL:
+            std::memcpy(&f, &double_bits, sizeof(double_bits));
+            ss << DoubleToString(f);
+            break;
+
+        default: {
+            // Infinity, NaN, and Subnormal
+            // TODO(dneto): It's unclear how Infinity and NaN should be handled.
+            // See https://github.com/gpuweb/gpuweb/issues/1769
+
+            // std::hexfloat prints 'nan' and 'inf' instead of an
+            // explicit representation like we want. Split it out
+            // manually.
+            const int kExponentBias = 1023;
+            const uint64_t kExponentMask = 0x7ff0000000000000;
+            const uint64_t kMantissaMask = 0x000fffffffffffff;
+            const int kMantissaBits = 52;
+
+            int mantissaNibbles = (kMantissaBits + 3) / 4;
+
+            const int biased_exponent =
+                static_cast<int>((double_bits & kExponentMask) >> kMantissaBits);
+            int exponent = biased_exponent - kExponentBias;
+            uint64_t mantissa = double_bits & kMantissaMask;
+
+            ss << "0x";
+
+            if (exponent == 1024) {
+                if (mantissa == 0) {
+                    //  Infinity case.
+                    ss << "1p+1024";
+                } else {
+                    //  NaN case.
+                    //  Emit the mantissa bits as if they are left-justified after the
+                    //  binary point.  This is what SPIRV-Tools hex float emitter does,
+                    //  and it's a justifiable choice independent of the bit width
+                    //  of the mantissa.
+                    mantissa <<= (4 - (kMantissaBits % 4));
+                    // Remove trailing zeroes, for tidyness.
+                    while (0 == (0xf & mantissa)) {
+                        mantissa >>= 4;
+                        mantissaNibbles--;
+                    }
+                    ss << "1." << std::hex << std::setfill('0') << std::setw(mantissaNibbles)
+                       << mantissa << "p+128";
+                }
+            } else {
+                // Subnormal, and not zero.
+                TINT_ASSERT(Writer, mantissa != 0);
+                const uint64_t kTopBit = (1ull << kMantissaBits);
 
                 // Shift left until we get 1.x
                 while (0 == (kTopBit & mantissa)) {
