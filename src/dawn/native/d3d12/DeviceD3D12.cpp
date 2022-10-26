5c35bb2b0b99d942fa8dc25545c01942ff42c653
Jiawei Shao <jiawei.shao@intel.com>
2022-10-26 11:16:59 +0800

    Disable workaround for T2T copy issue on Intel GPUs on latest driver
    This patch disables the workaround for the T2T copy issue on Intel
    GPUs on the latest Intel D3D driver 31.0.101.2114 as the driver bug
    has been fixed in that driver.
    
    Bug: chromium:1161355
    Test: dawn_end2end_tests
    Change-Id: I5a1d01e2da519a133aacfac695e180b72715fac6

--- src/dawn/native/d3d12/DeviceD3D12.cpp
+++ src/dawn/native/d3d12/DeviceD3D12.cpp
@@ -674,8 +674,13 @@ void Device::InitTogglesFromDriver() {
     // Currently this workaround is only needed on Intel Gen9, Gen9.5 and Gen11 GPUs.
     // See http://crbug.com/1161355 for more information.
     if (gpu_info::IsIntelGen9(vendorId, deviceId) || gpu_info::IsIntelGen11(vendorId, deviceId)) {
-        SetToggle(Toggle::UseTempBufferInSmallFormatTextureToTextureCopyFromGreaterToLessMipLevel,
-                  true);
+        const gpu_info::D3DDriverVersion version = {30, 0, 101, 2114};
+        if (gpu_info::CompareD3DDriverVersion(vendorId, ToBackend(GetAdapter())->GetDriverVersion(),
+                                              version) < 0) {
+            SetToggle(
+                Toggle::UseTempBufferInSmallFormatTextureToTextureCopyFromGreaterToLessMipLevel,
+                true);
+        }
     }
 
     // Currently this workaround is only needed on Intel Gen9, Gen9.5 and Gen12 GPUs.
