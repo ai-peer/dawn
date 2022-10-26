3ed09e8c7fe6958bc75c43d4a1d23dd596219ac4
dan sinclair <dsinclair@chromium.org>
2022-10-26 16:27:19 -0400

    Start adding IR.
    This CL starts adding the basics for an IR for Tint. The IR is
    a lower level representation then the AST with several of the
    restrictions removed. For example, the IR is mutable, is a DAG
    instead of a tree, and removes a number of WGSL constructs to
    simplify transforms.
    
    Bug: tint:1718
    Change-Id: I6a16ac3116cee31410896c3a0424af7b41f958c3

---
+++ src/tint/ir/module.h
@@ -0,0 +1,57 @@
+// Copyright 2022 The Tint Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SRC_TINT_IR_MODULE_H_
+#define SRC_TINT_IR_MODULE_H_
+
+#include "src/tint/ir/function_flow_node.h"
+#include "src/tint/utils/block_allocator.h"
+#include "src/tint/utils/vector.h"
+
+// Forward Declarations
+namespace tint {
+class Program;
+}  // namespace tint
+
+namespace tint::ir {
+
+/// Main module class for the IR.
+class Module {
+  public:
+    /// Constructor
+    /// @param program the program this module was constructed from
+    explicit Module(const Program* program);
+    /// Move constructor
+    /// @param o the module to move from
+    Module(Module&& o);
+    /// Destructor
+    ~Module();
+
+    /// Move assign
+    /// @param o the module to assign from
+    /// @returns a reference to this module
+    Module& operator=(Module&& o);
+
+    /// The flow node allocator
+    utils::BlockAllocator<FlowNode> flow_nodes;
+
+    /// List of functions in the program
+    utils::Vector<FunctionFlowNode*, 8> functions;
+    /// List of indexes into the functions list for the entry points
+    utils::Vector<FunctionFlowNode*, 8> entry_points;
+};
+
+}  // namespace tint::ir
+
+#endif  // SRC_TINT_IR_MODULE_H_
