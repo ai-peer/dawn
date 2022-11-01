bc7add3c3109dc168c7aa449e994d6002d7dc63b
dan sinclair <dsinclair@chromium.org>
2022-11-01 17:02:31 -0400

    Update GN to use tint generator by default.
    This CL flips the GN build to use the generator during the build
    by default.
    
    Change-Id: Iadba897b85f9b7114b33e6b6438a46c7812a02fe

--- tools/src/cmd/gen/main.go
+++ tools/src/cmd/gen/main.go
@@ -84,7 +84,7 @@ func run() error {
 	// Find clang-format
 	clangFormatPath := findClangFormat(projectRoot)
 	if clangFormatPath == "" {
-		return fmt.Errorf("cannot find clang-format in <dawn>/buildtools nor PATH")
+		return fmt.Errorf("cannot find clang-format in %v/buildtools nor PATH", projectRoot)
 	}
 
 	files := flag.Args()
