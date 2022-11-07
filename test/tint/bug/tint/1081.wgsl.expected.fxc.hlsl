a4faccc5cecc584d2b0d44e8fbcb30eb7e1ce073
James Price <jrprice@google.com>
2022-11-07 15:05:29 -0500

    tint: Use "demote-to-helper" semantics for discard
    Discard statements no longer affect the behavior or uniformity
    analysis. Update the resolver, validator, and several tests to reflect
    this.
    
    Some E2E tests were removed as they had loops that are now considered
    to be infinite.
    
    Use the DemoteToHelper transform to emulate the correct semantics on
    platforms where discard is (or may) terminate the invocation in a
    manner that would affect derivative operations. We no longer need the
    UnwindDiscardFunctions transform for HLSL, which already implements
    the correct semantics.
    
    Fixed: tint:1723
    Change-Id: Ib49ff187919ae81c4af8675e1b66acd57e2ff7d2

--- test/tint/bug/tint/1081.wgsl.expected.fxc.hlsl
+++ test/tint/bug/tint/1081.wgsl.expected.fxc.hlsl
@@ -1,11 +1,12 @@
-static bool tint_discard = false;
-
 int f(int x) {
-  if ((x == 10)) {
-    tint_discard = true;
-    return 0;
+  if (true) {
+    if ((x == 10)) {
+      discard;
+    }
+    return x;
   }
-  return x;
+  int unused;
+  return unused;
 }
 
 struct tint_symbol_1 {
@@ -19,9 +20,6 @@ int main_inner(int3 x) {
   int y = x.x;
   while (true) {
     const int r = f(y);
-    if (tint_discard) {
-      return 0;
-    }
     if ((r == 0)) {
       break;
     }
@@ -29,17 +27,8 @@ int main_inner(int3 x) {
   return y;
 }
 
-void tint_discard_func() {
-  discard;
-}
-
 tint_symbol_2 main(tint_symbol_1 tint_symbol) {
   const int inner_result = main_inner(tint_symbol.x);
-  if (tint_discard) {
-    tint_discard_func();
-    const tint_symbol_2 tint_symbol_3 = (tint_symbol_2)0;
-    return tint_symbol_3;
-  }
   tint_symbol_2 wrapper_result = (tint_symbol_2)0;
   wrapper_result.value = inner_result;
   return wrapper_result;
--- test/tint/bug/tint/1118.wgsl.expected.fxc.hlsl
+++ test/tint/bug/tint/1118.wgsl.expected.fxc.hlsl
@@ -10,7 +10,6 @@ cbuffer cbuffer_x_137 : register(b2, space0) {
   uint4 x_137[1];
 };
 static float4 glFragColor = float4(0.0f, 0.0f, 0.0f, 0.0f);
-static bool tint_discard = false;
 
 void main_1() {
   float3 viewDirectionW = float3(0.0f, 0.0f, 0.0f);
@@ -31,13 +30,11 @@ void main_1() {
   float4 color = float4(0.0f, 0.0f, 0.0f, 0.0f);
   const float x_9 = fClipDistance3;
   if ((x_9 > 0.0f)) {
-    tint_discard = true;
-    return;
+    discard;
   }
   const float x_17 = fClipDistance4;
   if ((x_17 > 0.0f)) {
-    tint_discard = true;
-    return;
+    discard;
   }
   const float4 x_34 = asfloat(x_29[0]);
   const float3 x_38 = (0.0f).xxx;
@@ -105,25 +102,12 @@ main_out main_inner(float fClipDistance3_param, float fClipDistance4_param) {
   fClipDistance3 = fClipDistance3_param;
   fClipDistance4 = fClipDistance4_param;
   main_1();
-  if (tint_discard) {
-    const main_out tint_symbol_8 = (main_out)0;
-    return tint_symbol_8;
-  }
-  const main_out tint_symbol_9 = {glFragColor};
-  return tint_symbol_9;
-}
-
-void tint_discard_func() {
-  discard;
+  const main_out tint_symbol_8 = {glFragColor};
+  return tint_symbol_8;
 }
 
 tint_symbol_2 main(tint_symbol_1 tint_symbol) {
   const main_out inner_result = main_inner(tint_symbol.fClipDistance3_param, tint_symbol.fClipDistance4_param);
-  if (tint_discard) {
-    tint_discard_func();
-    const tint_symbol_2 tint_symbol_10 = (tint_symbol_2)0;
-    return tint_symbol_10;
-  }
   tint_symbol_2 wrapper_result = (tint_symbol_2)0;
   wrapper_result.glFragColor_1 = inner_result.glFragColor_1;
   return wrapper_result;
