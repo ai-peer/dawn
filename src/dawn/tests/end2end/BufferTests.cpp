9b570646a12efa77078499e019804edb8ff4b576
Jiawei Shao <jiawei.shao@intel.com>
2022-11-07 15:16:51 +0800

    Wire: Make validation error prior to OOM if mappedAtCreation == false
    This patch updates the validations about CreateBuffer() with dawn_wire
    to match the latest WebGPU SPEC.
    
    According to the SPEC, the validations in CreateBuffer() should be
    executed in the below order:
    1. If mappedAtCreation == true, return nullptr and a RangeError will be
       generated in Chromium.
    2. Validate BufferDescriptor and check if there is OOM at device timeline
    3. Check if there is OOM at content timeline
    
    Bug: dawn:1586
    Change-Id: I97ff5f82a42208442ddf6e46e66381c3b3680450

--- src/dawn/tests/end2end/BufferTests.cpp
+++ src/dawn/tests/end2end/BufferTests.cpp
@@ -819,6 +819,9 @@ TEST_P(BufferTests, CreateBufferOOM) {
     descriptor.size = 1ull << 50;
     // TODO(dawn:1525): remove warning expectation after the deprecation period.
     ASSERT_DEVICE_ERROR(EXPECT_DEPRECATION_WARNING(device.CreateBuffer(&descriptor)));
+
+    descriptor.usage = wgpu::BufferUsage::MapRead | wgpu::BufferUsage::Uniform;
+    ASSERT_DEVICE_ERROR(device.CreateBuffer(&descriptor));
 }
 
 // Test that a very large buffer mappedAtCreation fails gracefully.
@@ -844,12 +847,20 @@ TEST_P(BufferTests, BufferMappedAtCreationOOM) {
 
         // Test an enormous buffer fails
         descriptor.size = std::numeric_limits<uint64_t>::max();
-        ASSERT_DEVICE_ERROR(device.CreateBuffer(&descriptor));
+        if (UsesWire()) {
+            device.CreateBuffer(&descriptor);
+        } else {
+            ASSERT_DEVICE_ERROR(device.CreateBuffer(&descriptor));
+        }
 
         // UINT64_MAX may be special cased. Test a smaller, but really large buffer also fails
         descriptor.size = 1ull << 50;
-        // TODO(dawn:1525): remove warning expectation after the deprecation period.
-        ASSERT_DEVICE_ERROR(EXPECT_DEPRECATION_WARNING(device.CreateBuffer(&descriptor)));
+        if (UsesWire()) {
+            device.CreateBuffer(&descriptor);
+        } else {
+            // TODO(dawn:1525): remove warning expectation after the deprecation period.
+            ASSERT_DEVICE_ERROR(EXPECT_DEPRECATION_WARNING(device.CreateBuffer(&descriptor)));
+        }
     }
 
     // Test mappable buffer
@@ -864,12 +875,20 @@ TEST_P(BufferTests, BufferMappedAtCreationOOM) {
 
         // Test an enormous buffer fails
         descriptor.size = std::numeric_limits<uint64_t>::max();
-        ASSERT_DEVICE_ERROR(device.CreateBuffer(&descriptor));
+        if (UsesWire()) {
+            device.CreateBuffer(&descriptor);
+        } else {
+            ASSERT_DEVICE_ERROR(device.CreateBuffer(&descriptor));
+        }
 
         // UINT64_MAX may be special cased. Test a smaller, but really large buffer also fails
         descriptor.size = 1ull << 50;
-        // TODO(dawn:1525): remove warning expectation after the deprecation period.
-        ASSERT_DEVICE_ERROR(EXPECT_DEPRECATION_WARNING(device.CreateBuffer(&descriptor)));
+        if (UsesWire()) {
+            device.CreateBuffer(&descriptor);
+        } else {
+            // TODO(dawn:1525): remove warning expectation after the deprecation period.
+            ASSERT_DEVICE_ERROR(EXPECT_DEPRECATION_WARNING(device.CreateBuffer(&descriptor)));
+        }
     }
 }
 
