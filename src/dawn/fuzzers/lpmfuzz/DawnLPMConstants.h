0e6b1b2827a29a2190e7ebb4edc9fb304d810ef4
Brendon Tiszka <tiszka@chromium.org>
2022-11-09 15:32:55 -0500

    Add Dawn Wire Server LPM Fuzzer
    This adds a gramamr-based Dawn fuzzer based on libprotobuf-mutator
    that uses the contents of dawn.json, dawn_wire.json, and
    dawn_lpm.json to fuzz Dawn commands.
    
    Change-Id: Ife1642dda13d01d3308bdd5fe56cf85978399fd3

---
+++ src/dawn/fuzzers/lpmfuzz/DawnLPMConstants.h
@@ -0,0 +1,42 @@
+// Copyright 2022 The Dawn Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#define INSTANCE_OBJECT_ID 1
+#define INVALID_OBJECTID 0
+#define CLAMP_SIZE (1048576)
+
+#define ADAPTER_LIMIT 2
+#define BIND_GROUP_LIMIT 16
+#define BIND_GROUP_LAYOUT_LIMIT 16
+#define BUFFER_LIMIT 16
+#define COMMAND_BUFFER_LIMIT 16
+#define COMMAND_ENCODER_LIMIT 16
+#define COMPUTE_PASS_ENCODER_LIMIT 16
+#define COMPUTE_PIPELINE_LIMIT 16
+#define DEVICE_LIMIT 2
+#define EXTERNAL_TEXTURE_LIMIT 16
+#define INSTANCE_LIMIT 16
+#define PIPELINE_LAYOUT_LIMIT 16
+#define QUERY_SET_LIMIT 16
+#define QUEUE_LIMIT 16
+#define RENDER_BUNDLE_LIMIT 16
+#define RENDER_BUNDLE_ENCODER_LIMIT 16
+#define RENDER_PASS_ENCODER_LIMIT 16
+#define RENDER_PIPELINE_LIMIT 16
+#define SAMPLER_LIMIT 16
+#define SHADER_MODULE_LIMIT 16
+#define SURFACE_LIMIT 16
+#define SWAP_CHAIN_LIMIT 16
+#define TEXTURE_LIMIT 16
+#define TEXTURE_VIEW_LIMIT 16
