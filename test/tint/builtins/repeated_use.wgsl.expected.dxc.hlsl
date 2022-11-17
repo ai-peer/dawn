649835f0b56cf02f02c534aca088b19218fb4a84
dan sinclair <dsinclair@chromium.org>
2022-11-17 15:41:19 -0500

    Add const-eval for `degrees` and `radians`
    This CL adds const-eval for `degrees` and `radians`.
    
    Bug: tint:1581
    Change-Id: I7f00e2b1e5ab7c8e895680a6b75b9531dac31f5a

--- test/tint/builtins/repeated_use.wgsl.expected.dxc.hlsl
+++ test/tint/builtins/repeated_use.wgsl.expected.dxc.hlsl
@@ -1,32 +1,16 @@
-float4 tint_degrees(float4 param_0) {
-  return param_0 * 57.295779513082322865;
-}
-
-float3 tint_degrees_1(float3 param_0) {
-  return param_0 * 57.295779513082322865;
-}
-
-float2 tint_degrees_2(float2 param_0) {
-  return param_0 * 57.295779513082322865;
-}
-
-float tint_degrees_3(float param_0) {
-  return param_0 * 57.295779513082322865;
-}
-
 [numthreads(1, 1, 1)]
 void main() {
-  const float4 a = tint_degrees((0.0f).xxxx);
-  const float4 b = tint_degrees((1.0f).xxxx);
-  const float4 c = tint_degrees(float4(1.0f, 2.0f, 3.0f, 4.0f));
-  const float3 d = tint_degrees_1((0.0f).xxx);
-  const float3 e = tint_degrees_1((1.0f).xxx);
-  const float3 f = tint_degrees_1(float3(1.0f, 2.0f, 3.0f));
-  const float2 g = tint_degrees_2((0.0f).xx);
-  const float2 h = tint_degrees_2((1.0f).xx);
-  const float2 i = tint_degrees_2(float2(1.0f, 2.0f));
-  const float j = tint_degrees_3(1.0f);
-  const float k = tint_degrees_3(2.0f);
-  const float l = tint_degrees_3(3.0f);
+  const float4 a = (0.0f).xxxx;
+  const float4 b = (57.295776367f).xxxx;
+  const float4 c = float4(57.295776367f, 114.591552734f, 171.887329102f, 229.183105469f);
+  const float3 d = (0.0f).xxx;
+  const float3 e = (57.295776367f).xxx;
+  const float3 f = float3(57.295776367f, 114.591552734f, 171.887329102f);
+  const float2 g = (0.0f).xx;
+  const float2 h = (57.295776367f).xx;
+  const float2 i = float2(57.295776367f, 114.591552734f);
+  const float j = 57.295780182f;
+  const float k = 114.591560364f;
+  const float l = 171.88734436f;
   return;
 }
