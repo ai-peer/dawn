8f070b331caa576ce47e6dc0cee0d57e04b50b80
Takahiro <hogehoge@gachapin.jp>
2022-11-26 11:43:54 -0800

    Add Buffer.GetMapState()
    Bug: dawn:1521
    Change-Id: I68962a4d6b17867cd17cfcb3a17910a2007764fa

--- src/dawn/wire/client/Buffer.cpp
+++ src/dawn/wire/client/Buffer.cpp
@@ -17,6 +17,7 @@
 #include <limits>
 #include <utility>
 
+#include "dawn/common/Log.h"
 #include "dawn/wire/BufferConsumer_impl.h"
 #include "dawn/wire/WireCmd_autogen.h"
 #include "dawn/wire/client/Client.h"
@@ -384,6 +385,11 @@ WGPUBufferUsage Buffer::GetUsage() const {
 
 uint64_t Buffer::GetSize() const {
     return mSize;
+}
+
+WGPUBufferMapState Buffer::GetMapState() const {
+    dawn::ErrorLog() << "buffer.GetMapState is unimplemented with dawn_wire.";
+    return WGPUBufferMapState_Unmapped;
 }
 
 bool Buffer::IsMappedForReading() const {
