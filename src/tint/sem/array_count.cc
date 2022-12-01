e762c392b7d35aa51ce8c36156b4161ce9bb373c
dan sinclair <dsinclair@chromium.org>
2022-12-01 08:56:17 -0500

    Moved sem::ArrayCount to an inherited structure
    This CL moves the ArrayCount from a variant to use inheritance. This
    will allow the sem to have different array count classes from the IR.
    The ArrayCounts, similar to types, are unique across the code base and
    are provided by the TypeManager.
    
    Bug: tint:1718
    Change-Id: Ib9c7c9df881e7a34cc3def2ff29571f536d66244

---
+++ src/tint/sem/array_count.cc
@@ -0,0 +1,98 @@
+// Copyright 2021 The Tint Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "src/tint/sem/array_count.h"
+
+TINT_INSTANTIATE_TYPEINFO(tint::sem::ArrayCount);
+TINT_INSTANTIATE_TYPEINFO(tint::sem::ConstantArrayCount);
+TINT_INSTANTIATE_TYPEINFO(tint::sem::RuntimeArrayCount);
+TINT_INSTANTIATE_TYPEINFO(tint::sem::NamedOverrideArrayCount);
+TINT_INSTANTIATE_TYPEINFO(tint::sem::UnnamedOverrideArrayCount);
+
+namespace tint::sem {
+
+ArrayCount::ArrayCount() : Base(TypeFlags{}) {}
+ArrayCount::~ArrayCount() = default;
+
+ConstantArrayCount::ConstantArrayCount(uint32_t val) : Base(), value(val) {}
+ConstantArrayCount::~ConstantArrayCount() = default;
+
+size_t ConstantArrayCount::Hash() const {
+    return static_cast<size_t>(TypeInfo::Of<ConstantArrayCount>().full_hashcode);
+}
+
+bool ConstantArrayCount::Equals(const Type& other) const {
+    if (auto* v = other.As<ConstantArrayCount>()) {
+        return value == v->value;
+    }
+    return false;
+}
+
+std::string ConstantArrayCount::FriendlyName(const SymbolTable&) const {
+    return "constant-array-count";
+}
+
+RuntimeArrayCount::RuntimeArrayCount() : Base() {}
+RuntimeArrayCount::~RuntimeArrayCount() = default;
+
+size_t RuntimeArrayCount::Hash() const {
+    return static_cast<size_t>(TypeInfo::Of<RuntimeArrayCount>().full_hashcode);
+}
+
+bool RuntimeArrayCount::Equals(const Type& other) const {
+    return other.Is<RuntimeArrayCount>();
+}
+
+std::string RuntimeArrayCount::FriendlyName(const SymbolTable&) const {
+    return "runtime-array-count";
+}
+
+NamedOverrideArrayCount::NamedOverrideArrayCount(const GlobalVariable* var)
+    : Base(), variable(var) {}
+NamedOverrideArrayCount::~NamedOverrideArrayCount() = default;
+
+size_t NamedOverrideArrayCount::Hash() const {
+    return static_cast<size_t>(TypeInfo::Of<NamedOverrideArrayCount>().full_hashcode);
+}
+
+bool NamedOverrideArrayCount::Equals(const Type& other) const {
+    if (auto* v = other.As<NamedOverrideArrayCount>()) {
+        return variable == v->variable;
+    }
+    return false;
+}
+
+std::string NamedOverrideArrayCount::FriendlyName(const SymbolTable&) const {
+    return "named-override-array-count";
+}
+
+UnnamedOverrideArrayCount::UnnamedOverrideArrayCount(const Expression* e) : Base(), expr(e) {}
+UnnamedOverrideArrayCount::~UnnamedOverrideArrayCount() = default;
+
+size_t UnnamedOverrideArrayCount::Hash() const {
+    return static_cast<size_t>(TypeInfo::Of<UnnamedOverrideArrayCount>().full_hashcode);
+}
+
+bool UnnamedOverrideArrayCount::Equals(const Type& other) const {
+    if (auto* v = other.As<UnnamedOverrideArrayCount>()) {
+        return expr == v->expr;
+    }
+    return false;
+}
+
+std::string UnnamedOverrideArrayCount::FriendlyName(const SymbolTable&) const {
+    return "unnamed-override-array-count";
+}
+
+}  // namespace tint::sem
