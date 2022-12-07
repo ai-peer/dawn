a3ac84dd830453218b25f8881a036a464b409da4
Zhaoming Jiang <zhaoming.jiang@intel.com>
2022-12-07 09:58:47 +0800

    Dawn: Refactor device toggles
    This CL refactor the logic adapter creating device toggles set when
    creating device and the way device holding its toggles. This CL also
    introduce the concept "toggle stage", currently "device stage" only but
    in future will add "instance stage" and "adapter stage" for instance and
    adapter toggles. No changes on Dawn API.
    More details:
    1. Introduce `TogglesState` objects that represent the complete toggles
       state of a device (and will used for instance and adapter in future).
       Introduce `RequiredTogglesSet` to represent the user-required toggles
       set, which is given by DawnTogglesDescriptor.
    2. When creating a device, adapter set up a TogglesState object for it
       in `Adapter::MakeDeviceToggles` and `Adapter::MakeDeviceTogglesImpl`,
       no other place would change the device's toggles state. This change
       simplify the logic.
    3. Introduce the `ToggleStage` enum for every toggle,
       `RequiredTogglesSet` and `TogglesState` object. Currently we only
       have `Device` toggle stage, but in future will have `Instance` and
       `Adapter` for instance and adapter toggles.
    
    Bug: dawn:1495
    Change-Id: Ifafac6a6a075b5b9a733159574ae5b6d4f3ebde9

--- src/dawn/native/d3d12/AdapterD3D12.h
+++ src/dawn/native/d3d12/AdapterD3D12.h
@@ -38,9 +38,12 @@ class Adapter : public AdapterBase {
     ComPtr<ID3D12Device> GetDevice() const;
 
   private:
-    ResultOrError<Ref<DeviceBase>> CreateDeviceImpl(
-        const DeviceDescriptor* descriptor,
-        const TripleStateTogglesSet& userProvidedToggles) override;
+    TogglesState MakeDeviceTogglesImpl(
+        const RequiredTogglesSet& requiredDeviceToggles) const override;
+
+    ResultOrError<Ref<DeviceBase>> CreateDeviceImpl(const DeviceDescriptor* descriptor,
+                                                    const TogglesState& deviceToggles) override;
+
     MaybeError ResetInternalDeviceForTestingImpl() override;
 
     bool AreTimestampQueriesSupported() const;
@@ -49,9 +52,9 @@ class Adapter : public AdapterBase {
     MaybeError InitializeSupportedFeaturesImpl() override;
     MaybeError InitializeSupportedLimitsImpl(CombinedLimits* limits) override;
 
-    MaybeError ValidateFeatureSupportedWithTogglesImpl(
+    MaybeError ValidateFeatureSupportedWithDeviceTogglesImpl(
         wgpu::FeatureName feature,
-        const TripleStateTogglesSet& userProvidedToggles) override;
+        const TogglesState& deviceTogglesState) override;
 
     MaybeError InitializeDebugLayerFilters();
     void CleanUpDebugLayerFilters();
