5c3488880b48c18a5cfb419e7c15c03e2f604a14
Yuly Novikov <ynovikov@chromium.org>
2022-12-15 18:44:58 +0000

    NOT FOR REVEIW: Skip failures on Win Optiplex 7080
    Bug: chromium:1349828
    Change-Id: I76d677157857dbc6ef7cb5ecbad6e37d2cc292e1

--- src/dawn/tests/white_box/QueryInternalShaderTests.cpp
+++ src/dawn/tests/white_box/QueryInternalShaderTests.cpp
@@ -196,6 +196,7 @@ class QueryInternalShaderTests : public DawnTest {
 TEST_P(QueryInternalShaderTests, TimestampComputeShader) {
     // TODO(crbug.com/dawn/741): Test output is wrong with D3D12 + WARP.
     DAWN_SUPPRESS_TEST_IF(IsD3D12() && IsWARP());
+    DAWN_SUPPRESS_TEST_IF(IsWindows() && IsVulkan() && IsIntel());
 
     constexpr std::array<float, 5> kPeriodsToTest = {
         1,
