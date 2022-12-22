b4672cc9453ec43945252a20bfd56c45b67243ee
Peng Huang <penghuang@chromium.org>
2022-12-22 10:33:38 -0500

    WIP vulkan: append pipeline barriers for transfer buffers
    Change-Id: I91ee66bdb35552f71f7d314b66c002f36fc86caa

--- src/dawn/native/vulkan/BufferVk.cpp
+++ src/dawn/native/vulkan/BufferVk.cpp
@@ -311,12 +311,23 @@ MaybeError Buffer::MapAsyncImpl(wgpu::MapMode mode, size_t offset, size_t size) 
     // TODO(crbug.com/dawn/852): initialize mapped buffer in CPU side.
     EnsureDataInitialized(recordingContext);
 
+#if defined(DAWN_ENABLE_ASSERTS)
+    // Barrier transition is appended to the related queue submissions, so the buffer should be able
+    // to be mapped directly.
+    VkBufferMemoryBarrier barrier;
+    VkPipelineStageFlags srcStages = 0;
+    VkPipelineStageFlags dstStages = 0;
+
     if (mode & wgpu::MapMode::Read) {
-        TransitionUsageNow(recordingContext, wgpu::BufferUsage::MapRead);
+        ASSERT(!TransitionUsageAndGetResourceBarrier(wgpu::BufferUsage::MapRead, &barrier,
+                                                     &srcStages, &dstStages));
     } else {
         ASSERT(mode & wgpu::MapMode::Write);
-        TransitionUsageNow(recordingContext, wgpu::BufferUsage::MapWrite);
+        ASSERT(!TransitionUsageAndGetResourceBarrier(wgpu::BufferUsage::MapWrite, &barrier,
+                                                     &srcStages, &dstStages));
     }
+#endif  // DAWN_ENABLE_ASSERTS
+
     return {};
 }
 
@@ -401,12 +412,17 @@ void Buffer::ClearBuffer(CommandRecordingContext* recordingContext,
     size = size > 0 ? size : GetAllocatedSize();
     ASSERT(size > 0);
 
-    TransitionUsageNow(recordingContext, wgpu::BufferUsage::CopyDst);
+    if (uint8_t* memory = mMemoryAllocation.GetMappedPointer()) {
+        memset(memory + offset, 0, size);
+    } else {
+        TransitionUsageNow(recordingContext, wgpu::BufferUsage::CopyDst);
 
-    Device* device = ToBackend(GetDevice());
-    // VK_WHOLE_SIZE doesn't work on old Windows Intel Vulkan drivers, so we don't use it.
-    // Note: Allocated size must be a multiple of 4.
-    ASSERT(size % 4 == 0);
-    device->fn.CmdFillBuffer(recordingContext->commandBuffer, mHandle, offset, size, clearValue);
+        Device* device = ToBackend(GetDevice());
+        // VK_WHOLE_SIZE doesn't work on old Windows Intel Vulkan drivers, so we don't use it.
+        // Note: Allocated size must be a multiple of 4.
+        ASSERT(size % 4 == 0);
+        device->fn.CmdFillBuffer(recordingContext->commandBuffer, mHandle, offset, size,
+                                 clearValue);
+    }
 }
 }  // namespace dawn::native::vulkan
