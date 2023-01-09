ae33af00fbd675ec27819d3e47360dce0de3b8de
Jiawei Shao <jiawei.shao@intel.com>
2023-01-09 16:15:22 +0800

    [Not for Review] Test
    Change-Id: I38062115d94933bd58e571c812ff6888e9781df6

--- src/dawn/native/Toggles.h
+++ src/dawn/native/Toggles.h
@@ -81,7 +81,7 @@ enum class Toggle {
     ApplyClearBigIntegerColorValueWithDraw,
     MetalUseMockBlitEncoderForWriteTimestamp,
     VulkanSplitCommandBufferOnDepthStencilComputeSampleAfterRenderPass,
-    D3D12Allocate2DTexturewithCopyDstAsCommittedResource,
+    D3D12Allocate2DTextureAsCommittedResource,
     MetalUseCombinedDepthStencilFormatForStencil8,
     MetalUseBothDepthAndStencilAttachmentsForCombinedDepthStencilFormats,
     UseBlitForBufferToDepthTextureCopy,
