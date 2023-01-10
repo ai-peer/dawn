47c0acdf4c99e879e7692c24282c5501fea92d06
Peng Huang <penghuang@chromium.org>
2023-01-10 13:03:27 -0500

    Add the last usage serial in Buffer
    Add the last usage serial in Buffer, it is used for optimizing
    MapAsyc(), so the callback of MapAsync() is called when the last usage
    serial is done instead of using the current pending serial when
    the MapAsync() is called.
    
    Bug: b/265151060
    Change-Id: I87cbffa47d3f114c4623e17635f8b4c5fbf85a1f

--- src/dawn/native/metal/BufferMTL.mm
+++ src/dawn/native/metal/BufferMTL.mm
@@ -237,6 +237,7 @@ void Buffer::ClearBuffer(CommandRecordingContext* commandContext,
     [commandContext->EnsureBlit() fillBuffer:mMtlBuffer.Get()
                                        range:NSMakeRange(offset, size)
                                        value:clearValue];
+    SetLastUsageSerial(GetDevice()->GetPendingCommandSerial());
 }
 
 }  // namespace dawn::native::metal
