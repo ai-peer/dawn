1d3568659fedf7ee202d38139192e81e8772fc75
Austin Eng <enga@chromium.org>
2023-01-17 11:18:19 -0800

    Delete StagingBuffer in favor of mappable buffers
    This CL removes StagingBuffer to start to unify implementation
    code paths for WriteTexture/Buffer and CopyBufferToTexture/Buffer.
    
    This will help implementing a buffer-to-stencil copy workaround.
    
    Bug: dawn:1389
    Change-Id: Ieb23b8d871f14544ef01445a495dc1077274c9f2

--- src/dawn/native/Buffer.cpp
+++ src/dawn/native/Buffer.cpp
@@ -88,7 +88,7 @@ class ErrorBuffer final : public BufferBase {
         UNREACHABLE();
     }
 
-    void* GetMappedPointerImpl() override { return mFakeMappedData.get(); }
+    void* GetMappedPointer() override { return mFakeMappedData.get(); }
 
     void UnmapImpl() override { mFakeMappedData.reset(); }
 
@@ -196,7 +196,7 @@ void BufferBase::DestroyImpl() {
         UnmapInternal(WGPUBufferMapAsyncStatus_DestroyedBeforeCallback);
     } else if (mState == BufferState::MappedAtCreation) {
         if (mStagingBuffer != nullptr) {
-            mStagingBuffer.reset();
+            mStagingBuffer = nullptr;
         } else if (mSize != 0) {
             UnmapInternal(WGPUBufferMapAsyncStatus_DestroyedBeforeCallback);
         }
@@ -259,7 +259,7 @@ MaybeError BufferBase::MapAtCreation() {
     size_t size;
     if (mSize == 0) {
         return {};
-    } else if (mStagingBuffer) {
+    } else if (mStagingBuffer != nullptr) {
         // If there is a staging buffer for initialization, clear its contents directly.
         // It should be exactly as large as the buffer allocation.
         ptr = mStagingBuffer->GetMappedPointer();
@@ -267,7 +267,7 @@ MaybeError BufferBase::MapAtCreation() {
         ASSERT(size == GetAllocatedSize());
     } else {
         // Otherwise, the buffer is directly mappable on the CPU.
-        ptr = GetMappedPointerImpl();
+        ptr = GetMappedPointer();
         size = GetAllocatedSize();
     }
 
@@ -302,7 +302,13 @@ MaybeError BufferBase::MapAtCreationInternal() {
             // is initialized.
             // TODO(crbug.com/dawn/828): Suballocate and reuse memory from a larger staging
             // buffer so we don't create many small buffers.
-            DAWN_TRY_ASSIGN(mStagingBuffer, GetDevice()->CreateStagingBuffer(GetAllocatedSize()));
+            BufferDescriptor stagingBufferDesc = {};
+            stagingBufferDesc.usage = wgpu::BufferUsage::CopySrc | wgpu::BufferUsage::MapWrite;
+            stagingBufferDesc.size = Align(GetAllocatedSize(), 4);
+            stagingBufferDesc.mappedAtCreation = true;
+
+            IgnoreLazyClearCountScope scope(GetDevice());
+            DAWN_TRY_ASSIGN(mStagingBuffer, GetDevice()->CreateBuffer(&stagingBufferDesc));
         }
     }
 
@@ -418,7 +424,7 @@ void* BufferBase::GetMappedRange(size_t offset, size_t size, bool writable) {
     if (mSize == 0) {
         return reinterpret_cast<uint8_t*>(intptr_t(0xCAFED00D));
     }
-    uint8_t* start = static_cast<uint8_t*>(GetMappedPointerImpl());
+    uint8_t* start = static_cast<uint8_t*>(GetMappedPointer());
     return start == nullptr ? nullptr : start + offset;
 }
 
@@ -431,7 +437,7 @@ uint64_t BufferBase::APIGetSize() const {
 }
 
 MaybeError BufferBase::CopyFromStagingBuffer() {
-    ASSERT(mStagingBuffer);
+    ASSERT(mStagingBuffer != nullptr);
     if (mSize == 0) {
         // Staging buffer is not created if zero size.
         ASSERT(mStagingBuffer == nullptr);
@@ -439,7 +445,7 @@ MaybeError BufferBase::CopyFromStagingBuffer() {
     }
 
     DAWN_TRY(
-        GetDevice()->CopyFromStagingToBuffer(mStagingBuffer.get(), 0, this, 0, GetAllocatedSize()));
+        GetDevice()->CopyFromStagingToBuffer(mStagingBuffer.Get(), 0, this, 0, GetAllocatedSize()));
 
     DynamicUploader* uploader = GetDevice()->GetDynamicUploader();
     uploader->ReleaseStagingBuffer(std::move(mStagingBuffer));
