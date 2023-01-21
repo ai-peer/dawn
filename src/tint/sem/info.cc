8ab5aa9b4cf4dd7a98a068b8bae6f703a58863df
James Price <jrprice@google.com>
2023-01-21 06:59:27 -0500

    tint/sem: Record diagnostic severity modifications
    Each sem::Node has a map which stores the diagnostic modifications
    applied to that node. The sem::Info class provides a query to get the
    diagnostic severity for a given AST node, by walking up the semantic
    tree to find the tightest diagnostic severity modification. The
    default severity is used if it was not overridden.
    
    This allows components outside of the Resolver/Validator to determine
    the diagnostic severity while walking the AST, which is required for
    the uniformity analysis.
    
    Bug: tint:1809
    Change-Id: I4caf99d7412fb22fb1183b2c8cfde349da2fefd3

--- src/tint/sem/info.cc
+++ src/tint/sem/info.cc
@@ -14,6 +14,11 @@
 
 #include "src/tint/sem/info.h"
 
+#include "src/tint/sem/expression.h"
+#include "src/tint/sem/function.h"
+#include "src/tint/sem/module.h"
+#include "src/tint/sem/statement.h"
+
 namespace tint::sem {
 
 Info::Info() = default;
@@ -23,5 +28,57 @@ Info::Info(Info&&) = default;
 Info::~Info() = default;
 
 Info& Info::operator=(Info&&) = default;
+
+ast::DiagnosticSeverity Info::DiagnosticSeverity(const ast::Node* ast_node,
+                                                 ast::DiagnosticRule rule) const {
+    auto* sem = Get(ast_node);
+    if (!sem) {
+        return ast::DiagnosticSeverity::kUndefined;
+    }
+
+    // Get the diagnostic severity modification for a node.
+    auto check = [rule](const sem::Node* node) {
+        if (node != nullptr) {
+            auto& severities = node->DiagnosticSeverities();
+            auto itr = severities.find(rule);
+            if (itr != severities.end()) {
+                return itr->second;
+            }
+        }
+        return ast::DiagnosticSeverity::kUndefined;
+    };
+
+    ast::DiagnosticSeverity severity;
+    auto* stmt = sem->As<sem::Statement>();
+    auto* func = sem->As<sem::Function>();
+    if (auto* expr = sem->As<sem::Expression>()) {
+        stmt = expr->Stmt();
+    }
+    if (stmt) {
+        // Walk up the statement hierarchy, checking for diagnostic severity modifications.
+        while (true) {
+            severity = check(stmt);
+            if (severity != ast::DiagnosticSeverity::kUndefined) {
+                return severity;
+            }
+            if (!stmt->Parent()) {
+                break;
+            }
+            stmt = stmt->Parent();
+        }
+        func = stmt->Function();
+    }
+
+    // Check for a diagnostic severity modification on the function.
+    severity = check(func);
+    if (severity != ast::DiagnosticSeverity::kUndefined) {
+        return severity;
+    }
+
+    // Use the global severity set on the module.
+    severity = check(module_);
+    TINT_ASSERT(Semantic, severity != ast::DiagnosticSeverity::kUndefined);
+    return severity;
+}
 
 }  // namespace tint::sem
