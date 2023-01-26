89a88ca80f8f3952fe19564171e02cf297382036
Ben Clayton <bclayton@google.com>
2023-01-26 12:10:13 +0000

    Fix code-coverage on macOS / turbocov target
    Emit coverage for 'AppleClang' compiler.
    Have run-cts find the tucked-away `llvm-profdata` executable.
    
    The turbocov build target (somehow) never got hooked up to the root CMakeLists.txt file. This is now fixed.
    
    Change-Id: I481cceafe2e72b544e13168172fc1456e5df2005

--- tools/src/cov/import.go
+++ tools/src/cov/import.go
@@ -42,9 +42,10 @@ type Coverage struct {
 
 // Env holds the environment settings for performing coverage processing.
 type Env struct {
-	LLVMBin  string // path to the LLVM bin directory
+	Profdata string // path to the llvm-profdata tool
 	Binary   string // path to the executable binary
-	TurboCov string // path to turbo-cov (optional)
+	Cov      string // path to the llvm-cov tool (one of Cov or TurboCov must be supplied)
+	TurboCov string // path to the turbo-cov tool (one of Cov or TurboCov must be supplied)
 }
 
 // RuntimeEnv returns the environment variable key=value pair for setting
@@ -92,25 +93,29 @@ func (e Env) AllSourceFiles() *Coverage {
 // Import uses the llvm-profdata and llvm-cov tools to import the coverage
 // information from a .profraw file.
 func (e Env) Import(profrawPath string) (*Coverage, error) {
-	llvmProfdataExe := filepath.Join(e.LLVMBin, "llvm-profdata"+fileutils.ExeExt)
-	llvmCovExe := filepath.Join(e.LLVMBin, "llvm-cov"+fileutils.ExeExt)
-
 	profdata := profrawPath + ".profdata"
+	defer os.Remove(profdata)
 
-	if err := exec.Command(
-		llvmProfdataExe,
+	if e.Profdata == "" {
+		return nil, fmt.Errorf("cov.Env.Profdata must be specified")
+	}
+	if e.TurboCov == "" && e.Cov == "" {
+		return nil, fmt.Errorf("One of cov.Env.TurboCov or cov.Env.Cov must be specified")
+	}
+
+	if out, err := exec.Command(
+		e.Profdata,
 		"merge",
 		"-sparse",
 		profrawPath,
 		"-output",
-		profdata).Run(); err != nil {
-		return nil, fmt.Errorf("llvm-profdata errored: %w", err)
+		profdata).CombinedOutput(); err != nil {
+		return nil, fmt.Errorf("llvm-profdata errored: %w\n%v", err, string(out))
 	}
-	defer os.Remove(profdata)
 
 	if e.TurboCov == "" {
 		data, err := exec.Command(
-			llvmCovExe,
+			e.Cov,
 			"export",
 			e.Binary,
 			"-instr-profile="+profdata,
