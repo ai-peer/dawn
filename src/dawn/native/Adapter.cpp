161070a0aa44b31e3cda1c4eee37bb5dc1124787
Stephen White <senorblanco@chromium.org>
2023-01-27 18:09:14 -0500

    Add FeatureLevel to AdapterBase; implement Compat.
    This introduces the notion of FeatureLevel, currently consisting of
    v1.0 or Compatibility. Each AdapterBase is now constructed with the
    FeatureLevel it supports.
    
    When discovering PhysicalDevices, create an AdapterBase for each of
    the FeaturLevels which that PhysicalDevice supports. For most of the
    backends, this will mean v1.0 and Compatibility, while for OpenGL, it will be
    only Compatibility.
    
    Bug: dawn:1796.
    
    Change-Id: I828247ef43e2220805ccf6c08827aa5e2382a026

--- src/dawn/native/Adapter.cpp
+++ src/dawn/native/Adapter.cpp
@@ -20,8 +20,10 @@
 
 namespace dawn::native {
 
-AdapterBase::AdapterBase(const Ref<PhysicalDeviceBase>& physicalDevice)
-    : mPhysicalDevice(std::move(physicalDevice)) {}
+AdapterBase::AdapterBase(const Ref<PhysicalDeviceBase>& physicalDevice, FeatureLevel featureLevel)
+    : mPhysicalDevice(std::move(physicalDevice)), mFeatureLevel(featureLevel) {
+    ASSERT(physicalDevice->SupportsFeatureLevel(featureLevel));
+}
 
 AdapterBase::~AdapterBase() = default;
 
@@ -39,6 +41,7 @@ bool AdapterBase::APIGetLimits(SupportedLimits* limits) const {
 
 void AdapterBase::APIGetProperties(AdapterProperties* properties) const {
     mPhysicalDevice->APIGetProperties(properties);
+    properties->compatibilityMode = mFeatureLevel == FeatureLevel::kCompatibilityMode;
 }
 
 bool AdapterBase::APIHasFeature(wgpu::FeatureName feature) const {
@@ -61,6 +64,10 @@ void AdapterBase::APIRequestDevice(const DeviceDescriptor* descriptor,
 
 const TogglesState& AdapterBase::GetTogglesState() const {
     return mPhysicalDevice->GetTogglesState();
+}
+
+FeatureLevel AdapterBase::GetFeatureLevel() const {
+    return mFeatureLevel;
 }
 
 }  // namespace dawn::native
