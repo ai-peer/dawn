b2cc56e546094489f47890355022e1d5ccc9d17a
Peng Huang <penghuang@chromium.org>
2023-01-31 14:16:37 -0500

    [GL] track buffer usage
    Change-Id: I9f8dfeb58faaed09ed62550371a30a680cd607d5

--- src/dawn/native/opengl/BufferGL.cpp
+++ src/dawn/native/opengl/BufferGL.cpp
@@ -40,7 +40,6 @@ ResultOrError<Ref<Buffer>> Buffer::CreateInternalBuffer(Device* device,
 Buffer::Buffer(Device* device, const BufferDescriptor* descriptor)
     : BufferBase(device, descriptor) {
     // TODO(penghuang): track usage for GL.
-    mLastUsageSerial = kMaxExecutionSerial;
     const OpenGLFunctions& gl = device->GetGL();
     // Allocate at least 4 bytes so clamped accesses are always in bounds.
     mAllocatedSize = std::max(GetSize(), uint64_t(4u));
@@ -55,9 +54,10 @@ Buffer::Buffer(Device* device, const BufferDescriptor* descriptor)
         std::vector<uint8_t> clearValues(mAllocatedSize, 1u);
         gl.BufferData(GL_ARRAY_BUFFER, mAllocatedSize, clearValues.data(), GL_STATIC_DRAW);
     } else {
-        // Buffers start zeroed if you pass nullptr to glBufferData.
+        // Buffers start uninitialized if you pass nullptr to glBufferData.
         gl.BufferData(GL_ARRAY_BUFFER, mAllocatedSize, nullptr, GL_STATIC_DRAW);
     }
+    MarkUsedInPendingCommands();
 }
 
 Buffer::Buffer(Device* device, const BufferDescriptor* descriptor, bool shouldLazyClear)
@@ -122,6 +122,7 @@ void Buffer::InitializeToZero() {
     gl.BufferSubData(GL_ARRAY_BUFFER, 0, size, clearValues.data());
     device->IncrementLazyClearCountForTesting();
 
+    MarkUsedInPendingCommands();
     SetIsDataInitialized();
 }
 
@@ -152,15 +153,15 @@ MaybeError Buffer::MapAsyncImpl(wgpu::MapMode mode, size_t offset, size_t size) 
 
     EnsureDataInitialized();
 
-    // This does GPU->CPU synchronization, we could require a high
-    // version of OpenGL that would let us map the buffer unsynchronized.
     gl.BindBuffer(GL_ARRAY_BUFFER, mBuffer);
     void* mappedData = nullptr;
     if (mode & wgpu::MapMode::Read) {
-        mappedData = gl.MapBufferRange(GL_ARRAY_BUFFER, offset, size, GL_MAP_READ_BIT);
+        mappedData = gl.MapBufferRange(GL_ARRAY_BUFFER, offset, size,
+                                       GL_MAP_READ_BIT | GL_MAP_UNSYNCHRONIZED_BIT);
     } else {
         ASSERT(mode & wgpu::MapMode::Write);
-        mappedData = gl.MapBufferRange(GL_ARRAY_BUFFER, offset, size, GL_MAP_WRITE_BIT);
+        mappedData = gl.MapBufferRange(GL_ARRAY_BUFFER, offset, size,
+                                       GL_MAP_WRITE_BIT | GL_MAP_UNSYNCHRONIZED_BIT);
     }
 
     // The frontend asks that the pointer returned by GetMappedPointer is from the start of
