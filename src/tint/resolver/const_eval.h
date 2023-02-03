a515e57fafc510e0d8b738ccba6a1e57c47484e5
James Price <jrprice@google.com>
2023-02-03 17:13:12 -0500

    tint: Make CreateScalar and ZeroValue members
    This avoids the need to pass the `soft_errors_` flag at each callsite.
    
    Change-Id: I2cce3f147226e1295b5dfa0239beeacd519d5bb4

--- src/tint/resolver/const_eval.h
+++ src/tint/resolver/const_eval.h
@@ -18,6 +18,7 @@
 #include <stddef.h>
 #include <string>
 
+#include "src/tint/number.h"
 #include "src/tint/type/type.h"
 #include "src/tint/utils/result.h"
 #include "src/tint/utils/vector.h"
@@ -92,6 +93,14 @@ class ConstEval {
     /// @param idx the index expression
     /// @return the result of the index, or null if the value cannot be calculated
     Result Index(const type::Type* ty, const sem::Expression* obj, const sem::Expression* idx);
+
+    /// CreateScalar constructs and returns a constant::Scalar<T>.
+    /// @param source the source location
+    /// @param t the result type
+    /// @param v the scalar value
+    /// @return the constant value with the same type and value
+    template <typename T>
+    ConstEval::Result CreateScalar(const Source& source, const type::Type* t, T v);
 
     /// @param ty the result type
     /// @param lit the literal AST node
@@ -1088,6 +1097,9 @@ class ConstEval {
 
     /// Adds the given note message to the diagnostics
     void AddNote(const std::string& msg, const Source& source) const;
+
+    /// ZeroValue returns a Constant for the zero-value of the type `type`.
+    const constant::Value* ZeroValue(const type::Type* type);
 
     /// Adds two Number<T>s
     /// @param source the source location
