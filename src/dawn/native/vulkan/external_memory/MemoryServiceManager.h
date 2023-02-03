857cfc48ea9ef56f14fe3610b0ba6212d12300a4
Yan,Shaobo <shaobo.yan@intel.com>
2023-02-03 11:01:34 +0800

    WIP-Fix-Linux-Dawn
    Change-Id: Ia35c6baeccf539e8c8d268f13ded9c3d356b075c

---
+++ src/dawn/native/vulkan/external_memory/MemoryServiceManager.h
@@ -0,0 +1,44 @@
+// Copyright 2023 The Dawn Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SRC_DAWN_NATIVE_VULKAN_EXTERNAL_MEMORY_MEMORYSERVICEMANAGER_H_
+#define SRC_DAWN_NATIVE_VULKAN_EXTERNAL_MEMORY_MEMORYSERVICEMANAGER_H_
+
+#include "dawn/native/vulkan/external_memory/MemoryService.h"
+
+namespace dawn::native::vulkan {
+class Device;
+struct VulkanDeviceInfo;
+}  // namespace dawn::native::vulkan
+
+namespace dawn::native::vulkan::external_memory {
+
+class ServiceManager {
+  public:
+    explicit ServiceManager(Device* device);
+    ~ServiceManager() = default;
+
+    static bool CheckSupport(const VulkanDeviceInfo& deviceInfo);
+
+    Service* GetService(ExternalImageType type);
+
+  private:
+    Device* mDevice = nullptr;
+    std::unordered_map<ExternalImageType, std::unique_ptr<Service>> services_;
+    std::unique_ptr<Service> null_service_;
+};
+
+}  // namespace dawn::native::vulkan::external_memory
+
+#endif  // SRC_DAWN_NATIVE_VULKAN_EXTERNAL_MEMORY_MEMORYSERVICEMANAGER_H_
---
+++ src/dawn/native/vulkan/external_memory/MemoryServiceZirconHandle.h
@@ -0,0 +1,67 @@
+// Copyright 2023 The Dawn Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SRC_DAWN_NATIVE_VULKAN_EXTERNAL_MEMORY_MEMORYSERVICEZIRONHANDLE_H_
+#define SRC_DAWN_NATIVE_VULKAN_EXTERNAL_MEMORY_MEMORYSERVICEZIRONHANDLE_H_
+
+#include "dawn/native/vulkan/external_memory/MemoryService.h"
+
+namespace dawn::native::vulkan::external_memory {
+
+class MemoryServiceZirconHandle : public Service {
+  public:
+    explicit MemoryServiceZirconHandle(Device* device);
+    ~MemoryServiceZirconHandle() override;
+
+    static bool CheckSupport(const VulkanDeviceInfo& deviceInfo);
+
+    // True if the device reports it supports importing external memory.
+    bool SupportsImportMemory(VkFormat format,
+                              VkImageType type,
+                              VkImageTiling tiling,
+                              VkImageUsageFlags usage,
+                              VkImageCreateFlags flags) override;
+
+    // True if the device reports it supports creating VkImages from external memory.
+    bool SupportsCreateImage(const ExternalImageDescriptor* descriptor,
+                             VkFormat format,
+                             VkImageUsageFlags usage,
+                             bool* supportsDisjoint) override;
+
+    // Returns the parameters required for importing memory
+    ResultOrError<MemoryImportParams> GetMemoryImportParams(
+        const ExternalImageDescriptor* descriptor,
+        VkImage image) override;
+
+    // Returns the index of the queue memory from this services should be exported with.
+    uint32_t GetQueueFamilyIndex() override;
+
+    // Given an external handle pointing to memory, import it into a VkDeviceMemory
+    ResultOrError<VkDeviceMemory> ImportMemory(ExternalMemoryHandle handle,
+                                               const MemoryImportParams& importParams,
+                                               VkImage image) override;
+
+    // Create a VkImage for the given handle type
+    ResultOrError<VkImage> CreateImage(const ExternalImageDescriptor* descriptor,
+                                       const VkImageCreateInfo& baseCreateInfo) override;
+
+    bool Supported() const override;
+
+  private:
+    bool mSupported = false;
+};
+
+}  // namespace dawn::native::vulkan::external_memory
+
+#endif  // SRC_DAWN_NATIVE_VULKAN_EXTERNAL_MEMORY_MEMORYSERVICEZIRONHANDLE_H_
