65133e4362dfff0b270b3f1da43d13d7e003d097
Yan,Shaobo <shaobo.yan@intel.com>
2023-02-03 11:01:34 +0800

    WIP-Fix-Linux-Dawn
    Change-Id: Ia35c6baeccf539e8c8d268f13ded9c3d356b075c

---
+++ src/dawn/native/vulkan/external_semaphore/SemaphoreServiceManager.cpp
@@ -0,0 +1,75 @@
+// Copyright 2023 The Dawn Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <unordered_map>
+
+#include "dawn/native/vulkan/external_semaphore/SemaphoreService.h"
+#include "dawn/native/vulkan/external_semaphore/SemaphoreServiceNull.h"
+
+#if DAWN_PLATFORM_IS(FUCHSIA)
+#include "dawn/native/vulkan/external_semaphore/SemaphoreServiceZirconHandle.h"
+#endif  // DAWN_PLATFORM_IS(FUCHSIA)
+
+#if DAWN_PLATFORM_IS(LINUX) || DAWN_PLATFORM_IS(ANDROID)
+#include "dawn/native/vulkan/external_semaphore/SemaphoreServiceFD.h"
+#endif  // DAWN_PLATFORM_IS(LINUX) || DAWN_PLATFORM_IS(ANDROID)
+
+#include "dawn/native/vulkan/external_semaphore/SemaphoreServiceManager.h"
+#include "dawn/native/vulkan/external_semaphore/SemaphoreServiceNull.h"
+
+#if DAWN_PLATFORM_IS(FUCHSIA)
+#include "dawn/native/vulkan/external_semaphore/SemaphoreServiceZirconHandle.h"
+#endif  // DAWN_PLATFORM_IS(FUCHSIA)
+
+namespace dawn::native::vulkan::external_semaphore {
+// static
+bool ServiceManager::CheckSupport(const VulkanDeviceInfo& deviceInfo,
+                                  VkPhysicalDevice physicalDevice,
+                                  const VulkanFunctions& fn) {
+#if DAWN_PLATFORM_IS(FUCHSIA)
+    return SemaphoreServiceZirconHandle::CheckSupport(deviceInfo, physicalDevice, fn);
+#elif DAWN_PLATFORM_IS(LINUX) || DAWN_PLATFORM_IS(ANDROID)
+    return SemaphoreServiceFD::CheckSupport(deviceInfo, physicalDevice, fn);
+#else
+    return SemaphoreServiceNull::CheckSupport(deviceInfo, physicalDevice, fn);
+#endif
+}
+
+// static
+void ServiceManager::CloseHandle(ExternalSemaphoreHandle handle) {
+#if DAWN_PLATFORM_IS(FUCHSIA)
+    return SemaphoreServiceZirconHandle::CloseHandle(handle);
+#endif  // DAWN_PLATFORM_IS(FUCHSIA)
+
+#if DAWN_PLATFORM_IS(LINUX) || DAWN_PLATFORM_IS(ANDROID)
+    return SemaphoreServiceFD::CloseHandle(handle);
+#endif  // DAWN_PLATFORM_IS(LINUX)
+}
+
+ServiceManager::ServiceManager(Device* device) : mDevice(device) {
+#if DAWN_PLATFORM_IS(FUCHSIA)
+    service_ = std::make_unique<SemaphoreServiceZirconHandle>(mDevice);
+#elif DAWN_PLATFORM_IS(LINUX) || DAWN_PLATFORM_IS(ANDROID)
+    service_ = std::make_unique<SemaphoreServiceFD>(mDevice);
+#else
+    service_ = std::make_unique<SemaphoreServiceNull>(mDevice);
+#endif
+}
+
+ServiceManager::~ServiceManager() = default;
+
+Service* ServiceManager::GetService() {
+    return service_.get();
+}
+}  // namespace dawn::native::vulkan::external_semaphore
