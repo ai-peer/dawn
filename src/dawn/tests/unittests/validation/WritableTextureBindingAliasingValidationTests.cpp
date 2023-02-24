ffbd44632fbc01f9f302edd2a8fe5be4a5bea587
shrekshao <shrekshao@google.com>
2023-02-23 16:48:31 -0800

    Storage texture aliasing validation
    Change-Id: I84bf33895320053630ed80d3503ff53d1eaa83b9

---
+++ src/dawn/tests/unittests/validation/WritableTextureBindingAliasingValidationTests.cpp
@@ -0,0 +1,25 @@
+// Copyright 2023 The Dawn Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <string>
+#include <vector>
+
+#include "dawn/common/Assert.h"
+#include "dawn/common/Constants.h"
+#include "dawn/common/Numeric.h"
+#include "dawn/tests/unittests/validation/ValidationTest.h"
+#include "dawn/utils/ComboRenderPipelineDescriptor.h"
+#include "dawn/utils/WGPUHelpers.h"
+
+namespace {}
