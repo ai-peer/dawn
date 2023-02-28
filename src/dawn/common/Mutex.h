0be3f97e316b10019ffb9a5504f8fb7ff11e7798
Le Hoang Quyen <lehoangquyen@chromium.org>
2023-02-28 20:14:24 +0800

    Add an option to enable implicit device synchronization.
    1. Auto-generate mutex locking code in DawnNative's ProcTable's
    functions. Using a mutex owned by the related Device.
      - Unless the function/class has "no autolock" attribute (new) in
      dawn.json. In which cases, no locking code will be auto-generated.
      - Currently Instance, Adapter, Surface, Encoder classes and
      Reference/Release method have "no autolock".
    
    2. Added Feature::ImplicitDeviceSynchronization to enable Device's
    mutex.
      - If this feature is disabled, lock/unlock Device's mutex is no-op.
      Auto-generated locking code will have no effect. This is the default.
      - This approach is used instead of generating two sets of ProcTable
      because it's cleaner and the internal code doesn't need to care about
      whether there is a mutex being locked or not. Furthermore, if there
      were two sets of ProcTable, and user used dawnProcSetProcs() to set
      global ProcTable, it would affect all other modules using different
      Devices. Even though those modules don't need thread safety.
    
    Bug: dawn:1662
    Change-Id: I75f0d28959f333318e4159916b259131581f79f5

---
+++ src/dawn/common/Mutex.h
@@ -0,0 +1,62 @@
+// Copyright 2023 The Dawn Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SRC_DAWN_COMMON_MUTEX_H_
+#define SRC_DAWN_COMMON_MUTEX_H_
+
+#include <atomic>
+#include <mutex>
+#include <thread>
+
+#include "dawn/common/Assert.h"
+#include "dawn/common/NonCopyable.h"
+
+namespace dawn {
+class Mutex : NonCopyable {
+  public:
+    struct AutoLock : NonMovable {
+        AutoLock() = delete;
+        explicit AutoLock(Mutex* mutex) : mMutex(mutex) {
+            if (mMutex) {
+                mMutex->Lock();
+            }
+        }
+        ~AutoLock() {
+            if (mMutex) {
+                mMutex->Unlock();
+            }
+        }
+
+      private:
+        Mutex* mMutex;
+    };
+
+    void Lock();
+    void Unlock();
+
+    // This method is only enabled when DAWN_ENABLE_ASSERTS is turned on.
+    // Thus it should only be wrapped inside ASSERT() macro.
+    // i.e. ASSERT(mutex.IsLockedByCurrentThread())
+    bool IsLockedByCurrentThread();
+
+  private:
+#if defined(DAWN_ENABLE_ASSERTS)
+    std::atomic<std::thread::id> mOwner;
+#endif  // DAWN_ENABLE_ASSERTS
+    std::mutex mNativeMutex;
+};
+
+}  // namespace dawn
+
+#endif
