72603b4afb8928a281edf9d4a550dfc1bbe0cba7
Ben Clayton <bclayton@google.com>
2023-03-01 19:51:00 +0000

    WIP: Predicated robustness
    Change-Id: I6fc21c713d6ec3642975855a188ce1328be8c629

--- test/tint/bug/tint/1563.wgsl.expected.dxc.hlsl
+++ test/tint/bug/tint/1563.wgsl.expected.dxc.hlsl
@@ -1,3 +1,5 @@
+SKIP: FAILED
+
 void set_float4(inout float4 vec, int idx, float val) {
   vec = (idx.xxxx == int4(0, 1, 2, 3)) ? val.xxxx : vec;
 }
@@ -9,8 +11,24 @@ void unused_entry_point() {
 
 float foo() {
   const int oob = 99;
-  const float b = (0.0f).xxxx[min(uint(oob), 3u)];
+  const int index = oob;
+  const bool predicate = (uint(index) <= 3u);
+  float predicated_load = 0.0f;
+  if (predicate) {
+    predicated_load = (0.0f).xxxx[index];
+  }
+  const float b = predicated_load;
   float4 v = float4(0.0f, 0.0f, 0.0f, 0.0f);
-  set_float4(v, min(uint(oob), 3u), b);
+  const int index_1 = oob;
+  const bool predicate_1 = (uint(index_1) <= 3u);
+  if (predicate_1) {
+    set_float4(v, index_1, b);
+  }
   return b;
 }
+DXC validation failure:
+shader.hlsl:16:35: error: vector element index '99' is out of bounds
+    predicated_load = (0.0f).xxxx[index];
+                                  ^
+
+
