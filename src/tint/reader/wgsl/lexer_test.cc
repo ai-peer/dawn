91c33546c5f9858dd5f6bfa6d2f234f32d234703
dan sinclair <dsinclair@chromium.org>
2023-03-02 10:53:42 -0500

    Use stream parsing instead of strtod
    In order to set the locale of the parsed double, this CL switches from
    using `strtod` which honours the users locale, to a StringStream which
    explicitly sets the `classic` locale to provide `C` style parsing.
    
    Bug: tint:1686
    Change-Id: I97bc7a8ba39e3d7fff0d42b2bd8d630eea3dec26

--- src/tint/reader/wgsl/lexer_test.cc
+++ src/tint/reader/wgsl/lexer_test.cc
@@ -397,7 +397,7 @@ TEST_P(FloatTest, Parse) {
     Lexer l(&file);
 
     auto list = l.Lex();
-    ASSERT_EQ(2u, list.size());
+    ASSERT_EQ(2u, list.size()) << "Token type: " << static_cast<int32_t>(list[0].type());
 
     {
         auto& t = list[0];
