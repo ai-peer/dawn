ec9e5314e172c727d8887a306b308cca3cbe3ada
Ben Clayton <bclayton@google.com>
2023-03-02 14:26:28 +0000

    tint/transform/robustness: Implement predicated mode
    This change overhauls the Robustness transform to support three modes,
    per address space:
    * ignore    - Disable robustness checks for the address space
    * clamp     - Clamp indices / texture args to ensure they're in bounds
                  This was the old behavior, and continues to be the default.
    * predicate - Condition all indexing / textureLoad / textureStore / atomic*
                  operations on the bounds check. If any dependent value is out
                  of bounds, then the operation is skipped.
    
    This change also fixes multiple expression evaluation of the texture
    builtin 'level' argument.
    
    Change-Id: I2e300ddff2c8d3183a9701f06985ce1b262baf2c

--- src/tint/writer/spirv/generator_impl.cc
+++ src/tint/writer/spirv/generator_impl.cc
@@ -69,6 +69,7 @@ SanitizedResult Sanitize(const Program* in, const Options& options) {
 
     if (!options.disable_robustness) {
         // Robustness must come before BuiltinPolyfill
+        // Robustness must come after PromoteSideEffectsToDecl
         manager.Add<transform::Robustness>();
     }
 
