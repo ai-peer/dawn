e08d10c83d47b46551997bb1cba07aac5e73a886
dan sinclair <dsinclair@chromium.org>
2023-03-03 23:51:16 -0500

    [ir] Generate ir builtins
    This CL generates the IR builtin classes.
    
    Bug: tint:1718
    Change-Id: If6ef9f7f58fb59fcd8c3e155d0c85bb15e339f2d

---
+++ src/tint/ir/builtins/builtin.cc
@@ -0,0 +1,43 @@
+// Copyright 2023 The Tint Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "src/tint/ir/builtins/builtin.h"
+
+TINT_INSTANTIATE_TYPEINFO(tint::ir::builtins::Builtin);
+
+/// \cond DO_NOT_DOCUMENT
+namespace tint::ir::builtins {
+
+Builtin::Builtin(Value* result, utils::VectorRef<Value*> args) : Base(result), args_(args) {
+    for (auto* arg : args) {
+        arg->AddUsage(this);
+    }
+}
+
+Builtin::~Builtin() = default;
+
+void Builtin::EmitArgs(utils::StringStream& out, const SymbolTable& st) const {
+    bool first = true;
+    for (const auto* arg : args_) {
+        if (!first) {
+            out << ", ";
+        }
+        first = false;
+
+        arg->ToString(out, st);
+    }
+}
+/// \endcond
+
+}  // namespace tint::ir::builtins
---
+++ src/tint/ir/builtins/builtin.h
@@ -0,0 +1,54 @@
+// Copyright 2023 The Tint Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SRC_TINT_IR_BUILTINS_BUILTIN_H_
+#define SRC_TINT_IR_BUILTINS_BUILTIN_H_
+
+#include "src/tint/castable.h"
+#include "src/tint/ir/instruction.h"
+#include "src/tint/symbol_table.h"
+#include "src/tint/type/type.h"
+#include "src/tint/utils/string_stream.h"
+
+namespace tint::ir::builtins {
+
+/// A builtin instruction in the IR.
+class Builtin : public Castable<Builtin, Instruction> {
+  public:
+    /// Constructor
+    /// @param result the result value
+    /// @param args the builtin arguments
+    Builtin(Value* result, utils::VectorRef<Value*> args);
+    Builtin(const Builtin& instr) = delete;
+    Builtin(Builtin&& instr) = delete;
+    ~Builtin() override;
+
+    Builtin& operator=(const Builtin& instr) = delete;
+    Builtin& operator=(Builtin&& instr) = delete;
+
+    /// @returns the builtin arguments
+    utils::VectorRef<Value*> Args() const { return args_; }
+
+    /// Emits the arguments to the given stream
+    /// @param out the stream
+    /// @param st the symbol table
+    void EmitArgs(utils::StringStream& out, const SymbolTable& st) const;
+
+  private:
+    utils::Vector<Value*, 1> args_;
+};
+
+}  // namespace tint::ir::builtins
+
+#endif  // SRC_TINT_IR_BUILTINS_BUILTIN_
