a11b9660bd0e54a666e8ff9dc77f285ef32fe2b4
Le Hoang Quyen <lehoangquyen@chromium.org>
2023-03-07 02:56:27 +0800

    WIP: D3D12's external image: Add multithread support.
    Bug: dawn:1662
    Change-Id: Id2d720af4e5e654b5f34aebf485bededc6d247ca

--- src/dawn/native/d3d12/DeviceD3D12.cpp
+++ src/dawn/native/d3d12/DeviceD3D12.cpp
@@ -396,6 +396,8 @@ void Device::ForceEventualFlushOfCommands() {
 }
 
 MaybeError Device::ExecutePendingCommandContext() {
+    GetMutex()->AssertIsLockedByCurrentThread();
+
     return mPendingCommands.ExecuteCommandList(this);
 }
 
