a7a797cc4d6c49deb8720cf77712bcd849a839fa
Le Hoang Quyen <lehoangquyen@chromium.org>
2023-03-07 02:56:27 +0800

    WIP: D3D12's external image: Add multithread support.
    Bug: dawn:1662
    Change-Id: Id2d720af4e5e654b5f34aebf485bededc6d247ca

--- src/dawn/native/d3d12/D3D12Backend.cpp
+++ src/dawn/native/d3d12/D3D12Backend.cpp
@@ -94,6 +94,7 @@ std::unique_ptr<ExternalImageDXGI> ExternalImageDXGI::Create(
     WGPUDevice device,
     const ExternalImageDescriptorDXGISharedHandle* descriptor) {
     Device* backendDevice = ToBackend(FromAPI(device));
+    auto deviceLock(backendDevice->GetScopedLock());
     std::unique_ptr<ExternalImageDXGIImpl> impl =
         backendDevice->CreateExternalImageDXGIImpl(descriptor);
     if (!impl) {
@@ -107,6 +108,8 @@ uint64_t SetExternalMemoryReservation(WGPUDevice device,
                                       uint64_t requestedReservationSize,
                                       MemorySegment memorySegment) {
     Device* backendDevice = ToBackend(FromAPI(device));
+
+    auto deviceLock(backendDevice->GetScopedLock());
 
     return backendDevice->GetResidencyManager()->SetExternalMemoryReservation(
         memorySegment, requestedReservationSize);
