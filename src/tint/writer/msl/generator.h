cb7b2ef2eaa4eca24a6506c6ad01a7cd9e2e8209
dan sinclair <dsinclair@chromium.org>
2023-03-08 12:31:47 -0500

    Add ExternalTextureOptions
    This CL adds a `writer/ExternalTextureOptions` to allow providing
    external texture information to the generators. The generators are
    updated to have the option in their config, but do not use it yet.
    
    Bug: tint:1855 chromium:1421379
    Change-Id: I99b122c5cae145e8527158f300da81743f89775a

--- src/tint/writer/msl/generator.h
+++ src/tint/writer/msl/generator.h
@@ -23,6 +23,7 @@
 
 #include "src/tint/reflection.h"
 #include "src/tint/writer/array_length_from_uniform_options.h"
+#include "src/tint/writer/external_texture_options.h"
 #include "src/tint/writer/text.h"
 
 // Forward declarations
@@ -64,6 +65,9 @@ struct Options {
 
     /// Set to 'true' to generates binding mappings for external textures
     bool generate_external_texture_bindings = false;
+
+    /// Options used in the binding mappings for external textures
+    ExternalTextureOptions external_texture_options = {};
 
     /// Options used to specify a mapping of binding points to indices into a UBO
     /// from which to load buffer sizes.
