0fb45385daa0a37673a4c7a998efb0b8b29518e9
Brendon Tiszka <tiszka@chromium.org>
2023-03-12 00:47:31 -0500

    Add Dawn Wire Server LPM Fuzzer [4/N]
    Implements C++ serializer implementation that translates protobuf
    objects into Dawn serial data.
    
    1) A generator for the C++ implementation of the fuzzer that
    translates protobuf data into dawn wire serialized data.
    2) Object store for dawn objects that are allocated and
    freed
    
    Bug: chromium:1374747
    Change-Id: I09c1be6cdc2eccf4a91de808f19494d97d01b3d6

--- generator/templates/dawn/fuzzers/lpmfuzz/DawnLPMConstants.h
+++ generator/templates/dawn/fuzzers/lpmfuzz/DawnLPMConstants.h
@@ -16,27 +16,10 @@
 #define INVALID_OBJECTID 0
 #define CLAMP_SIZE (1048576)
 
-#define ADAPTER_LIMIT 2
-#define BIND_GROUP_LIMIT 16
-#define BIND_GROUP_LAYOUT_LIMIT 16
-#define BUFFER_LIMIT 16
-#define COMMAND_BUFFER_LIMIT 16
-#define COMMAND_ENCODER_LIMIT 16
-#define COMPUTE_PASS_ENCODER_LIMIT 16
-#define COMPUTE_PIPELINE_LIMIT 16
-#define DEVICE_LIMIT 2
-#define EXTERNAL_TEXTURE_LIMIT 16
-#define INSTANCE_LIMIT 16
-#define PIPELINE_LAYOUT_LIMIT 16
-#define QUERY_SET_LIMIT 16
-#define QUEUE_LIMIT 16
-#define RENDER_BUNDLE_LIMIT 16
-#define RENDER_BUNDLE_ENCODER_LIMIT 16
-#define RENDER_PASS_ENCODER_LIMIT 16
-#define RENDER_PIPELINE_LIMIT 16
-#define SAMPLER_LIMIT 16
-#define SHADER_MODULE_LIMIT 16
-#define SURFACE_LIMIT 16
-#define SWAP_CHAIN_LIMIT 16
-#define TEXTURE_LIMIT 16
-#define TEXTURE_VIEW_LIMIT 16
+{% for type in by_category["object"] %}
+    {% if type.name.canonical_case() in cmd_records["lpm_info"]["limits"] %}
+        #define {{ type.name.SNAKE_CASE() }}_LIMIT {{ cmd_records["lpm_info"]["limits"][type.name.canonical_case()] }}
+    {% else %}
+        #define {{ type.name.SNAKE_CASE() }}_LIMIT {{ cmd_records["lpm_info"]["limits"]["default"] }}
+    {% endif %}
+{% endfor %}
