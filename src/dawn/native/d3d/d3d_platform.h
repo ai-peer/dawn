dc79db5bdf345c06bf122d8bb25ca4aa0353c822
Peng Huang <penghuang@chromium.org>
2023-03-21 10:10:27 -0400

    Move D3DCompliationRequest struct to d3d folder
    So it can be shared between D3D11 and D3D12 backends.
    
    Bug: dawn:1705
    Change-Id: Iafd2a1d66a9989229892f86a61a0d4df26f3d672

--- src/dawn/native/d3d/d3d_platform.h
+++ src/dawn/native/d3d/d3d_platform.h
@@ -21,10 +21,11 @@
 // NOLINTNEXTLINE(build/include_order)
 #include "dawn/common/windows_with_undefs.h"
 
-#include <d3d11_2.h>  // NOLINT(build/include_order)
-#include <dxcapi.h>   // NOLINT(build/include_order)
-#include <dxgi1_4.h>  // NOLINT(build/include_order)
-#include <wrl.h>      // NOLINT(build/include_order)
+#include <d3d11_2.h>      // NOLINT(build/include_order)
+#include <d3dcompiler.h>  // NOLINT(build/include_order)
+#include <dxcapi.h>       // NOLINT(build/include_order)
+#include <dxgi1_4.h>      // NOLINT(build/include_order)
+#include <wrl.h>          // NOLINT(build/include_order)
 
 // DXProgrammableCapture.h takes a dependency on other platform header
 // files, so it must be defined after them.
