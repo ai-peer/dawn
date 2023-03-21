af22934c4ac8fcde8d7bf6005438bb3caac563a0
Yan,Shaobo <shaobo.yan@intel.com>
2023-03-21 17:31:25 +0800

    WIP: Refactor Memory Service In Vulkan Backend
    Dawn provides external memory service in static way by compiling
    implementation files based on OS type.
    
    A better way is to select external memory in runtime.
    
    This CL adds ServiceImplementation interface and implements it for
    different memory type. Memory service delegate work to
    implementation in runtime.
    
    Bug=dawn:1593
    
    Change-Id: I09938409e99cf30a17be0f551822352b22635eac

--- src/dawn/native/vulkan/external_memory/MemoryService.cpp
+++ src/dawn/native/vulkan/external_memory/MemoryService.cpp
@@ -12,46 +12,132 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <utility>
+
 #include "dawn/native/vulkan/external_memory/MemoryService.h"
 
-#include "dawn/native/vulkan/DeviceVk.h"
+#include "dawn/native/vulkan/external_memory/ServiceImplementation.h"
+
+#if DAWN_PLATFORM_IS(LINUX)
+#include "dawn/native/vulkan/external_memory/ServiceImplementationDmaBuf.h"
+#include "dawn/native/vulkan/external_memory/ServiceImplementationOpaqueFD.h"
+#endif  // DAWN_PLATFORM_IS(LINUX)
+
+#if DAWN_PLATFORM_IS(ANDROID)
+#include "dawn/native/vulkan/external_memory/ServiceImplementationAHardwareBuffer.h"
+#endif  // DAWN_PLATFORM_IS(ANDROID)
+
+#if DAWN_PLATFORM_IS(FUCHSIA)
+#include "dawn/native/vulkan/external_memory/ServiceImplementationZirconHandle.h"
+#endif  // DAWN_PLATFORM_IS(FUCHSIA)
 
 namespace dawn::native::vulkan::external_memory {
+// static
+bool ServiceManager::CheckSupport(const VulkanDeviceInfo& deviceInfo) {
+#if DAWN_PLATFORM_IS(ANDROID)
+    return CheckAHardwareBufferSupport(deviceInfo);
+#elif DAWN_PLATFORM_IS(FUCHSIA)
+    return CheckZirconHandleSupport(deviceInfo);
+#elif DAWN_PLATFORM_IS(LINUX)
+    return CheckOpaqueFDSupport(deviceInfo) || CheckDmaBufSupport(deviceInfo);
+#elif DAWN_PLATFORM_IS(CHROMEOS)
+    return CheckDmaBufSupport(deviceInfo);
+#else
+    return false;
+#endif
+}
 
-bool Service::RequiresDedicatedAllocation(const ExternalImageDescriptorVk* descriptor,
-                                          VkImage image) {
-    switch (descriptor->dedicatedAllocation) {
-        case NeedsDedicatedAllocation::Yes:
-            return true;
+Service::Service(Device* device) : mDevice(device) {}
+Service::~Service() = default;
 
-        case NeedsDedicatedAllocation::No:
-            return false;
+bool Service::SupportsImportMemory(const ExternalImageType externalImageType,
+                                   VkFormat format,
+                                   VkImageType type,
+                                   VkImageTiling tiling,
+                                   VkImageUsageFlags usage,
+                                   VkImageCreateFlags flags) {
+    return GetServiceImplementation(externalImageType)
+        ->SupportsImportMemory(format, type, tiling, usage, flags);
+}
 
-        case NeedsDedicatedAllocation::Detect:
-            if (!mDevice->GetDeviceInfo().HasExt(DeviceExt::DedicatedAllocation)) {
-                return false;
-            }
+bool Service::SupportsCreateImage(const ExternalImageDescriptor* descriptor,
+                                  VkFormat format,
+                                  VkImageUsageFlags usage,
+                                  bool* supportsDisjoint) {
+    return GetServiceImplementation(descriptor->GetType())
+        ->SupportsCreateImage(descriptor, format, usage, supportsDisjoint);
+}
 
-            VkMemoryDedicatedRequirements dedicatedRequirements;
-            dedicatedRequirements.sType = VK_STRUCTURE_TYPE_MEMORY_DEDICATED_REQUIREMENTS;
-            dedicatedRequirements.pNext = nullptr;
+ResultOrError<MemoryImportParams> Service::GetMemoryImportParams(
+    const ExternalImageDescriptor* descriptor,
+    VkImage image) {
+    return GetServiceImplementation(descriptor->GetType())
+        ->GetMemoryImportParams(descriptor, image);
+}
 
-            VkMemoryRequirements2 baseRequirements;
-            baseRequirements.sType = VK_STRUCTURE_TYPE_MEMORY_REQUIREMENTS_2;
-            baseRequirements.pNext = &dedicatedRequirements;
+uint32_t Service::GetQueueFamilyIndex(const ExternalImageType externalImageType) {
+    return GetServiceImplementation(externalImageType)->GetQueueFamilyIndex();
+}
 
-            VkImageMemoryRequirementsInfo2 imageInfo;
-            imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_REQUIREMENTS_INFO_2;
-            imageInfo.pNext = nullptr;
-            imageInfo.image = image;
+ResultOrError<VkDeviceMemory> Service::ImportMemory(const ExternalImageType externalImageType,
+                                                    ExternalMemoryHandle handle,
+                                                    const MemoryImportParams& importParams,
+                                                    VkImage image) {
+    return GetServiceImplementation(externalImageType)->ImportMemory(handle, importParams, image);
+}
 
-            mDevice->fn.GetImageMemoryRequirements2(mDevice->GetVkDevice(), &imageInfo,
-                                                    &baseRequirements);
+ResultOrError<VkImage> Service::CreateImage(const ExternalImageDescriptor* descriptor,
+                                            const VkImageCreateInfo& baseCreateInfo) {
+    return GetServiceImplementation(descriptor->GetType())->CreateImage(descriptor, baseCreateInfo);
+}
 
-            // The Vulkan spec requires that prefersDA is set if requiresDA is, so we can just check
-            // for prefersDA.
-            return dedicatedRequirements.prefersDedicatedAllocation;
+ServiceImplementation* Service::GetServiceImplementation(
+    const ExternalImageType externalImageType) {
+    if (!mServiceImpls.contains(externalImageType)) {
+        switch (externalImageType) {
+            case ExternalImageType::OpaqueFD:
+#if DAWN_PLATFORM_IS(FUCHSIA)
+                auto zicronHandleService = CreateZicronHandleService(mDevice);
+                if (zicronHandleService->Supported()) {
+                    mServiceImpls.emplace(std::make_pair(ExternalImageType::OpaqueFD,
+                                                         std::move(zicronHandleService)));
+                }
+#elif DAWN_PLATFORM_IS(LINUX)  // DAWN_PLATFORM_IS(FUCHSIA)
+                auto opaqueFDService = CreateOpaqueFDService(mDevice);
+                if (opaqueFDService->Supported()) {
+                    mServiceImpls.emplace(
+                        std::make_pair(ExternalImageType::OpaqueFD, std::move(opaqueFDService)));
+                }
+#endif
+                break;
+            case ExternalImageType::DmaBuf:
+#if DAWN_PLATFORM_IS(LINUX) || DAWN_PLATFORM_IS(CHROMEOS)
+                auto dmaBufSerivce = CreateDmaBufService(mDevice);
+                if (dmaBufSerivce->Supported()) {
+                    mServiceImpls.emplace(
+                        std::make_pair(ExternalImageType::DmaBuf, std::move(dmaBufSerivce)));
+                }
+#endif  // DAWN_PLATFORM_IS(LINUX) || DAWN_PLATFORM_IS(CHROMEOS)
+                break;
+            case ExternalImageType::AHardwareBuffer:
+#if DAWN_PLATFORM_IS(ANDROID)
+                auto aHardwareBufferService = CreateAHardwareBufferService(mDevice);
+                if (aHardwareBufferService->Supported()) {
+                    mServiceImpls.emplace(std::make_pair(ExternalImageType::AHardwareBuffer,
+                                                         std::move(aHardwareBufferService)));
+                }
+#endif  // DAWN_PLATFORM_IS(ANDROID)
+                break;
+            default:
+                DAWN_UNREACHABLE();
+        }
     }
+    auto search = mServiceImpls.find(type);
+
+    if (search != mServiceImpls.end()) {
+        return search->second.get();
+    }
+
     DAWN_UNREACHABLE();
 }
 
