207d280dafe39386154f95511f9d10a024984a1d
Yan,Shaobo <shaobo.yan@intel.com>
2023-03-27 16:22:37 +0800

    MemoryService Choose Implementation based on ExternalImageType
    This CL add supports for MemoryService choosing implementation based on
    ExternalImageType in runtime instead of choosing in compile time.
    
    This CL also removes MemoryServiceNull.cpp.
    
    Bug: dawn:1593
    Change-Id: Ib60e04931ab3259d4f715cb81b2a478c981da31b

--- src/dawn/native/vulkan/external_memory/MemoryService.cpp
+++ src/dawn/native/vulkan/external_memory/MemoryService.cpp
@@ -12,47 +12,133 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <utility>
+
+#include "dawn/native/vulkan/DeviceVk.h"
 #include "dawn/native/vulkan/external_memory/MemoryService.h"
+
 #include "dawn/native/vulkan/external_memory/MemoryServiceImplementation.h"
 
+#if DAWN_PLATFORM_IS(LINUX) || DAWN_PLATFORM_IS(CHROMEOS)
+#include "dawn/native/vulkan/external_memory/MemoryServiceImplementationDmaBuf.h"
+#include "dawn/native/vulkan/external_memory/MemoryServiceImplementationOpaqueFD.h"
+#endif  // DAWN_PLATFORM_IS(LINUX)
+
+#if DAWN_PLATFORM_IS(ANDROID)
+#include "dawn/native/vulkan/external_memory/MemoryServiceImplementationAHardwareBuffer.h"
+#endif  // DAWN_PLATFORM_IS(ANDROID)
+
+#if DAWN_PLATFORM_IS(FUCHSIA)
+#include "dawn/native/vulkan/external_memory/MemoryServiceImplementationZirconHandle.h"
+#endif  // DAWN_PLATFORM_IS(FUCHSIA)
+
 namespace dawn::native::vulkan::external_memory {
+// static
+bool Service::CheckSupport(const VulkanDeviceInfo& deviceInfo) {
+#if DAWN_PLATFORM_IS(ANDROID)
+    return CheckAHardwareBufferSupport(deviceInfo);
+#elif DAWN_PLATFORM_IS(FUCHSIA)
+    return CheckZirconHandleSupport(deviceInfo);
+#elif DAWN_PLATFORM_IS(LINUX) || DAWN_PLATFORM_IS(CHROMEOS)
+    return CheckOpaqueFDSupport(deviceInfo) || CheckDmaBufSupport(deviceInfo);
+#else
+    return false;
+#endif
+}
+
+Service::Service(Device* device) : mDevice(device) {
+#if DAWN_PLATFORM_IS(FUCHSIA)
+    if (CheckZirconHandleSupport(mDevice->GetDeviceInfo())) {
+        mServiceImpls.emplace(
+            std::make_pair(ExternalImageType::OpaqueFD, CreateZirconHandleService(mDevice)));
+    }
+#endif  // DAWN_PLATFORM_IS(FUCHSIA)
+
+#if DAWN_PLATFORM_IS(LINUX) || DAWN_PLATFORM_IS(CHROMEOS)
+    if (CheckOpaqueFDSupport(mDevice->GetDeviceInfo())) {
+        mServiceImpls.emplace(
+            std::make_pair(ExternalImageType::OpaqueFD, CreateOpaqueFDService(mDevice)))
+    }
+
+    if (CheckDmaBufSupport(mDevice->GetDeviceInfo())) {
+        mServiceImpls.emplace(
+            std::make_pair(ExternalImageType::DmaBuf, CreateDmaBufService(mDevice)));
+    }
+#endif  // DAWN_PLATFORM_IS(LINUX) || DAWN_PLATFORM_IS(CHROMEOS)
+
+#if DAWN_PLATFORM_IS(ANDROID)
+    if (CheckAHardwareBufferSupport(mDevice->GetDeviceInfo())) {
+        mServiceImpls.emplace(std::make_pair(ExternalImageType::AHardwareBuffer,
+                                             CreateAHardwareBufferService(mDevice)));
+    }
+#endif  // DAWN_PLATFORM_IS(ANDROID)
+}
 
 Service::~Service() = default;
 
-bool Service::SupportsImportMemory(VkFormat format,
+bool Service::SupportsImportMemory(ExternalImageType externalImageType,
+                                   VkFormat format,
                                    VkImageType type,
                                    VkImageTiling tiling,
                                    VkImageUsageFlags usage,
                                    VkImageCreateFlags flags) {
-    return mImpl->SupportsImportMemory(format, type, tiling, usage, flags);
+    ServiceImplementation* serviceImpl = GetServiceImplementation(externalImageType);
+    ASSERT(serviceImpl);
+
+    return serviceImpl->SupportsImportMemory(format, type, tiling, usage, flags);
 }
 
 bool Service::SupportsCreateImage(const ExternalImageDescriptor* descriptor,
                                   VkFormat format,
                                   VkImageUsageFlags usage,
                                   bool* supportsDisjoint) {
-    return mImpl->SupportsCreateImage(descriptor, format, usage, supportsDisjoint);
+    ServiceImplementation* serviceImpl = GetServiceImplementation(descriptor->GetType());
+    ASSERT(serviceImpl);
+
+    return serviceImpl->SupportsCreateImage(descriptor, format, usage, supportsDisjoint);
 }
 
 ResultOrError<MemoryImportParams> Service::GetMemoryImportParams(
     const ExternalImageDescriptor* descriptor,
     VkImage image) {
-    return mImpl->GetMemoryImportParams(descriptor, image);
+    ServiceImplementation* serviceImpl = GetServiceImplementation(descriptor->GetType());
+    ASSERT(serviceImpl);
+
+    return serviceImpl->GetMemoryImportParams(descriptor, image);
 }
 
-uint32_t Service::GetQueueFamilyIndex() {
-    return mImpl->GetQueueFamilyIndex();
+uint32_t Service::GetQueueFamilyIndex(ExternalImageType externalImageType) {
+    ServiceImplementation* serviceImpl = GetServiceImplementation(externalImageType);
+    ASSERT(serviceImpl);
+
+    return serviceImpl->GetQueueFamilyIndex();
 }
 
-ResultOrError<VkDeviceMemory> Service::ImportMemory(ExternalMemoryHandle handle,
+ResultOrError<VkDeviceMemory> Service::ImportMemory(ExternalImageType externalImageType,
+                                                    ExternalMemoryHandle handle,
                                                     const MemoryImportParams& importParams,
                                                     VkImage image) {
-    return mImpl->ImportMemory(handle, importParams, image);
+    ServiceImplementation* serviceImpl = GetServiceImplementation(externalImageType);
+    ASSERT(serviceImpl);
+
+    return serviceImpl->ImportMemory(handle, importParams, image);
 }
 
 ResultOrError<VkImage> Service::CreateImage(const ExternalImageDescriptor* descriptor,
                                             const VkImageCreateInfo& baseCreateInfo) {
-    return mImpl->CreateImage(descriptor, baseCreateInfo);
+    ServiceImplementation* serviceImpl = GetServiceImplementation(descriptor->GetType());
+    ASSERT(serviceImpl);
+
+    return serviceImpl->CreateImage(descriptor, baseCreateInfo);
+}
+
+ServiceImplementation* Service::GetServiceImplementation(ExternalImageType externalImageType) {
+    auto search = mServiceImpls.find(externalImageType);
+    if (search != mServiceImpls.end()) {
+        return search->second.get();
+    }
+
+    return nullptr;
 }
 
 }  // namespace dawn::native::vulkan::external_memory
