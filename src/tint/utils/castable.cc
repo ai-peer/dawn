b39326d97b069626a4e1c893458681eb4599a2e8
dan sinclair <dsinclair@chromium.org>
2023-04-14 18:11:03 -0400

    Move castable into utils.
    The Castable class is used by utils/. This Cl moves the implementation
    into the utils/ folder. The namespace is _not_ changed with this move as
    there is a lot of code accessing `Is` and `As` currently. We can decide
    if changing the namespace is useful in the future.
    
    Change-Id: I0decedb92ebed01b6aa12d2e3efa7190742e9a33

--- src/tint/utils/castable.cc
+++ src/tint/utils/castable.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "src/tint/castable.h"
+#include "src/tint/utils/castable.h"
 
 namespace tint {
 
