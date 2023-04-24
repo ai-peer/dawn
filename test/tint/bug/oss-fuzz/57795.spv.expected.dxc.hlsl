842afdc3065515179a93fa9cdf6361eb3d09bfcd
James Price <jrprice@google.com>
2023-04-24 17:11:04 -0400

    spirv-reader: Declare multiuse constant composites
    When an OpConstantComposite result is used by multiple instructions,
    declare it as a module-scope `const` instead of inlining the constant
    at each use site. This fixes an issue whereby the spirv-reader was
    massively inflating the size of the WGSL it produces, which was caught
    via an OOM fuzzer bug.
    
    Bug: oss-fuzz:57795
    Change-Id: Iac8c6a2147a7e2ebfddbaacae9fcb1dbe0b59e9d

---
+++ test/tint/bug/oss-fuzz/57795.spv.expected.dxc.hlsl
@@ -0,0 +1,18 @@
+struct S_6 {
+  float3 field0[98][87];
+};
+
+static float4x4 x_3183[58] = (float4x4[58])0;
+
+static S_6 x_3189[46] = (S_6[46])0;
+static float3 x_3196[37] = (float3[37])0;
+
+void main_1() {
+  const uint x_3181 = 58u;
+  return;
+}
+
+void main() {
+  main_1();
+  return;
+}
