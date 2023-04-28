902a95e521f13d886909f52c928af8fafb4c42a2
Ben Clayton <bclayton@google.com>
2023-04-28 10:22:26 +0100

    tint: Remove Program|ProgramBuilder::FriendlyName()
    Now that we don't need the symbol table to get the names of the types, we can just call FriendlyName() on the type directly.
    
    Change-Id: I39478f5b8847ee032e77c15fd0de0665ddbf4811

--- src/tint/writer/glsl/generator_impl.cc
+++ src/tint/writer/glsl/generator_impl.cc
@@ -2147,9 +2147,8 @@ void GeneratorImpl::EmitConstant(utils::StringStream& out, const constant::Value
             }
         },
         [&](Default) {
-            diagnostics_.add_error(
-                diag::System::Writer,
-                "unhandled constant type: " + builder_.FriendlyName(constant->Type()));
+            diagnostics_.add_error(diag::System::Writer,
+                                   "unhandled constant type: " + constant->Type()->FriendlyName());
         });
 }
 
