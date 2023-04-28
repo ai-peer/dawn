c4a544deedcd38898c084b14a4425e4352603856
Loko Kung <lokokung@google.com>
2023-04-28 13:54:45 -0700

    Adds generation for WGSL descriptor to rename source->code.
    - Adds a deprecation warning for the incorrect usage.
    - Adds test to verify that the warning is emitted and that the results
      are equal.
    - Fixes all existing tests to use the newer code path.
    
    Change-Id: I01bf85137ad1c66966e1aa0259ae03fc5247ba25

--- src/dawn/native/ShaderModule.cpp
+++ src/dawn/native/ShaderModule.cpp
@@ -955,7 +955,7 @@ MaybeError ValidateAndParseShaderModule(DeviceBase* device,
         DAWN_INVALID_IF(!result.success, "Tint WGSL failure: Generator: %s", result.error);
 
         newWgslCode = std::move(result.wgsl);
-        newWgslDesc.source = newWgslCode.c_str();
+        newWgslDesc.code = newWgslCode.c_str();
 
         spirvDesc = nullptr;
         wgslDesc = &newWgslDesc;
@@ -980,11 +980,13 @@ MaybeError ValidateAndParseShaderModule(DeviceBase* device,
 
     ASSERT(wgslDesc != nullptr);
 
-    auto tintSource = std::make_unique<TintSource>("", wgslDesc->source);
+    auto tintSource =
+        std::make_unique<TintSource>("", wgslDesc->code ? wgslDesc->code : wgslDesc->source);
 
     if (device->IsToggleEnabled(Toggle::DumpShaders)) {
         std::ostringstream dumpedMsg;
-        dumpedMsg << "// Dumped WGSL:" << std::endl << wgslDesc->source;
+        dumpedMsg << "// Dumped WGSL:" << std::endl
+                  << (wgslDesc->code ? wgslDesc->code : wgslDesc->source);
         device->EmitLog(WGPULoggingType_Info, dumpedMsg.str().c_str());
     }
 
@@ -1101,7 +1103,14 @@ ShaderModuleBase::ShaderModuleBase(DeviceBase* device,
         mOriginalSpirv.assign(spirvDesc->code, spirvDesc->code + spirvDesc->codeSize);
     } else if (wgslDesc) {
         mType = Type::Wgsl;
-        mWgsl = std::string(wgslDesc->source);
+        if (wgslDesc->code) {
+            mWgsl = std::string(wgslDesc->code);
+        } else {
+            device->EmitDeprecationWarning(
+                "ShaderModuleWGSLDescriptor.source is deprecated, use "
+                "ShaderModuleWGSLDescriptor.code instead.");
+            mWgsl = std::string(wgslDesc->source);
+        }
     }
 }
 
