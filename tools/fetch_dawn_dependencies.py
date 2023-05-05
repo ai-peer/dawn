8e6e46fc7e38eda490cf4b9301c193b5bc299c22
Elie Michel <elie.michel.fr@gmail.com>
2023-05-05 21:52:23 +0200

    Add fetch_dawn_dependencies
    Change-Id: Iaedb4af78036a1696b68787c36f1d3d70e18ba2c

---
+++ tools/fetch_dawn_dependencies.py
@@ -0,0 +1,101 @@
+# Copyright 2023 The Dawn & Tint Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Dawn uses a non standard way of downloading some of its dependencies (gclient)
+We replace it with this script so that we can use Dawn in a third party project
+with a simple add_subdirectory (or FetchContent).
+"""
+
+import os
+import sys
+import subprocess
+
+required_submodules = [
+    'third_party/vulkan-deps',
+    'spirv-headers/src',
+    'spirv-tools/src',
+    'vulkan-headers/src',
+    'vulkan-loader/src',
+    'vulkan-tools/src',
+    'third_party/glfw',
+    'third_party/abseil-cpp',
+    'third_party/jinja2',
+    'third_party/markupsafe',
+
+    'third_party/googletest',
+]
+
+class cd:
+    """Context manager for changing the current working directory"""
+    def __init__(self, newPath):
+        self.newPath = os.path.expanduser(newPath)
+
+    def __enter__(self):
+        self.savedPath = os.getcwd()
+        os.chdir(self.newPath)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.savedPath)
+
+class Var:
+    def __init__(self, name):
+        self.name = name
+    def __add__(self, str):
+        return self.name + str
+    def __radd__(self, str):
+        return str + self.name
+
+def log(msg):
+    print(f"-- -- {msg}")
+
+def main():
+    if not os.path.isfile('DEPS'):
+        return
+
+    log(f"Listing dependencies from {os.getcwd()}")
+    DEPS = open('DEPS').read()
+
+    ldict = {}
+    exec(DEPS, globals(), ldict)  # yeah I know this is dangerous
+    deps = ldict.get('deps')
+    if deps is None:
+        return
+
+    for path in required_submodules:
+        if path not in deps:
+            continue
+        url, tag = parse_url(deps[path], ldict['vars'])
+        log(f"Fetching dependency '{path}'")
+
+        if not os.path.isdir(path):
+            log(f"Cloning '{url}' into '{path}'")
+            subprocess.run(['git', 'clone', '--recurse-submodules', url, path], capture_output=True)
+            with cd(path):
+                log(f"Checking out tag '{tag}'")
+                subprocess.run(['git', 'checkout', tag], capture_output=True)
+        else:
+            log(f"(Already cloned)")
+        with cd(path):
+            subprocess.run([sys.executable, __file__])
+
+def parse_url(dependency, vars):
+    url = dependency['url']
+    tokens = url.split('@')
+    tag = tokens[-1].format(**vars)
+    url = '@'.join(tokens[:-1]).format(**vars)
+    return url, tag
+
+if __name__ == "__main__":
+    main()
