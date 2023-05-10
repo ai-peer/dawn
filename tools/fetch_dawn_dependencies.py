9377d960d10b4dfbe6e260f0d6dd64ca16d06819
Elie Michel <elie.michel.fr@gmail.com>
2023-05-11 00:38:34 +0200

    Update fetch_dawn_dependencies with all remarks from cwallez
    Change-Id: I271ed7cd2830442b99855d0a85a7a022d7b3dcb1

--- tools/fetch_dawn_dependencies.py
+++ tools/fetch_dawn_dependencies.py
@@ -13,89 +13,177 @@
 # limitations under the License.
 
 """
-Dawn uses a non standard way of downloading some of its dependencies (gclient)
-We replace it with this script so that we can use Dawn in a third party project
-with a simple add_subdirectory (or FetchContent).
+Helper script to download Dawn's source dependencies without the need to
+install depot_tools by manually. This script implements a subset of
+`gclient sync`.
+
+This helps embedders, for example through CMake, get all the sources with
+a single add_subdirectory call (or FetchContent) instead of more complex setups
+
+Note that this script executes blindly the content of DEPS file, run it only on
+a project that you trust not to contain malicious DEPS files.
 """
 
 import os
 import sys
 import subprocess
+import argparse
+from pathlib import Path
 
-required_submodules = [
-    'third_party/vulkan-deps',
-    'spirv-headers/src',
-    'spirv-tools/src',
-    'vulkan-headers/src',
-    'vulkan-loader/src',
-    'vulkan-tools/src',
-    'third_party/glfw',
-    'third_party/abseil-cpp',
-    'third_party/jinja2',
-    'third_party/markupsafe',
+parser = argparse.ArgumentParser(
+    prog='fetch_dawn_dependencies',
+    description=__doc__,
+)
 
-    'third_party/googletest',
-]
+parser.add_argument(
+    '-d', '--directory', default="",
+    help="""
+    Working directory, in which we read and apply DEPS files recusively. If not
+    specified, the current working directory is used.
+    """
+)
 
-class cd:
-    """Context manager for changing the current working directory"""
-    def __init__(self, newPath):
-        self.newPath = os.path.expanduser(newPath)
+parser.add_argument(
+    '-g', '--git', default="git",
+    help="""
+    Path to the git command used to. By default, git is retrieved from the PATH.
+    You may also use this option to specify extra argument for all calls to git.
+    """
+)
 
-    def __enter__(self):
-        self.savedPath = os.getcwd()
-        os.chdir(self.newPath)
+def main(args):
+    # The dependencies that we need to pull from the DEPS files.
+    # Dependencies of dependencies are prefixed by their ancestors.
+    required_submodules = {
+        'third_party/vulkan-deps',
+        'third_party/vulkan-deps/spirv-headers/src',
+        'third_party/vulkan-deps/spirv-tools/src',
+        'third_party/vulkan-deps/vulkan-headers/src',
+        'third_party/vulkan-deps/vulkan-loader/src',
+        'third_party/vulkan-deps/vulkan-tools/src',
+        'third_party/glfw',
+        'third_party/abseil-cpp',
+        'third_party/jinja2',
+        'third_party/markupsafe',
 
-    def __exit__(self, etype, value, traceback):
-        os.chdir(self.savedPath)
+        'third_party/googletest',
+    }
 
-class Var:
-    def __init__(self, name):
-        self.name = name
-    def __add__(self, str):
-        return self.name + str
-    def __radd__(self, str):
-        return str + self.name
+    root_dir = Path(args.directory).resolve()
 
-def log(msg):
-    print(f"-- -- {msg}")
+    process_dir(args, root_dir, required_submodules)
 
-def main():
-    if not os.path.isfile('DEPS'):
+def process_dir(args, dir_path, required_submodules):
+    """
+    Install dependencies for the provided directory by processing the DEPS file
+    that it contains (if it exists).
+    Recursively install dependencies in sub-directories that are created by
+    cloning dependencies.
+    """
+    deps_path = dir_path / 'DEPS'
+    if not deps_path.is_file():
         return
 
-    log(f"Listing dependencies from {os.getcwd()}")
-    DEPS = open('DEPS').read()
+    log(f"Listing dependencies from {dir_path}")
+    DEPS = open(deps_path).read()
 
     ldict = {}
-    exec(DEPS, globals(), ldict)  # yeah I know this is dangerous
+    exec(DEPS, globals(), ldict)
     deps = ldict.get('deps')
+    variables = ldict.get('vars', {})
+
     if deps is None:
-        return
+        log(f"ERROR: DEPS file '{deps_path}' does not define a 'deps' variable")
+        exit(1)
 
-    for path in required_submodules:
-        if path not in deps:
+    for submodule in required_submodules:
+        if submodule not in deps:
             continue
-        url, tag = parse_url(deps[path], ldict['vars'])
-        log(f"Fetching dependency '{path}'")
+        submodule_path = dir_path / Path(submodule)
 
-        if not os.path.isdir(path):
-            log(f"Cloning '{url}' into '{path}'")
-            subprocess.run(['git', 'clone', '--recurse-submodules', url, path], capture_output=True)
-            with cd(path):
-                log(f"Checking out tag '{tag}'")
-                subprocess.run(['git', 'checkout', tag], capture_output=True)
+        raw_url = deps[submodule]['url']
+        git_url, git_tag = raw_url.format(**variables).rsplit('@', 1)
+
+        log(f"Fetching dependency '{submodule}'")
+        if not submodule_path.is_dir():
+            log(f"Cloning '{git_url}' into '{submodule_path}'")
+            subprocess.run([
+                args.git,
+                'clone',
+                '--recurse-submodules',
+                git_url,
+                submodule_path,
+            ], capture_output=True)
+            log(f"Checking out tag '{git_tag}'")
+            subprocess.run([
+                args.git,
+                '-C', submodule_path,
+                'checkout', git_tag,
+            ], capture_output=True)
+
+        elif (submodule_path / ".git").is_dir():
+            # The module was already cloned, but we may need to update it
+            proc = subprocess.run([
+                args.git,
+                '-C', submodule_path,
+                'rev-parse',
+                'HEAD',
+            ], capture_output=True)
+            need_update = proc.stdout.decode().strip() != git_tag
+
+            if need_update:
+                # The module was already cloned, but we may need to update it
+                proc = subprocess.run([
+                    args.git,
+                    '-C', submodule_path,
+                    'cat-file',
+                    '-t', git_tag,
+                ], capture_output=True)
+                git_tag_exists = proc.returncode == 0
+
+                if not git_tag_exists:
+                    log(f"Updating '{submodule_path}' from '{git_url}'")
+                    subprocess.run([
+                        args.git,
+                        '-C', submodule_path,
+                        'fetch', 'origin',
+                    ], capture_output=True)
+
+                log(f"Checking out tag '{git_tag}'")
+                subprocess.run([
+                    args.git,
+                    '-C', submodule_path,
+                    'checkout', git_tag,
+                ], capture_output=True)
+
         else:
-            log(f"(Already cloned)")
-        with cd(path):
-            subprocess.run([sys.executable, __file__])
+            # The caller may have "flattened" the source tree to get rid of
+            # some heavy submodules.
+            log(f"(Overridden by a local copy of the submodule)")
 
-def parse_url(dependency, vars):
-    url = dependency['url']
-    tokens = url.split('@')
-    tag = tokens[-1].format(**vars)
-    url = '@'.join(tokens[:-1]).format(**vars)
-    return url, tag
+            # Recursive call
+        required_subsubmodules = [
+            m[len(submodule)+1:]
+            for m in required_submodules
+            if m.startswith(submodule + "/")
+        ]
+        process_dir(args, submodule_path, required_subsubmodules)
+
+def log(msg):
+    """Just makes it look good in the CMake log flow."""
+    print(f"-- -- {msg}")
+
+class Var:
+    """
+    Mock Var class, that the content of DEPS files assume to exist when they
+    are exec-ed.
+    """
+    def __init__(self, name):
+        self.name = name
+    def __add__(self, text):
+        return self.name + text
+    def __radd__(self, text):
+        return text + self.name
 
 if __name__ == "__main__":
-    main()
+    main(parser.parse_args())
