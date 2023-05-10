a48ad5352567027097ea680cb05f2ae79a4f3709
Shrek Shao <shrekshao@google.com>
2023-05-10 13:53:33 -0700

    Compat GL/GLES: blit a stencil texture to a buffer using compute
    Add compute blit emulation path for Stencil8 textures for
    OpenGLES backend.
    
    Bug: dawn:1782, dawn:1835
    Change-Id: I4719d339ee78fd5fc524d809417504125d2c0aee

--- src/dawn/native/Texture.cpp
+++ src/dawn/native/Texture.cpp
@@ -599,6 +599,12 @@ TextureBase::TextureBase(DeviceBase* device,
             AddInternalUsage(wgpu::TextureUsage::TextureBinding);
         }
     }
+    if (mFormat.HasStencil() &&
+        device->IsToggleEnabled(Toggle::UseBlitForStencilTextureToBufferCopy)) {
+        if (mInternalUsage & wgpu::TextureUsage::CopySrc) {
+            AddInternalUsage(wgpu::TextureUsage::TextureBinding);
+        }
+    }
 }
 
 TextureBase::~TextureBase() = default;
