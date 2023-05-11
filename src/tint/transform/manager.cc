651b9ca92509fc50adb3e35873d143ae6d55e3ff
James Price <jrprice@google.com>
2023-05-11 16:22:01 -0400

    [ir] Add base `ir::transform::Transform` class
    Enable the transform manager to run a pipeline that mixes AST and IR
    transforms, automatically converting the current program as
    necessary. The IR->AST conversion is left unimplemented for now
    (blocked on tint:1902).
    
    Bug: tint:1718
    Change-Id: I8df76db61edd94e0b1d7c2aaabc18b394db3d8de

--- src/tint/transform/manager.cc
+++ src/tint/transform/manager.cc
@@ -15,6 +15,8 @@
 #include "src/tint/transform/manager.h"
 
 #include "src/tint/ast/transform/transform.h"
+#include "src/tint/ir/from_program.h"
+#include "src/tint/ir/transform/transform.h"
 #include "src/tint/program_builder.h"
 
 /// If set to 1 then the transform::Manager will dump the WGSL of the program
@@ -23,6 +25,7 @@
 
 #if TINT_PRINT_PROGRAM_FOR_EACH_TRANSFORM
 #include <iostream>
+#include "src/tint/ir/disassembler.h"
 #define TINT_IF_PRINT_PROGRAM(x) x
 #else  // TINT_PRINT_PROGRAM_FOR_EACH_TRANSFORM
 #define TINT_IF_PRINT_PROGRAM(x)
@@ -33,62 +36,125 @@ namespace tint::transform {
 Manager::Manager() = default;
 Manager::~Manager() = default;
 
-Program Manager::Run(const Program* program,
-                     const transform::DataMap& inputs,
-                     transform::DataMap& outputs) const {
+template <typename OUTPUT, typename INPUT>
+OUTPUT Manager::RunTransforms(INPUT in,
+                              const transform::DataMap& inputs,
+                              transform::DataMap& outputs) const {
+    static_assert(std::is_same<INPUT, const Program*>() || std::is_same<INPUT, ir::Module*>());
+    static_assert(std::is_same<OUTPUT, Program>() || std::is_same<OUTPUT, ir::Module*>());
+
+    // The current transform target, which could be either AST or IR.
+    std::variant<const Program*, ir::Module*> target = in;
+    // A local AST program to hold the result of AST transforms.
+    Program ast_result;
+    // A local IR module to hold the result of AST->IR conversions.
+    ir::Module ir_result;
+
 #if TINT_PRINT_PROGRAM_FOR_EACH_TRANSFORM
-    auto print_program = [&](const char* msg, const Transform* transform) {
-        auto wgsl = Program::printer(program);
+    auto print_program = [&](const char* msg, const char* name) {
         std::cout << "=========================================================" << std::endl;
-        std::cout << "== " << msg << " " << transform->TypeInfo().name << ":" << std::endl;
+        std::cout << "== " << msg << " " << name << ":" << std::endl;
         std::cout << "=========================================================" << std::endl;
-        std::cout << wgsl << std::endl;
-        if (!program->IsValid()) {
-            std::cout << "-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --" << std::endl;
-            std::cout << program->Diagnostics().str() << std::endl;
+        if (std::holds_alternative<const Program*>(target)) {
+            auto* program = std::get<const Program*>(target);
+            auto wgsl = Program::printer(program);
+            std::cout << wgsl << std::endl;
+            if (!program->IsValid()) {
+                std::cout << "-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --"
+                          << std::endl;
+                std::cout << program->Diagnostics().str() << std::endl;
+            }
+        } else if (std::holds_alternative<ir::Module*>(target)) {
+            ir::Disassembler dis(*std::get<ir::Module*>(target));
+            std::cout << dis.Disassemble();
         }
         std::cout << "=========================================================" << std::endl
                   << std::endl;
     };
 #endif
 
-    std::optional<Program> output;
+    // Helper functions to get the current program state as either an AST program or IR module,
+    // performing a conversion if necessary.
+    auto get_ast = [&]() {
+        if (std::holds_alternative<ir::Module*>(target)) {
+            // TODO(crbug.com/tint/1902): Convert the IR module to an AST program.
+            TINT_ASSERT(Transform, false);
+        }
+        TINT_ASSERT(Transform, std::holds_alternative<const Program*>(target));
+        return std::get<const Program*>(target);
+    };
+    auto get_ir = [&]() {
+        if (std::holds_alternative<const Program*>(target)) {
+            // Convert the AST program to an IR module.
+            auto converted = ir::FromProgram(std::get<const Program*>(target));
+            TINT_ASSERT(Transform, converted);
+            ir_result = converted.Move();
+            target = &ir_result;
+        }
+        TINT_ASSERT(Transform, std::holds_alternative<ir::Module*>(target));
+        return std::get<ir::Module*>(target);
+    };
 
-    TINT_IF_PRINT_PROGRAM(print_program("Input of", this));
+    TINT_IF_PRINT_PROGRAM(print_program("Input of", "transform manager"));
 
     for (const auto& transform : transforms_) {
         if (auto* ast_transform = transform->As<ast::transform::Transform>()) {
-            if (auto result = ast_transform->Apply(program, inputs, outputs)) {
-                output.emplace(std::move(result.value()));
-                program = &output.value();
+            if (auto result = ast_transform->Apply(get_ast(), inputs, outputs)) {
+                ast_result = std::move(result.value());
+                target = &ast_result;
 
-                if (!program->IsValid()) {
-                    TINT_IF_PRINT_PROGRAM(print_program("Invalid output of", transform.get()));
+                if (!ast_result.IsValid()) {
+                    TINT_IF_PRINT_PROGRAM(
+                        print_program("Invalid output of", transform->TypeInfo().name));
                     break;
                 }
 
-                TINT_IF_PRINT_PROGRAM(print_program("Output of", transform.get()));
+                TINT_IF_PRINT_PROGRAM(print_program("Output of", transform->TypeInfo().name));
             } else {
                 TINT_IF_PRINT_PROGRAM(std::cout << "Skipped " << transform->TypeInfo().name
                                                 << std::endl);
             }
+        } else if (auto* ir_transform = transform->As<ir::transform::Transform>()) {
+            ir_transform->Run(get_ir(), inputs, outputs);
+            TINT_IF_PRINT_PROGRAM(print_program("Output of", transform->TypeInfo().name));
         } else {
-            ProgramBuilder b;
-            TINT_ICE(Transform, b.Diagnostics()) << "unhandled transform type";
-            return Program(std::move(b));
+            TINT_ASSERT(Transform, false && "unhandled transform type");
         }
     }
 
-    TINT_IF_PRINT_PROGRAM(print_program("Final output of", this));
+    TINT_IF_PRINT_PROGRAM(print_program("Final output of", "transform manager"));
 
-    if (!output) {
-        ProgramBuilder b;
-        CloneContext ctx{&b, program, /* auto_clone_symbols */ true};
-        ctx.Clone();
-        output = Program(std::move(b));
+    if constexpr (std::is_same<OUTPUT, Program>()) {
+        auto* result = get_ast();
+        if (result == in) {
+            // AST transform pipelines are expected to return a clone of the program, so make sure
+            // the input is cloned at least once even if nothing changed.
+            ProgramBuilder b;
+            CloneContext ctx{&b, result, /* auto_clone_symbols */ true};
+            ctx.Clone();
+            ast_result = Program(std::move(b));
+        }
+        return ast_result;
+    } else if constexpr (std::is_same<OUTPUT, ir::Module*>()) {
+        auto* result = get_ir();
+        if (result == &ir_result) {
+            // IR transform pipelines are expected to mutate the module in place, so move the local
+            // temporary result to the original input.
+            *in = std::move(ir_result);
+        }
+        return in;
     }
+}
 
-    return std::move(output.value());
+Program Manager::Run(const Program* program,
+                     const transform::DataMap& inputs,
+                     transform::DataMap& outputs) const {
+    return RunTransforms<Program>(program, inputs, outputs);
+}
+
+void Manager::Run(ir::Module* module, const DataMap& inputs, DataMap& outputs) const {
+    auto* output = RunTransforms<ir::Module*>(module, inputs, outputs);
+    TINT_ASSERT(Transform, output == module);
 }
 
 }  // namespace tint::transform
