b5adbc238e36bda08b5ee27e29f9b66959e6dd1d
Brian Sheedy <bsheedy@google.com>
2023-05-11 12:51:42 -0700

    Test Dawn branch builders
    Change-Id: Icc33b176beed1f6dfa94b3d236ee3799a6cfc8da

--- src/dawn/tests/end2end/RenderPassLoadOpTests.cpp
+++ src/dawn/tests/end2end/RenderPassLoadOpTests.cpp
@@ -714,9 +714,7 @@ TEST_P(RenderPassLoadOpTests, MixedUseOfLoadOpLoadAndLoadOpClearWithBigIntegerVa
 }
 
 DAWN_INSTANTIATE_TEST(RenderPassLoadOpTests,
-                      D3D11Backend(),
                       D3D12Backend(),
-                      MetalBackend(),
                       OpenGLBackend(),
                       OpenGLESBackend(),
                       VulkanBackend());
