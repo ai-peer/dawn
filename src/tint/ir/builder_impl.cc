c73accbd654931c639cca60b08cc85483e04d743
dan sinclair <dsinclair@chromium.org>
2023-05-15 14:25:23 -0400

    [ir] Move ir::Builder to hold module reference
    This CL moves the module from being owned by the builder, to be passed
    into the builder and stored as a reference. This allows the transforms
    to create builders based on the same module as needed.
    
    Bug: tint:1718
    Change-Id: I4863f368582242626c2a2c6996fd62f053314bb1

--- src/tint/ir/builder_impl.cc
+++ src/tint/ir/builder_impl.cc
@@ -197,7 +197,7 @@ ResultType BuilderImpl::Build() {
         return utils::Failure;
     }
 
-    return ResultType{std::move(builder.ir)};
+    return ResultType{std::move(mod)};
 }
 
 void BuilderImpl::EmitFunction(const ast::Function* ast_func) {
