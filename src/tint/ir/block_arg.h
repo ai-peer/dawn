b3f86f75c887aba548f196816dc503017a670b28
dan sinclair <dsinclair@chromium.org>
2023-05-16 16:39:00 -0400

    [ir] Add basic block arguments.
    This CL adds the ability to add parameters to blocks and emits the
    params as needed in the disassembly.
    
    The ShortCircuit is updated to use block arguments instead of creating
    a return value.
    
    Bug: tint:1909
    Change-Id: I92afa6cf8ff4e01bfa3de46e76c26c465f0d6062

---
+++ src/tint/ir/block_arg.h
@@ -0,0 +1,40 @@
+// Copyright 2023 The Tint Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SRC_TINT_IR_BLOCK_ARG_H_
+#define SRC_TINT_IR_BLOCK_ARG_H_
+
+#include "src/tint/ir/value.h"
+#include "src/tint/utils/castable.h"
+
+namespace tint::ir {
+
+/// An instruction in the IR.
+class BlockArg : public utils::Castable<BlockArg, Value> {
+  public:
+    /// Constructor
+    /// @param type the type of the var
+    explicit BlockArg(const type::Type* type);
+    ~BlockArg() override;
+
+    /// @returns the type of the var
+    const type::Type* Type() const override { return type; }
+
+    /// the result type of the instruction
+    const type::Type* type;
+};
+
+}  // namespace tint::ir
+
+#endif  // SRC_TINT_IR_BLOCK_ARG_H_
