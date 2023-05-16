34ae369a6c7df84910592689cded33999d0a7a1b
dan sinclair <dsinclair@chromium.org>
2023-05-16 12:13:25 -0400

    [ir] Update disassembly output.
    This CL updates the disassembly output to make it clearer when
    instructions are flow node traversals, and when they're actual
    instructions in a block.
    
    Bug: tint:1718
    Change-Id: I95e103a5672b5579081b73e9633bf07b31e51187

--- src/tint/ir/disassembler.cc
+++ src/tint/ir/disassembler.cc
@@ -138,13 +138,14 @@ void Disassembler::Walk(const FlowNode* node) {
 
                 out_ << "]";
             }
-            out_ << std::endl;
+            out_ << " {" << std::endl;
 
             {
                 ScopedIndent func_indent(indent_size_);
                 ScopedStopNode scope(stop_nodes_, f->end_target);
                 Walk(f->start_target);
             }
+            out_ << "} ";
             Walk(f->end_target);
         },
         [&](const ir::Block* b) {
@@ -153,16 +154,20 @@ void Disassembler::Walk(const FlowNode* node) {
                 return;
             }
 
-            Indent() << "%fn" << IdOf(b) << " = block" << std::endl;
-            EmitBlockInstructions(b);
+            Indent() << "%fn" << IdOf(b) << " = block {" << std::endl;
+            {
+                ScopedIndent si(indent_size_);
+                EmitBlockInstructions(b);
+            }
+            Indent() << "}";
 
             if (b->branch.target->Is<FunctionTerminator>()) {
-                Indent() << "ret";
+                out_ << " -> %func_end (return)";
             } else if (b->branch.target->Is<RootTerminator>()) {
                 // Nothing to do
             } else {
-                Indent() << "branch "
-                         << "%fn" << IdOf(b->branch.target);
+                out_ << " -> "
+                     << "%fn" << IdOf(b->branch.target) << " (branch)";
             }
             if (!b->branch.args.IsEmpty()) {
                 out_ << " ";
@@ -294,7 +299,7 @@ void Disassembler::Walk(const FlowNode* node) {
         },
         [&](const ir::FunctionTerminator*) {
             TINT_ASSERT(IR, in_function_);
-            Indent() << "func_end" << std::endl << std::endl;
+            Indent() << "%func_end" << std::endl << std::endl;
         },
         [&](const ir::RootTerminator*) {
             TINT_ASSERT(IR, !in_function_);
