ab0448f7d715e0aff7034a74a917289d73eff9e3
Le Hoang Quyen <lehoangquyen@chromium.org>
2023-05-31 21:49:50 +0800

    Metal: Initial implementation of MSAA render to single sampled.
    This CL implements initial support for Multisampled render to single
    sampled feature on Metal.
    - This first version will only support one single color attachment
    (plus any depth stencil attachment if any).
    - Implicit multi-sampled textures are allocated internally by Dawn and
    cached. It could be reused for multiple single-sampled textures of the
    same size and format.
      - The implicit multi-sampled texture is deallocated when the last
      single sampled texture using it is released.
    - The single-sampled texture is internally treated as resolve target for
    the implicit multi-sampled texture in the render pass.
    - To support LoadOp=Load, at the beginning of the render pass, the
    single sampled texture will be blitted to the multi-sampled texture
    using a full screen draw step. This step is implemented on front-end
    layer rather than back-end specific layer. It can be reused by D3D
    implementation in future patch.
    
    Bug: dawn:1710
    Change-Id: I35990b528d13bde14afd0a42cbd95b349466c370

--- src/dawn/native/Texture.cpp
+++ src/dawn/native/Texture.cpp
@@ -362,7 +362,8 @@ MaybeError ValidateTextureDescriptor(const DeviceBase* device,
 
     DAWN_INVALID_IF(
         internalUsageDesc != nullptr && !device->HasFeature(Feature::DawnInternalUsages),
-        "The internalUsageDesc is not empty while the dawn-internal-usages feature is not enabled");
+        "The internalUsageDesc is not empty while the dawn-internal-usages feature is not "
+        "enabled");
 
     const Format* format;
     DAWN_TRY_ASSIGN(format, device->GetInternalFormat(descriptor->format));
@@ -577,8 +578,9 @@ TextureBase::TextureBase(DeviceBase* device,
     }
     GetObjectTrackingList()->Track(this);
 
-    // dawn:1569: If a texture with multiple array layers or mip levels is specified as a texture
-    // attachment when this toggle is active, it needs to be given CopyDst usage internally.
+    // dawn:1569: If a texture with multiple array layers or mip levels is specified as a
+    // texture attachment when this toggle is active, it needs to be given CopyDst usage
+    // internally.
     bool applyAlwaysResolveIntoZeroLevelAndLayerToggle =
         device->IsToggleEnabled(Toggle::AlwaysResolveIntoZeroLevelAndLayer) &&
         (GetArrayLayers() > 1 || GetNumMipLevels() > 1) &&
@@ -644,6 +646,8 @@ void TextureBase::DestroyImpl() {
 
     // Destroy all of the views associated with the texture as well.
     mTextureViews.Destroy();
+
+    mCachedImplicitMSAttachment = nullptr;
 }
 
 // static
@@ -857,6 +861,28 @@ TextureViewBase* TextureBase::APICreateView(const TextureViewDescriptor* descrip
         return TextureViewBase::MakeError(device, descriptor ? descriptor->label : nullptr);
     }
     return result.Detach();
+}
+
+bool TextureBase::IsImplicitMSAARenderTextureViewSupported() const {
+    return (GetUsage() & wgpu::TextureUsage::TextureBinding) != 0;
+}
+
+ResultOrError<TextureViewBase*> TextureBase::GetImplicitMSAARenderTextureView(
+    uint32_t sampleCount) {
+    ASSERT(GetDevice()->IsLockedByCurrentThreadIfNeeded());
+
+    // We currently only support sample count = 4
+    ASSERT(sampleCount == 4);
+
+    if (mCachedImplicitMSAttachment == nullptr) {
+        DAWN_TRY_ASSIGN(mCachedImplicitMSAttachment,
+                        GetDevice()->GetOrCreateMultisampleAttachment(
+                            GetFormat().format, GetWidth(), GetHeight(), sampleCount));
+    }
+
+    ASSERT(mCachedImplicitMSAttachment->GetTexture()->GetSampleCount() == sampleCount);
+
+    return mCachedImplicitMSAttachment->GetTextureView();
 }
 
 void TextureBase::APIDestroy() {
