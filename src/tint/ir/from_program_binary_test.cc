e3743834e50e4a76e8a30030f3dda427e97c78ce
dan sinclair <dsinclair@chromium.org>
2023-06-01 09:25:33 -0400

    [ir] Cleanup ir test helper.
    There are two ways tests are created for the IR, they either use the
    program builder to make an AST and transform that to the IR, or they
    create IR directly.
    
    Currently `test_helper` is the ProgramBuilder variant. In the other case
    we have individual test helpers.
    
    This CL moves `test_helper` to `program_test_helper` and creates an
    `ir_test_helper` for the second variant.
    
    Bug: tint:1718
    Change-Id: I5d1aeb63ae9372c9a504d83eb0c71260f253db0b

--- src/tint/ir/from_program_binary_test.cc
+++ src/tint/ir/from_program_binary_test.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "src/tint/ir/test_helper.h"
+#include "src/tint/ir/program_test_helper.h"
 
 #include "gmock/gmock.h"
 #include "src/tint/ast/case_selector.h"
@@ -24,7 +24,7 @@ namespace {
 
 using namespace tint::number_suffixes;  // NOLINT
 
-using IR_BuilderImplTest = TestHelper;
+using IR_BuilderImplTest = ProgramTestHelper;
 
 TEST_F(IR_BuilderImplTest, EmitExpression_Binary_Add) {
     Func("my_func", utils::Empty, ty.u32(), utils::Vector{Return(0_u)});
