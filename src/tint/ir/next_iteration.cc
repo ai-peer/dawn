9f06e7ce45ef5db7d56f165a72589d7ce1e46ea6
Ben Clayton <bclayton@google.com>
2023-06-06 21:53:32 +0100

    [tint][ir] Add FlowControlInstruction
    To sit between ir::Instruction and [ir::If, ir::Loop, ir::Switch].
    
    All of these instructions are special in that they hold their own
    blocks, and they do not have block arguments.
    
    Bug: tint:1718
    Change-Id: Ibd1c6fc1a1d0153aa58891e9c4f5f3d95b0061d7

--- src/tint/ir/next_iteration.cc
+++ src/tint/ir/next_iteration.cc
@@ -17,6 +17,7 @@
 #include <utility>
 
 #include "src/tint/ir/loop.h"
+#include "src/tint/ir/block.h"
 
 TINT_INSTANTIATE_TYPEINFO(tint::ir::NextIteration);
 
--- src/tint/ir/switch.h
+++ src/tint/ir/switch.h
@@ -15,15 +15,34 @@
 #ifndef SRC_TINT_IR_SWITCH_H_
 #define SRC_TINT_IR_SWITCH_H_
 
-#include "src/tint/ir/block.h"
-#include "src/tint/ir/branch.h"
-#include "src/tint/ir/constant.h"
-#include "src/tint/ir/value.h"
+#include "src/tint/ir/flow_control_instruction.h"
+
+// Forward declarations
+namespace tint::ir {
+class Constant;
+}  // namespace tint::ir
 
 namespace tint::ir {
-
-/// Flow node representing a switch statement
-class Switch : public utils::Castable<Switch, Branch> {
+/// Switch instruction.
+///
+/// ```
+///                           in
+///                            ┃
+///     ╌╌╌╌╌╌╌╌┲━━━━━━━━━━━━━━╋━━━━━━━━━━━━━━┱╌╌╌╌╌╌╌╌
+///             ▼              ▼              ▼
+///        ┌────────┐     ┌────────┐     ┌────────┐
+///        │ Case A │     │ Case B │     │ Case C │
+///        └────────┘     └────────┘     └────────┘
+///  ExitSwitch ┃   ExitSwitch ┃   ExitSwitch ┃
+///             ┃              ▼              ┃
+///             ┃          ┌───────┐          ┃
+///     ╌╌╌╌╌╌╌╌┺━━━━━━━━━▶│ Merge │◀━━━━━━━━━┹╌╌╌╌╌╌╌╌
+///                        └───────┘
+///                            ┃
+///                            ▼
+///                           out
+/// ```
+class Switch : public utils::Castable<Switch, FlowControlInstruction> {
   public:
     /// A case selector
     struct CaseSelector {
