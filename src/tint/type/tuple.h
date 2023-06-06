749f3ff75e89698db718aa9e44c15d742d2c4179
Ben Clayton <bclayton@google.com>
2023-06-06 16:00:19 +0100

    [tint][type] Add Tuple
    Not currently used by WGSL, but can be used by the IR to pack multiple
    values together as a single ir::Value.
    
    Change-Id: Icc965c9e033f8f401b817885b560998afc4f211e

---
+++ src/tint/type/tuple.h
@@ -0,0 +1,63 @@
+// Copyright 2023 The Tint Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SRC_TINT_TYPE_TUPLE_H_
+#define SRC_TINT_TYPE_TUPLE_H_
+
+#include <string>
+
+#include "src/tint/type/type.h"
+#include "src/tint/utils/vector.h"
+
+namespace tint::type {
+
+/// A signed int 32 type.
+class Tuple final : public utils::Castable<Tuple, Type> {
+  public:
+    /// Constructor
+    /// @param types the tuple types
+    explicit Tuple(utils::VectorRef<const Type*> types);
+
+    /// Destructor
+    ~Tuple() override;
+
+    /// @param other the other node to compare against
+    /// @returns true if the this type is equal to @p other
+    bool Equals(const UniqueNode& other) const override;
+
+    /// @returns the name for this type that closely resembles how it would be
+    /// declared in WGSL.
+    std::string FriendlyName() const override;
+
+    /// @copydoc Type::Elements
+    TypeAndCount Elements(const Type* type_if_invalid = nullptr,
+                          uint32_t count_if_invalid = 0) const override;
+
+    /// @copydoc Type::Element
+    const Type* Element(uint32_t index) const override;
+
+    /// @param ctx the clone context
+    /// @returns a clone of this type
+    Tuple* Clone(CloneContext& ctx) const override;
+
+    /// @returns the tuple's types
+    utils::VectorRef<const Type*> Types() const { return types_; }
+
+  private:
+    utils::Vector<const Type*, 4> types_;
+};
+
+}  // namespace tint::type
+
+#endif  // SRC_TINT_TYPE_TUPLE_H_
