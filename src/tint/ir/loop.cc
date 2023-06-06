8e8f376ca7a41658187ab5077bdfd1a2526b32e5
Ben Clayton <bclayton@google.com>
2023-06-06 16:22:10 +0100

    [tint][ir] Fold away merge targets
    Instead make `If`, `Loop`, `Switch` regular instructions instead of branches.
    
    Change-Id: I2f7d9e0f97113c8076dafc6541980e87bc21a62a

--- src/tint/ir/loop.cc
+++ src/tint/ir/loop.cc
@@ -22,12 +22,11 @@ TINT_INSTANTIATE_TYPEINFO(tint::ir::Loop);
 
 namespace tint::ir {
 
-Loop::Loop(ir::Block* i, ir::MergeBlock* b, ir::MergeBlock* c, ir::MergeBlock* m)
-    : initializer_(i), body_(b), continuing_(c), merge_(m) {
+Loop::Loop(ir::Block* i, ir::MergeBlock* b, ir::MergeBlock* c)
+    : initializer_(i), body_(b), continuing_(c) {
     TINT_ASSERT(IR, initializer_);
     TINT_ASSERT(IR, body_);
     TINT_ASSERT(IR, continuing_);
-    TINT_ASSERT(IR, merge_);
 
     if (initializer_) {
         initializer_->SetParent(this);
@@ -37,9 +36,6 @@ Loop::Loop(ir::Block* i, ir::MergeBlock* b, ir::MergeBlock* c, ir::MergeBlock* m
     }
     if (continuing_) {
         continuing_->SetParent(this);
-    }
-    if (merge_) {
-        merge_->SetParent(this);
     }
 }
 
