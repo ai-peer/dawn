d946cf4596432ebc9f380b514429b43d27873c08
Ben Clayton <bclayton@google.com>
2023-06-07 14:28:17 +0100

    [tint][ir] Rename Case::start -> Case::block
    Without merge blocks, the whole case is held by a single block.
    
    Change-Id: If0fb23448abf7d36c7ae8adbe11b3f0975f5935b

--- src/tint/writer/spirv/ir/generator_impl_ir.cc
+++ src/tint/writer/spirv/ir/generator_impl_ir.cc
@@ -848,7 +848,7 @@ void GeneratorImplIr::EmitSwitch(const ir::Switch* swtch) {
     for (auto& c : swtch->Cases()) {
         for (auto& sel : c.selectors) {
             if (sel.IsDefault()) {
-                default_label = Label(c.Start());
+                default_label = Label(c.Block());
             }
         }
     }
@@ -857,7 +857,7 @@ void GeneratorImplIr::EmitSwitch(const ir::Switch* swtch) {
     // Build the operands to the OpSwitch instruction.
     OperandList switch_operands = {Value(swtch->Condition()), default_label};
     for (auto& c : swtch->Cases()) {
-        auto label = Label(c.Start());
+        auto label = Label(c.Block());
         for (auto& sel : c.selectors) {
             if (sel.IsDefault()) {
                 continue;
@@ -877,7 +877,7 @@ void GeneratorImplIr::EmitSwitch(const ir::Switch* swtch) {
 
     // Emit the cases.
     for (auto& c : swtch->Cases()) {
-        EmitBlock(c.Start());
+        EmitBlock(c.Block());
     }
 
     // Emit the switch merge block.
