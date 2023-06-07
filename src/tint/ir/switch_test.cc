2db143ae1adf832544d3ce35741a4b4628618105
Ben Clayton <bclayton@google.com>
2023-06-07 14:28:17 +0100

    [tint][ir] Rename Case::start -> Case::block
    Without merge blocks, the whole case is held by a single block.
    
    Change-Id: If0fb23448abf7d36c7ae8adbe11b3f0975f5935b

--- src/tint/ir/switch_test.cc
+++ src/tint/ir/switch_test.cc
@@ -33,7 +33,7 @@ TEST_F(IR_SwitchTest, Usage) {
 TEST_F(IR_SwitchTest, Parent) {
     auto* switch_ = b.Switch(1_i);
     b.Case(switch_, utils::Vector{Switch::CaseSelector{nullptr}});
-    EXPECT_THAT(switch_->Cases().Front().Start()->Parent(), switch_);
+    EXPECT_THAT(switch_->Cases().Front().Block()->Parent(), switch_);
 }
 
 TEST_F(IR_SwitchTest, Fail_NullCondition) {
