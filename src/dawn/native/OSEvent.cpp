606d3e59b0b8425e48c93eba6e839829d9395f41
Kai Ninomiya <kainino@chromium.org>
2023-06-13 14:40:43 -0700

    (WIP) Prototype of proposed future-based async API (0.8)
    Commit: false
    Bug: dawn:1987
    Change-Id: If0c82279addf419ef16cc0280416ce2bfb71895f

---
+++ src/dawn/native/OSEvent.cpp
@@ -0,0 +1,194 @@
+// Copyright 2023 The Dawn Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "dawn/native/OSEvent.h"
+
+#if DAWN_PLATFORM_IS(WINDOWS)
+#include <windows.h>
+#else
+#include <poll.h>
+#include <unistd.h>
+#endif
+
+#include <limits>
+#include <utility>
+#include <vector>
+
+#include "dawn/common/Compiler.h"
+#include "dawn/native/TrackedEvent.h"
+
+namespace dawn::native {
+
+namespace {
+
+template <typename T, T Infinity>
+T ToMillisecondsGeneric(Nanoseconds timeout) {
+    uint64_t ns = uint64_t{timeout};
+    uint64_t ms = 0;
+    if (ns > 0) {
+        ms = (ns - 1) / 1'000'000 + 1;
+        if (ms > std::numeric_limits<T>::max()) {
+            return Infinity;  // Round long timeout up to infinity
+        }
+    }
+    return static_cast<T>(ms);
+}
+
+#if DAWN_PLATFORM_IS(WINDOWS)
+#define ToMilliseconds ToMillisecondsGeneric<DWORD, INFINITE>
+#else
+#define ToMilliseconds ToMillisecondsGeneric<int, -1>
+#endif
+
+}  // namespace
+
+// OSEventPrimitive
+
+bool OSEventPrimitive::IsValid() const {
+#if DAWN_PLATFORM_IS(WINDOWS)
+    return v != nullptr;
+#else
+    return v >= 0;
+#endif
+}
+
+void OSEventPrimitive::Close() {
+    if (IsValid()) {
+#if DAWN_PLATFORM_IS(WINDOWS)
+        CloseHandle(v);
+#else
+        close(v);
+#endif
+        static constexpr T kInvalid = OSEventPrimitive{}.v;
+        v = kInvalid;
+    }
+}
+
+// OSEventReceiver
+
+OSEventReceiver OSEventReceiver::CreateAlreadySignaled() {
+#if DAWN_PLATFORM_IS(WINDOWS)
+    UNREACHABLE();  // FIXME: unimplemented
+#else
+    int status;
+
+    int pipeFds[2];
+    status = pipe(pipeFds);
+    if (DAWN_UNLIKELY(status < 0)) {
+        abort();
+    }
+
+    OSEventReceiver receiver{pipeFds[0]};
+
+    int sender = pipeFds[1];
+
+    char zero[1] = {0};
+    status = write(sender, zero, 1);
+    if (DAWN_UNLIKELY(status < 0)) {
+        abort();
+    }
+
+    status = close(sender);
+    if (DAWN_UNLIKELY(status < 0)) {
+        abort();
+    }
+
+    return receiver;
+#endif
+}
+
+bool OSEventReceiver::Wait(size_t count, TrackedFutureWaitInfo* futures, Nanoseconds timeout) {
+#if DAWN_PLATFORM_IS(WINDOWS)
+    ASSERT(count <= MAXIMUM_WAIT_OBJECTS);
+    std::vector<HANDLE> handles(count);
+    for (size_t i = 0; i < count; ++i) {
+        handles[i] = futures[i].future->GetPrimitive();
+    }
+
+    DWORD code = WaitForMultipleObjects(handles.size(), handles.data(), /* bWaitAll */ false,
+                                        ToMilliseconds(timeout));
+    if (DAWN_UNLIKELY(code == WAIT_FAILED)) {
+        return DAWN_INTERNAL_ERROR("WaitForMultipleObjects failed");
+    }
+
+    for (size_t i = 0; i < count; ++i) {
+        futures[i].ready = false;
+    }
+    if (code == WAIT_TIMEOUT) {
+        return false;
+    }
+
+    ASSERT(code < WAIT_ABANDONED_0);
+    DWORD signaledIndex = code - WAIT_OBJECT_0;
+    if (signaledIndex < count) {
+        futures[signaledIndex].ready = true;
+    } else {
+        ASSERT(false);
+    }
+
+    return true;
+#else
+    std::vector<pollfd> pollfds(count);
+    for (size_t i = 0; i < count; ++i) {
+        pollfds[i] = pollfd{futures[i].event->GetPrimitive(), POLLIN, 0};
+    }
+
+    int status = poll(pollfds.data(), pollfds.size(), ToMilliseconds(timeout));
+
+    if (DAWN_UNLIKELY(status < 0)) {
+        abort();
+    }
+    if (status == 0) {
+        return false;
+    }
+
+    for (size_t i = 0; i < count; ++i) {
+        int revents = pollfds[i].revents;
+        static constexpr int kAllowedEvents = POLLIN | POLLHUP;
+        if (DAWN_UNLIKELY((revents & kAllowedEvents) != revents)) {
+            abort();
+        }
+    }
+
+    for (size_t i = 0; i < count; ++i) {
+        bool ready = (pollfds[i].revents & POLLIN) != 0;
+        futures[i].ready = ready;
+    }
+
+    return true;
+#endif
+}
+
+OSEventReceiver::OSEventReceiver(OSEventPrimitive::T primitive) : mPrimitive{primitive} {}
+
+OSEventReceiver::OSEventReceiver(OSEventReceiver&& other) {
+    *this = std::move(other);
+}
+
+OSEventReceiver& OSEventReceiver::operator=(OSEventReceiver&& other) {
+    mPrimitive = other.mPrimitive;
+    other.mPrimitive = OSEventPrimitive{};
+    return *this;
+}
+
+OSEventReceiver::~OSEventReceiver() {
+    mPrimitive.Close();
+}
+
+OSEventPrimitive::T OSEventReceiver::Get() const {
+    ASSERT(mPrimitive.IsValid());
+    return mPrimitive.v;
+}
+
+}  // namespace dawn::native
---
+++ src/dawn/native/TrackedEvent.h
@@ -0,0 +1,130 @@
+// Copyright 2023 The Dawn Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SRC_DAWN_NATIVE_TRACKEDEVENT_H_
+#define SRC_DAWN_NATIVE_TRACKEDEVENT_H_
+
+#include <atomic>
+
+#include "dawn/common/Ref.h"
+#include "dawn/common/RefCounted.h"
+#include "dawn/native/Error.h"
+#include "dawn/native/IntegerTypes.h"
+#include "dawn/native/OSEvent.h"
+#include "dawn/webgpu_cpp.h"
+
+namespace dawn::native {
+
+class DeviceBase;
+class InstanceBase;
+class QueueBase;
+
+using FutureID = uint64_t;
+
+class TrackedEvent : public RefCounted {
+  protected:
+    TrackedEvent(InstanceBase* instance, WGPUCallbackModeFlags callbackMode);
+
+  public:
+    ~TrackedEvent() override;
+
+    OSEventPrimitive::T GetPrimitive() const;
+    void SetReceiver(OSEventReceiver&&);
+    bool IsEarlyReady() const;
+    void EnsureCompleteFromProcessEvents();
+    void EnsureCompleteFromWaitAny();
+
+    virtual DeviceBase* GetWaitDevice() const = 0;
+
+    class WaitRef;
+    WaitRef TakeWaitRef();
+
+    // A Ref<TrackedEvent>, but with extra ASSERTs.
+    class WaitRef {
+      public:
+        WaitRef(WaitRef&& other) = default;
+        WaitRef& operator=(WaitRef&& other) = default;
+
+        explicit WaitRef(TrackedEvent* future);
+        ~WaitRef();
+
+        TrackedEvent* operator->();
+        const TrackedEvent* operator->() const;
+
+      private:
+        Ref<TrackedEvent> mRef;
+    };
+
+  protected:
+    // Override the state to Ready, in early-failure cases.
+    void TriggerEarlyFailure();
+
+    virtual void Complete() = 0;
+
+    Ref<InstanceBase> mInstance;
+    WGPUCallbackModeFlags mCallbackMode;
+    OSEventReceiver mReceiver;
+
+    // Marked as early-ready, but may or may not be completed yet.
+    std::atomic<bool> mEarlyReady = false;
+
+#if DAWN_ENABLE_ASSERTS
+    std::atomic<bool> mCurrentlyBeingWaited;
+#endif
+
+  private:
+    void EnsureComplete();
+
+    // Callback has been called.
+    std::atomic<bool> mCompleted = false;
+};
+
+// TrackedEvent::WaitRef plus a few extra fields needed for WaitAny implementations.
+struct TrackedFutureWaitInfo {
+    FutureID futureID;
+    TrackedEvent::WaitRef event;
+    size_t indexInInfos;
+    bool ready;
+};
+
+class WorkDoneEvent final : public TrackedEvent {
+  public:
+    static FutureID Create(QueueBase* queue, const WGPUQueueWorkDoneCallbackInfo& callbackInfo);
+
+    WorkDoneEvent(QueueBase* queue, const WGPUQueueWorkDoneCallbackInfo& callbackInfo);
+    ~WorkDoneEvent() override = default;
+
+    // Mark the event as having become ready early, and return a new
+    // already-completed FutureID (if needed by the callback mode).
+    void FailEarly(WGPUQueueWorkDoneStatus status);
+
+    DeviceBase* GetWaitDevice() const override;
+
+  private:
+    MaybeError Validate(WGPUQueueWorkDoneStatus* earlyStatus);
+    void Complete() override;
+
+    Ref<QueueBase> mQueue;
+    ExecutionSerial mSerial;
+
+    // The early-ready status (used iff TrackedEvent::mState is EarlyReady).
+    WGPUQueueWorkDoneStatus mEarlyStatus = WGPUQueueWorkDoneStatus_Unknown;
+
+    WGPUQueueWorkDoneCallback mCallback;
+    void* mUserdata;
+};
+
+}  // namespace dawn::native
+
+#endif  // SRC_DAWN_NATIVE_TRACKEDEVENT_H_
