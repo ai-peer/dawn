786576e48a56f57c30c1d323316d51f817cd6fb7
jchen10 <jie.a.chen@intel.com>
2023-06-14 14:19:03 +0800

    Bisect e2e cases
    Change-Id: I4c807548e0c9ff7d6676187ec4b835e9a585f555

--- src/dawn/tests/end2end/ColorStateTests.cpp
+++ src/dawn/tests/end2end/ColorStateTests.cpp
@@ -1192,7 +1192,6 @@ TEST_P(ColorStateTest, SrcBlendFactorDstAlphaDstBlendFactorZero) {
 }
 
 DAWN_INSTANTIATE_TEST(ColorStateTest,
-                      D3D11Backend(),
                       D3D12Backend(),
                       MetalBackend(),
                       OpenGLBackend(),
