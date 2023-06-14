bd72a62555dc6a737510ca2fbed6c8ffbb7fed19
jchen10 <jie.a.chen@intel.com>
2023-06-14 10:54:11 +0800

    d3d11: Skip CompressedTextureFormatTests on Nvidia
    Bug: dawn:1847
    Bug: dawn:1705
    
    Change-Id: I3f2e2529f382614d3f01064a683872c247b1d36a

--- src/dawn/tests/end2end/CompressedTextureFormatTests.cpp
+++ src/dawn/tests/end2end/CompressedTextureFormatTests.cpp
@@ -1184,7 +1184,6 @@ TEST_P(CompressedTextureFormatSpecificTest, BC1RGBAUnorm_UnalignedDynamicUploade
 }
 
 DAWN_INSTANTIATE_TEST(CompressedTextureFormatSpecificTest,
-                      D3D11Backend(),
                       D3D12Backend(),
                       MetalBackend(),
                       OpenGLBackend(),
@@ -1321,8 +1320,8 @@ TEST_P(CompressedTextureWriteTextureTest,
 }
 
 DAWN_INSTANTIATE_TEST_P(CompressedTextureWriteTextureTest,
-                        {D3D11Backend(), D3D12Backend(), MetalBackend(), OpenGLBackend(),
-                         OpenGLESBackend(), VulkanBackend()},
+                        {D3D12Backend(), MetalBackend(), OpenGLBackend(), OpenGLESBackend(),
+                         VulkanBackend()},
                         std::vector<wgpu::TextureFormat>(utils::kCompressedFormats.begin(),
                                                          utils::kCompressedFormats.end()));
 
