9508950395e4a37622a7c4f69347204f1bf718f5
jchen10 <jie.a.chen@intel.com>
2023-06-14 10:06:18 +0800

    d3d11: Enable MultithreadTests
    ID3D11DeviceContext is not thread-safe. This adds some robust checks
    to ensure ID3D11DeviceContext is always used under the protection of
    lock.
    
    Bug: dawn:1705
    
    Change-Id: I25e483b5c33652cfaa75673e3c46b20d635a1c8b

--- src/dawn/tests/end2end/MultithreadTests.cpp
+++ src/dawn/tests/end2end/MultithreadTests.cpp
@@ -876,6 +876,9 @@ TEST_P(MultithreadTextureCopyTests, CopyStencilToStencilNoRace) {
     // stencil.
     DAWN_TEST_UNSUPPORTED_IF(HasToggleEnabled("disable_depth_stencil_read"));
 
+    // TODO(dawn:1848): support depth-stencil texture write on D3D11.
+    DAWN_SUPPRESS_TEST_IF(IsD3D11());
+
     enum class Step {
         Begin,
         WriteTexture,
@@ -1346,6 +1349,7 @@ TEST_P(MultithreadTimestampQueryTests, ResolveQuerySets_InParallel) {
 }
 
 DAWN_INSTANTIATE_TEST(MultithreadTests,
+                      D3D11Backend(),
                       D3D12Backend(),
                       MetalBackend(),
                       OpenGLBackend(),
@@ -1353,6 +1357,7 @@ DAWN_INSTANTIATE_TEST(MultithreadTests,
                       VulkanBackend());
 
 DAWN_INSTANTIATE_TEST(MultithreadCachingTests,
+                      D3D11Backend(),
                       D3D12Backend(),
                       MetalBackend(),
                       OpenGLBackend(),
