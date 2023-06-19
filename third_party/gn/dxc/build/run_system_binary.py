a65a4a608fee02ea4c0e1e6af1b8ae8ddd3277e2
Antonio Maiorano <amaiorano@google.com>
2023-06-19 14:53:42 -0400

    Add GN build of DXC
    * Currently will only build in dawn standalone builds on Windows.
    * When enabled, defines DAWN_BUILD_DXC macro, which will be used in a
    future CL to enable DXC usage by default when possible.
    
    Change-Id: I3d821506657cd522dce8b183d45b44adf10ad232

---
+++ third_party/gn/dxc/build/run_system_binary.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import subprocess
+import sys
+
+env_file, bin, rest = sys.argv[1], sys.argv[2], sys.argv[3:]
+
+# Read the environment block from the file. This is stored in the format used
+# by CreateProcess. Drop last 2 NULs, one for list terminator, one for
+# trailing vs. separator.
+env_pairs = open(env_file).read()[:-2].split('\0')
+env_dict = dict([item.split('=', 1) for item in env_pairs])
+
+sys.exit(subprocess.call([bin] + rest, env=env_dict, shell=True))
