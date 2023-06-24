3569ad01fc03361b82026eb58c4aa2dc2f92a4af
dan sinclair <dsinclair@chromium.org>
2023-06-23 23:46:07 -0400

    [ir][msl] Emit atomic types
    Add emission of the atomic types to the MSL IR generator.
    
    Bug: tint:1967
    Change-Id: I6fd56e58717da9a3867e04d91ac75cc68f2e0c1a

--- src/tint/writer/msl/ir/generator_impl_ir_type_test.cc
+++ src/tint/writer/msl/ir/generator_impl_ir_type_test.cc
@@ -190,11 +190,22 @@ TEST_F(MslGeneratorImplIrTest, EmitType_Matrix_F16) {
     ASSERT_TRUE(generator_.Diagnostics().empty()) << generator_.Diagnostics().str();
     EXPECT_EQ(utils::TrimSpace(generator_.result()), "half2x3");
 }
-
 TEST_F(MslGeneratorImplIrTest, EmitType_U32) {
     generator_.EmitType(generator_.line(), ty.u32());
     ASSERT_TRUE(generator_.Diagnostics().empty()) << generator_.Diagnostics().str();
     EXPECT_EQ(utils::TrimSpace(generator_.result()), "uint");
+}
+
+TEST_F(MslGeneratorImplIrTest, EmitType_Atomic_U32) {
+    generator_.EmitType(generator_.line(), ty.atomic<u32>());
+    ASSERT_TRUE(generator_.Diagnostics().empty()) << generator_.Diagnostics().str();
+    EXPECT_EQ(utils::TrimSpace(generator_.result()), "atomic_uint");
+}
+
+TEST_F(MslGeneratorImplIrTest, EmitType_Atomic_I32) {
+    generator_.EmitType(generator_.line(), ty.atomic<i32>());
+    ASSERT_TRUE(generator_.Diagnostics().empty()) << generator_.Diagnostics().str();
+    EXPECT_EQ(utils::TrimSpace(generator_.result()), "atomic_int");
 }
 
 TEST_F(MslGeneratorImplIrTest, EmitType_Vector) {
