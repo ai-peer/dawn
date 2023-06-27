b7add72767763021b05f4377f7fade2882c853a5
Antonio Maiorano <amaiorano@google.com>
2023-06-27 17:00:37 -0400

    Add support for UMA
    Adds HistogramMacros.h, inspired by the same header in ANGLE (and
    Chromium). Similar to event tracing, is enabled only when
    Platform::MonotonicallyIncreasingTime returns non-zero values; thus
    disabled in Dawn standalone builds, but enabled in Chrome.
    
    Bug: dawn:1087
    Change-Id: Ibd2c62b069d303cc9f7fa0f1db454a4abf1c6ad7

---
+++ src/dawn/platform/metrics/Histogram.h
@@ -0,0 +1,55 @@
+// Copyright 2023 The Dawn Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SRC_DAWN_PLATFORM_METRICS_HISTOGRAM_H_
+#define SRC_DAWN_PLATFORM_METRICS_HISTOGRAM_H_
+
+#include <cstdint>
+
+#include "dawn/platform/dawn_platform_export.h"
+
+namespace dawn::platform {
+
+class Platform;
+
+namespace histogram {
+
+// The functions in this header are used to forward their respective calls
+// onto the Platform object. This layer of indirection is used to keep the
+// Platform implementation private (PIMPL).
+
+// Forwards call to platform->HistogramCustomCounts
+DAWN_PLATFORM_EXPORT void CustomCounts(Platform* platform,
+                                       const char* name,
+                                       int sample,
+                                       int min,
+                                       int max,
+                                       int bucketCount);
+
+// Forwards call to platform->HistogramEnumeration
+DAWN_PLATFORM_EXPORT void Enumeration(Platform* platform,
+                                      const char* name,
+                                      int sample,
+                                      int boundaryValue);
+
+// Forwards call to platform->HistogramSparse
+DAWN_PLATFORM_EXPORT void Sparse(Platform* platform, const char* name, int sample);
+
+// Forwards call to platform->HistogramBoolean
+DAWN_PLATFORM_EXPORT void Boolean(Platform* platform, const char* name, bool sample);
+
+}  // namespace histogram
+}  // namespace dawn::platform
+
+#endif  // SRC_DAWN_PLATFORM_METRICS_HISTOGRAM_H_
