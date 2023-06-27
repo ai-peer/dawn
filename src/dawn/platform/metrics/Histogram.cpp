fd71b003def5c7976187eab4907b0647156ad177
Antonio Maiorano <amaiorano@google.com>
2023-06-27 17:00:37 -0400

    Add support for UMA
    Adds histogram_macros.h, inspired by the same header in ANGLE (and
    Chromium). Similar to event tracing, is enabled only when
    Platform::MonotonicallyIncreasingTime returns non-zero values; thus
    disabled in Dawn standalone builds, but enabled in Chrome.
    
    Bug: dawn:1087
    Change-Id: Ibd2c62b069d303cc9f7fa0f1db454a4abf1c6ad7

---
+++ src/dawn/platform/metrics/Histogram.cpp
@@ -0,0 +1,58 @@
+// Copyright 2023 The Dawn Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "dawn/platform/metrics/Histogram.h"
+#include "dawn/common/Assert.h"
+#include "dawn/platform/DawnPlatform.h"
+
+namespace dawn::platform::histogram {
+
+void CustomCounts(Platform* platform,
+                  const char* name,
+                  int sample,
+                  int min,
+                  int max,
+                  int bucketCount) {
+    ASSERT(platform != nullptr);
+    double timestamp = platform->MonotonicallyIncreasingTime();
+    if (timestamp != 0) {
+        platform->HistogramCustomCounts(name, sample, min, max, bucketCount);
+    }
+}
+
+void Enumeration(Platform* platform, const char* name, int sample, int boundaryValue) {
+    ASSERT(platform != nullptr);
+    double timestamp = platform->MonotonicallyIncreasingTime();
+    if (timestamp != 0) {
+        platform->HistogramEnumeration(name, sample, boundaryValue);
+    }
+}
+
+void Sparse(Platform* platform, const char* name, int sample) {
+    ASSERT(platform != nullptr);
+    double timestamp = platform->MonotonicallyIncreasingTime();
+    if (timestamp != 0) {
+        platform->HistogramSparse(name, sample);
+    }
+}
+
+void Boolean(Platform* platform, const char* name, bool sample) {
+    ASSERT(platform != nullptr);
+    double timestamp = platform->MonotonicallyIncreasingTime();
+    if (timestamp != 0) {
+        platform->HistogramBoolean(name, sample);
+    }
+}
+
+}  // namespace dawn::platform::histogram
