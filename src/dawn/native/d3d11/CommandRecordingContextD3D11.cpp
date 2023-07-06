961e98b5b7cb61c3cc6f6ded96dfffd006335014
Corentin Wallez <cwallez@chromium.org>
2023-07-06 15:56:56 +0200

    D3D11 queue stuff
    Change-Id: I3f547a8dc53e55b0d99e988eca645ce886c3ae4c

--- src/dawn/native/d3d11/CommandRecordingContextD3D11.cpp
+++ src/dawn/native/d3d11/CommandRecordingContextD3D11.cpp
@@ -27,7 +27,7 @@
 
 namespace dawn::native::d3d11 {
 
-MaybeError CommandRecordingContext::Intialize(Device* device) {
+MaybeError CommandRecordingContext::Initialize(Device* device) {
     ASSERT(!IsOpen());
     ASSERT(device);
     mDevice = device;
@@ -76,7 +76,7 @@ MaybeError CommandRecordingContext::Intialize(Device* device) {
     return {};
 }
 
-MaybeError CommandRecordingContext::ExecuteCommandList(Device* device) {
+MaybeError CommandRecordingContext::ExecuteCommandList() {
     // Consider using deferred DeviceContext.
     mNeedsSubmit = false;
     return {};
