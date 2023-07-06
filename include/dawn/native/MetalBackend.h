9df94de2c259a407bd1c55b81c06bb0df6680ea9
Austin Eng <enga@chromium.org>
2023-07-06 16:45:13 -0700

    Remove double-definition in ExternalImageMTLSharedEventDescriptor
    Having two definitions for it is bad with ARC because id<> is treated
    as a strong reference, but void* is not. Now, always use void*.
    
    Bug: 1462725
    Change-Id: I8846ae367afd7d1baac65e0b50cb612c764adc9a

--- include/dawn/native/MetalBackend.h
+++ include/dawn/native/MetalBackend.h
@@ -44,16 +44,10 @@ struct DAWN_NATIVE_EXPORT PhysicalDeviceDiscoveryOptions
 using AdapterDiscoveryOptions = PhysicalDeviceDiscoveryOptions;
 
 struct DAWN_NATIVE_EXPORT ExternalImageMTLSharedEventDescriptor {
-    // Shared event handle `id<MTLSharedEvent>`.
+    // Shared event handle `MTLSharedEvent`.
     // This never passes ownership to the callee (when used as an input
     // parameter) or to the caller (when used as a return value or output parameter).
-#ifdef __OBJC__
-    id<MTLSharedEvent> sharedEvent = nil;
-    static_assert(sizeof(id<MTLSharedEvent>) == sizeof(void*));
-    static_assert(alignof(id<MTLSharedEvent>) == alignof(void*));
-#else
-    void* sharedEvent = nullptr;
-#endif
+    void* sharedEvent;
 
     // The value that was previously signaled on this event and should be waited on.
     uint64_t signaledValue = 0;
