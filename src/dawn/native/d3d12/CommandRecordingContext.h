510547b693c451781a3dea7cb33958c8fb6d8575
Corentin Wallez <cwallez@chromium.org>
2023-07-06 15:21:03 +0200

    D3D12 move to queue stuff
    Change-Id: I56e1f3b996487e79aceeccb78aa5a2f676902f47

--- src/dawn/native/d3d12/CommandRecordingContext.h
+++ src/dawn/native/d3d12/CommandRecordingContext.h
@@ -40,7 +40,7 @@ class CommandRecordingContext {
     bool NeedsSubmit() const;
     void SetNeedsSubmit();
 
-    MaybeError ExecuteCommandList(Device* device);
+    MaybeError ExecuteCommandList(Device* device, ID3D12CommandQueue* commandQueue);
 
     void TrackHeapUsage(Heap* heap, ExecutionSerial serial);
 
--- src/dawn/tests/white_box/D3D12DescriptorHeapTests.cpp
+++ src/dawn/tests/white_box/D3D12DescriptorHeapTests.cpp
@@ -244,7 +244,7 @@ TEST_P(D3D12DescriptorHeapTests, PoolHeapsInMultipleSubmits) {
         // CheckPassedSerials() will update the last internally completed serial.
         EXPECT_TRUE(mD3DQueue->CheckPassedSerials().IsSuccess());
         // NextSerial() will increment the last internally submitted serial.
-        EXPECT_TRUE(mD3DDevice->NextSerial().IsSuccess());
+        EXPECT_TRUE(mD3DQueue->NextSerial().IsSuccess());
     }
 
     // Repeat up to |kFrameDepth| again but ensure heaps are the same in the expected order
@@ -256,7 +256,7 @@ TEST_P(D3D12DescriptorHeapTests, PoolHeapsInMultipleSubmits) {
         EXPECT_TRUE(heaps.front() == heap);
         heaps.pop_front();
         EXPECT_TRUE(mD3DQueue->CheckPassedSerials().IsSuccess());
-        EXPECT_TRUE(mD3DDevice->NextSerial().IsSuccess());
+        EXPECT_TRUE(mD3DQueue->NextSerial().IsSuccess());
     }
 
     EXPECT_TRUE(heaps.empty());
@@ -1051,7 +1051,7 @@ TEST_P(D3D12DescriptorHeapTests, InvalidateAllocationAfterSerial) {
 
     EXPECT_TRUE(gpuAllocator->IsAllocationStillValid(gpuHeapDescAllocation));
 
-    EXPECT_TRUE(d3dDevice->NextSerial().IsSuccess());
+    EXPECT_TRUE(mD3DQueue->NextSerial().IsSuccess());
 
     EXPECT_FALSE(gpuAllocator->IsAllocationStillValid(gpuHeapDescAllocation));
 }
