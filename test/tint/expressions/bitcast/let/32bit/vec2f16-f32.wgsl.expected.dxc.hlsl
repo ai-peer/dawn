22f3513f42d43d8418060315fcb6ffd648d6f221
Zhaoming Jiang <zhaoming.jiang@intel.com>
2023-07-07 20:16:23 +0800

    Tint: Implement bitcast for f16
    This CL implement the missing bitcast from/to f16 types, including
    resolver validation and HLSL/GLSL backends. MSL and SPIRV backends have
    native support. Related unittests and tint tests are also implemented.
    
    Bug: tint:1977, dawn:1510, tint:1502
    Change-Id: I04499864df4115eeb130aceb359151e8e1ffbfa9

---
+++ test/tint/expressions/bitcast/let/32bit/vec2f16-f32.wgsl.expected.dxc.hlsl
@@ -0,0 +1,11 @@
+float tint_bitcast(vector<float16_t, 2> src) {
+  uint2 r = f32tof16(float2(src));
+  return asfloat(uint((r.x & 0xffff) | ((r.y & 0xffff) << 16)));
+}
+
+[numthreads(1, 1, 1)]
+void f() {
+  const vector<float16_t, 2> a = vector<float16_t, 2>(float16_t(1.0h), float16_t(2.0h));
+  const float b = tint_bitcast(a);
+  return;
+}
