775121552e155f628f9219ed998f288481420eaa
Zhaoming Jiang <zhaoming.jiang@intel.com>
2023-07-07 20:16:23 +0800

    Tint: Implement bitcast for f16
    This CL implement the missing bitcast from/to f16 types, including
    resolver validation and HLSL/GLSL backends. MSL and SPIRV backends have
    native support. Related unittests and tint tests are also implemented.
    
    Bug: tint:1977, dawn:1510, tint:1502
    Change-Id: I04499864df4115eeb130aceb359151e8e1ffbfa9

---
+++ test/tint/expressions/bitcast/const/64bit/vec2i32-vec4f16.wgsl.expected.fxc.hlsl
@@ -0,0 +1,10 @@
+SKIP: FAILED
+
+[numthreads(1, 1, 1)]
+void f() {
+  const vector<float16_t, 4> b = vector<float16_t, 4>(float16_t(1.0h), float16_t(2.0h), float16_t(3.0h), float16_t(-4.0h));
+  return;
+}
+FXC validation failure:
+D:\Projects\RampUp\dawn\test\tint\Shader@0x00000205C47A6D30(3,16-24): error X3000: syntax error: unexpected token 'float16_t'
+
