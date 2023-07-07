ec532127697449e185a8fe69055e418fb4f0be12
Zhaoming Jiang <zhaoming.jiang@intel.com>
2023-07-07 20:16:23 +0800

    Tint: Implement bitcast for f16
    This CL implement the missing bitcast from/to f16 types, including
    resolver validation and HLSL/GLSL backends. MSL and SPIRV backends have
    native support. Related unittests and tint tests are also implemented.
    
    Bug: tint:1977, dawn:1510, tint:1502
    Change-Id: I04499864df4115eeb130aceb359151e8e1ffbfa9

---
+++ test/tint/expressions/bitcast/const/16bit/f16-f16.wgsl.expected.fxc.hlsl
@@ -0,0 +1,10 @@
+SKIP: FAILED
+
+[numthreads(1, 1, 1)]
+void f() {
+  const float16_t b = float16_t(1.0h);
+  return;
+}
+FXC validation failure:
+D:\Projects\RampUp\dawn\test\tint\Shader@0x000001D1C0295030(3,9-17): error X3000: unrecognized identifier 'float16_t'
+
