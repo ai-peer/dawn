4619b1ec45b3c17b3ed1c4e0652cfb43caa978da
dan sinclair <dsinclair@chromium.org>
2023-07-11 19:56:44 -0400

    [ir][validation] Add ExitLoop validation
    Add validation of the ExitLoop IR instruction.
    
    Bug: tint:1952
    Change-Id: I147e5381e1280c5b125021e7d7223b0e39d9bbeb

--- src/tint/ir/validate.cc
+++ src/tint/ir/validate.cc
@@ -492,7 +492,7 @@ class Validator {
         tint::Switch(
             e,                                               //
             [&](ir::ExitIf* i) { CheckExitIf(i); },          //
-            [&](ir::ExitLoop*) {},                           //
+            [&](ir::ExitLoop* l) { CheckExitLoop(l); },      //
             [&](ir::ExitSwitch* s) { CheckExitSwitch(s); },  //
             [&](Default) {
                 AddError(std::string("missing validation of exit: ") + e->TypeInfo().name);
@@ -506,24 +506,32 @@ class Validator {
         }
     }
 
-    void CheckExitSwitch(ExitSwitch* s) {
+    void CheckControlsAllowingIf(Exit* exit, Instruction* control, std::string_view name) {
         bool found = false;
         for (auto ctrl : utils::Reverse(control_stack_)) {
-            if (ctrl == s->ControlInstruction()) {
+            if (ctrl == control) {
                 found = true;
                 break;
             }
             // A exit switch can step over if instructions, but no others.
             if (!ctrl->Is<ir::If>()) {
-                AddError(s, "exit_switch: switch target jumps over other control instructions");
+                AddError(exit, "exit: " + std::string(name) +
+                                   " target jumps over other control instructions");
                 AddNote(ctrl, "first control instruction jumped");
                 return;
             }
         }
         if (!found) {
-            AddError(s, "exit_switch: switch not found in parent control instructions");
+            AddError(exit,
+                     "exit: " + std::string(name) + " not found in parent control instructions");
         }
     }
+
+    void CheckExitSwitch(ExitSwitch* s) {
+        CheckControlsAllowingIf(s, s->ControlInstruction(), "switch");
+    }
+
+    void CheckExitLoop(ExitLoop* l) { CheckControlsAllowingIf(l, l->ControlInstruction(), "loop"); }
 };
 
 }  // namespace
