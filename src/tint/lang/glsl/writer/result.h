510f2a8c6e0846d7c13921d38f4cfb5818e9d336
dan sinclair <dsinclair@chromium.org>
2023-07-25 17:56:10 -0400

    [shuffle] Move glsl writer to new structure.
    This CL moves the `glsl/ast_writer` to `glsl/writer/ast_printer/` and
    renames the `GeneratorImpl` to `ASTPrinter`.
    
    Bug: tint:1988
    Change-Id: Ieaf6b67885e942abb853c68777e9547e93ff98dc

---
+++ src/tint/lang/glsl/writer/result.h
@@ -0,0 +1,47 @@
+// Copyright 2023 The Tint Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "src/tint/lang/wgsl/ast/pipeline_stage.h"
+
+namespace tint::glsl::writer {
+
+/// The result produced when generating GLSL.
+struct Result {
+    /// Constructor
+    Result();
+
+    /// Destructor
+    ~Result();
+
+    /// Copy constructor
+    Result(const Result&);
+
+    /// True if generation was successful.
+    bool success = false;
+
+    /// The errors generated during code generation, if any.
+    std::string error;
+
+    /// The generated GLSL.
+    std::string glsl = "";
+
+    /// The list of entry points in the generated GLSL.
+    std::vector<std::pair<std::string, ast::PipelineStage>> entry_points;
+};
+
+}  // namespace tint::glsl::writer
