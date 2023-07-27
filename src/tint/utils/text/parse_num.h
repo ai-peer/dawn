1ef70eba914c014e4d61a18a544ad96c1cf4d92a
Ben Clayton <bclayton@google.com>
2023-07-27 12:42:15 +0100

    [tint] Fix a load of doxygen warnings
    Something I changed made it wake up.
    
    Change-Id: I2dd6c289d0a7eba00a7f81852b873e4dfd41d542

--- src/tint/utils/text/parse_num.h
+++ src/tint/utils/text/parse_num.h
@@ -79,12 +79,11 @@ Result<int8_t, ParseNumberError> ParseInt8(std::string_view str);
 /// @returns the string @p str parsed as a uint8_t
 Result<uint8_t, ParseNumberError> ParseUint8(std::string_view str);
 
-TINT_BEGIN_DISABLE_WARNING(UNREACHABLE_CODE);
-
 /// @param str the string
 /// @returns the string @p str parsed as a the number @p T
 template <typename T>
 inline Result<T, ParseNumberError> ParseNumber(std::string_view str) {
+    TINT_BEGIN_DISABLE_WARNING(UNREACHABLE_CODE);
     if constexpr (std::is_same_v<T, float>) {
         return ParseFloat(str);
     }
@@ -122,9 +121,8 @@ inline Result<T, ParseNumberError> ParseNumber(std::string_view str) {
         return ParseUint8(str);
     }
     return ParseNumberError::kUnparsable;
+    TINT_END_DISABLE_WARNING(UNREACHABLE_CODE);
 }
-
-TINT_END_DISABLE_WARNING(UNREACHABLE_CODE);
 
 }  // namespace tint
 
