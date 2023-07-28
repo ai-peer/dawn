c696fdea4177cce43f215b3d4fcfd96782abf42a
Ben Clayton <bclayton@google.com>
2023-07-28 21:59:32 +0100

    [tint] Use consistent file pattern for test files
    Always append with _test.* for files to be used in tests.
    
    Change-Id: Ide629f6974afeca5c30227c5808cd97d2ee75585

--- src/tint/lang/glsl/writer/ast_printer/helper_test.h
+++ src/tint/lang/glsl/writer/ast_printer/helper_test.h
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef SRC_TINT_LANG_GLSL_WRITER_AST_PRINTER_TEST_HELPER_H_
-#define SRC_TINT_LANG_GLSL_WRITER_AST_PRINTER_TEST_HELPER_H_
+#ifndef SRC_TINT_LANG_GLSL_WRITER_AST_PRINTER_HELPER_TEST_H_
+#define SRC_TINT_LANG_GLSL_WRITER_AST_PRINTER_HELPER_TEST_H_
 
 #include <memory>
 #include <string>
@@ -103,4 +103,4 @@ using TestParamHelper = TestHelperBase<testing::TestWithParam<T>>;
 
 }  // namespace tint::glsl::writer
 
-#endif  // SRC_TINT_LANG_GLSL_WRITER_AST_PRINTER_TEST_HELPER_H_
+#endif  // SRC_TINT_LANG_GLSL_WRITER_AST_PRINTER_HELPER_TEST_H_
