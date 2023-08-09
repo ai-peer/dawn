1ffbbfe899da234bc248f9377e9c8caf0658ca60
Shrek Shao <shrekshao@google.com>
2023-08-08 17:38:24 -0700

    Add TextureBuiltinsFromUniform transform
    TextureNumLevels/TextureNumSamples counterpart builtin functions
    are not available in GLSL for OpenGLES.
    
    This transform replace those builtin calls by creating an internal
    uniform buffer to store these builtin values. A bindpoint to
    byte offset and data type is included in the result of the transform
    so that caller will know how to upload the data.
    
    But: dawn:1299, tint:2006
    Change-Id: Idc1b6e699443b8b656208d49e73ec6001f1e77ea

--- src/tint/api/options/BUILD.cmake
+++ src/tint/api/options/BUILD.cmake
@@ -28,6 +28,7 @@ tint_add_target("api/options"
   api/options/binding_remapper.h
   api/options/external_texture.h
   api/options/options.cc
+  api/options/texture_builtins_from_uniform.h
 )
 
 tint_target_add_dependencies("api/options"
