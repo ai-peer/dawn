5426748caaf0c14b1e9c64245aaa313b00096314
Le Hoang Quyen <lehoangquyen@chromium.org>
2023-08-21 22:27:21 +0800

    Added MultiPlanarFormatExtendedUsages feature.
    This feature allows:
    - Creating multiplanar textures without importing external image.
    - Including CopyDst as texture's usage.
    - Copy per-plane data between texture and buffer.
    
    In future this feature would allow:
    - Using the texture as render target.
    - Copy per-plane data between textures.
    
    This is useful for Chrome to be able to use a multiplanar shared image
    with SwiftShader. Chrome emulates the support by creating "shadow"
    WGPUTexture to copy/from the shared image. The WGPUTexture needs to be
    multiplanar formatted.
    
    Bug: dawn:1923
    Bug: chromium:1467566
    Change-Id: I00674c41a59cb3b39a39194520c470662efd56ae

--- src/dawn/native/vulkan/TextureVk.cpp
+++ src/dawn/native/vulkan/TextureVk.cpp
@@ -1271,15 +1271,17 @@ MaybeError Texture::ClearTexture(CommandRecordingContext* recordingContext,
     imageRange.levelCount = 1;
     imageRange.layerCount = 1;
 
-    if (GetFormat().isCompressed) {
+    if (GetFormat().isCompressed || GetFormat().IsMultiPlanar()) {
         if (range.aspects == Aspect::None) {
             return {};
         }
         // need to clear the texture with a copy from buffer
-        ASSERT(range.aspects == Aspect::Color);
+        ASSERT(range.aspects == Aspect::Color || range.aspects == Aspect::Plane0 ||
+               range.aspects == Aspect::Plane1);
         const TexelBlockInfo& blockInfo = GetFormat().GetAspectInfo(range.aspects).block;
 
         Extent3D largestMipSize = GetMipLevelSingleSubresourcePhysicalSize(range.baseMipLevel);
+        largestMipSize = GetFormat().GetAspectSize(range.aspects, largestMipSize);
 
         uint32_t bytesPerRow = Align((largestMipSize.width / blockInfo.width) * blockInfo.byteSize,
                                      device->GetOptimalBytesPerRowAlignment());
