8091346b5e24e82a66d28034edd2f53b50260e61
Albin Bernhardsson <albin.bernhardsson@arm.com>
2023-09-12 17:00:02 +0200

    Improve Vulkan synchronization
    Track resources per shader stage by using the visibility flags in the
    bind group layout. This means we can insert more precise barriers,
    allowing vertex and/or compute to execute in parallel with fragment
    shading. Eg. if we have two render passes where the second render
    pass samples an image rendered in the first in the fragment shader,
    this would previously result in a FRAGMENT->VERTEX|FRAGMENT|COMPUTE
    barrier as a texture can be sampled in any shader stage and only the
    usage is considered when inserting the barrier. By considering the
    shader stages, we can instead insert a FRAGMENT->FRAGMENT barrier.
    
    Fix an issue where empty image memory barriers cause a
    TOP_OF_PIPE->TOP_OF_PIPE barrier. On its own, this is a NOP, but when
    merged with other barriers, the TOP_OF_PIPE in the destination stages
    could cause unnecessarily conservative barriers.
    
    Previously, all barriers in a sync scope were merged into one
    pipeline barrier. Eg. if we have a render pass with one resource
    being used in a vertex shader and was last written in compute (eg.
    compute skinning), and a resource used in a fragment shader and last
    written to by a render pass (eg. ambient occlusion), merging these
    COMPUTE->VERTEX and FRAGMENT->FRAGMENT barriers would lead to a
    COMPUTE|FRAGMENT->VERTEX|FRAGMENT barrier, inadvertently introducing
    an unnecessary FRAGMENT->VERTEX dependency and disallowing parallel
    execution. Change this so that all barriers which contain a vertex
    stage in the destination stages are merged into one pipeline barrier,
    and all remaining barriers into another.
    
    In a Sponza test scene this reduces frame times by 17% on a Mali-G78
    device.
    
    Bug: dawn:851
    Change-Id: I50e2f00f845670d7f161a0afa6579eba6057afa3

--- src/dawn/native/vulkan/TextureVk.cpp
+++ src/dawn/native/vulkan/TextureVk.cpp
@@ -173,7 +173,9 @@ VkAccessFlags VulkanAccessFlags(wgpu::TextureUsage usage, const Format& format) 
 }
 
 // Computes which Vulkan pipeline stage can access a texture in the given Dawn usage
-VkPipelineStageFlags VulkanPipelineStage(wgpu::TextureUsage usage, const Format& format) {
+VkPipelineStageFlags VulkanPipelineStage(wgpu::TextureUsage usage,
+                                         wgpu::ShaderStage shaderStage,
+                                         const Format& format) {
     if (usage & kReservedTextureUsage) {
         // Handle the special readonly usages for mixed depth-stencil.
         DAWN_ASSERT(IsSubset(kDepthReadOnlyStencilWritableAttachment, usage) ||
@@ -184,7 +186,7 @@ VkPipelineStageFlags VulkanPipelineStage(wgpu::TextureUsage usage, const Format&
             usage &
             ~(kDepthReadOnlyStencilWritableAttachment | kDepthWritableStencilReadOnlyAttachment);
         return VulkanPipelineStage(nonAttachmentUsages | wgpu::TextureUsage::RenderAttachment,
-                                   format);
+                                   shaderStage, format);
     }
 
     VkPipelineStageFlags flags = 0;
@@ -197,15 +199,16 @@ VkPipelineStageFlags VulkanPipelineStage(wgpu::TextureUsage usage, const Format&
     if (usage & (wgpu::TextureUsage::CopySrc | wgpu::TextureUsage::CopyDst)) {
         flags |= VK_PIPELINE_STAGE_TRANSFER_BIT;
     }
-    if (usage & (wgpu::TextureUsage::TextureBinding | kReadOnlyStorageTexture)) {
-        // TODO(crbug.com/dawn/851): Only transition to the usage we care about to avoid
-        // introducing FS -> VS dependencies that would prevent parallelization on tiler
-        // GPUs
-        flags |= VK_PIPELINE_STAGE_VERTEX_SHADER_BIT | VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT |
-                 VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT;
-    }
-    if (usage & (wgpu::TextureUsage::StorageBinding | kWriteOnlyStorageTexture)) {
-        flags |= VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT | VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT;
+    if (usage & kShaderTextureUsages) {
+        if (shaderStage & wgpu::ShaderStage::Vertex) {
+            flags |= VK_PIPELINE_STAGE_VERTEX_SHADER_BIT;
+        }
+        if (shaderStage & wgpu::ShaderStage::Fragment) {
+            flags |= VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+        }
+        if (shaderStage & wgpu::ShaderStage::Compute) {
+            flags |= VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT;
+        }
     }
     if (usage & (wgpu::TextureUsage::RenderAttachment | kReadOnlyRenderAttachment)) {
         if (format.HasDepthOrStencil()) {
@@ -779,11 +782,11 @@ Texture::Texture(Device* device, const TextureDescriptor* descriptor)
       mCombinedAspect(ComputeCombinedAspect(device, GetFormat())),
       // A usage of none will make sure the texture is transitioned before its first use as
       // required by the Vulkan spec.
-      mSubresourceLastUsages(
+      mSubresourceLastSyncInfos(
           mCombinedAspect != Aspect::None ? mCombinedAspect : GetFormat().aspects,
           GetArrayLayers(),
           GetNumMipLevels(),
-          wgpu::TextureUsage::None) {}
+          TextureSyncInfo{wgpu::TextureUsage::None, wgpu::ShaderStage::None}) {}
 
 MaybeError Texture::InitializeAsInternalTexture(VkImageUsageFlags extraUsages) {
     Device* device = ToBackend(GetDevice());
@@ -1010,14 +1013,17 @@ void Texture::TransitionEagerlyForExport(CommandRecordingContext* recordingConte
     DAWN_ASSERT(GetNumMipLevels() == 1 && GetArrayLayers() == 1);
     SubresourceRange range = {GetDisjointVulkanAspects(), {0, 1}, {0, 1}};
 
-    wgpu::TextureUsage usage = mSubresourceLastUsages.Get(range.aspects, 0, 0);
+    wgpu::TextureUsage usage = mSubresourceLastSyncInfos.Get(range.aspects, 0, 0).usage;
+    wgpu::ShaderStage shaderStages =
+        mSubresourceLastSyncInfos.Get(range.aspects, 0, 0).shaderStages;
 
     std::vector<VkImageMemoryBarrier> barriers;
     VkPipelineStageFlags srcStages = 0;
     VkPipelineStageFlags dstStages = 0;
 
     // Same usage as last.
-    TransitionUsageAndGetResourceBarrier(usage, range, &barriers, &srcStages, &dstStages);
+    TransitionUsageAndGetResourceBarrier(usage, shaderStages, range, &barriers, &srcStages,
+                                         &dstStages);
 
     DAWN_ASSERT(barriers.size() == 1);
     VkImageMemoryBarrier& barrier = barriers[0];
@@ -1064,7 +1070,8 @@ MaybeError Texture::ExportExternalTexture(VkImageLayout desiredLayout,
     mExternalState = ExternalState::Released;
 
     DAWN_ASSERT(GetNumMipLevels() == 1 && GetArrayLayers() == 1);
-    wgpu::TextureUsage usage = mSubresourceLastUsages.Get(GetDisjointVulkanAspects(), 0, 0);
+    wgpu::TextureUsage usage =
+        mSubresourceLastSyncInfos.Get(GetDisjointVulkanAspects(), 0, 0).usage;
 
     // Compute the layouts for the queue transition for export. desiredLayout == UNDEFINED is a tag
     // value used to export with whatever the current layout is. However queue transitioning to the
@@ -1241,73 +1248,75 @@ void Texture::TweakTransitionForExternalUsage(CommandRecordingContext* recording
     mLastExternalState = mExternalState;
 }
 
-bool Texture::CanReuseWithoutBarrier(wgpu::TextureUsage lastUsage, wgpu::TextureUsage usage) {
+bool Texture::CanReuseWithoutBarrier(wgpu::TextureUsage lastUsage,
+                                     wgpu::TextureUsage usage,
+                                     wgpu::ShaderStage lastShaderStage,
+                                     wgpu::ShaderStage shaderStage) {
     // Reuse the texture directly and avoid encoding barriers when it isn't needed.
     bool lastReadOnly = IsSubset(lastUsage, kReadOnlyTextureUsages);
-    if (lastReadOnly && lastUsage == usage && mLastExternalState == mExternalState) {
+    if (lastReadOnly && lastUsage == usage && IsSubset(shaderStage, lastShaderStage) &&
+        mLastExternalState == mExternalState) {
         return true;
     }
     return false;
 }
 
 void Texture::TransitionUsageForPass(CommandRecordingContext* recordingContext,
-                                     const TextureSubresourceUsage& textureUsages,
+                                     const TextureSubresourceSyncInfo& textureSyncInfos,
                                      std::vector<VkImageMemoryBarrier>* imageBarriers,
                                      VkPipelineStageFlags* srcStages,
                                      VkPipelineStageFlags* dstStages) {
     if (!UseCombinedAspects()) {
-        TransitionUsageForPassImpl(recordingContext, textureUsages, imageBarriers, srcStages,
+        TransitionUsageForPassImpl(recordingContext, textureSyncInfos, imageBarriers, srcStages,
                                    dstStages);
         return;
     }
-
     // We need to combine aspects for the transition, use a new subresource storage that will
     // contain the combined usages for the aspects.
-    SubresourceStorage<wgpu::TextureUsage> combinedUsages(mCombinedAspect, GetArrayLayers(),
-                                                          GetNumMipLevels());
-
+    SubresourceStorage<TextureSyncInfo> combinedUsages(mCombinedAspect, GetArrayLayers(),
+                                                       GetNumMipLevels());
     if (mCombinedAspect == Aspect::CombinedDepthStencil) {
         // For depth-stencil we can't just combine the aspect with an | operation because there
         // needs to be special handling for readonly aspects. Instead figure out which aspect is
         // currently being added (and which one is already present) and call the custom merging
         // function for depth-stencil.
-        textureUsages.Iterate([&](const SubresourceRange& range, wgpu::TextureUsage usage) {
+        textureSyncInfos.Iterate([&](const SubresourceRange& range, TextureSyncInfo syncInfo) {
             SubresourceRange updateRange = range;
             updateRange.aspects = mCombinedAspect;
             Aspect aspectsToMerge = range.aspects;
-
             combinedUsages.Update(
-                updateRange, [&](const SubresourceRange&, wgpu::TextureUsage* combinedUsage) {
+                updateRange, [&](const SubresourceRange&, TextureSyncInfo* combinedInfo) {
                     if (aspectsToMerge == Aspect::Depth) {
-                        *combinedUsage = MergeDepthStencilUsage(usage, *combinedUsage);
+                        combinedInfo->usage =
+                            MergeDepthStencilUsage(syncInfo.usage, combinedInfo->usage);
                     } else if (aspectsToMerge == Aspect::Stencil) {
-                        *combinedUsage = MergeDepthStencilUsage(*combinedUsage, usage);
+                        combinedInfo->usage =
+                            MergeDepthStencilUsage(combinedInfo->usage, syncInfo.usage);
                     } else {
                         DAWN_ASSERT(aspectsToMerge == (Aspect::Depth | Aspect::Stencil));
-                        *combinedUsage = usage;
+                        combinedInfo->usage = syncInfo.usage;
                     }
                 });
         });
     } else {
         // Combine aspect's usages with the | operation.
-        textureUsages.Iterate([&](const SubresourceRange& range, wgpu::TextureUsage usage) {
+        textureSyncInfos.Iterate([&](const SubresourceRange& range, TextureSyncInfo syncInfo) {
             SubresourceRange updateRange = range;
             updateRange.aspects = mCombinedAspect;
-
             combinedUsages.Update(updateRange,
-                                  [&](const SubresourceRange&, wgpu::TextureUsage* combinedUsage) {
-                                      *combinedUsage |= usage;
+                                  [&](const SubresourceRange&, TextureSyncInfo* combinedInfo) {
+                                      combinedInfo->usage |= syncInfo.usage;
+                                      combinedInfo->shaderStages |= syncInfo.shaderStages;
                                   });
         });
     }
-
     TransitionUsageForPassImpl(recordingContext, combinedUsages, imageBarriers, srcStages,
                                dstStages);
 }
 
 void Texture::TransitionUsageForPassImpl(
     CommandRecordingContext* recordingContext,
-    const SubresourceStorage<wgpu::TextureUsage>& subresourceUsages,
+    const SubresourceStorage<TextureSyncInfo>& subresourceSyncInfos,
     std::vector<VkImageMemoryBarrier>* imageBarriers,
     VkPipelineStageFlags* srcStages,
     VkPipelineStageFlags* dstStages) {
@@ -1317,38 +1326,58 @@ void Texture::TransitionUsageForPassImpl(
     wgpu::TextureUsage allUsages = wgpu::TextureUsage::None;
     wgpu::TextureUsage allLastUsages = wgpu::TextureUsage::None;
 
-    mSubresourceLastUsages.Merge(subresourceUsages, [&](const SubresourceRange& range,
-                                                        wgpu::TextureUsage* lastUsage,
-                                                        const wgpu::TextureUsage& newUsage) {
-        if (newUsage == wgpu::TextureUsage::None || CanReuseWithoutBarrier(*lastUsage, newUsage)) {
-            return;
-        }
+    wgpu::ShaderStage allShaderStages = wgpu::ShaderStage::None;
+    wgpu::ShaderStage allLastShaderStages = wgpu::ShaderStage::None;
 
-        imageBarriers->push_back(BuildMemoryBarrier(this, *lastUsage, newUsage, range));
+    mSubresourceLastSyncInfos.Merge(
+        subresourceSyncInfos, [&](const SubresourceRange& range, TextureSyncInfo* lastSyncInfo,
+                                  const TextureSyncInfo& newSyncInfo) {
+            if (newSyncInfo.usage == wgpu::TextureUsage::None ||
+                CanReuseWithoutBarrier(lastSyncInfo->usage, newSyncInfo.usage,
+                                       lastSyncInfo->shaderStages, newSyncInfo.shaderStages)) {
+                return;
+            }
 
-        allLastUsages |= *lastUsage;
-        allUsages |= newUsage;
+            imageBarriers->push_back(
+                BuildMemoryBarrier(this, lastSyncInfo->usage, newSyncInfo.usage, range));
 
-        *lastUsage = newUsage;
-    });
+            allLastUsages |= lastSyncInfo->usage;
+            allUsages |= newSyncInfo.usage;
+
+            allLastShaderStages |= lastSyncInfo->shaderStages;
+            allShaderStages |= newSyncInfo.shaderStages;
+
+            if (lastSyncInfo->usage != newSyncInfo.usage ||
+                !IsSubset(lastSyncInfo->usage, kReadOnlyTextureUsages)) {
+                lastSyncInfo->shaderStages = wgpu::ShaderStage::None;
+            }
+            lastSyncInfo->usage = newSyncInfo.usage;
+            lastSyncInfo->shaderStages |= newSyncInfo.shaderStages;
+        });
 
     if (mExternalState != ExternalState::InternalOnly) {
         TweakTransitionForExternalUsage(recordingContext, imageBarriers, transitionBarrierStart);
     }
 
-    *srcStages |= VulkanPipelineStage(allLastUsages, format);
-    *dstStages |= VulkanPipelineStage(allUsages, format);
+    // Skip adding pipeline stages if no barrier was needed to avoid putting TOP_OF_PIPE in the
+    // destination stages.
+    if (allUsages != wgpu::TextureUsage::None) {
+        *srcStages |= VulkanPipelineStage(allLastUsages, allLastShaderStages, format);
+        *dstStages |= VulkanPipelineStage(allUsages, allShaderStages, format);
+    }
 }
 
 void Texture::TransitionUsageNow(CommandRecordingContext* recordingContext,
                                  wgpu::TextureUsage usage,
+                                 wgpu::ShaderStage shaderStages,
                                  const SubresourceRange& range) {
     std::vector<VkImageMemoryBarrier> barriers;
 
     VkPipelineStageFlags srcStages = 0;
     VkPipelineStageFlags dstStages = 0;
 
-    TransitionUsageAndGetResourceBarrier(usage, range, &barriers, &srcStages, &dstStages);
+    TransitionUsageAndGetResourceBarrier(usage, shaderStages, range, &barriers, &srcStages,
+                                         &dstStages);
 
     if (mExternalState != ExternalState::InternalOnly) {
         TweakTransitionForExternalUsage(recordingContext, &barriers, 0);
@@ -1363,6 +1392,7 @@ void Texture::TransitionUsageNow(CommandRecordingContext* recordingContext,
 }
 
 void Texture::TransitionUsageAndGetResourceBarrier(wgpu::TextureUsage usage,
+                                                   wgpu::ShaderStage shaderStages,
                                                    const SubresourceRange& range,
                                                    std::vector<VkImageMemoryBarrier>* imageBarriers,
                                                    VkPipelineStageFlags* srcStages,
@@ -1370,15 +1400,17 @@ void Texture::TransitionUsageAndGetResourceBarrier(wgpu::TextureUsage usage,
     if (UseCombinedAspects()) {
         SubresourceRange updatedRange = range;
         updatedRange.aspects = mCombinedAspect;
-        TransitionUsageAndGetResourceBarrierImpl(usage, updatedRange, imageBarriers, srcStages,
-                                                 dstStages);
+        TransitionUsageAndGetResourceBarrierImpl(usage, shaderStages, updatedRange, imageBarriers,
+                                                 srcStages, dstStages);
     } else {
-        TransitionUsageAndGetResourceBarrierImpl(usage, range, imageBarriers, srcStages, dstStages);
+        TransitionUsageAndGetResourceBarrierImpl(usage, shaderStages, range, imageBarriers,
+                                                 srcStages, dstStages);
     }
 }
 
 void Texture::TransitionUsageAndGetResourceBarrierImpl(
     wgpu::TextureUsage usage,
+    wgpu::ShaderStage shaderStages,
     const SubresourceRange& range,
     std::vector<VkImageMemoryBarrier>* imageBarriers,
     VkPipelineStageFlags* srcStages,
@@ -1387,20 +1419,28 @@ void Texture::TransitionUsageAndGetResourceBarrierImpl(
     const Format& format = GetFormat();
 
     wgpu::TextureUsage allLastUsages = wgpu::TextureUsage::None;
-    mSubresourceLastUsages.Update(
-        range, [&](const SubresourceRange& range, wgpu::TextureUsage* lastUsage) {
-            if (CanReuseWithoutBarrier(*lastUsage, usage)) {
+    wgpu::ShaderStage allLastShaderStages = wgpu::ShaderStage::None;
+    mSubresourceLastSyncInfos.Update(
+        range, [&](const SubresourceRange& range, TextureSyncInfo* lastSyncInfo) {
+            if (CanReuseWithoutBarrier(lastSyncInfo->usage, usage, lastSyncInfo->shaderStages,
+                                       shaderStages)) {
                 return;
             }
 
-            imageBarriers->push_back(BuildMemoryBarrier(this, *lastUsage, usage, range));
+            imageBarriers->push_back(BuildMemoryBarrier(this, lastSyncInfo->usage, usage, range));
 
-            allLastUsages |= *lastUsage;
-            *lastUsage = usage;
+            allLastUsages |= lastSyncInfo->usage;
+            allLastShaderStages |= lastSyncInfo->shaderStages;
+
+            if (lastSyncInfo->usage != usage || !IsSubset(usage, kReadOnlyTextureUsages)) {
+                lastSyncInfo->shaderStages = wgpu::ShaderStage::None;
+            }
+            lastSyncInfo->usage = usage;
+            lastSyncInfo->shaderStages |= shaderStages;
         });
 
-    *srcStages |= VulkanPipelineStage(allLastUsages, format);
-    *dstStages |= VulkanPipelineStage(usage, format);
+    *srcStages |= VulkanPipelineStage(allLastUsages, allLastShaderStages, format);
+    *dstStages |= VulkanPipelineStage(usage, shaderStages, format);
 }
 
 MaybeError Texture::ClearTexture(CommandRecordingContext* recordingContext,
@@ -1413,7 +1453,8 @@ MaybeError Texture::ClearTexture(CommandRecordingContext* recordingContext,
     int32_t sClearColor = isZero ? 0 : 1;
     float fClearColor = isZero ? 0.f : 1.f;
 
-    TransitionUsageNow(recordingContext, wgpu::TextureUsage::CopyDst, range);
+    TransitionUsageNow(recordingContext, wgpu::TextureUsage::CopyDst, wgpu::ShaderStage::None,
+                       range);
 
     VkImageSubresourceRange imageRange = {};
     imageRange.levelCount = 1;
@@ -1566,7 +1607,7 @@ void Texture::UpdateExternalSemaphoreHandle(ExternalSemaphoreHandle handle) {
 
 VkImageLayout Texture::GetCurrentLayoutForSwapChain() const {
     DAWN_ASSERT(GetFormat().aspects == Aspect::Color);
-    return VulkanImageLayout(GetFormat(), mSubresourceLastUsages.Get(Aspect::Color, 0, 0));
+    return VulkanImageLayout(GetFormat(), mSubresourceLastSyncInfos.Get(Aspect::Color, 0, 0).usage);
 }
 
 bool Texture::UseCombinedAspects() const {
