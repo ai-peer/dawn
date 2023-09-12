ebf91c16ca532f8c7a4a7e2bde1349dedb7cf0c8
Antonio Maiorano <amaiorano@google.com>
2023-09-12 12:51:48 -0400

    tint/hlsl: fix RemoveContinueInSwitch transform to handle nested switch statements
    Rework the transform so that it handles a continue within a switch
    nested within switches within a loop. Also make the transform only
    generate a single bool variable per loop, rather than per continue
    statement.
    
    Bug: tint:1080
    Change-Id: I626cf4aa35e900ccc8ad08712ff1393c91587f10

--- src/tint/lang/hlsl/writer/ast_raise/remove_continue_in_switch.cc
+++ src/tint/lang/hlsl/writer/ast_raise/remove_continue_in_switch.cc
@@ -15,7 +15,6 @@
 #include "src/tint/lang/hlsl/writer/ast_raise/remove_continue_in_switch.h"
 
 #include <string>
-#include <unordered_map>
 #include <utility>
 
 #include "src/tint/lang/wgsl/ast/continue_statement.h"
@@ -25,11 +24,10 @@
 #include "src/tint/lang/wgsl/program/program_builder.h"
 #include "src/tint/lang/wgsl/resolver/resolve.h"
 #include "src/tint/lang/wgsl/sem/block_statement.h"
-#include "src/tint/lang/wgsl/sem/for_loop_statement.h"
 #include "src/tint/lang/wgsl/sem/loop_statement.h"
 #include "src/tint/lang/wgsl/sem/switch_statement.h"
-#include "src/tint/lang/wgsl/sem/while_statement.h"
-#include "src/tint/utils/containers/map.h"
+#include "src/tint/utils/containers/hashmap.h"
+#include "src/tint/utils/containers/hashset.h"
 
 TINT_INSTANTIATE_TYPEINFO(tint::hlsl::writer::RemoveContinueInSwitch);
 
@@ -44,8 +42,7 @@ struct RemoveContinueInSwitch::State {
     /// Runs the transform
     /// @returns the new program or SkipTransform if the transform is not required
     ApplyResult Run() {
-        bool made_changes = false;
-
+        // First collect all switch statements within loops that contain a continue statement.
         for (auto* node : src->ASTNodes().Objects()) {
             auto* cont = node->As<ast::ContinueStatement>();
             if (!cont) {
@@ -58,35 +55,69 @@ struct RemoveContinueInSwitch::State {
                 continue;
             }
 
-            made_changes = true;
+            const auto& r = switch_info_map.Add(switch_stmt, SwitchInfo{});
+            auto* info = r.value;
+            if (r) {
+                info->switch_stmt = switch_stmt;
+                info->loop_block = sem.Get(switch_stmt)->FindFirstParent<sem::LoopBlockStatement>();
+                switch_infos.Push(info);
+            }
+            info->continues.Push(cont);
+        }
 
-            auto cont_var_name = tint::GetOrCreate(switch_to_cont_var_name, switch_stmt, [&] {
-                // Create and insert 'var tint_continue : bool = false;' before the
-                // switch.
+        if (switch_infos.IsEmpty()) {
+            return SkipTransform;
+        }
+
+        // For each switch statement:
+        // 1. Declare a 'tint_continue' var just before the parent loop, and reset it to false at
+        // the top of the loop body.
+        // 2. Replace 'continue' with '{ tint_continue = true; break; }'
+        // 3. Insert 'if (tint_continue) { break; }' after switch, and all parent switches, except
+        // for the parent-most, for which we insert 'if (tint_continue) { continue; }'
+        for (auto* info : switch_infos) {
+            auto var_name = loop_to_var.GetOrCreate(info->loop_block, [&] {
+                // Create and insert 'var tint_continue : bool;' before loop
                 auto var_name = b.Symbols().New("tint_continue");
-                auto* decl = b.Decl(b.Var(var_name, b.ty.bool_(), b.Expr(false)));
-                auto ip = ast::transform::utils::GetInsertionPoint(ctx, switch_stmt);
+                auto* decl = b.Decl(b.Var(var_name, b.ty.bool_()));
+                auto ip = ast::transform::utils::GetInsertionPoint(
+                    ctx, info->loop_block->Parent()->Declaration());
                 ctx.InsertBefore(ip.first->Declaration()->statements, ip.second, decl);
 
-                // Create and insert 'if (tint_continue) { continue; }' after
-                // switch.
-                auto* if_stmt = b.If(b.Expr(var_name), b.Block(b.Continue()));
-                ctx.InsertAfter(ip.first->Declaration()->statements, ip.second, if_stmt);
+                // Insert 'tint_continue = false' at top of loop body
+                auto assign_false = b.Assign(var_name, false);
+                ctx.InsertFront(info->loop_block->Declaration()->statements, assign_false);
 
-                // Return the new var name
                 return var_name;
             });
 
-            // Replace 'continue;' with '{ tint_continue = true; break; }'
-            auto* new_stmt = b.Block(                   //
-                b.Assign(b.Expr(cont_var_name), true),  //
-                b.Break());
+            for (auto& c : info->continues) {
+                // Replace 'continue;' with '{ tint_continue = true; break; }'
+                ctx.Replace(c, b.Assign(b.Expr(var_name), true));
+                auto ip = ast::transform::utils::GetInsertionPoint(ctx, c);
+                ctx.InsertAfter(ip.first->Declaration()->statements, ip.second, b.Break());
+            }
 
-            ctx.Replace(cont, new_stmt);
-        }
+            // Insert 'if (tint_continue) { break; }' after switch, and all parent switches,
+            // except for the parent-most, for which we insert 'if (tint_continue) { continue; }'
+            auto* curr_switch = info->switch_stmt;
+            while (curr_switch) {
+                auto* parent = sem.Get(curr_switch)->Parent()->Declaration();
+                auto* next_switch = GetParentSwitchInLoop(sem, parent);
 
-        if (!made_changes) {
-            return SkipTransform;
+                if (switch_handles_continue.Add(curr_switch)) {
+                    const ast::IfStatement* if_stmt = nullptr;
+                    if (next_switch) {
+                        if_stmt = b.If(b.Expr(var_name), b.Block(b.Break()));
+                    } else {
+                        if_stmt = b.If(b.Expr(var_name), b.Block(b.Continue()));
+                    }
+                    auto ip = ast::transform::utils::GetInsertionPoint(ctx, curr_switch);
+                    ctx.InsertAfter(ip.first->Declaration()->statements, ip.second, if_stmt);
+                }
+
+                curr_switch = next_switch;
+            }
         }
 
         ctx.Clone();
@@ -103,17 +134,38 @@ struct RemoveContinueInSwitch::State {
     /// Alias to src->sem
     const sem::Info& sem = src->Sem();
 
-    // Map of switch statement to 'tint_continue' variable.
-    std::unordered_map<const ast::SwitchStatement*, Symbol> switch_to_cont_var_name;
+    // Info for each switch statement within a loop that contains at least one continue statement.
+    struct SwitchInfo {
+        const ast::SwitchStatement* switch_stmt;
+        const sem::LoopBlockStatement* loop_block;
+        Vector<const ast::ContinueStatement*, 4> continues;
+    };
 
-    // If `cont` is within a switch statement within a loop, returns a pointer to
+    // Map of switch statements to per-switch info for switch statements within a loop that contains
+    // at least one continue statement.
+    Hashmap<const ast::SwitchStatement*, SwitchInfo, 4> switch_info_map;
+
+    // Vector of SwitchInfos added to switch_info_map to ensure ordering for deterministic
+    // traversal.
+    Vector<SwitchInfo*, 4> switch_infos;
+
+    // Map of loop block statement to the single 'tint_continue' variable used to replace 'continue'
+    // control flow.
+    Hashmap<const sem::LoopBlockStatement*, Symbol, 4> loop_to_var;
+
+    // Set used to avoid duplicating 'if (tint_continue) { break/continue; }' after each switch
+    // within a loop.
+    Hashset<const ast::SwitchStatement*, 4> switch_handles_continue;
+
+    // If `stmt` is within a switch statement within a loop, returns a pointer to
     // that switch statement.
     static const ast::SwitchStatement* GetParentSwitchInLoop(const sem::Info& sem,
-                                                             const ast::ContinueStatement* cont) {
+                                                             const ast::Statement* stmt) {
         // Find whether first parent is a switch or a loop
-        auto* sem_stmt = sem.Get(cont);
-        auto* sem_parent = sem_stmt->FindFirstParent<sem::SwitchStatement, sem::LoopBlockStatement,
-                                                     sem::ForLoopStatement, sem::WhileStatement>();
+        auto* sem_stmt = sem.Get(stmt);
+        auto* sem_parent =
+            sem_stmt->FindFirstParent<sem::SwitchStatement, sem::LoopBlockStatement>();
+
         if (!sem_parent) {
             return nullptr;
         }
