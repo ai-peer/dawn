d0de180d1a4483ab2a52f7c336f1d6875c8562a0
Peng Huang <penghuang@chromium.org>
2023-09-13 16:06:02 -0400

    Use the metal sdk to compile metal shaders
    Change-Id: If85b92f787195771fa8a5a5aca0fae2344b4e952

---
+++ src/dawn/native/metal/Process.h
@@ -0,0 +1,102 @@
+// Copyright 2023 The Dawn Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SRC_DAWN_NATIVE_METAL_PROCESS_H_
+#define SRC_DAWN_NATIVE_METAL_PROCESS_H_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "dawn/common/NonCopyable.h"
+
+namespace dawn::native::metal {
+
+class ScopedFD : public NonCopyable {
+  public:
+    ScopedFD() = default;
+    ScopedFD(int fd) : mFd(fd) {}
+    ScopedFD(ScopedFD&& fd) { this->operator=(std::move(fd)); }
+    ~ScopedFD() { Reset(); }
+
+    void Reset(int fd = -1) {
+        if (mFd != -1) {
+            close(mFd);
+        }
+        mFd = fd;
+    }
+
+    int Release() {
+        int fd = mFd;
+        mFd = -1;
+        return fd;
+    }
+
+    int Get() const { return mFd; }
+
+    const ScopedFD& operator=(int fd) {
+        Reset();
+        mFd = fd;
+        return *this;
+    }
+
+    const ScopedFD& operator=(ScopedFD&& other) {
+        Reset();
+        mFd = other.mFd;
+        other.mFd = -1;
+        return *this;
+    }
+
+  private:
+    int mFd = -1;
+};
+
+class Process : public NonCopyable {
+  public:
+    // Create a process and use the input string as the new process stdin.
+    static std::unique_ptr<Process> MakeWithStringInput(const char* const args[],
+                                                        const std::string& input);
+    // Create a process and use the input process's stdout as the new process stdin.
+    static std::unique_ptr<Process> MakeWithProcessInput(const char* const args[],
+                                                         std::unique_ptr<Process> input);
+    ~Process();
+
+    bool WriteToStdin(const std::string& data);
+    bool CloseStdin();
+    bool ReadFromStdout(std::vector<char>& buffer, size_t size);
+    bool CloseStdout();
+    ScopedFD TakeStdin() { return std::move(mStdin); }
+    ScopedFD TakeStdout() { return std::move(mStdout); }
+
+    bool WaitForExit(int& exit_code);
+    bool IsExited() const { return mPid == -1; }
+
+  private:
+    static std::unique_ptr<Process> MakeInternal(const char* const args[],
+                                                 ScopedFD pipeStdin[2],
+                                                 ScopedFD pipeStdout[2]);
+    Process(pid_t pid, ScopedFD stdinFd, ScopedFD stdoutFd);
+
+    pid_t mPid;
+    ScopedFD mStdin;
+    ScopedFD mStdout;
+};
+
+}  // namespace dawn::native::metal
+
+#endif  // SRC_DAWN_NATIVE_METAL_PROCESS_H_
