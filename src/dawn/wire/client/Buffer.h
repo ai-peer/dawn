fc04a01d8d7f5e74a60da2930f3dc9abe4356ef0
Loko Kung <lokokung@google.com>
2023-09-21 15:27:07 -0700

    [WGPUFuture] Use futures in wire for OnSubmittedWorkDone and MapAsync.
    - Removes a test that is no longer relevant since `signalValue` is to be
      deprecated, and was failing since the new path does not provide it.
    - Note for the wire we just use Spontaneous for the old path for now
      because that's effectively what it was doing before.
    
    Bug: dawn:2052, dawn:2060
    Change-Id: I2ab2d8e65f09d2685625b8bfa6a633c907eab49d

--- src/dawn/wire/client/Buffer.h
+++ src/dawn/wire/client/Buffer.h
@@ -32,7 +32,7 @@ class Buffer final : public ObjectBase {
     Buffer(const ObjectBaseParams& params, const WGPUBufferDescriptor* descriptor);
     ~Buffer() override;
 
-    bool OnMapAsyncCallback(uint64_t requestSerial,
+    bool OnMapAsyncCallback(WGPUFuture future,
                             uint32_t status,
                             uint64_t readDataUpdateInfoLength,
                             const uint8_t* readDataUpdateInfo);
@@ -58,7 +58,6 @@ class Buffer final : public ObjectBase {
     WGPUBufferMapState GetMapState() const;
 
   private:
-    void CancelCallbacksForDisconnect() override;
     void InvokeAndClearCallback(WGPUBufferMapAsyncStatus status);
 
     bool IsMappedForReading() const;
@@ -79,15 +78,13 @@ class Buffer final : public ObjectBase {
     // Up to only one request can exist at a single time.
     // Other requests are rejected.
     struct MapRequestData {
-        WGPUBufferMapCallback callback = nullptr;
-        void* userdata = nullptr;
+        WGPUFuture future;
         size_t offset = 0;
         size_t size = 0;
         MapRequestType type = MapRequestType::None;
     };
     MapRequestData mRequest;
     bool mPendingMap = false;
-    uint64_t mSerial = 0;
     uint64_t mSize = 0;
     WGPUBufferUsage mUsage;
 
