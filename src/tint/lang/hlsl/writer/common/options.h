6ad7f890601831ee8db111ded61ce981fa7fcaef
dan sinclair <dsinclair@chromium.org>
2023-09-21 18:23:48 -0400

    Updated HLSL options
    Use new HLSL options
    
    Bug: tint:1501
    Change-Id: Ic4e7ebbf54bb31869dfdb6d8b11379d8ecf838e7

--- src/tint/lang/hlsl/writer/common/options.h
+++ src/tint/lang/hlsl/writer/common/options.h
@@ -26,6 +26,58 @@
 #include "src/tint/utils/reflection/reflection.h"
 
 namespace tint::hlsl::writer {
+namespace binding {
+
+/// The HLSL register type.
+/// https://learn.microsoft.com/en-us/windows/win32/direct3dhlsl/dx-graphics-hlsl-variable-register
+enum class RegisterType {
+    kNone,
+    kConstantBuffer,       // b#
+    kTexture,              // t#
+    kBufferOffset,         // c#
+    kSampler,              // s#
+    kUnorderedAccessView,  // u#
+};
+
+/// Uniform binding
+struct Uniform {
+    /// The binding value
+    uint32_t value = 0;
+};
+
+/// Storage binding
+struct Storage {
+    /// The binding value
+    uint32_t value = 0;
+    /// The register space
+    uint32_t space = 0;
+    /// The binding register type
+    RegisterType register_type;
+};
+
+/// Texture binding
+struct Texture {
+    /// The binding value
+    uint32_t value = 0;
+};
+
+/// Sampler binding
+struct Sampler {
+    /// The binding value
+    uint32_t value = 0;
+};
+
+/// External texture binding
+struct ExternalTexture {
+    /// The binding metadata
+    Uniform metadata;
+    /// The binding value for plane 0
+    uint32_t plane0;
+    /// The binding value for plane 1
+    uint32_t plane1;
+};
+
+}  // namespace binding
 
 /// Configuration options used for generating HLSL.
 struct Options {
@@ -71,6 +123,20 @@ struct Options {
     /// The binding points that will be ignored in the rebustness transform.
     std::vector<BindingPoint> binding_points_ignored_in_robustness_transform;
 
+    /// TODO(crbug/tint/1501): The below are not fully hooked up yet, use the above options to
+    /// configure the transforms.
+
+    /// Uniform binding points
+    std::unordered_map<BindingPoint, binding::Uniform> uniforms;
+    /// Storage binding points
+    std::unordered_map<BindingPoint, binding::Storage> storage;
+    /// Texture binding points
+    std::unordered_map<BindingPoint, binding::Texture> texture;
+    /// Sampler binding points
+    std::unordered_map<BindingPoint, binding::Sampler> sampler;
+    /// External texture binding points
+    std::unordered_map<BindingPoint, binding::ExternalTexture> external_texture;
+
     /// Reflect the fields of this class so that it can be used by tint::ForeachField()
     TINT_REFLECT(disable_robustness,
                  root_constant_binding_point,
@@ -81,7 +147,17 @@ struct Options {
                  interstage_locations,
                  truncate_interstage_variables,
                  polyfill_reflect_vec2_f32,
-                 binding_points_ignored_in_robustness_transform);
+                 array_length_from_uniform,
+                 interstage_locations,
+                 root_constant_binding_point,
+                 external_texture_options,
+                 binding_remapper_options,
+                 binding_points_ignored_in_robustness_transform,
+                 uniforms,
+                 storage,
+                 texture,
+                 sampler,
+                 external_texture);
 };
 
 }  // namespace tint::hlsl::writer
