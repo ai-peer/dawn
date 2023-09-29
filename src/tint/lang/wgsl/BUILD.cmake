f871c159410635c7af316ce93c7bbed47d511189
Ben Clayton <bclayton@google.com>
2023-09-29 13:31:46 +0100

    [tint][fuzzers] Port IRRoundtripFuzzer to tint_wgsl_fuzzer
    Change-Id: I8052f45136eb27ef63af98be3825e8128ff24375

--- src/tint/lang/wgsl/BUILD.cmake
+++ src/tint/lang/wgsl/BUILD.cmake
@@ -138,3 +138,45 @@ tint_target_add_dependencies(tint_lang_wgsl_bench bench
 tint_target_add_external_dependencies(tint_lang_wgsl_bench bench
   "google-benchmark"
 )
+
+################################################################################
+# Target:    tint_lang_wgsl_fuzz
+# Kind:      fuzz
+################################################################################
+tint_add_target(tint_lang_wgsl_fuzz fuzz
+  lang/wgsl/ir_roundtrip_fuzz.cc
+)
+
+tint_target_add_dependencies(tint_lang_wgsl_fuzz fuzz
+  tint_api_common
+  tint_cmd_fuzz_wgsl_fuzz
+  tint_lang_core
+  tint_lang_core_constant
+  tint_lang_core_ir
+  tint_lang_core_type
+  tint_lang_wgsl
+  tint_lang_wgsl_ast
+  tint_lang_wgsl_helpers
+  tint_lang_wgsl_program
+  tint_lang_wgsl_reader_lower
+  tint_lang_wgsl_reader_parser
+  tint_lang_wgsl_reader_program_to_ir
+  tint_lang_wgsl_resolver
+  tint_lang_wgsl_sem
+  tint_lang_wgsl_writer
+  tint_lang_wgsl_writer_ir_to_program
+  tint_lang_wgsl_writer_raise
+  tint_utils_containers
+  tint_utils_diagnostic
+  tint_utils_ice
+  tint_utils_id
+  tint_utils_macros
+  tint_utils_math
+  tint_utils_memory
+  tint_utils_reflection
+  tint_utils_result
+  tint_utils_rtti
+  tint_utils_symbol
+  tint_utils_text
+  tint_utils_traits
+)
