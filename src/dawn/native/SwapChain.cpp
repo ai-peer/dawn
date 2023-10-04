4d49fe5a0a98ab53339dcf2d92ab87b9aaded0e0
Le Hoang Quyen <lehoangquyen@chromium.org>
2023-10-04 08:07:45 +0800

    Pass Aspect to TextureBase::GetSize for multiplanar sizes.
    Bug: dawn:2099
    Change-Id: Ie25ee6686f0ccfcfd33116c1d6316d671c91a36c

--- src/dawn/native/SwapChain.cpp
+++ src/dawn/native/SwapChain.cpp
@@ -200,8 +200,8 @@ ResultOrError<Ref<TextureBase>> SwapChainBase::GetCurrentTexture() {
     DAWN_ASSERT(mCurrentTexture->GetFormat().format == mFormat);
     DAWN_ASSERT(IsSubset(mUsage, mCurrentTexture->GetUsage()));
     DAWN_ASSERT(mCurrentTexture->GetDimension() == wgpu::TextureDimension::e2D);
-    DAWN_ASSERT(mCurrentTexture->GetWidth() == mWidth);
-    DAWN_ASSERT(mCurrentTexture->GetHeight() == mHeight);
+    DAWN_ASSERT(mCurrentTexture->GetBaseSize().width == mWidth);
+    DAWN_ASSERT(mCurrentTexture->GetBaseSize().height == mHeight);
     DAWN_ASSERT(mCurrentTexture->GetNumMipLevels() == 1);
     DAWN_ASSERT(mCurrentTexture->GetArrayLayers() == 1);
 
