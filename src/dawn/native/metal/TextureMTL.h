4889c88388e17278c58ff0ceec5b1b62ece563c6
Le Hoang Quyen <lehoangquyen@chromium.org>
2023-10-04 21:16:24 +0800

    Metal: Add MultiPlanarFormatExtendedUsages support.
    This feature allows Metal backend to:
    - Create Multiplanar textures directly without importing external
    IOSurface. This is currently done by creating internal IOSurface. This
    is just for conforming to the feature spec. No realworld use cases
    really need it yet.
    - Copy data between buffers and multiplanar textures. Note that copying
    data between multiplanar textures are not yet allowed.
    
    Bug: dawn:2109
    Bug: dawn:1853
    Change-Id: Ib0c53b329a0bb6b89f8e39320ed266f302dc7561

--- src/dawn/native/metal/TextureMTL.h
+++ src/dawn/native/metal/TextureMTL.h
@@ -23,6 +23,7 @@
 
 #include "dawn/common/CoreFoundationRef.h"
 #include "dawn/common/NSRef.h"
+#include "dawn/common/StackContainer.h"
 #include "dawn/native/DawnNative.h"
 #include "dawn/native/MetalBackend.h"
 
@@ -55,7 +56,7 @@ class Texture final : public TextureBase {
 
     Texture(DeviceBase* device, const TextureDescriptor* descriptor);
 
-    id<MTLTexture> GetMTLTexture() const;
+    id<MTLTexture> GetMTLTexture(Aspect aspect) const;
     IOSurfaceRef GetIOSurface();
     NSPRef<id<MTLTexture>> CreateFormatView(wgpu::TextureFormat format);
 
@@ -84,11 +85,14 @@ class Texture final : public TextureBase {
     void DestroyImpl() override;
     void SetLabelImpl() override;
 
+    MaybeError ClearTextureOnCreation(CommandRecordingContext* commandContext,
+                                      const SubresourceRange& range);
     MaybeError ClearTexture(CommandRecordingContext* commandContext,
                             const SubresourceRange& range,
                             TextureBase::ClearValue clearValue);
 
-    NSPRef<id<MTLTexture>> mMtlTexture;
+    StackVector<NSPRef<id<MTLTexture>>, kMaxPlanesPerFormat> mMtlPlaneTextures;
+    MTLPixelFormat mMtlFormat = MTLPixelFormatInvalid;
 
     MTLTextureUsage mMtlUsage;
     CFRef<IOSurfaceRef> mIOSurface = nullptr;
