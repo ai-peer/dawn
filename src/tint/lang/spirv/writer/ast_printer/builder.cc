c102866bc6f68d51759482c612818485d124c503
Ben Clayton <bclayton@google.com>
2023-10-06 13:54:18 +0100

    WIP: Recursionless type resolving
    Change-Id: I4ad9283f33b85a58cc39e4887f699ef34f0d3617

--- src/tint/lang/spirv/writer/ast_printer/builder.cc
+++ src/tint/lang/spirv/writer/ast_printer/builder.cc
@@ -1223,29 +1223,9 @@ uint32_t Builder::GenerateConstructorExpression(const ast::Variable* var,
     return 0;
 }
 
-bool Builder::IsConstructorConst(const ast::Expression* expr) {
-    bool is_const = true;
-    ast::TraverseExpressions(expr, [&](const ast::Expression* e) {
-        if (e->Is<ast::LiteralExpression>()) {
-            return ast::TraverseAction::Descend;
-        }
-        if (auto* ce = e->As<ast::CallExpression>()) {
-            auto* sem = builder_.Sem().Get(ce);
-            if (sem->Is<sem::Materialize>()) {
-                // Materialize can only occur on compile time expressions, so this sub-tree must be
-                // constant.
-                return ast::TraverseAction::Skip;
-            }
-            auto* call = sem->As<sem::Call>();
-            if (call->Target()->Is<sem::ValueConstructor>()) {
-                return ast::TraverseAction::Descend;
-            }
-        }
-
-        is_const = false;
-        return ast::TraverseAction::Stop;
-    });
-    return is_const;
+bool Builder::IsConstructorConst(const ast::CallExpression* expr) {
+    auto* sem = builder_.Sem().Get(expr);
+    return sem->Stage() == core::EvaluationStage::kConstant;
 }
 
 uint32_t Builder::GenerateValueConstructorOrConversion(const sem::Call* call,
