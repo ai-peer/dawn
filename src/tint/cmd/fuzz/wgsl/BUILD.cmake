72332f7e4b6d76030c0de9ccd8e33cf577d3d7a5
Ben Clayton <bclayton@google.com>
2023-10-09 10:47:01 +0100

    [tint][fuzzers] Port ast_printer_fuzz to tint_wgsl_fuzzer
    Change-Id: I47b49494ea6c4f27c72ed4dda35b679b78a0a3e0

--- src/tint/cmd/fuzz/wgsl/BUILD.cmake
+++ src/tint/cmd/fuzz/wgsl/BUILD.cmake
@@ -40,6 +40,7 @@ tint_target_add_dependencies(tint_cmd_fuzz_wgsl_fuzz_cmd fuzz_cmd
   tint_lang_wgsl_program
   tint_lang_wgsl_program_fuzz
   tint_lang_wgsl_sem
+  tint_lang_wgsl_writer_ast_printer_fuzz
   tint_lang_wgsl_fuzz
   tint_utils_containers
   tint_utils_diagnostic
