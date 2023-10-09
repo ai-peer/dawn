18e97a5c36bf72d9a9d3c8886b9426a12a57ba4b
Ben Clayton <bclayton@google.com>
2023-10-09 14:15:43 +0100

    [tint][fuzzers] Add --concurrent flag to tint_wgsl_fuzzer
    Checks that all the WGSL fuzzers can be run concurrently
    
    Change-Id: Iad7a95918954848ac7ffd6cee17c5538cf5c2cd3

--- src/tint/cmd/fuzz/wgsl/BUILD.cmake
+++ src/tint/cmd/fuzz/wgsl/BUILD.cmake
@@ -54,6 +54,7 @@ tint_target_add_dependencies(tint_cmd_fuzz_wgsl_fuzz_cmd fuzz_cmd
   tint_lang_wgsl_program_fuzz
   tint_lang_wgsl_sem
   tint_lang_wgsl_fuzz
+  tint_utils_cli
   tint_utils_containers
   tint_utils_diagnostic
   tint_utils_ice
@@ -63,6 +64,7 @@ tint_target_add_dependencies(tint_cmd_fuzz_wgsl_fuzz_cmd fuzz_cmd
   tint_utils_memory
   tint_utils_result
   tint_utils_rtti
+  tint_utils_strconv
   tint_utils_symbol
   tint_utils_text
   tint_utils_traits
@@ -118,6 +120,10 @@ tint_target_add_dependencies(tint_cmd_fuzz_wgsl_fuzz fuzz
   tint_utils_symbol
   tint_utils_text
   tint_utils_traits
+)
+
+tint_target_add_external_dependencies(tint_cmd_fuzz_wgsl_fuzz fuzz
+  "thread"
 )
 
 if(TINT_BUILD_WGSL_READER)
