3fc2d36ad9d65c172a32e8da7b25c24033689e8a
Austin Eng <enga@chromium.org>
2023-10-16 12:08:45 -0700

    Track OnSubmittedOnWorkDoneF futures using execution serials
    This is needed on Vulkan because Vulkan cannot always make
    system events from VkFences. Doing it on other backends
    helps as well as an optimization. We don't need to create
    a SystemEventPipe for *every* future, and instead create
    them on demand when waiting is necessary.
    
    Bug: dawn:2051
    Fixed: dawn:2064
    Bug: dawn:2150
    Change-Id: I939d94c20b70c47013130d477029080c5240b804

--- src/dawn/native/Device.h
+++ src/dawn/native/Device.h
@@ -446,9 +446,9 @@ class DeviceBase : public RefCountedWithExternalCount {
     virtual void AppendDebugLayerMessages(ErrorData* error) {}
     virtual void AppendDeviceLostMessage(ErrorData* error) {}
 
-    [[nodiscard]] virtual bool WaitAnyImpl(size_t futureCount,
-                                           TrackedFutureWaitInfo* futures,
-                                           Nanoseconds timeout);
+    virtual ResultOrError<bool> WaitAnyImpl(size_t futureCount,
+                                            TrackedFutureWaitInfo* futures,
+                                            Nanoseconds timeout);
 
     // It is guaranteed that the wrapped mutex will outlive the Device (if the Device is deleted
     // before the AutoLockAndHoldRef).
