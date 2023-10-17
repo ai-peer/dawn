6ac7154db26d24d8f8504f20fe8b1b1c10798f6e
François Beaufort <beaufort.francois@gmail.com>
2023-10-17 17:08:58 +0200

    Remove timestamp-query feature
    Change-Id: I34e6138d9f43b372d3236246142c46c1b4031bc6

--- src/dawn/native/d3d12/PhysicalDeviceD3D12.cpp
+++ src/dawn/native/d3d12/PhysicalDeviceD3D12.cpp
@@ -131,7 +131,6 @@ void PhysicalDevice::InitializeSupportedFeaturesImpl() {
 
     if (AreTimestampQueriesSupported()) {
         EnableFeature(Feature::TimestampQuery);
-        EnableFeature(Feature::TimestampQueryInsidePasses);
         EnableFeature(Feature::ChromiumExperimentalTimestampQueryInsidePasses);
     }
     EnableFeature(Feature::PipelineStatisticsQuery);
