6a283502e7a436c687ddb11435a6b48175afe999
Ben Clayton <bclayton@google.com>
2023-10-20 21:58:30 +0000

    [tint] Use TINT_SWITCH_MUST_MATCH_CASE
    Change-Id: I6937282b5252e6034b0d2f2a6bc78f0d565f59da

--- src/tint/lang/core/ir/transform/zero_init_workgroup_memory.cc
+++ src/tint/lang/core/ir/transform/zero_init_workgroup_memory.cc
@@ -262,8 +262,8 @@ struct State {
                     new_indices.Push(member->Index());
                     PrepareStores(var, member->Type(), iteration_count, new_indices, stores);
                 }
-            },
-            [&](Default) { TINT_UNREACHABLE(); });
+            },  //
+            TINT_SWITCH_MUST_MATCH_CASE);
     }
 
     /// Get or inject an entry point builtin for the local invocation index.
