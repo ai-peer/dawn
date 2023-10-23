1b4e38af4ead7103b134a344a8122b21dc6550ba
Austin Eng <enga@chromium.org>
2023-10-23 15:11:33 -0700

    Factor SystemHandle out of SystemEventPrimitive
    An OS-independent handle abstraction is useful for
    importing/exporting fence/texture payloads as well.
    
    Bug: dawn:1745
    Change-Id: I40a3130f1034a7fa6ae15b1d6bd2845c09354416

---
+++ src/dawn/native/SystemHandle.cpp
@@ -0,0 +1,211 @@
+// Copyright 2023 The Dawn & Tint Authors
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "src/dawn/native/SystemHandle.h"
+
+#include <utility>
+
+#include "dawn/common/Log.h"
+
+#if DAWN_PLATFORM_IS(WINDOWS)
+#include "dawn/common/windows_with_undefs.h"
+#elif DAWN_PLATFORM_IS(FUCHSIA)
+#include <zircon/syscalls.h>
+#elif DAWN_PLATFORM_IS(POSIX)
+#include <unistd.h>
+#endif
+
+namespace dawn::native {
+
+namespace {
+
+template <typename Handle>
+inline Handle kInvalidHandle;
+
+// Default implementation of IsHandleValid which returns false.
+// Specialized per-platform.
+template <typename Handle>
+inline bool IsHandleValid(Handle handle) {
+    return false;
+}
+
+// Default implementation of DuplicateHandle which generates an error.
+// Specialized per-platform.
+template <typename Handle>
+inline ResultOrError<Handle> DuplicateHandle(Handle handle) {
+    return DAWN_UNIMPLEMENTED_ERROR("Not implemented.");
+}
+
+// Default implementation of CloseHandle which generates an error.
+// Specialized per-platform.
+template <typename Handle>
+inline MaybeError CloseHandle(Handle handle) {
+    return DAWN_UNIMPLEMENTED_ERROR("Not implemented.");
+}
+
+#if DAWN_PLATFORM_IS(WINDOWS)
+
+template <>
+constexpr inline HANDLE kInvalidHandle<HANDLE> = nullptr;
+
+template <>
+inline bool IsHandleValid(HANDLE handle) {
+    return handle != nullptr;
+}
+
+template <>
+inline ResultOrError<HANDLE> DuplicateHandle(HANDLE handle) {
+    HANDLE currentProcess = ::GetCurrentProcess();
+    HANDLE outHandle;
+    if (DAWN_UNLIKELY(!::DuplicateHandle(currentProcess, handle, currentProcess, &outHandle, 0,
+                                         FALSE, DUPLICATE_SAME_ACCESS))) {
+        return DAWN_INTERNAL_ERROR("DuplicateHandle failed");
+    }
+    return outHandle;
+}
+
+template <>
+inline MaybeError CloseHandle(HANDLE handle) {
+    if (DAWN_LIKELY(::CloseHandle(handle))) {
+        return {};
+    }
+    return DAWN_INTERNAL_ERROR("CloseHandle failed");
+}
+
+#elif DAWN_PLATFORM_IS(FUCHSIA)
+
+template <>
+constexpr inline zx_handle_t kInvalidHandle<zx_handle_t> = 0;
+
+template <>
+inline bool IsHandleValid(zx_handle_t handle) {
+    return handle > 0;
+}
+
+template <>
+inline ResultOrError<zx_handle_t> DuplicateHandle(zx_handle_t handle) {
+    zx_handle_t outHandle = ZX_HANDLE_INVALID;
+    zx_status_t status = zx_handle_duplicate(descriptor->handle, ZX_RIGHT_SAME_RIGHTS, &outHandle);
+    if (DAWN_UNLIKELY(status != ZK_OK)) {
+        return DAWN_INTERNAL_ERROR("zx_handle_duplicate failed");
+    }
+    return outHandle;
+}
+
+template <>
+inline bool CloseHandle(zx_handle_t handle) {
+    zx_status_t status = zx_handle_close(handle);
+    if (DAWN_UNLIKELY(status != ZX_OK)) {
+        return DAWN_INTERNAL_ERROR("zx_handle_close failed");
+    }
+    return {};
+}
+
+#elif DAWN_PLATFORM_IS(POSIX)
+
+template <>
+constexpr inline int kInvalidHandle<int> = -1;
+
+template <>
+inline bool IsHandleValid(int handle) {
+    return handle >= 0;
+}
+
+template <>
+inline ResultOrError<int> DuplicateHandle(int handle) {
+    int outHandle = dup(handle);
+    if (DAWN_UNLIKELY(outHandle < 0)) {
+        return DAWN_INTERNAL_ERROR("dup failed");
+    }
+    return outHandle;
+}
+
+template <>
+inline MaybeError CloseHandle(int handle) {
+    if (DAWN_UNLIKELY(close(handle) < 0)) {
+        return DAWN_INTERNAL_ERROR("close failed");
+    }
+    return {};
+}
+
+#endif
+
+}  // anonymous namespace
+
+SystemHandle::SystemHandle() : mHandle(kInvalidHandle<Handle>) {}
+SystemHandle::SystemHandle(Handle handle) : mHandle(handle) {}
+
+bool SystemHandle::IsValid() const {
+    return IsHandleValid(mHandle);
+}
+
+SystemHandle::SystemHandle(SystemHandle&& rhs) {
+    mHandle = rhs.mHandle;
+    rhs.mHandle = kInvalidHandle<Handle>;
+}
+
+SystemHandle& SystemHandle::operator=(SystemHandle&& rhs) {
+    if (this != &rhs) {
+        std::swap(mHandle, rhs.mHandle);
+    }
+    return *this;
+}
+
+SystemHandle::Handle SystemHandle::Get() const {
+    return mHandle;
+}
+
+SystemHandle::Handle SystemHandle::Detach() {
+    Handle handle = mHandle;
+    mHandle = kInvalidHandle<Handle>;
+    return handle;
+}
+
+ResultOrError<SystemHandle> SystemHandle::Duplicate() const {
+    Handle handle;
+    DAWN_TRY_ASSIGN(handle, DuplicateHandle(mHandle));
+    return SystemHandle(handle);
+}
+
+void SystemHandle::Close() {
+    DAWN_ASSERT(IsValid());
+    auto result = CloseHandle(mHandle);
+    // Still invalidate the handle if Close failed.
+    // If Close failed, the handle surely was invalid already.
+    mHandle = kInvalidHandle<Handle>;
+    if (DAWN_UNLIKELY(result.IsError())) {
+        dawn::ErrorLog() << result.AcquireError()->GetFormattedMessage();
+    }
+}
+
+SystemHandle::~SystemHandle() {
+    if (IsValid()) {
+        Close();
+    }
+}
+
+}  // namespace dawn::native
