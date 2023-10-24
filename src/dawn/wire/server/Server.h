916967df27f2bb0d0a513363444497ef5e586435
Loko Kung <lokokung@google.com>
2023-10-24 11:20:19 -0700

    [WGPUFuture] Implement and use Futures in wire for requestDevice.
    Bug: dawn:1987, dawn:2060
    Change-Id: I6d415878f6d3738722dbbfe1f98c0dc0857cff5a

--- src/dawn/wire/server/Server.h
+++ src/dawn/wire/server/Server.h
@@ -156,7 +156,7 @@ struct RequestDeviceUserdata : CallbackUserdata {
     using CallbackUserdata::CallbackUserdata;
 
     ObjectHandle adapter;
-    uint64_t requestSerial;
+    WGPUFuture future;
     ObjectId deviceObjectId;
 };
 
