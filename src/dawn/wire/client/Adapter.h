3488360cad47dcc8305b46690d616c7eb0c5f627
Loko Kung <lokokung@google.com>
2023-10-24 11:20:19 -0700

    [WGPUFuture] Implement and use Futures in wire for requestDevice.
    Bug: dawn:1987, dawn:2060
    Change-Id: I6d415878f6d3738722dbbfe1f98c0dc0857cff5a

--- src/dawn/wire/client/Adapter.h
+++ src/dawn/wire/client/Adapter.h
@@ -41,9 +41,6 @@ namespace dawn::wire::client {
 class Adapter final : public ObjectBase {
   public:
     using ObjectBase::ObjectBase;
-    ~Adapter() override;
-
-    void CancelCallbacksForDisconnect() override;
 
     bool GetLimits(WGPUSupportedLimits* limits) const;
     bool HasFeature(WGPUFeatureName feature) const;
@@ -55,8 +52,10 @@ class Adapter final : public ObjectBase {
     void RequestDevice(const WGPUDeviceDescriptor* descriptor,
                        WGPURequestDeviceCallback callback,
                        void* userdata);
+    WGPUFuture RequestDeviceF(const WGPUDeviceDescriptor* descriptor,
+                              const WGPURequestDeviceCallbackInfo& callbackInfo);
 
-    bool OnRequestDeviceCallback(uint64_t requestSerial,
+    bool OnRequestDeviceCallback(WGPUFuture future,
                                  WGPURequestDeviceStatus status,
                                  const char* message,
                                  const WGPUSupportedLimits* limits,
