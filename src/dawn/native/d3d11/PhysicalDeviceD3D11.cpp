cba961f9e46f55bb1d57e8d5101b6eb68c79efdf
Zhaoming Jiang <zhaoming.jiang@intel.com>
2023-10-26 13:24:08 +0800

    Dawn: Make ValidateFeatureSupportedWithToggles generate no error
    This CL make PhysicalDevice::ValidateFeatureSupportedWithToggles returns
    a structure to indicate if the validation failed and the failure message,
    instead of creating an error that may by caught when debugging.
    
    Issue: dawn:2176
    Change-Id: Ifde957a8d23553ef4a19079d1bc5d3e19a58bae3

--- src/dawn/native/d3d11/PhysicalDeviceD3D11.cpp
+++ src/dawn/native/d3d11/PhysicalDeviceD3D11.cpp
@@ -227,7 +227,7 @@ MaybeError PhysicalDevice::InitializeSupportedLimitsImpl(CombinedLimits* limits)
     return {};
 }
 
-MaybeError PhysicalDevice::ValidateFeatureSupportedWithTogglesImpl(
+FeatureValidationResult PhysicalDevice::ValidateFeatureSupportedWithTogglesImpl(
     wgpu::FeatureName feature,
     const TogglesState& toggles) const {
     return {};
--- src/dawn/native/metal/BackendMTL.mm
+++ src/dawn/native/metal/BackendMTL.mm
@@ -818,8 +818,9 @@ class PhysicalDevice : public PhysicalDeviceBase {
         return {};
     }
 
-    MaybeError ValidateFeatureSupportedWithTogglesImpl(wgpu::FeatureName feature,
-                                                       const TogglesState& toggles) const override {
+    FeatureValidationResult ValidateFeatureSupportedWithTogglesImpl(
+        wgpu::FeatureName feature,
+        const TogglesState& toggles) const override {
         return {};
     }
 
