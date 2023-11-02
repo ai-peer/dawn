4f35a87d1af0964118a07112176cadd36a8bb7d6
Loko Kung <lokokung@google.com>
2023-11-01 17:48:37 -0700

    Clear all callbacks when the Instance is dropped.
    - Fixes fuzzer issue in the bug. The userdata was not cleaned up
      because an async function (MapAsync) was called on a destroyed
      device, and the associated callback was never called in the
      fuzzer(s).
    - Modernizes the code a bit to use MutexProtected.
    - Updates MutexProtected to work with const-ness.
    
    Bug: chromium:1497701
    Change-Id: I15617bd00e49e6c2d1c5c2805e4335438b9ce0e2

--- src/dawn/native/Instance.cpp
+++ src/dawn/native/Instance.cpp
@@ -152,6 +152,22 @@ InstanceBase::InstanceBase(const TogglesState& instanceToggles) : mToggles(insta
 
 InstanceBase::~InstanceBase() = default;
 
+void InstanceBase::DeleteThis() {
+    // Flush all remaining callback tasks on all devices and on the instance.
+    mDevicesList.Use([&](auto deviceList) {
+        for (auto device : *deviceList) {
+            device->GetCallbackTaskManager()->HandleShutDown();
+            do {
+                device->GetCallbackTaskManager()->Flush();
+            } while (!device->GetCallbackTaskManager()->IsEmpty());
+        }
+    });
+    mCallbackTaskManager->HandleShutDown();
+    do {
+        mCallbackTaskManager->Flush();
+    } while (!mCallbackTaskManager->IsEmpty());
+}
+
 void InstanceBase::WillDropLastExternalRef() {
     // InstanceBase uses RefCountedWithExternalCount to break refcycles.
 
@@ -451,28 +467,24 @@ BlobCache* InstanceBase::GetBlobCache(bool enabled) {
 }
 
 uint64_t InstanceBase::GetDeviceCountForTesting() const {
-    std::lock_guard<std::mutex> lg(mDevicesListMutex);
-    return mDevicesList.size();
+    return mDevicesList.Use([](auto deviceList) { return deviceList->size(); });
 }
 
 void InstanceBase::AddDevice(DeviceBase* device) {
-    std::lock_guard<std::mutex> lg(mDevicesListMutex);
-    mDevicesList.insert(device);
+    mDevicesList.Use([&](auto deviceList) { deviceList->insert(device); });
 }
 
 void InstanceBase::RemoveDevice(DeviceBase* device) {
-    std::lock_guard<std::mutex> lg(mDevicesListMutex);
-    mDevicesList.erase(device);
+    mDevicesList.Use([&](auto deviceList) { deviceList->erase(device); });
 }
 
 void InstanceBase::APIProcessEvents() {
     std::vector<Ref<DeviceBase>> devices;
-    {
-        std::lock_guard<std::mutex> lg(mDevicesListMutex);
-        for (auto device : mDevicesList) {
+    mDevicesList.Use([&](auto deviceList) {
+        for (auto device : *deviceList) {
             devices.push_back(device);
         }
-    }
+    });
 
     for (auto device : devices) {
         device->APITick();
