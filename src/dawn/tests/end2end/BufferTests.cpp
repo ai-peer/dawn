688b8ce68ea0f442c82b69956355a4b4806da83b
Austin Eng <enga@chromium.org>
2023-11-02 20:24:23 -0700

    Implement queue-related futures on Vulkan
    Bug: dawn:2056
    Change-Id: Ida58db0c2a9d911e58e13e927252090b3e24e4ef

--- src/dawn/tests/end2end/BufferTests.cpp
+++ src/dawn/tests/end2end/BufferTests.cpp
@@ -664,7 +664,7 @@ DAWN_INSTANTIATE_PREFIXED_TEST_P(Legacy,
 
 DAWN_INSTANTIATE_PREFIXED_TEST_P(Future,
                                  BufferMappingTests,
-                                 {MetalBackend()},
+                                 {MetalBackend(), VulkanBackend()},
                                  std::initializer_list<std::optional<wgpu::CallbackMode>>{
                                      std::nullopt, wgpu::CallbackMode::WaitAnyOnly,
                                      wgpu::CallbackMode::AllowProcessEvents,
@@ -876,7 +876,7 @@ DAWN_INSTANTIATE_PREFIXED_TEST_P(Legacy,
 
 DAWN_INSTANTIATE_PREFIXED_TEST_P(Future,
                                  BufferMappingCallbackTests,
-                                 {MetalBackend()},
+                                 {MetalBackend(), VulkanBackend()},
                                  std::initializer_list<std::optional<wgpu::CallbackMode>>{
                                      std::nullopt, wgpu::CallbackMode::WaitAnyOnly,
                                      wgpu::CallbackMode::AllowProcessEvents,
