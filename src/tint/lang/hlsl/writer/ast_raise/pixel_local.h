d307b2962ca1386e55d0a8e1f49ffcd73a566122
Jiawei Shao <jiawei.shao@intel.com>
2023-11-07 09:23:22 +0800

    Tint/HLSL: Implement AST transform for pixel local variables
    This patch implements the AST transform "pixel_local" for pixel local
    variables on Tint HLSL backends.
    
    Below are the steps that the transform "pixel_local" do:
    1. Translate the var<pixel_local> varaible into a var<private> and a
       bunch of read-write storage textures with an internal attribute
       "rasterizer ordered view".
    2. Call function "load_from_pixel_local_storage()" before the original
       main function to load the data from the read-write storage textures
       added in step 1 into the var<private> variable.
    3. Call function "store_into_pixel_local_storage()" after the original
       main function to store the data from the var<private> variable into
       the read-write storage textures added in step 1.
    
    Bug: tint:2083
    Test: tint_unittests
    Change-Id: I7d98a8c65c0bdfe2febdbbfe4cb075dc34651a37

--- src/tint/lang/hlsl/writer/ast_raise/pixel_local.h
+++ src/tint/lang/hlsl/writer/ast_raise/pixel_local.h
@@ -25,27 +25,31 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef SRC_TINT_LANG_MSL_WRITER_AST_RAISE_PIXEL_LOCAL_H_
-#define SRC_TINT_LANG_MSL_WRITER_AST_RAISE_PIXEL_LOCAL_H_
+#ifndef SRC_TINT_LANG_HLSL_WRITER_AST_RAISE_PIXEL_LOCAL_H_
+#define SRC_TINT_LANG_HLSL_WRITER_AST_RAISE_PIXEL_LOCAL_H_
 
 #include <string>
 
+#include "src/tint/lang/core/texel_format.h"
 #include "src/tint/lang/wgsl/ast/internal_attribute.h"
 #include "src/tint/lang/wgsl/ast/transform/transform.h"
-#include "src/tint/utils/containers/hashmap.h"
 
-namespace tint::msl::writer {
+namespace tint::hlsl::writer {
 
 /// PixelLocal transforms module-scope `var<pixel_local>`s and fragment entry point functions that
 /// use them:
-/// * `var<pixel_local>` will be transformed to `var<private>`.
+/// * `var<pixel_local>` will be transformed to `var<private>`, a bunch of read-write storage
+///   textures with a special `RasterizerOrderedView` attribute, a function to read the data from
+///   thes read-write storage textures into the `var<private>` (ROV Reader), and a function to write
+///   the data from the `var<private>` into the read-write storage textures (ROV Writer).
 /// * The entry point function will be wrapped with another function ('outer') that calls the
 ///  'inner' function.
-/// * The outer function will have an additional parameter of the pixel local struct type, which is
-///   copied to the module-scope var before calling the 'inner' function.
-/// * The outer function will have a new struct return type which holds both the pixel local members
-///   and the returned value(s) of the 'inner' function.
+/// * The outer function will always have `@builtin(position)` in its input parameters.
+/// * The outer function will call the ROV reader before the original entry point function, and call
+///   the ROV writer after the original entry point function.
 /// @note PixelLocal requires that the SingleEntryPoint transform has already been run
+/// TODO(tint:2083): Optimize this in the future by inserting the load as late as possible and the
+/// store as early as possible.
 class PixelLocal final : public Castable<PixelLocal, ast::transform::Transform> {
   public:
     /// Transform configuration options
@@ -59,22 +63,28 @@ class PixelLocal final : public Castable<PixelLocal, ast::transform::Transform> 
         /// Destructor
         ~Config() override;
 
-        /// Index of pixel_local structure member index to attachment index
-        Hashmap<uint32_t, uint32_t, 8> attachments;
+        /// Index of pixel_local structure member index to ROV register
+        Hashmap<uint32_t, uint32_t, 8> pls_member_to_rov_reg;
+
+        /// Index of pixel_local structure member index to ROV format
+        Hashmap<uint32_t, core::TexelFormat, 8> pls_member_to_rov_format;
+
+        /// Group index of all ROVs
+        uint32_t rov_group_index = 0;
     };
 
-    /// Attachment is an InternalAttribute that's used to decorate a pixel local attachment
-    /// parameter, return value or structure member.
-    class Attachment final : public Castable<Attachment, ast::InternalAttribute> {
+    /// RasterizerOrderedView is an InternalAttribute that's used to decorate a read-write storage
+    /// texture object.
+    class RasterizerOrderedView final
+        : public Castable<RasterizerOrderedView, ast::InternalAttribute> {
       public:
         /// Constructor
         /// @param pid the identifier of the program that owns this node
         /// @param nid the unique node identifier
-        /// @param idx the attachment index
-        Attachment(GenerationID pid, ast::NodeID nid, uint32_t idx);
+        RasterizerOrderedView(GenerationID pid, ast::NodeID nid);
 
         /// Destructor
-        ~Attachment() override;
+        ~RasterizerOrderedView() override;
 
         /// @return a short description of the internal attribute which will be
         /// displayed as `@internal(<name>)`
@@ -83,10 +93,7 @@ class PixelLocal final : public Castable<PixelLocal, ast::transform::Transform> 
         /// Performs a deep clone of this object using the program::CloneContext `ctx`.
         /// @param ctx the clone context
         /// @return the newly cloned object
-        const Attachment* Clone(ast::CloneContext& ctx) const override;
-
-        /// The attachment index
-        const uint32_t index;
+        const RasterizerOrderedView* Clone(ast::CloneContext& ctx) const override;
     };
 
     /// Constructor
@@ -104,6 +111,6 @@ class PixelLocal final : public Castable<PixelLocal, ast::transform::Transform> 
     struct State;
 };
 
-}  // namespace tint::msl::writer
+}  // namespace tint::hlsl::writer
 
-#endif  // SRC_TINT_LANG_MSL_WRITER_AST_RAISE_PIXEL_LOCAL_H_
+#endif  // SRC_TINT_LANG_HLSL_WRITER_AST_RAISE_PIXEL_LOCAL_H_
