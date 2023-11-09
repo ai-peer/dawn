088e3ec55ed8cff3624cc2a14f21479d4bc04747
Peng Huang <penghuang@chromium.org>
2023-11-09 15:49:35 -0500

    d3d11: check debug layer for shared d3d11 device
    Bug: dawn:1938
    Change-Id: Ie504c722717783473e235ba1db7d5e65c3e60abc

--- src/dawn/native/d3d11/PhysicalDeviceD3D11.h
+++ src/dawn/native/d3d11/PhysicalDeviceD3D11.h
@@ -50,7 +50,7 @@ class PhysicalDevice : public d3d::PhysicalDevice {
     bool SupportsFeatureLevel(FeatureLevel featureLevel) const override;
 
     const DeviceInfo& GetDeviceInfo() const;
-    ResultOrError<ComPtr<ID3D11Device>> CreateD3D11Device();
+    ResultOrError<ComPtr<ID3D11Device>> CreateD3D11Device(bool needsValidation);
 
     uint32_t GetUAVSlotCount() const { return mUAVSlotCount; }
     bool IsSharedD3D11Device() const { return mIsSharedD3D11Device; }
