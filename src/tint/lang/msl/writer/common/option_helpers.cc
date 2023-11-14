c24211803daaff2511c90ef12a326399b861e76c
dan sinclair <dsinclair@chromium.org>
2023-11-14 15:19:12 -0500

    Rename option_builder to option_helpers.
    As requested in review dawn-review.googlesource.com/c/dawn/+/156761
    
    Change-Id: Iccd787191602fd2c1d95f3f01ed1384ebefeb904

--- src/tint/lang/msl/writer/common/option_helpers.cc
+++ src/tint/lang/msl/writer/common/option_helpers.cc
@@ -25,7 +25,7 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include "src/tint/lang/msl/writer/common/option_builder.h"
+#include "src/tint/lang/msl/writer/common/option_helpers.h"
 
 #include "src/tint/utils/containers/hashset.h"
 
