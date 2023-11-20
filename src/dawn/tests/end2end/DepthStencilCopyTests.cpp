7ce709eaa76d1df4ddce3721b0961b2581fd270f
Stephen White <senorblanco@chromium.org>
2023-11-20 12:12:43 -0500

    OpenGL: use blit for stencil-only texture uploads.
    Enable the UseBlitForBufferToStencilTextureCopy workaround for
    stencil-only texture uploads on OpenGL.
    
    Bug: dawn:2710
    Change-Id: I652c5dcc6fed482a26143cec52326b5a9ba6f71c

--- src/dawn/tests/end2end/DepthStencilCopyTests.cpp
+++ src/dawn/tests/end2end/DepthStencilCopyTests.cpp
@@ -962,9 +962,9 @@ class StencilCopyTests : public DepthStencilCopyTests {
     }
 
     void DoCopyToStencilTest(uint32_t bufferCopyOffset) {
-        // Copies to a single aspect are unsupported on OpenGL.
-        DAWN_TEST_UNSUPPORTED_IF(IsOpenGL());
-        DAWN_TEST_UNSUPPORTED_IF(IsOpenGLES());
+        // TODO(crbug.com/dawn/2202): Failing on native GLES drivers due to lack of
+        // glDrawArraysInstancedBaseInstance.
+        DAWN_TEST_UNSUPPORTED_IF(IsOpenGLES() && !IsANGLE());
 
         // Create a stencil texture
         constexpr uint32_t kWidth = 4;
