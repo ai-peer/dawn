9ba9894753bf77aa607205e6506c2ae89b8166eb
Gregg Tavares <gman@chromium.org>
2023-11-28 16:06:53 -0800

    Compat: Validate depthOrArrayLayers and textureBindingViewDimension
    Generate a validation error if:
    
    * textureBindingViewDimension is "2d" and depthOrArrayLayers > 1
    * textureBindingViewDimension is cube and depthOrArrayLayers != 6
    
    Bug: dawn:2131
    Change-Id: I8798e49e6e0ee506f6ebc8561792c4d8bbc0ccf7

--- src/dawn/native/Texture.cpp
+++ src/dawn/native/Texture.cpp
@@ -133,6 +133,23 @@ bool IsTextureViewDimensionCompatibleWithTextureDimension(
     DAWN_UNREACHABLE();
 }
 
+MaybeError ValidateDepthOrArrayLayersIsCompatibleWithTextureBindingViewDimension(
+    wgpu::TextureViewDimension textureBindingViewDimension,
+    uint32_t depthOrArrayLayers) {
+    switch (textureBindingViewDimension) {
+        case wgpu::TextureViewDimension::e2D:
+            return DAWN_VALIDATION_ERROR(
+                "A resolved TextureViewDimension of e2D is only "
+                "compatible with depthOrArrayLayers equals 1.");
+        case wgpu::TextureViewDimension::Cube:
+            return DAWN_VALIDATION_ERROR(
+                "A resolved TextureViewDimension of Cube is only "
+                "compatible with depthOrArrayLayers equals 6.");
+        default:
+            return {};
+    }
+}
+
 bool IsArrayLayerValidForTextureViewDimension(wgpu::TextureViewDimension textureViewDimension,
                                               uint32_t textureViewArrayLayer) {
     switch (textureViewDimension) {
@@ -543,6 +560,9 @@ MaybeError ValidateTextureDescriptor(
                                                                   descriptor->dimension),
             "The textureBindingViewDimension (%s) is not compatible with the dimension (%s)",
             textureBindingViewDimension, descriptor->dimension);
+
+        DAWN_TRY(ValidateDepthOrArrayLayersIsCompatibleWithTextureBindingViewDimension(
+            textureBindingViewDimension, descriptor->size.depthOrArrayLayers));
     }
     DAWN_TRY(ValidateSampleCount(descriptor, usage, format));
 
