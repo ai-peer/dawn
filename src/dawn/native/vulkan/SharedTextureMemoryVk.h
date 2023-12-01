035f4b7eceda88963930b8b376b701f2732b42d9
Loko Kung <lokokung@google.com>
2023-11-30 16:37:21 -0800

    More uses of utilities...
    Bug: dawn:1955
    Change-Id: I1cc97cca763b9667d7313b85c2efefbdf4bd9206

--- src/dawn/native/vulkan/SharedTextureMemoryVk.h
+++ src/dawn/native/vulkan/SharedTextureMemoryVk.h
@@ -56,7 +56,8 @@ class SharedTextureMemory final : public SharedTextureMemoryBase {
 
     ResultOrError<Ref<TextureBase>> CreateTextureImpl(
         const Unpacked<TextureDescriptor>& descriptor) override;
-    MaybeError BeginAccessImpl(TextureBase* texture, const BeginAccessDescriptor*) override;
+    MaybeError BeginAccessImpl(TextureBase* texture,
+                               const Unpacked<BeginAccessDescriptor>&) override;
     ResultOrError<FenceAndSignalValue> EndAccessImpl(TextureBase* texture,
                                                      EndAccessState* state) override;
 
