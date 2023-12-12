e59c90c505a0bcddec8216aba78441ae78ffbff6
Arthur Sonzogni <arthursonzogni@chromium.org>
2023-12-12 14:43:27 +0000

    MiraclePtr: Rewrite dawn/native (linux)
    1. Run the clang rewriter:
       ./tools/clang/rewrite_raw_ptr_fields/rewrite.sh
    2. Add manually the missing AllowPtrArithmetic and DanglingUntriaged.
    3. Split the patch
    
    This is step 6 of using MiraclePtr in third_party/dawn project:
    https://docs.google.com/document/d/1wz45t0alQthsIU9P7_rQcfQyqnrBMXzrOjSzdQo-V-A/edit#heading=h.vn4i6wy373x7
    
    Bug: chromium:1464560
    Change-Id: I9db12c97503daf9ac20485347c935692ab2144bb

--- src/dawn/native/vulkan/ShaderModuleVk.h
+++ src/dawn/native/vulkan/ShaderModuleVk.h
@@ -38,6 +38,7 @@
 #include "dawn/common/vulkan_platform.h"
 #include "dawn/native/Error.h"
 #include "dawn/native/ShaderModule.h"
+#include "partition_alloc/pointers/raw_ptr.h"
 
 namespace dawn::native {
 
@@ -46,7 +47,7 @@ struct ProgrammableStage;
 namespace vulkan {
 
 struct TransformedShaderModuleCacheKey {
-    const PipelineLayoutBase* layout;
+    raw_ptr<const PipelineLayoutBase, DanglingUntriaged> layout;
     std::string entryPoint;
     PipelineConstantEntries constants;
     std::optional<uint32_t> maxSubgroupSizeForFullSubgroups;
