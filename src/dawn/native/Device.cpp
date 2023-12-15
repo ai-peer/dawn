e983d4b0c495cffe0a931e36c49d8d1887777f54
Loko Kung <lokokung@google.com>
2023-12-14 18:56:28 -0800

    [WGPUFuture] Implement and use Futures in wire for pipeline creation.
    Bug: dawn:1987, dawn:2060
    Change-Id: I279d5716b0171267120a8bec2b9f3b77c1cf3dcf

--- src/dawn/native/Device.cpp
+++ src/dawn/native/Device.cpp
@@ -1137,6 +1137,12 @@ void DeviceBase::APICreateComputePipelineAsync(const ComputePipelineDescriptor* 
                                            userdata);
     }
 }
+Future DeviceBase::APICreateComputePipelineAsyncF(
+    const ComputePipelineDescriptor* descriptor,
+    const CreateComputePipelineAsyncCallbackInfo& callbackInfo) {
+    // TODO(dawn:1987) Implement this.
+    DAWN_UNREACHABLE();
+}
 PipelineLayoutBase* DeviceBase::APICreatePipelineLayout(
     const PipelineLayoutDescriptor* descriptor) {
     Ref<PipelineLayoutBase> result;
@@ -1195,7 +1201,12 @@ void DeviceBase::APICreateRenderPipelineAsync(const RenderPipelineDescriptor* de
                                           userdata);
     }
 }
-
+Future DeviceBase::APICreateRenderPipelineAsyncF(
+    const RenderPipelineDescriptor* descriptor,
+    const CreateRenderPipelineAsyncCallbackInfo& callbackInfo) {
+    // TODO(dawn:1987) Implement this.
+    DAWN_UNREACHABLE();
+}
 RenderBundleEncoder* DeviceBase::APICreateRenderBundleEncoder(
     const RenderBundleEncoderDescriptor* descriptor) {
     Ref<RenderBundleEncoder> result;
