0fe1d698286877c5ac08014d0bc043bb061ac412
Hao Li <hao.x.li@intel.com>
2023-12-20 23:18:16 +0800

    Validate depthSlice not defined for non-3D color attachments
    The depthSlice has been initialized with WGPU_DEPTH_SLICE_UNDEFINED in
    Blink, we can add the validation back for 2D color attachments.
    
    And use constexpr instead of macro for the undefined value.
    
    Bug: dawn:1020
    Change-Id: Idbd275cbcdc8f17f9294c1fb0e5551ce05e1c52b

--- src/dawn/samples/CHelloTriangle.cpp
+++ src/dawn/samples/CHelloTriangle.cpp
@@ -117,6 +117,8 @@ void frame() {
     WGPURenderPassColorAttachment colorAttachment = {};
     {
         colorAttachment.view = backbufferView;
+        // The depthSlice must be initialized with the 'undefined' value for 2d color attachments.
+        colorAttachment.depthSlice = wgpu::kDepthSliceUndefined;
         colorAttachment.resolveTarget = nullptr;
         colorAttachment.clearValue = {0.0f, 0.0f, 0.0f, 0.0f};
         colorAttachment.loadOp = WGPULoadOp_Clear;
