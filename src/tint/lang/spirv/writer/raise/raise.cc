1c28552a5d6e4a6db6deeb680cd0e759bb793fd2
Jiawei Shao <jiawei.shao@intel.com>
2023-12-22 13:59:54 +0800

    Tint/HLSL: Support Shader Model 6.6 Pack/Unpack intrinsics
    This patch adds the support of Shader Model 6.6 pack/unpack intrinsics
    to implement the packing and unpacking built-in functions in
    `packed_4x8_integer_dot_product`.
    
    Note that currently we cannot use the HLSL intrinsic `pack_clamp_u8()`
    because now this function accepts an `int32_t4` as its parameter instead
    of `uint32_t4` (see below DXC issue:
    https://github.com/microsoft/DirectXShaderCompiler/issues/5091).
    
    Bug: tint:1497
    Test: dawn_end2end_tests
    Change-Id: I00d869d349e7f8bd323a0b091ee417776e68721d

--- src/tint/lang/spirv/writer/raise/raise.cc
+++ src/tint/lang/spirv/writer/raise/raise.cc
@@ -87,6 +87,7 @@ Result<SuccessType> Raise(core::ir::Module& module, const Options& options) {
     core_polyfills.saturate = true;
     core_polyfills.texture_sample_base_clamp_to_edge_2d_f32 = true;
     core_polyfills.pack_unpack_4x8 = true;
+    core_polyfills.pack_4xu8_clamp = true;
     RUN_TRANSFORM(core::ir::transform::BuiltinPolyfill, module, core_polyfills);
 
     core::ir::transform::ConversionPolyfillConfig conversion_polyfills;
