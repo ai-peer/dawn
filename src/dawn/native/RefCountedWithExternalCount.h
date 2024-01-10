4491863873e80c5eff6a33393ca77b47ee2a33b9
Peng Huang <penghuang@chromium.org>
2024-01-10 16:34:57 -0500

    Release mTintProgram when it is not needed
    This CL will release mTintProgram and mTintSource in
    ShaderModuleBase, when the ShaderModule is not referenced
    externally, and the ShaderModule is not needed for compile any
    pipeline. And mTintProgram and mTintSource will be re-created
    from shader source code when the ShaderModule got a referenced
    externally again.
    
    This change also change the base class of all API objects to
    RefCountedWithExternalCount.
    
    Bug: dawn:2184
    Change-Id: I0d715cacfd0a36cba43faaad6bd4452325cd9889

--- src/dawn/native/RefCountedWithExternalCount.h
+++ src/dawn/native/RefCountedWithExternalCount.h
@@ -34,11 +34,9 @@ namespace dawn::native {
 
 // RecCountedWithExternalCount is a version of RefCounted which tracks a separate
 // refcount for calls to APIReference/APIRelease (refs added/removed by the application).
-// The external refcount starts at 1, and the total refcount starts at 1 - i.e. the first
-// ref is the external ref.
-// Then, when the external refcount drops to zero, WillDropLastExternalRef is called.
-// The derived class should override the behavior of WillDropLastExternalRef.
-class RefCountedWithExternalCount : private RefCounted {
+// The external refcount starts at 0, and the total refcount starts at 1 - i.e. the first
+// ref is the internal ref.
+class RefCountedWithExternalCount : public RefCounted {
   public:
     using RefCounted::RefCounted;
     using RefCounted::Reference;
@@ -51,9 +49,15 @@ class RefCountedWithExternalCount : private RefCounted {
     using RefCounted::DeleteThis;
 
   private:
+    // Called when the object got the first external ref. It could be called more than once for
+    // internal cached objects.
+    virtual void WillHaveFirstExternalRef() = 0;
+    // Called when the object lost the last external ref. It could be called more than once for
+    // internal cached objects.
     virtual void WillDropLastExternalRef() = 0;
 
-    RefCount mExternalRefCount;
+    // For a new created object, the ref count is 1, but mExternalRefCount is 0.
+    RefCountBase</*RefCountStartsFromZero=*/true> mExternalRefCount;
 };
 
 }  // namespace dawn::native
