2d6ed4b7a1f7f12cafdf237334f22eabe36f7c48
Ben Clayton <bclayton@google.com>
2024-01-16 14:13:56 +0000

    [tint] Fix emission of module-scope struct initializers
    Constant structure initializers need to be assigned directly to a
    variable in HLSL. We were emitting these as `const` variables for both
    module-scope and function-scope.
    
    Module-scope `const` variables are not held in the program bitcode, but
    are expected to be provided as a cbuffer binding, which dawn was not
    providing.
    
    Instead emit these always as global scope `static const` variables.
    
    Add a dawn end2end test for `var<private>` variables initialized with a
    `struct`
    
    Bug: tint:2142
    Change-Id: I03c7c94a1729dcfbc077bcef01495a9fbbff0290

--- src/tint/lang/hlsl/writer/ast_printer/ast_printer.cc
+++ src/tint/lang/hlsl/writer/ast_printer/ast_printer.cc
@@ -416,6 +416,8 @@ bool ASTPrinter::Generate() {
         }
         last_kind = kind;
 
+        global_insertion_point_ = current_buffer_->lines.size();
+
         bool ok = Switch(
             decl,
             [&](const ast::Variable* global) {  //
@@ -3849,12 +3851,13 @@ bool ASTPrinter::EmitConstant(StringStream& out,
                 // HLSL requires structure initializers to be assigned directly to a variable.
                 auto name = UniqueIdentifier("c");
                 {
-                    auto decl = Line();
-                    decl << "const " << StructName(s) << " " << name << " = ";
+                    StringStream decl;
+                    decl << "static const " << StructName(s) << " " << name << " = ";
                     if (!emit_member_values(decl)) {
                         return false;
                     }
                     decl << ";";
+                    current_buffer_->Insert(decl.str(), global_insertion_point_++, 0);
                 }
                 out << name;
             }
