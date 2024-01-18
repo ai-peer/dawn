295aa3bb26d5eab0ec97ec2a533d0ce1f5252d44
Austin Eng <enga@chromium.org>
2024-01-18 15:47:00 -0800

    Break Instance->Backend->PhysicalDevice->Instance refcyle
    Simplifies the ref heirarchy to minimize ref cycles to make
    Dawn less prone to leaking backend objects.
    
    Previously we had:
     Instance->Backend->PhysicalDevice->Instance
     Adapter->PhysicalDevice->Instance
    
    Now, we have:
     Instance->Backend->PhysicalDevice
     Adapter->PhysicalDevice
     Adapter->Instance
    
    Bug: none
    Change-Id: I7882eb3b8580146c0ccc56bcba36eefda060fafc

--- src/dawn/native/metal/BackendMTL.mm
+++ src/dawn/native/metal/BackendMTL.mm
@@ -261,7 +261,7 @@ class PhysicalDevice : public PhysicalDeviceBase {
     PhysicalDevice(InstanceBase* instance,
                    NSPRef<id<MTLDevice>> device,
                    bool metalValidationEnabled)
-        : PhysicalDeviceBase(instance, wgpu::BackendType::Metal),
+        : PhysicalDeviceBase(wgpu::BackendType::Metal),
           mDevice(std::move(device)),
           mMetalValidationEnabled(metalValidationEnabled) {
         mName = std::string([[*mDevice name] UTF8String]);
@@ -343,9 +343,11 @@ class PhysicalDevice : public PhysicalDeviceBase {
         return Device::Create(adapter, mDevice, descriptor, deviceToggles, std::move(lostEvent));
     }
 
-    void SetupBackendAdapterToggles(TogglesState* adapterToggles) const override {}
+    void SetupBackendAdapterToggles(dawn::platform::Platform* platform,
+                                    TogglesState* adapterToggles) const override {}
 
-    void SetupBackendDeviceToggles(TogglesState* deviceToggles) const override {
+    void SetupBackendDeviceToggles(dawn::platform::Platform* platform,
+                                   TogglesState* deviceToggles) const override {
         {
             bool haveStoreAndMSAAResolve = false;
 #if DAWN_PLATFORM_IS(MACOS)
@@ -1040,14 +1042,6 @@ std::vector<Ref<PhysicalDeviceBase>> Backend::DiscoverPhysicalDevices(
 #endif
     }
     return std::vector<Ref<PhysicalDeviceBase>>{mPhysicalDevices};
-}
-
-void Backend::ClearPhysicalDevices() {
-    mPhysicalDevices.clear();
-}
-
-size_t Backend::GetPhysicalDeviceCountForTesting() const {
-    return mPhysicalDevices.size();
 }
 
 BackendConnection* Connect(InstanceBase* instance) {
--- src/dawn/native/vulkan/PhysicalDeviceVk.h
+++ src/dawn/native/vulkan/PhysicalDeviceVk.h
@@ -28,11 +28,16 @@
 #ifndef SRC_DAWN_NATIVE_VULKAN_ADAPTERVK_H_
 #define SRC_DAWN_NATIVE_VULKAN_ADAPTERVK_H_
 
-#include "dawn/native/PhysicalDevice.h"
+#include <memory>
 
 #include "dawn/common/Ref.h"
 #include "dawn/common/vulkan_platform.h"
+#include "dawn/native/PhysicalDevice.h"
 #include "dawn/native/vulkan/VulkanInfo.h"
+
+namespace dawn::native {
+class AHBFunctions;
+}  // namespace dawn::native
 
 namespace dawn::native::vulkan {
 
@@ -40,9 +45,7 @@ class VulkanInstance;
 
 class PhysicalDevice : public PhysicalDeviceBase {
   public:
-    PhysicalDevice(InstanceBase* instance,
-                   VulkanInstance* vulkanInstance,
-                   VkPhysicalDevice physicalDevice);
+    PhysicalDevice(VulkanInstance* vulkanInstance, VkPhysicalDevice physicalDevice);
     ~PhysicalDevice() override;
 
     // PhysicalDeviceBase Implementation
@@ -63,6 +66,7 @@ class PhysicalDevice : public PhysicalDeviceBase {
 
     ResultOrError<PhysicalDeviceSurfaceCapabilities> GetSurfaceCapabilities(
         const Surface* surface) const override;
+    const AHBFunctions* GetOrLoadAHBFunctions();
 
   private:
     MaybeError InitializeImpl() override;
@@ -73,8 +77,10 @@ class PhysicalDevice : public PhysicalDeviceBase {
         wgpu::FeatureName feature,
         const TogglesState& toggles) const override;
 
-    void SetupBackendAdapterToggles(TogglesState* adapterToggles) const override;
-    void SetupBackendDeviceToggles(TogglesState* deviceToggles) const override;
+    void SetupBackendAdapterToggles(dawn::platform::Platform* platform,
+                                    TogglesState* adapterToggles) const override;
+    void SetupBackendDeviceToggles(dawn::platform::Platform* platform,
+                                   TogglesState* deviceToggles) const override;
     ResultOrError<Ref<DeviceBase>> CreateDeviceImpl(
         AdapterBase* adapter,
         const UnpackedPtr<DeviceDescriptor>& descriptor,
@@ -95,6 +101,10 @@ class PhysicalDevice : public PhysicalDeviceBase {
     VulkanDeviceInfo mDeviceInfo = {};
 
     uint32_t mDefaultComputeSubgroupSize = 0;
+
+#if DAWN_PLATFORM_IS(ANDROID)
+    std::unique_ptr<AHBFunctions> mAHBFunctions;
+#endif  // DAWN_PLATFORM_IS(ANDROID)
 };
 
 }  // namespace dawn::native::vulkan
