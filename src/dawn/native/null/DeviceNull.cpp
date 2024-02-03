c36d500975b68812d9625597e57ececfba95b192
Sunny Sachanandani <sunnyps@chromium.org>
2024-02-02 16:59:53 -0800

    d3d: Fix missing synchronization for queue writeTexture
    Queue writeTexture needs to wait for and signal fences separately since
    it doesn't happen within a command buffer submission. To ensure that the
    fence is signaled correctly here and elsewhere, a new method is added to
    ensure commands are flushed up to a specified serial. This allows all
    backends to share the fix for writeTexture and any other places where we
    might've missed synchronization outside of command buffer submission.
    
    This CL also includes a SharedTextureMemoryTest for writeTexture.
    
    Bug: dawn:2351
    Change-Id: Ibc896370a379d88abd7a63a855c843dafc330d88

--- src/dawn/native/null/DeviceNull.cpp
+++ src/dawn/native/null/DeviceNull.cpp
@@ -444,6 +444,10 @@ bool Queue::HasPendingCommands() const {
     return false;
 }
 
+MaybeError SubmitPendingCommands() {
+    return {};
+}
+
 ResultOrError<bool> Queue::WaitForQueueSerial(ExecutionSerial serial, Nanoseconds timeout) {
     return true;
 }
