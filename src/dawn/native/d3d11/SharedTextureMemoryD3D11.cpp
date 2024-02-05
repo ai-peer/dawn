d2bc6339021e9123d0020b5fd9d29342ba079010
Peng Huang <penghuang@chromium.org>
2024-02-05 09:56:30 -0500

    D3D11: add needSynchronization flag for importing D3D11 texture
    In chrome, ANGLE and Graphite will share the same ID3D11Device
    and ID3D11DeviceContext, and all textures are shared directly as
    well, so fences are not needed for this case. So add the
    needSynchronization flag to allow explicitly disable
    synchronization in dawn.
    
    Bug: b/41497000
    Change-Id: I680fc838d164a1035107eb3991acdda717fcbb6a

--- src/dawn/native/d3d11/SharedTextureMemoryD3D11.cpp
+++ src/dawn/native/d3d11/SharedTextureMemoryD3D11.cpp
@@ -109,8 +109,8 @@ ResultOrError<Ref<SharedTextureMemory>> SharedTextureMemory::Create(
     SharedTextureMemoryProperties properties;
     DAWN_TRY_ASSIGN(properties, PropertiesFromD3D11Texture(device, d3d11Texture.Get()));
 
-    auto result =
-        AcquireRef(new SharedTextureMemory(device, label, properties, std::move(d3d11Resource)));
+    auto result = AcquireRef(new SharedTextureMemory(
+        device, label, properties, std::move(d3d11Resource), /*needSynchronization=*/true));
     result->Initialize();
     return result;
 }
@@ -135,8 +135,8 @@ ResultOrError<Ref<SharedTextureMemory>> SharedTextureMemory::Create(
     SharedTextureMemoryProperties properties;
     DAWN_TRY_ASSIGN(properties, PropertiesFromD3D11Texture(device, descriptor->texture.Get()));
 
-    auto result =
-        AcquireRef(new SharedTextureMemory(device, label, properties, std::move(d3d11Resource)));
+    auto result = AcquireRef(new SharedTextureMemory(
+        device, label, properties, std::move(d3d11Resource), descriptor->needSynchronization));
     result->Initialize();
     return result;
 }
@@ -144,8 +144,9 @@ ResultOrError<Ref<SharedTextureMemory>> SharedTextureMemory::Create(
 SharedTextureMemory::SharedTextureMemory(Device* device,
                                          const char* label,
                                          SharedTextureMemoryProperties properties,
-                                         ComPtr<ID3D11Resource> resource)
-    : d3d::SharedTextureMemory(device, label, properties, resource.Get()),
+                                         ComPtr<ID3D11Resource> resource,
+                                         bool needSynchronization)
+    : d3d::SharedTextureMemory(device, label, properties, resource.Get(), needSynchronization),
       mResource(std::move(resource)) {}
 
 void SharedTextureMemory::DestroyImpl() {
