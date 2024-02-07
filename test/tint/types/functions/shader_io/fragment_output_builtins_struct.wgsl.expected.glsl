1d6a948daa7b42e0021c16f778cd54ee63a522c3
Stephen White <senorblanco@chromium.org>
2024-02-07 09:30:51 -0500

    Add ClampFragDepth transform to GLSL writer.
    Change-Id: Ieb9e282f94b8c74374f11b90324dacf2325183f0

--- test/tint/types/functions/shader_io/fragment_output_builtins_struct.wgsl.expected.glsl
+++ test/tint/types/functions/shader_io/fragment_output_builtins_struct.wgsl.expected.glsl
@@ -3,14 +3,29 @@
 precision highp float;
 precision highp int;
 
+struct PushConstants {
+  float min;
+  float max;
+};
+
+layout(location=0) uniform PushConstants push_constants;
+float clamp_frag_depth(float v) {
+  return clamp(v, push_constants.min, push_constants.max);
+}
+
 struct FragmentOutputs {
   float frag_depth;
   uint sample_mask;
 };
 
-FragmentOutputs tint_symbol() {
-  FragmentOutputs tint_symbol_1 = FragmentOutputs(1.0f, 1u);
+FragmentOutputs clamp_frag_depth_FragmentOutputs(FragmentOutputs s) {
+  FragmentOutputs tint_symbol_1 = FragmentOutputs(clamp_frag_depth(s.frag_depth), s.sample_mask);
   return tint_symbol_1;
+}
+
+FragmentOutputs tint_symbol() {
+  FragmentOutputs tint_symbol_2 = FragmentOutputs(1.0f, 1u);
+  return clamp_frag_depth_FragmentOutputs(tint_symbol_2);
 }
 
 void main() {
