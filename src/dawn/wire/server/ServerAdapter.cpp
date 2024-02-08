4a6e3a232a5c33a5d35f31889e3ccd496ed2d881
François Beaufort <beaufort.francois@gmail.com>
2024-02-08 10:17:37 +0100

    [WIP] Add WGPUAdapterRequestAdapterInfo
    WIP
    
    Change-Id: I2b30f91ff3bdb87d5c8f3360067bb6a413ee7577

--- src/dawn/wire/server/ServerAdapter.cpp
+++ src/dawn/wire/server/ServerAdapter.cpp
@@ -32,6 +32,34 @@
 
 namespace dawn::wire::server {
 
+WireResult Server::DoAdapterRequestAdapterInfo(Known<WGPUAdapter> adapter,
+                                               ObjectHandle eventManager,
+                                               WGPUFuture future) {
+    auto userdata = MakeUserdata<RequestAdapterInfoUserdata>();
+    userdata->eventManager = eventManager;
+    userdata->future = future;
+
+    mProcs.adapterRequestAdapterInfo(adapter->handle,
+                                     ForwardToServer<&Server::OnRequestAdapterInfoCallback>,
+                                     userdata.release());
+    return WireResult::Success;
+}
+
+void Server::OnRequestAdapterInfoCallback(RequestAdapterInfoUserdata* data,
+                                          WGPURequestAdapterInfoStatus status,
+                                          const WGPUAdapterInfo* adapterInfo) {
+    ReturnAdapterRequestAdapterInfoCallbackCmd cmd = {};
+    cmd.eventManager = data->eventManager;
+    cmd.future = data->future;
+    cmd.status = status;
+    cmd.adapterInfo = adapterInfo;
+
+    SerializeCommand(cmd);
+    if (adapterInfo) {
+        mProcs.adapterInfoFreeMembers(*adapterInfo);
+    }
+}
+
 WireResult Server::DoAdapterRequestDevice(Known<WGPUAdapter> adapter,
                                           ObjectHandle eventManager,
                                           WGPUFuture future,
