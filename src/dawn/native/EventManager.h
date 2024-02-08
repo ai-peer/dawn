4a09c589c1fb391460446360eea681d15bc54cd5
Austin Eng <enga@chromium.org>
2024-02-07 16:12:08 -0800

    wgpufuture export stuff
    Change-Id: I42b325e2ac68d4200869861c25662625c87b6807

--- src/dawn/native/EventManager.h
+++ src/dawn/native/EventManager.h
@@ -79,6 +79,8 @@ class EventManager final : NonMovable {
                                            FutureWaitInfo* infos,
                                            Nanoseconds timeout);
 
+    const SystemHandle& GetOrCreateSharedSystemEventReceiver(Future future);
+
   private:
     bool mTimedWaitAnyEnable = false;
     size_t mTimedWaitAnyMaxCount = kTimedWaitAnyMaxCountDefault;
@@ -159,6 +161,7 @@ class EventManager::TrackedEvent : public RefCounted {
     friend class EventManager;
 
     CompletionData mCompletionData;
+    Ref<SharedSystemEventReceiver> mCompletionSystemEventReceiver = nullptr;
     // Callback has been called.
     std::atomic<bool> mCompleted = false;
 };
