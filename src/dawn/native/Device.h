83f5c481188e20770468e94af1d14cd1b86f98bf
Elie Michel <elie.michel.fr@gmail.com>
2024-02-16 10:17:54 +0100

    Remove SwapChain from the API
    This applies the switch from manipulating a SwapChain object to calling
    Surface::configure that was brung to the standard webgpu.h a couple of
    months ago.
    
    I believe this needs a bit of extra work, but at this stage I need a
    review from people more familiar with the codebase to tell me if I am
    going in the right direction.
    
    The SwapChain object is still here, but no longer present in the API.
    Instances of the swap chain are now owned by the surface object. The
    test for surface capabilities is partly calling a method of the
    PhysicalDevice in order to have backend-specific capabilities.
    
    So far I mostly tested with the Vulkan backend, though I tried to
    provide a valid implementation for all backends.
    
    Bug: dawn:2320
    Change-Id: I69d3c64873213eca375c66170cb648debd8ec91d
    
    Minor changes to surface validation tests
    
    Change-Id: If981e57c9db866fe4f80b98cad529c56c90a5c36

--- src/dawn/native/Device.h
+++ src/dawn/native/Device.h
@@ -265,8 +265,13 @@ class DeviceBase : public RefCountedWithExternalCount {
     ResultOrError<Ref<ShaderModuleBase>> CreateShaderModule(
         const ShaderModuleDescriptor* descriptor,
         OwnedCompilationMessages* compilationMessages = nullptr);
+    // Deprecated: this was the way to create a SwapChain when it was explicitly manipulated by the
+    // end user.
     ResultOrError<Ref<SwapChainBase>> CreateSwapChain(Surface* surface,
                                                       const SwapChainDescriptor* descriptor);
+    ResultOrError<Ref<SwapChainBase>> CreateSwapChain(Surface* surface,
+                                                      SwapChainBase* previousSwapChain,
+                                                      const SurfaceConfiguration* config);
     ResultOrError<Ref<TextureBase>> CreateTexture(const TextureDescriptor* rawDescriptor);
     ResultOrError<Ref<TextureViewBase>> CreateTextureView(TextureBase* texture,
                                                           const TextureViewDescriptor* descriptor);
@@ -303,7 +308,9 @@ class DeviceBase : public RefCountedWithExternalCount {
     ShaderModuleBase* APICreateShaderModule(const ShaderModuleDescriptor* descriptor);
     ShaderModuleBase* APICreateErrorShaderModule(const ShaderModuleDescriptor* descriptor,
                                                  const char* errorMessage);
-    SwapChainBase* APICreateSwapChain(Surface* surface, const SwapChainDescriptor* descriptor);
+    // TODO (dawn:2320) Remove after deprecation
+    SwapChainBase* APICreateSwapChain(Surface* surface,
+                                      const SwapChainDescriptor* descriptor);  // Deprecated
     TextureBase* APICreateTexture(const TextureDescriptor* descriptor);
 
     wgpu::TextureUsage APIGetSupportedSurfaceUsage(Surface* surface);
@@ -503,7 +510,7 @@ class DeviceBase : public RefCountedWithExternalCount {
     virtual ResultOrError<Ref<SwapChainBase>> CreateSwapChainImpl(
         Surface* surface,
         SwapChainBase* previousSwapChain,
-        const SwapChainDescriptor* descriptor) = 0;
+        const SurfaceConfiguration* config) = 0;
     virtual ResultOrError<Ref<TextureBase>> CreateTextureImpl(
         const UnpackedPtr<TextureDescriptor>& descriptor) = 0;
     virtual ResultOrError<Ref<TextureViewBase>> CreateTextureViewImpl(
--- src/dawn/native/metal/SwapChainMTL.mm
+++ src/dawn/native/metal/SwapChainMTL.mm
@@ -40,14 +40,14 @@ namespace dawn::native::metal {
 ResultOrError<Ref<SwapChain>> SwapChain::Create(Device* device,
                                                 Surface* surface,
                                                 SwapChainBase* previousSwapChain,
-                                                const SwapChainDescriptor* descriptor) {
-    Ref<SwapChain> swapchain = AcquireRef(new SwapChain(device, surface, descriptor));
+                                                const SurfaceConfiguration* config) {
+    Ref<SwapChain> swapchain = AcquireRef(new SwapChain(device, surface, config));
     DAWN_TRY(swapchain->Initialize(previousSwapChain));
     return swapchain;
 }
 
-SwapChain::SwapChain(DeviceBase* dev, Surface* sur, const SwapChainDescriptor* desc)
-    : SwapChainBase(dev, sur, desc) {}
+SwapChain::SwapChain(DeviceBase* dev, Surface* sur, const SurfaceConfiguration* config)
+    : SwapChainBase(dev, sur, config) {}
 
 SwapChain::~SwapChain() = default;
 
@@ -72,6 +72,12 @@ MaybeError SwapChain::Initialize(SwapChainBase* previousSwapChain) {
 
     mLayer = static_cast<CAMetalLayer*>(GetSurface()->GetMetalLayer());
     DAWN_ASSERT(mLayer != nullptr);
+    // TODO(dawn:2320) Check that this behaves as expected by the spec
+    if (GetAlphaMode() == wgpu::CompositeAlphaMode::Premultiplied) {
+        mLayer->isOpaque = false;
+    } else {
+        mLayer->isOpaque = true;
+    }
 
     CGSize size = {};
     size.width = GetWidth();
@@ -103,7 +109,7 @@ MaybeError SwapChain::PresentImpl() {
     return {};
 }
 
-ResultOrError<Ref<TextureBase>> SwapChain::GetCurrentTextureImpl() {
+ResultOrError<SwapChainTextureInfo> SwapChain::GetCurrentTextureImpl() {
     @autoreleasepool {
         DAWN_ASSERT(mCurrentDrawable == nullptr);
         mCurrentDrawable = [*mLayer nextDrawable];
@@ -112,7 +118,13 @@ ResultOrError<Ref<TextureBase>> SwapChain::GetCurrentTextureImpl() {
 
         mTexture = Texture::CreateWrapping(ToBackend(GetDevice()), Unpack(&textureDesc),
                                            NSPRef<id<MTLTexture>>([*mCurrentDrawable texture]));
-        return mTexture;
+
+        SwapChainTextureInfo info;
+        info.texture = mTexture;
+        info.status = wgpu::SurfaceGetCurrentTextureStatus::Success;
+        // TODO(dawn:2320) Check for optimality
+        info.suboptimal = false;
+        return info;
     }
 }
 
