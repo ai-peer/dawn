5f952c97347f46f7077b8de31fccdf6c4dfd6322
Ben Clayton <bclayton@google.com>
2024-02-23 16:02:43 +0000

    [tint][core] Highlight intrinsic table matches / mismatches.
    Helps highlight what didn't match.
    
    Fixed: tint:2158
    Change-Id: Ibdafd1ad3be1c846bfae8b93e1ec7b1669a3a556

--- src/tint/lang/wgsl/resolver/call_validation_test.cc
+++ src/tint/lang/wgsl/resolver/call_validation_test.cc
@@ -522,8 +522,8 @@ TEST_F(ResolverCallValidationTest, UnexpectedBuiltinTemplateArgs) {
               R"(12:34 error: no matching call to 'min<i32>(abstract-int, abstract-int)'
 
 2 candidate functions:
-  'min(T, T) -> T'  where: 'T' is 'abstract-float', 'abstract-int', 'f32', 'i32', 'u32' or 'f16'
-  'min(vecN<T>, vecN<T>) -> vecN<T>'  where: 'T' is 'abstract-float', 'abstract-int', 'f32', 'i32', 'u32' or 'f16'
+  'min(T  ✓ , T  ✓ ) -> T'  where: 'T' is 'abstract-float', 'abstract-int', 'f32', 'i32', 'u32' or 'f16'
+  'min(vecN<T>  ✗ , vecN<T>  ✗ ) -> vecN<T>'  where: 'T' is 'abstract-float', 'abstract-int', 'f32', 'i32', 'u32' or 'f16'
 )");
 }
 
