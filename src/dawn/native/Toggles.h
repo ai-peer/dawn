82fab79ec646dde473bab67e768ed711c9e12c24
Peng Huang <penghuang@chromium.org>
2024-03-19 18:54:05 -0400

    D3D11: add color_clear_with_draw toggle
    D3D11 ClearRenderTargetView() could be buggy with some old driver
    or GPUs. So add this toggle to force dawn using draw to clear
    color attacment of a render pass.
    
    Bug: chromium:329702368
    Change-Id: I66c473b5870378c85f72ebbba6ea2bb382ccc8f0

--- src/dawn/native/Toggles.h
+++ src/dawn/native/Toggles.h
@@ -137,6 +137,8 @@ enum class Toggle {
     NoWorkaroundIndirectBaseVertexNotApplied,
     NoWorkaroundDstAlphaAsSrcBlendFactorForBothColorAndAlphaDoesNotWork,
 
+    ColorClearWithDraw,
+
     EnumCount,
     InvalidEnum = EnumCount,
 };
