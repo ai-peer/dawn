e383606a82b7b6434af7c4d6046d67ef1ecd5a91
Austin Eng <enga@chromium.org>
2024-04-01 15:08:53 -0400

    Gracefully log errors if D3D12 pipeline state GetCachedBlob fails
    Crash reports show that sometimes this fails for unknown reasons.
    We should continue gracefully, and can wire messages to
    DumpWithoutCrashing or histograms in Chrome.
    
    Bug: dawn:1833
    Change-Id: I03a886ff7fb926798bd138cc661ccab3167a1ffd

--- src/dawn/native/d3d12/RenderPipelineD3D12.cpp
+++ src/dawn/native/d3d12/RenderPipelineD3D12.cpp
@@ -480,9 +480,11 @@ MaybeError RenderPipeline::InitializeImpl() {
         // Cache misses, need to get pipeline cached blob and store.
         cacheTimer.RecordMicroseconds("D3D12.CreateGraphicsPipelineState.CacheMiss");
         ComPtr<ID3DBlob> d3dBlob;
-        DAWN_TRY(CheckHRESULT(GetPipelineState()->GetCachedBlob(&d3dBlob),
-                              "D3D12 render pipeline state get cached blob"));
-        device->StoreCachedBlob(GetCacheKey(), CreateBlob(std::move(d3dBlob)));
+        if (!device->GetInstance()->ConsumedError(
+                CheckHRESULT(GetPipelineState()->GetCachedBlob(&d3dBlob),
+                             "D3D12 render pipeline state get cached blob"))) {
+            device->StoreCachedBlob(GetCacheKey(), CreateBlob(std::move(d3dBlob)));
+        }
     } else {
         cacheTimer.RecordMicroseconds("D3D12.CreateGraphicsPipelineState.CacheHit");
     }
