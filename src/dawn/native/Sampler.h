dce6cf194bd8bf744952d84f5e896b4dfc5615ed
Saifuddin Hitawala <hitawala@chromium.org>
2024-04-10 16:36:50 -0400

    [YCbCr Samplers] Add SamplerYCbCrVulkanDescriptor struct
    Add SamplerYCbCrVulkanDescriptor as a chained struct on VulkanBackend.
    Also add corresponding logic in ChainUtilsImpl and feature validation
    in Sampler.cpp.
    
    Change-Id: I253f30b8b88059fc304d9ef2cd66dc2c92ca9529
    Bug: dawn:2476

--- src/dawn/native/Sampler.h
+++ src/dawn/native/Sampler.h
@@ -38,6 +38,10 @@
 
 namespace dawn::native {
 
+namespace vulkan {
+struct SamplerYCbCrVulkanDescriptor;
+}
+
 class DeviceBase;
 
 MaybeError ValidateSamplerDescriptor(DeviceBase* device, const SamplerDescriptor* descriptor);
