f0dbc8533e18befd6657db2b9fc7c501d7c91588
Jiawei Shao <jiawei.shao@intel.com>
2024-04-16 16:07:05 +0800

    Implement AlignedAlloc and AlignedFree
    This patch implements dawn::AlignedAlloc and dawn::AlignedFree
    with std::aligned_alloc (on Linux and macOS) or _aligned_alloc
    and _aligned_free (on Windows).
    
    Bug: dawn:824
    Change-Id: Ifcd794a14be29c006e9a39323252dd6b867a30dc

--- src/dawn/common/SlabAllocator.cpp
+++ src/dawn/common/SlabAllocator.cpp
@@ -31,6 +31,7 @@
 #include <cstdlib>
 #include <limits>
 #include <new>
+#include "dawn/common/AlignedAlloc.h"
 #include "dawn/common/Assert.h"
 #include "dawn/common/Math.h"
 
@@ -62,7 +63,7 @@ SlabAllocatorImpl::SentinelSlab::~SentinelSlab() {
         DAWN_ASSERT(slab->blocksInUse == 0);
         char* allocation = slab->allocation;
         slab->~Slab();  // Placement delete.
-        delete[] allocation;
+        dawn::AlignedFree(allocation);
     }
 }
 
@@ -232,10 +233,8 @@ void SlabAllocatorImpl::GetNewSlab() {
         return;
     }
 
-    // TODO(crbug.com/dawn/824): Use aligned_alloc when possible. It should be available with
-    // C++17 but on macOS it also requires macOS 10.15 to work.
-    char* allocation = new char[mTotalAllocationSize];
-    char* alignedPtr = AlignPtr(allocation, mAllocationAlignment);
+    char* alignedPtr =
+        static_cast<char*>(dawn::AlignedAlloc(mAllocationAlignment, mTotalAllocationSize));
 
     char* dataStart = alignedPtr + mSlabBlocksOffset;
 
@@ -247,7 +246,7 @@ void SlabAllocatorImpl::GetNewSlab() {
     IndexLinkNode* lastNode = OffsetFrom(node, mBlocksPerSlab - 1);
     lastNode->nextIndex = kInvalidIndex;
 
-    mAvailableSlabs.Prepend(new (alignedPtr) Slab(allocation, node));
+    mAvailableSlabs.Prepend(new (alignedPtr) Slab(alignedPtr, node));
 }
 
 }  // namespace dawn
