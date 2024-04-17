080c4068c22f2acd96b90ce4c3508b43087dfaab
François Beaufort <beaufort.francois@gmail.com>
2024-04-17 16:20:23 +0200

    dawn.node: Add GPUAdapter::getInfo()
    Bug: 335383516
    Change-Id: I833ecccf7a38b53067e1e6b21d6ae5469cf03504

--- src/dawn/node/binding/GPUAdapter.cpp
+++ src/dawn/node/binding/GPUAdapter.cpp
@@ -113,6 +113,13 @@ interop::Interface<interop::GPUSupportedLimits> GPUAdapter::getLimits(Napi::Env 
     return interop::GPUSupportedLimits::Create<GPUSupportedLimits>(env, wgpuLimits);
 }
 
+interop::Interface<interop::GPUAdapterInfo> GPUAdapter::getInfo(Napi::Env env) {
+    WGPUAdapterInfo info = {};
+    adapter_.GetInfo(&info);
+
+    return interop::GPUAdapterInfo::Create<GPUAdapterInfo>(env, info);
+}
+
 bool GPUAdapter::getIsFallbackAdapter(Napi::Env) {
     WGPUAdapterProperties adapterProperties = {};
     adapter_.GetProperties(&adapterProperties);
