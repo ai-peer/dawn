1594365a3ff31cf4930f379f4573303e34d9e2bb
Antonio Maiorano <amaiorano@google.com>
2024-04-18 00:03:35 -0400

    dawn/dxc: disable buggy DXC pass that may crash
    Disable 'structurize-loop-exits-for-unroll' as it sometimes messes up the IR when dealing with infinite loops.
    
    Bug: chromium:333508731
    Change-Id: I5c9d7180ed09e7417c120595937bcb1013b6ce66
    Include-Ci-Only-Tests: true

--- src/dawn/native/d3d/ShaderUtils.cpp
+++ src/dawn/native/d3d/ShaderUtils.cpp
@@ -58,6 +58,11 @@ std::vector<const wchar_t*> GetDXCArguments(std::wstring_view entryPointNameW,
     arguments.push_back(L"-E");
     arguments.push_back(entryPointNameW.data());
 
+    // TODO(chromium:333508731): Disable buggy HLSL pass that sometimes messes up the
+    // IR when dealing with complex infinite loops.
+    arguments.push_back(L"-opt-disable");
+    arguments.push_back(L"structurize-loop-exits-for-unroll");
+
     uint32_t compileFlags = r.compileFlags;
     if (compileFlags & D3DCOMPILE_ENABLE_BACKWARDS_COMPATIBILITY) {
         arguments.push_back(L"/Gec");
