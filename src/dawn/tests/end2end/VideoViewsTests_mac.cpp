4d260d81df91cc14345037b3e4d298c896c306f7
Peng Huang <penghuang@chromium.org>
2024-04-22 14:09:33 -0400

    d3d: allow importing texture with multiple layers
    On Windows, HW video decoder uses one texture 2d array (a texture with
    multiple layers) to store all decoded frames. So dawn has to support
    importing the texture 2d array.
    
    Note: multi-layer is not multi-planar. multi-planar textures store
    channels in separate planar.
    
    Bug: chromium:335564847
    Change-Id: Ib888c21b8c858c248516bb741eefdbfd9ff05e6f

--- src/dawn/tests/end2end/VideoViewsTests_mac.cpp
+++ src/dawn/tests/end2end/VideoViewsTests_mac.cpp
@@ -60,6 +60,7 @@ class VideoViewsTestBackendIOSurface : public VideoViewsTestBackend {
   private:
     std::unique_ptr<VideoViewsTestBackend::PlatformTexture> CreateVideoTextureForTest(
         wgpu::TextureFormat format,
+        uint32_t layerCount,
         wgpu::TextureUsage usage,
         bool isCheckerboard,
         bool initialized) override {
@@ -168,6 +169,11 @@ std::vector<Format> VideoViewsTestBackend::Formats() {
     return {wgpu::TextureFormat::R8BG8Biplanar420Unorm,
             wgpu::TextureFormat::R10X6BG10X6Biplanar420Unorm,
             wgpu::TextureFormat::R8BG8A8Triplanar420Unorm};
+}
+
+// static
+std::vector<LayerCount> VideoViewsTestBackend::LayerCounts() {
+    return {1};
 }
 
 // static
