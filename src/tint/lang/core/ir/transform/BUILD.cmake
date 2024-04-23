2406b8f875ffb23d93c43f407981c6416a9ac54c
Ben Clayton <bclayton@google.com>
2024-04-23 12:01:36 +0000

    [tools][utils] Split BufferReader out to separate files
    This was already done for BufferWriter, but for some reason BufferReader got missed.
    
    Change-Id: I633f195ca76abe1f2ea5e65c73ee95387ce97560

--- src/tint/lang/core/ir/transform/BUILD.cmake
+++ src/tint/lang/core/ir/transform/BUILD.cmake
@@ -208,18 +208,23 @@ tint_target_add_dependencies(tint_lang_core_ir_transform_fuzz fuzz
   tint_api_common
   tint_api_options
   tint_cmd_fuzz_ir_fuzz
+  tint_lang_core
+  tint_lang_core_constant
   tint_lang_core_ir
   tint_lang_core_ir_transform
+  tint_lang_core_type
   tint_utils_bytes
   tint_utils_containers
   tint_utils_diagnostic
   tint_utils_ice
+  tint_utils_id
   tint_utils_macros
   tint_utils_math
   tint_utils_memory
   tint_utils_reflection
   tint_utils_result
   tint_utils_rtti
+  tint_utils_symbol
   tint_utils_text
   tint_utils_traits
 )
