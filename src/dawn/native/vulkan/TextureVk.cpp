e93fe0063369d906d580a5958a99f0472b16cc65
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-04-25 07:30:10 +0000

    Reorder member around PNextChainBuilder.
    This stack allocated struct stores pointers to the stack. It should be
    declared last to match the dependency.
    
    This only matters when using:
    ```
    use_asan_unowned_ptr=true
    ```
    option, which check for dangling pointers on the stack.
    
    Bug: chromium:336222179
    Change-Id: Ibd877bcf610cf85b6ad84a5b18fa786d0e06c3ac

--- src/dawn/native/vulkan/TextureVk.cpp
+++ src/dawn/native/vulkan/TextureVk.cpp
@@ -825,9 +825,6 @@ MaybeError Texture::InitializeAsInternalTexture(VkImageUsageFlags extraUsages) {
     // frontend already based on the minimum supported formats in the Vulkan spec
     VkImageCreateInfo createInfo = {};
     FillVulkanCreateInfoSizesAndType(*this, &createInfo);
-
-    PNextChainBuilder createInfoChain(&createInfo);
-
     createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
     createInfo.format = VulkanImageFormat(device, GetFormat().format);
     createInfo.tiling = VK_IMAGE_TILING_OPTIMAL;
@@ -835,7 +832,6 @@ MaybeError Texture::InitializeAsInternalTexture(VkImageUsageFlags extraUsages) {
     createInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
     createInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
 
-    VkImageFormatListCreateInfo imageFormatListInfo = {};
     std::vector<VkFormat> viewFormats;
     bool requiresViewFormatsList = GetViewFormats().any();
     // As current SPIR-V SPEC doesn't support 'bgra8' as a valid image format, to support the
@@ -860,6 +856,8 @@ MaybeError Texture::InitializeAsInternalTexture(VkImageUsageFlags extraUsages) {
     // Add the view format list only when the usage does not have storage. Otherwise, the VVL will
     // say creation of the texture is invalid.
     // See https://github.com/gpuweb/gpuweb/issues/4426.
+    VkImageFormatListCreateInfo imageFormatListInfo = {};
+    PNextChainBuilder createInfoChain(&createInfo);
     if (requiresViewFormatsList && device->GetDeviceInfo().HasExt(DeviceExt::ImageFormatList) &&
         !(createInfo.usage & VK_IMAGE_USAGE_STORAGE_BIT)) {
         createInfoChain.Add(&imageFormatListInfo, VK_STRUCTURE_TYPE_IMAGE_FORMAT_LIST_CREATE_INFO);
@@ -970,9 +968,6 @@ MaybeError Texture::InitializeFromExternal(const ExternalImageDescriptorVk* desc
 
     VkImageCreateInfo baseCreateInfo = {};
     FillVulkanCreateInfoSizesAndType(*this, &baseCreateInfo);
-
-    PNextChainBuilder createInfoChain(&baseCreateInfo);
-
     baseCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
     baseCreateInfo.format = format;
     baseCreateInfo.usage = usage;
@@ -986,6 +981,7 @@ MaybeError Texture::InitializeFromExternal(const ExternalImageDescriptorVk* desc
     baseCreateInfo.usage |= VK_IMAGE_USAGE_TRANSFER_DST_BIT;
 
     VkImageFormatListCreateInfo imageFormatListInfo = {};
+    PNextChainBuilder createInfoChain(&baseCreateInfo);
     std::vector<VkFormat> viewFormats;
     if (GetViewFormats().any()) {
         baseCreateInfo.flags |= VK_IMAGE_CREATE_MUTABLE_FORMAT_BIT;
