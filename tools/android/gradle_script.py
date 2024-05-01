221fb099d13053b42098623fbfa65551fe9fe8de
Trevor David Black <vantablack@google.com>
2024-05-01 22:23:49 +0000

    Adding BUILD.gn to test webgpu kotlin unit tests
    Change-Id: I9d850678db92aec5b3da2ec8a946169b375ae436

---
+++ tools/android/gradle_script.py
@@ -0,0 +1,21 @@
+import subprocess
+import os
+
+cwd = os.getcwd()
+source_sets = """android.sourceSets {
+    main.kotlin.srcDirs += \'""" + cwd + """/gen/java/'
+}"""
+f = open("../../tools/android/webgpu/sourceSets.gradle", "w")
+f.write(source_sets)
+f.close()
+
+print(
+    subprocess.run(
+        ["gradle", "wrapper"],
+        cwd="../../tools/android",
+        capture_output=True))
+print(
+    subprocess.run(
+        ["./gradlew", ":webgpu:testDebugUnitTest"],
+        cwd="../../tools/android",
+        capture_output=True))
