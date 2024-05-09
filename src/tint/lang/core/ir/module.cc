0ad41418f7373be1f460f4edacacb96028905a75
James Price <jrprice@google.com>
2024-05-09 17:16:17 -0400

    [ir] Add Module::DependencyOrderedFunctions()
    This returns the list of functions in the module in dependency order,
    which is needed by multiple backends in order to generate valid code.
    
    Change-Id: I4c0d9a8ee439ed0272504d3f848cf80738455244

--- src/tint/lang/core/ir/module.cc
+++ src/tint/lang/core/ir/module.cc
@@ -28,7 +28,10 @@
 #include "src/tint/lang/core/ir/module.h"
 
 #include <limits>
+#include <utility>
 
+#include "src/tint/lang/core/ir/control_instruction.h"
+#include "src/tint/lang/core/ir/user_call.h"
 #include "src/tint/utils/ice/ice.h"
 
 namespace tint::core::ir {
@@ -69,6 +72,48 @@ void Module::SetName(Value* value, Symbol name) {
 
 void Module::ClearName(Value* value) {
     value_to_name_.Remove(value);
+}
+
+namespace {
+
+/// Helper to sort a module's function in dependency order.
+struct FunctionSorter {
+    const Module* mod;
+    Vector<const Function*, 16> ordered_functions{};
+    Hashset<const Function*, 16> visited{};
+
+    /// Visit a function and look for dependencies, if the function hasn't already been visited.
+    /// @param func the function to visit
+    void Visit(const Function* func) {
+        if (visited.Add(func)) {
+            Visit(func->Block());
+            ordered_functions.Push(func);
+        }
+    }
+
+    /// Visit a block and look for dependencies.
+    /// @param block the block to visit
+    void Visit(const Block* block) {
+        for (auto* inst : *block) {
+            if (auto* control = inst->As<ControlInstruction>()) {
+                // Recurse into child blocks.
+                control->ForeachBlock([&](const Block* b) { Visit(b); });
+            } else if (auto* call = inst->As<UserCall>()) {
+                // Visit the function that is being called.
+                Visit(call->Target());
+            }
+        }
+    }
+};
+
+}  // namespace
+
+Vector<const Function*, 16> Module::DependencyOrderedFunctions() const {
+    FunctionSorter sorter{this};
+    for (auto& func : functions) {
+        sorter.Visit(func);
+    }
+    return std::move(sorter.ordered_functions);
 }
 
 }  // namespace tint::core::ir
