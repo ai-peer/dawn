83410fd43d7bf93e10cdedcac8de0ef3a960b4ca
Loko Kung <lokokung@google.com>
2024-05-10 17:11:24 -0700

    [dawn][native] Move deprecation count for testing to instance.
    - Test infrastructure can more easily validate deprecation warnings on
      all devices via the instance, especially when tests create multiple
      devices.
    
    Change-Id: Ic5e8bf5d080a7ad605ece4611e6701b307240576

--- src/dawn/tests/unittests/validation/ValidationTest.cpp
+++ src/dawn/tests/unittests/validation/ValidationTest.cpp
@@ -184,10 +184,7 @@ void ValidationTest::TearDown() {
     FlushWire();
     ASSERT_FALSE(mExpectError);
 
-    if (device) {
-        EXPECT_EQ(mLastWarningCount,
-                  dawn::native::GetDeprecationWarningCountForTesting(backendDevice));
-    }
+    EXPECT_EQ(mLastWarningCount, mDawnInstance->GetDeprecationWarningCountForTesting());
 
     // The device will be destroyed soon after, so we want to set the expectation.
     ExpectDeviceDestruction();
