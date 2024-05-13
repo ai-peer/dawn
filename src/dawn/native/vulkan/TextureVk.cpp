d149dc6baea75250e32c3bbb404346921ba6565a
Saifuddin Hitawala <hitawala@chromium.org>
2024-05-13 16:34:06 -0400

    [YCbCr Samplers] Add wgpu::TextureFormat::External
    Add wgpu::TextureFormat::External to be used with creating YCbCr
    samplers.
    
    Change-Id: I2f82d591fccf9786d11d554a0a23cec070653565
    Bug: dawn:2476

--- src/dawn/native/vulkan/TextureVk.cpp
+++ src/dawn/native/vulkan/TextureVk.cpp
@@ -524,6 +524,10 @@ VkFormat VulkanImageFormat(const Device* device, wgpu::TextureFormat format) {
                 return VulkanImageFormat(device, wgpu::TextureFormat::Depth24PlusStencil8);
             }
 
+        case wgpu::TextureFormat::External:
+            // The VkFormat is Undefined when TextureFormat::External is passed for YCbCr samplers.
+            return VK_FORMAT_UNDEFINED;
+
         // R8BG8A8Triplanar420Unorm format is only supported on macOS.
         case wgpu::TextureFormat::R8BG8A8Triplanar420Unorm:
         case wgpu::TextureFormat::Undefined:
@@ -1768,6 +1772,12 @@ MaybeError TextureView::Initialize(const UnpackedPtr<TextureViewDescriptor>& des
     if (auto* yCbCrVkDescriptor = descriptor.Get<YCbCrVkDescriptor>()) {
         mYCbCrVkDescriptor = *yCbCrVkDescriptor;
         mYCbCrVkDescriptor.nextInChain = nullptr;
+
+        if (GetFormat().format != wgpu::TextureFormat::External) {
+            return DAWN_VALIDATION_ERROR("%u format is not supported with YCbCr samplers.",
+                                         GetFormat().format);
+        }
+
         DAWN_TRY_ASSIGN(mSamplerYCbCrConversion,
                         CreateSamplerYCbCrConversionCreateInfo(mYCbCrVkDescriptor, device));
 
