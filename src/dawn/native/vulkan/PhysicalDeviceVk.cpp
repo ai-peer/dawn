e26b0fe46f985a1403a08674dc5b62bd262aa4b7
Austin Eng <enga@chromium.org>
2024-05-16 14:24:40 -0700

    Add a toggle to disable validation of AHB VkImage required size
    - Move the check of the required size till after the VkImage is
      bound to memory. This is required by the Vulkan spec.
    
    - also add AHARDWAREBUFFER_USAGE_GPU_FRAMEBUFFER to test
      CPUWriteThenGPURead. Some GPU drivers require at least one
      non-CPU usage to import into Vulkan
    
    Bug: 333424893
    Change-Id: I80abc52eec19c093576bf68f2f1ed2bfa16929ee

--- src/dawn/native/vulkan/PhysicalDeviceVk.cpp
+++ src/dawn/native/vulkan/PhysicalDeviceVk.cpp
@@ -662,6 +662,10 @@ void PhysicalDevice::SetupBackendDeviceToggles(dawn::platform::Platform* platfor
         deviceToggles->Default(Toggle::ResolveMultipleAttachmentInSeparatePasses, true);
     }
 
+    if (IsAndroidSamsung()) {
+        deviceToggles->Default(Toggle::IgnoreImportedAHardwareBufferVulkanImageSize, true);
+    }
+
     if (IsIntelMesa() && gpu_info::IsIntelGen12LP(GetVendorId(), GetDeviceId())) {
         // dawn:1688: Intel Mesa driver has a bug about reusing the VkDeviceMemory that was
         // previously bound to a 2D VkImage. To work around that bug we have to disable the resource
@@ -802,6 +806,15 @@ bool PhysicalDevice::IsAndroidARM() const {
     return gpu_info::IsARM(GetVendorId());
 #else
     return false;
+#endif
+}
+
+bool PhysicalDevice::IsAndroidSamsung() const {
+#if DAWN_PLATFORM_IS(ANDROID)
+    return true;
+    // return gpu_info::IsSamsung(GetVendorId());
+#else
+    return true;
 #endif
 }
 
