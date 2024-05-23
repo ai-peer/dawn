c86d7830c2e28d302f94209371cf194d8ec2c1d5
Le Hoang Quyen <lehoangquyen@chromium.org>
2024-05-23 08:37:54 +0800

    Tint: parse @input_attachment_index
    Bug: 341117913
    Change-Id: I1cf43f5df1f85623294d8c113cdb817fa78720df

--- src/tint/lang/wgsl/resolver/input_attachments_extension_test.cc
+++ src/tint/lang/wgsl/resolver/input_attachments_extension_test.cc
@@ -25,6 +25,15 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#include <memory>
+#include <string>
+#include <tuple>
+#include <utility>
+
+#if TINT_BUILD_WGSL_READER
+#include "src/tint/lang/wgsl/reader/reader.h"
+#endif
+#include "src/tint/lang/wgsl/resolver/resolve.h"
 #include "src/tint/lang/wgsl/resolver/resolver.h"
 #include "src/tint/lang/wgsl/resolver/resolver_helper_test.h"
 
@@ -33,6 +42,25 @@ namespace {
 
 using namespace tint::core::fluent_types;     // NOLINT
 using namespace tint::core::number_suffixes;  // NOLINT
+
+void ParseSource([[maybe_unused]] std::string src, [[maybe_unused]] bool should_pass) {
+#if TINT_BUILD_WGSL_READER
+    wgsl::reader::Options options;
+    options.allowed_features = wgsl::AllowedFeatures::Everything();
+    auto file = std::make_unique<Source::File>("test", src);
+    auto program = wgsl::reader::Parse(file.get(), options);
+
+    auto error = program.Diagnostics().Str();
+
+    bool valid = program.IsValid();
+    if (should_pass) {
+        EXPECT_TRUE(valid) << error;
+        EXPECT_FALSE(program.Diagnostics().ContainsErrors());
+    } else {
+        EXPECT_FALSE(valid);
+    }
+#endif
+}
 
 using InputAttachmenExtensionTest = ResolverTest;
 
@@ -67,6 +95,26 @@ TEST_F(InputAttachmenExtensionTest, InputAttachmentIndexInvalidType) {
 
     EXPECT_FALSE(r()->Resolve());
     EXPECT_EQ(r()->error(), R"(error: '@input_attachment_index' must be an 'i32' or 'u32' value)");
+}
+
+// Test that Parser can parse input_attachment_index with integer value.
+TEST_F(InputAttachmenExtensionTest, InputAttachmentIndexFromSource) {
+    ParseSource(R"(
+enable chromium_internal_input_attachments;
+@group(0) @binding(0) @input_attachment_index(3)
+var input_tex : input_attachment<f32>;
+    )",
+                /*should_pass=*/true);
+}
+
+// Test that input_attachment_index from source cannot have float value.
+TEST_F(InputAttachmenExtensionTest, InputAttachmentIndexInvalidTypeFromSource) {
+    ParseSource(R"(
+enable chromium_internal_input_attachments;
+@group(0) @binding(0) @input_attachment_index(3.0)
+var input_tex : input_attachment<f32>;
+    )",
+                /*should_pass=*/false);
 }
 
 }  // namespace
