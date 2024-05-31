1f6b59a71b53fe67eca0dfb0387b6f94b82b1136
Saifuddin Hitawala <hitawala@chromium.org>
2024-05-31 16:51:24 -0400

    [YCbCr Samplers] Add wgpu::TextureFormat::External
    Add wgpu::TextureFormat::External to be used with creating YCbCr
    samplers. Also add useExternalFormat bool for picking externalFormat
    vs vkFormat when creating vkImage. Add necessary tests and validations.
    
    Change-Id: I2f82d591fccf9786d11d554a0a23cec070653565
    Bug: dawn:2476

--- src/dawn/native/Texture.cpp
+++ src/dawn/native/Texture.cpp
@@ -87,6 +87,11 @@ MaybeError ValidateCanViewTextureAs(const DeviceBase* device,
         return {};
     }
 
+    DAWN_INVALID_IF(format.format == wgpu::TextureFormat::External &&
+                        viewFormat.format != wgpu::TextureFormat::External,
+                    "TextureView format (%s) is not (%s).", viewFormat.format,
+                    wgpu::TextureFormat::External);
+
     const FormatSet& compatibleViewFormats = texture->GetViewFormats();
     if (compatibleViewFormats[viewFormat]) {
         // Validation of this list is done on texture creation, so we don't need to
@@ -604,7 +609,6 @@ MaybeError ValidateTextureViewDescriptor(const DeviceBase* device,
     DAWN_ASSERT(!texture->IsError());
 
     DAWN_TRY(ValidateTextureViewDimension(descriptor->dimension));
-    // TODO(crbug.com/dawn/2476): Add necessary validation for TextureFormat::External.
     DAWN_TRY(ValidateTextureFormat(descriptor->format));
     DAWN_TRY(ValidateTextureAspect(descriptor->aspect));
 
@@ -635,15 +639,19 @@ MaybeError ValidateTextureViewDescriptor(const DeviceBase* device,
         "texture's mip level count (%u).",
         descriptor->baseMipLevel, descriptor->mipLevelCount, texture->GetNumMipLevels());
 
-    // TODO(crbug.com/dawn/2476): Add necessary validations to CanViewTextureAs for
-    // TextureFormat::External.
-    DAWN_TRY(ValidateCanViewTextureAs(device, texture, *viewFormat, descriptor->aspect));
-    DAWN_TRY(ValidateTextureViewDimensionCompatibility(device, texture, descriptor));
-
     if (descriptor.Get<YCbCrVkDescriptor>()) {
         DAWN_INVALID_IF(!device->HasFeature(Feature::YCbCrVulkanSamplers), "%s is not enabled.",
                         wgpu::FeatureName::YCbCrVulkanSamplers);
+        DAWN_INVALID_IF(format.format != wgpu::TextureFormat::External,
+                        "Texture format (%s) is not (%s).", format.format,
+                        wgpu::TextureFormat::External);
+    } else if (format.format == wgpu::TextureFormat::External) {
+        return DAWN_VALIDATION_ERROR("Invalid TextureViewDescriptor with Texture format (%s).",
+                                     wgpu::TextureFormat::External);
     }
+
+    DAWN_TRY(ValidateCanViewTextureAs(device, texture, *viewFormat, descriptor->aspect));
+    DAWN_TRY(ValidateTextureViewDimensionCompatibility(device, texture, descriptor));
 
     return {};
 }
@@ -683,7 +691,6 @@ ResultOrError<TextureViewDescriptor> GetTextureViewDescriptorWithDefaults(
         }
     }
 
-    // TODO(crbug.com/dawn/2476): Add TextureFormat::External validation.
     if (desc.format == wgpu::TextureFormat::Undefined) {
         const Format& format = texture->GetFormat();
 
