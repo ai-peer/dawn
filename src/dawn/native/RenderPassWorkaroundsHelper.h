3d94b7adfa44fa80ea59f6afb731dd9338485863
Le Hoang Quyen <lehoangquyen@chromium.org>
2024-06-08 20:23:29 +0000

    vk: Make ExpandResolve work with AlwaysResolveIntoZeroLevelAndLayer.
    Previously, ExpandResolveTexture with two subpasses didn't work with
    AlwaysResolveIntoZeroLevelAndLayer workaround.
    Because this workaround modifies the resolve targets of the
    BeginRenderPassCmd before it reaches the Vulkan backend.
    
    This CL makes ExpandResolveTexture work with this workaround by
    copying the resolve targets to the temporary resolve targets before
    starting the render pass.
    
    Also move all render pass workarounds into one single file.
    
    Bug: 42240662
    Change-Id: I5fce0b8fb607bbf1208c1b7c1f8040e4e7a9afeb

---
+++ src/dawn/native/RenderPassWorkaroundsHelper.h
@@ -0,0 +1,73 @@
+// Copyright 2024 The Dawn & Tint Authors
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef SRC_DAWN_NATIVE_RENDERPASSWORKAROUNDSHELPER_H_
+#define SRC_DAWN_NATIVE_RENDERPASSWORKAROUNDSHELPER_H_
+
+#include <functional>
+
+#include "dawn/common/Ref.h"
+#include "dawn/common/ityp_array.h"
+#include "dawn/common/ityp_bitset.h"
+#include "dawn/native/Error.h"
+#include "dawn/native/IntegerTypes.h"
+
+namespace dawn::native {
+struct BeginRenderPassCmd;
+class CommandEncoder;
+class RenderPassEncoder;
+struct RenderPassDescriptor;
+class RenderPassResourceUsageTracker;
+class TextureBase;
+class TextureViewBase;
+
+class RenderPassWorkaroundsHelper {
+  public:
+    RenderPassWorkaroundsHelper();
+    ~RenderPassWorkaroundsHelper();
+
+    MaybeError Initialize(CommandEncoder* encoder,
+                          const RenderPassDescriptor* renderPassDescriptor);
+    MaybeError Apply(CommandEncoder* encoder,
+                     RenderPassResourceUsageTracker* usageTracker,
+                     BeginRenderPassCmd* cmd,
+                     std::function<MaybeError(RenderPassEncoder*, const RenderPassDescriptor*)>*
+                         passStartCallbackOut,
+                     std::function<void()>* passEndCallbackOut);
+
+  private:
+    struct TextureAndView {
+        Ref<TextureBase> texture;
+        Ref<TextureViewBase> view;
+    };
+    PerColorAttachment<TextureAndView> mTempResolveTargets;
+    ColorAttachmentMask mTempResolveTargetsMask;
+};
+
+}  // namespace dawn::native
+
+#endif
