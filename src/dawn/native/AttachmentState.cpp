646af62ff9d893fc9997d5bafec8c592c29fba7c
Le Hoang Quyen <lehoangquyen@chromium.org>
2024-06-09 15:02:28 +0000

    vk: ExpandResolve works with ResolveMultipleAttachmentInSeparatePasses.
    If ResolveMultipleAttachmentInSeparatePasses is turned on, we will
    skip the ExpandResolveTexture step in the vulkan backend and instead do
    it in front-end with standard textureLoad().
    
    Bug: 42240662
    Change-Id: Id9d661baeb7a19e0a94f961592a8bad20a6c4355

--- src/dawn/native/AttachmentState.cpp
+++ src/dawn/native/AttachmentState.cpp
@@ -337,4 +337,11 @@ AttachmentState::ComputeStorageAttachmentPackingInColorAttachments() const {
     return result;
 }
 
+bool AttachmentState::WillRenderPassRequireResolveSplitIntoSeperatePasses(
+    DeviceBase* device,
+    bool renderPassHasResolveTargets) const {
+    return device->IsToggleEnabled(Toggle::ResolveMultipleAttachmentInSeparatePasses) &&
+           mColorAttachmentsSet.count() > 1 && renderPassHasResolveTargets;
+}
+
 }  // namespace dawn::native
