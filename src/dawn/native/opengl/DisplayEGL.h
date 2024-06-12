5aa841f3b27570edcf80fdad674da986fce5aeea
Corentin Wallez <cwallez@chromium.org>
2024-06-12 17:39:38 +0200

    EGL: Move initialization of EGL inside a DisplayEGL class
    The moves initialization and checks of EGLDisplays to be in its own
    class instead of split across opengl::Backend and
    opengl::PhysicalDevice. That class also contains data that's per EGL
    connection such as the EGLDisplay and the procs. This removes the need
    for opengl::Backend to return a single PhysicalDevice ever.
    
    The ContextEGL class now only deals with creating context and doing
    MakeCurrent, with some of its validation checks moved to classes using
    it. This will make it easier to dynamically handle EGL extension being
    present (e.g. to support webgpu.h when EGL_KHR_image_base is not present).
    
    Finally the encapsulation in these classes is step towards supporting
    more embedder APIs in the future (like GLX).
    
    Bug: 344814083
    Bug: 42241384
    Change-Id: I34d7c24c518a791da920a640a5764458073f976e

--- src/dawn/native/opengl/DisplayEGL.h
+++ src/dawn/native/opengl/DisplayEGL.h
@@ -1,4 +1,4 @@
-// Copyright 2022 The Dawn & Tint Authors
+// Copyright 2024 The Dawn & Tint Authors
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
@@ -25,41 +25,51 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef SRC_DAWN_NATIVE_OPENGL_CONTEXTEGL_H_
-#define SRC_DAWN_NATIVE_OPENGL_CONTEXTEGL_H_
+#ifndef SRC_DAWN_NATIVE_OPENGL_DISPLAYEGL_H_
+#define SRC_DAWN_NATIVE_OPENGL_DISPLAYEGL_H_
 
 #include <memory>
 
+#include "dawn/common/DynamicLib.h"
 #include "dawn/common/NonMovable.h"
 #include "dawn/common/egl_platform.h"
-#include "dawn/native/opengl/DeviceGL.h"
 #include "dawn/native/opengl/EGLFunctions.h"
 
 namespace dawn::native::opengl {
 
-class DisplayEGL;
-
-class ContextEGL : NonMovable {
+// Represents a connection to an EGL driver, with its EGLDisplay, its functions and other metadata
+// global to EGL.
+class DisplayEGL : NonMovable {
   public:
-    static ResultOrError<std::unique_ptr<ContextEGL>> Create(DisplayEGL* display,
-                                                             wgpu::BackendType backend,
-                                                             bool useRobustness,
-                                                             bool useANGLETextureSharing);
+    static ResultOrError<std::unique_ptr<DisplayEGL>> CreateFromDynamicLoading(
+        wgpu::BackendType backend,
+        const char* libName);
 
-    explicit ContextEGL(DisplayEGL* display);
-    ~ContextEGL();
+    static ResultOrError<std::unique_ptr<DisplayEGL>>
+    CreateFromProcAndDisplay(wgpu::BackendType backend, EGLGetProcProc getProc, EGLDisplay display);
 
-    MaybeError Initialize(wgpu::BackendType backend,
-                          bool useRobustness,
-                          bool useANGLETextureSharing);
+    explicit DisplayEGL(wgpu::BackendType backend);
+    ~DisplayEGL();
 
-    void MakeCurrent();
+    // A convenience ref to avoid having to call an accessor function every time we need to use EGL
+    const EGLFunctions& egl;
+    EGLDisplay GetDisplay() const;
+    EGLint GetAPIEnum() const;
+    EGLint GetAPIBit() const;
 
   private:
-    const DisplayEGL* mDisplay;
-    EGLContext mContext = EGL_NO_CONTEXT;
+    MaybeError InitializeWithDynamicLoading(const char* libName);
+    MaybeError InitializeWithProcAndDisplay(EGLGetProcProc getProc, EGLDisplay display);
+
+    DynamicLib mLib;
+
+    EGLFunctions mFunctions;
+    EGLDisplay mDisplay = EGL_NO_DISPLAY;
+
+    EGLint mApiEnum;
+    EGLint mApiBit;
 };
 
 }  // namespace dawn::native::opengl
 
-#endif  // SRC_DAWN_NATIVE_OPENGL_CONTEXTEGL_H_
+#endif  // SRC_DAWN_NATIVE_OPENGL_DISPLAYEGL_H_
