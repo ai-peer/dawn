3a41a345736ed90beae1f7468914ffb04547a42b
François Beaufort <beaufort.francois@gmail.com>
2024-06-13 17:01:35 +0200

    Replace GPUAdapter GetProperties() with GetInfo()
    WIP WIP
    
    Bug: 335383516
    Change-Id: Ic7df2c293555662b41a8ab590cd50dd0b8dc38e1

--- src/dawn/native/opengl/PhysicalDeviceGL.cpp
+++ src/dawn/native/opengl/PhysicalDeviceGL.cpp
@@ -462,6 +462,6 @@ FeatureValidationResult PhysicalDevice::ValidateFeatureSupportedWithTogglesImpl(
     return {};
 }
 
-void PhysicalDevice::PopulateBackendProperties(UnpackedPtr<AdapterProperties>& properties) const {}
+void PhysicalDevice::PopulateBackendProperties(UnpackedPtr<AdapterInfo>& info) const {}
 
 }  // namespace dawn::native::opengl
