0dbc9a47242b375572de4ff1cd75202d799b48fe
Le Hoang Quyen <lehoangquyen@chromium.org>
2024-06-24 20:00:40 +0800

    D3D11: Use compute shader for T2B with certain formats.
    Bug: 348654098
    Change-Id: I4002386395213b5e8a05394305fecc4f1eff152c

--- src/dawn/native/BlitTextureToBuffer.h
+++ src/dawn/native/BlitTextureToBuffer.h
@@ -32,8 +32,11 @@
 
 namespace dawn::native {
 
+struct Format;
 struct TextureCopy;
 struct BufferCopy;
+
+bool IsFormatSuportedByColorTextureToBufferBlit(const Format& format);
 
 MaybeError BlitTextureToBuffer(DeviceBase* device,
                                CommandEncoder* commandEncoder,
