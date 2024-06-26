a5996c787d837c508e7c49143b1ae585b47b09d3
Jaswant Panchumarti <jaswant.panchumarti@kitware.com>
2024-06-26 14:44:04 -0400

    Refine the bundling step for `webgpu_dawn`
    - Build `webgpu_dawn` only when `DAWN_BUILD_MONOLITHIC_LIBRARY`
    is enabled
    - Link dawn_native with "-framework Foundation". This is needed for iOS.
    - Export symbols from `dawn_native` and `dawn/webgpu.h` when
    building monolithic shared library.
    - Remove install rules for unexported static libraries.
    - Provide a single `webgpu_dawn` library, C/C++ headers
    and cmake config file for consumers to use `wgpu` methods.
    - The exported symbols also permit instantiation of public
    classes from `dawn::native` namespaces.
    
    Change-Id: Ie8575d49366f3c333b860ce2617960067edfc404

---
+++ src/dawn/tests/bundle_tests/WebGPUTest.cpp
@@ -0,0 +1,70 @@
+// Copyright 2017 The Dawn & Tint Authors
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+/**
+ * This is a short test which creates an instance and requests an adapter using webgpu_cpp.h.
+ * Unlike other tests/samples, this executable links with the monolithic shared
+ * library `webgpu_dawn`.
+ * It's purpose is to verify that symbols from `dawn/webgpu.h` are exported
+ * from `webgpu_dawn`.
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+#include "webgpu/webgpu_cpp.h"
+
+int main() {
+    wgpu::InstanceDescriptor instanceDescriptor{};
+    instanceDescriptor.features.timedWaitAnyEnable = true;
+    wgpu::Instance instance = wgpu::CreateInstance(&instanceDescriptor);
+    if (instance == nullptr) {
+        std::cerr << "Instance creation failed!\n";
+        return EXIT_FAILURE;
+    }
+    // Synchronously request the adapter.
+    wgpu::RequestAdapterOptions options = {};
+    wgpu::Adapter adapter;
+    instance.WaitAny(instance.RequestAdapter(
+                         &options, {nullptr, wgpu::CallbackMode::WaitAnyOnly,
+                                    [](WGPURequestAdapterStatus status, WGPUAdapter adapter,
+                                       const char* message, void* userdata) {
+                                        if (status != WGPURequestAdapterStatus_Success) {
+                                            std::cerr << "Failed to get an adapter:" << message;
+                                            return;
+                                        }
+                                        *static_cast<wgpu::Adapter*>(userdata) =
+                                            wgpu::Adapter::Acquire(adapter);
+                                    },
+                                    &adapter}),
+                     UINT64_MAX);
+    if (adapter == nullptr) {
+        std::cerr << "RequestAdapter failed!\n";
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
